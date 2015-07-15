b74f7df0a0e79105808872e997999cfc2faf825b
cerok <info@cerok.com>
2015-07-15 13:51:54 -0500

    Commit automatico editor saia. Cambios locales 2015-07-15 13:51:54

--- GitApi/Git0K.php
+++ GitApi/Git0K.php
@@ -520,6 +520,7 @@ class Git0K extends Git {
 						 * AA unmerged, agregado por ambos
 						 * UU unmerged, modificado por ambos
 						 */
+					    echo $output_array[1];
 						if (in_array($output_array[1], $problemas)) {
 							$lista[] = $output_array[2];
 						}
