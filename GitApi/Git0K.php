602c5f032e51e165d8303f355ab7cb9bad802222
cerok <info@cerok.com>
2015-07-27 17:36:21 -0500

    Commit automatico editor saia. Cambios locales 2015-07-27 17:36:21

--- GitApi/Git0K.php
+++ GitApi/Git0K.php
@@ -403,7 +403,7 @@ class Git0K extends Git {
         }
         
         if (count($lista_agregados) > 0) {
-            $files = $this->filesInIndex($lista_archivos);
+            $files = $this->filesInIndex($lista_agregados);
             if (count($files) > 0 && count($files["tree"]) > 0) {
                 $estado_git = $this->sincronizarSubtree($mensaje, $files["tree"]);
             }
