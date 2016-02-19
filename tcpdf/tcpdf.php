fcde5794853b60a48e13833d6ee46fadce7b5ab9
hernando.trejos <hernando.trejos@cerok.com>
2016-02-19 11:48:40 +0000

    [PDF][hernando.trejos]Aumenta la velocidad de creacion de PDF basado en la creacion de Cache para los archivos tipo font

--- tcpdf/tcpdf.php
+++ tcpdf/tcpdf.php
@@ -11765,7 +11765,28 @@ class TCPDF {
 						$subsetchars += $fontinfo['subsetchars'];
 					}
 					// rebuild a font subset
-					$font = $this->_getTrueTypeFontSubset($font, $subsetchars);
+					//$font = $this->_getTrueTypeFontSubset($font, $subsetchars);
+					// Alcal: $font2cache modification
+                    // This modification creates utf-8 fonts only the first time,
+                    // after that it uses cache file which dramatically reduces execution time
+                    if (!file_exists($fontfile.'.cached')){
+                        // calculate $font first time
+                        $subsetchars = array_fill(0, 512, true); // fill subset for all chars 0-512
+                        $font = $this->_getTrueTypeFontSubset($font, $subsetchars); // this part is actually slow!
+                        // and then save $font to file for further use
+                        $fp=fopen($fontfile.'.cached','w');
+                        $flat_array = serialize($font); //
+                        fwrite($fp,$flat_array);
+                        fclose($fp);
+                    }
+                    else {
+                        // cache file exist, load file
+                        $fp=fopen($fontfile.'.cached','r');
+                        $flat_array = fread($fp,filesize($fontfile.'.cached'));
+                        fclose($fp);
+                        $font = unserialize($flat_array);
+                    }
+					
 					// calculate new font length
 					$info['length1'] = strlen($font);
 					if ($compressed) {
