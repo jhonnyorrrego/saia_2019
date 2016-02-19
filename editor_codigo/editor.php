e940b1ca6f1048bf12da6808b4f1df962db1c974
hernando.trejos <hernando.trejos@cerok.com>
2016-02-19 22:43:48 +0000

    [Editor][hernando.trejos]Autocomplete debe retornar la ayuda de php [Editor][hernando.trejos]Autocomplete debe retornar la ayuda de php [Editor][hernando.trejos]Autocomplete debe retornar la ayuda de php [Editor][hernando.trejos]Autocomplete debe retornar la ayuda de php [Editor][hernando.trejos]Autocomplete debe retornar la ayuda de php [Editor][hernando.trejos]Autocomplete debe retornar la ayuda de php [Editor][hernando.trejos]Autocomplete debe retornar la ayuda de php [Editor][hernando.trejos]Autocomplete debe retornar la ayuda de php [Editor][hernando.trejos]Autocomplete debe reto [Editor][hernando.trejos]Autocomplete debe reto [Editor][hernando.trejos]Autocomplete debe reto [Editor][hernando.trejos]Autocomplete debe reto [Editor][hernando.trejos]Autocomplete debe reto [Editor][hernando.trejos]Autocomplete debe reto [Editor][hernando.trejos]Autocomplete debe reto [Editor][hernando.trejos]Autocomplete debe imprimir el snippeta usar y no la cadena

--- editor_codigo/editor.php
+++ editor_codigo/editor.php
@@ -201,22 +201,21 @@ if(!isset($_SESSION["LOGIN".LLAVE_SAIA_EDITOR]) || !isset($_SESSION["EMAIL".LLAV
         name: 'find_help',
         bindKey: {win: 'Ctrl-Shift-A', mac: 'Command-Shift-A'},
         exec: function(editor) {
-            if(!permite_consulta){
+            /*if(!permite_consulta){
                 return;
-            }
+            }*/
             $.ajax({
               type:'POST',
               url: 'http://php.net/manual/es/function.'+editor.getSession().doc.getTextRange(editor.selection.getRange())+'.php', 
               dataType:"json", 
               success: function(datos) { 
                 $("#ayuda_editor").html(datos);
-              }
+              },
               error:function(){
                 $("#ayuda_editor").html('<div class="alert alert-danger">No soportado</div>');  
                 permite_consulta=0;
               }
             });
-            
         },
         readOnly: true
     });
