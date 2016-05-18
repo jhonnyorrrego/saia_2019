22a679c0d4e7af7ce0afd9d2b662ed6bf9b42eec
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-05-18 20:17:25 +0000

    [desarrollo cargar series][ruben.pulgarin]avance avance

--- tareas_administrativas_saia/cargar_series/cargar_series.php
+++ tareas_administrativas_saia/cargar_series/cargar_series.php
@@ -60,11 +60,7 @@ for($i=6;$i<count($records);$i++){ //EMPIEZSA A VALIDAD APARTIR DE LA COLUMNA 6
         //PENDIENTE DESARROLLAR INSERT ENTIDAD SERIE 
         //$entidad : llega el nombre, solo hay que hacer la busqueda like a dependencia.
 
-	
-		
-		
-		
-		
+
     }
 }
  
