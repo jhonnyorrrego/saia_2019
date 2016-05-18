fb7485b9bbafaf9343e9ed36eb6afd43e86b8af8
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-05-18 19:04:30 +0000

    [desarrollo cargar series][ruben.pulgarin]avance avance

--- tareas_administrativas_saia/cargar_series/cargar_series.php
+++ tareas_administrativas_saia/cargar_series/cargar_series.php
@@ -46,26 +46,6 @@ global $conn;
 	}
  }
  
- 
  die();
- 
- for($i=0; $i<count($records)-1; $i++)
- {
-    $fila = explode(";",str_replace('"','',strtolower(utf8_encode($records[$i]))));
-    for($ind=0; $ind<count($fila);$ind++)
-      if($fila[$ind]=="\\N")
-        $fila[$ind] = "";
-
-  $conservacion=1;
-  $digitalizacion=1;
-  $seleccion=1;
-  if($fila[6]=="")
-    $conservacion=0; 
-  if($fila[8]=="")
-    $digitalizacion=0;
-  if($fila[7]=="")
-    $seleccion=0;     
-  $sql="insert into serie(nombre,dias_entrega,codigo,retencion_gestion,retencion_central,conservacion,digitalizacion,seleccion,procedimiento) values('".strtolower($fila[3])."','15','".$fila[0]."','".$fila[4]."','".$fila[5]."','$conservacion','$digitalizacion','$seleccion','".$fila[10]."')";
-  echo($sql.'<br><br><br>');
- }       
+      
 ?> 
