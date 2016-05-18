b3abdf1ad795660242eabd42bcf0725a25db9d24
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-05-18 19:07:49 +0000

    [desarrollo cargar series][ruben.pulgarin]avance avance

--- tareas_administrativas_saia/cargar_series/cargar_series.php
+++ tareas_administrativas_saia/cargar_series/cargar_series.php
@@ -1,4 +1,16 @@
 <?php
+
+$max_salida=6; // Previene algun posible ciclo infinito limitando a 10 los ../
+$ruta_db_superior=$ruta="";
+while($max_salida>0){
+  if(is_file($ruta."db.php")){
+    $ruta_db_superior=$ruta; //Preserva la ruta superior encontrada
+  }
+  $ruta.="../";
+  $max_salida--;
+}
+
+
 include_once("db.php");
 global $conn;
  $archivo="prueba_carga.csv";
