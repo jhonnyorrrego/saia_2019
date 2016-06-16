6e53e600a77d562ec053e60b0e94f470fe0c289d
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-06-16 21:29:54 +0000

    [organizar mostrar formato contexto Estrategico (calidad)][ruben.pulgarin]organizar mostrar formato contexto Estrategico (calidad) organizar mostrar formato contexto Estrategico (calidad)

--- formatos/contexto_extrategico/funciones.php
+++ formatos/contexto_extrategico/funciones.php
@@ -77,6 +77,9 @@ function adiconar_factores_contexto($idformato,$iddoc){
 			  </table>
 			 ';
 			 
+			 $tabla_factores='<table border="1" style="border-collapse: collapse;width: 100%; float: left;">';
+			 
+			 
 	echo($factores_internos);
 	echo($factores_externos);
 }
