3302f3e16e34e2354d7697d93004fe66b3a3db89
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-06-17 13:45:02 +0000

    [correccion test arbol de calidad][ruben.pulgarin]correccion test arbol de calidad correccion test arbol de calidad

--- prueba_nuevo.php
+++ prueba_nuevo.php
@@ -6,14 +6,14 @@ include_once('db.php');
 
 $nombre="ft_proceso";
 $formato=busca_filtro_tabla("A.idformato,A.nombre,A.nombre_tabla,A.etiqueta","formato A","A.nombre_tabla LIKE '".$nombre."'","idformato DESC",$conn);
-print_r($formato);
+
 
 
  $imagenes=' im0="'.strtolower($formato[0]["nombre"]).'.gif" im1="'.strtolower($formato[0]["nombre"]).'.gif" im2="'.strtolower($formato[0]["nombre"]).'.gif" ';
  $iddoc=$formato[0]["idformato"]."-".$formato[0]["nombre"]."-".$formato[0]["nombre_tabla"];
  
  
- 
+ print_r($iddoc);
  
 ?>
 
