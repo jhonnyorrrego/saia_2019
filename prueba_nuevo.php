9cd29e897c51c64fdb1334373df256e081e10e77
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-06-17 14:03:24 +0000

    [correccion test arbol de calidad][ruben.pulgarin]correccion test arbol de calidad correccion test arbol de calidad

--- prueba_nuevo.php
+++ prueba_nuevo.php
@@ -15,11 +15,20 @@ $formato=busca_filtro_tabla("A.idformato,A.nombre,A.nombre_tabla,A.etiqueta","fo
  $arreglo=explode("-",$iddoc);  
  $campo_ordenar=busca_filtro_tabla("c.nombre,nombre_tabla","campos_formato c,formato f","formato_idformato=idformato and (c.banderas like 'oc' or c.banderas like '%,oc' or c.banderas like 'oc,%' or c.banderas like '%,oc,%') and f.nombre='".strtolower($arreglo[1])."'","",$conn);
  
+  if($campo_ordenar["numcampos"])
+   { $orden="b.".$campo_ordenar[0]["nombre"]." asc";
+   }
+ else
+   $orden="iddocumento asc"; 
+  $imagen_nota="";
+  $validacion_macroproceso='';  
+  if(@$arreglo[3] && $arreglo[1]=="proceso"){
+    $validacion_macroproceso=" AND documento_iddocumento=".$arreglo[3];
+  }
  
  
  
- 
- print_r($campo_ordenar);
+ print_r($validacion_macroproceso);
  
 ?>
 
