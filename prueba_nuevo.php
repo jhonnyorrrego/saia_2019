cfd2eee626a4d12de34bbf8856e553d7d95cec8c
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-06-17 14:46:20 +0000

    [correccion test arbol de calidad][ruben.pulgarin]correccion test arbol de calidad correccion test arbol de calidad

--- prueba_nuevo.php
+++ prueba_nuevo.php
@@ -113,7 +113,7 @@ function llena_datos($idformato,$tabla,$campo,$categoria){//--
 	if($tabla=="ft_proceso" && !$validar_macro){
 	  $dato = busca_filtro_tabla("",$tabla,$arreglo[2]."=".$arreglo[1],"",$conn);
 	  
-	  print_r($dato);die();
+	  print_r('entra');die();
 	  if($dato["numcampos"] && @$dato[0]["macroproceso"]!=''){
 	    return($texto);
 	  }
