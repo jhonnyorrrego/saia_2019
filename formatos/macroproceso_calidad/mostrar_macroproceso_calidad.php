5d48613ff9ed57d54523efa628d43367124a62b4
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-06-21 13:12:43 +0000

    Commit automatico editor saia. Cambios locales 2016-06-21 08:12:41

--- formatos/macroproceso_calidad/mostrar_macroproceso_calidad.php
+++ formatos/macroproceso_calidad/mostrar_macroproceso_calidad.php
@@ -1,1 +1,17 @@
-<?php include_once("../carta/funciones.php"); ?><?php include_once("../pqr/funciones.php"); ?><?php include_once("../experiencia_laboral/funciones.php"); ?><?php include_once("../librerias/funciones_generales.php"); ?><script type="text/javascript" src="../../js/jquery.js"></script><?php include_once("../librerias/header_nuevo.php"); ?><?php include_once("../../class_transferencia.php"); ?><tr><td></td></tr><?php include_once("../librerias/footer_nuevo.php"); ?>
+<?php include_once("../carta/funciones.php"); ?><?php include_once("../pqr/funciones.php"); ?><?php include_once("../experiencia_laboral/funciones.php"); ?><?php include_once("../correo_saia/funciones.php"); ?><?php include_once("../librerias/funciones_generales.php"); ?><script type="text/javascript" src="../../js/jquery.js"></script><?php include_once("../librerias/header_nuevo.php"); ?><?php include_once("../../class_transferencia.php"); ?><tr><td><table style="width: 100%; border-collapse: collapse;" border="1">
+<tbody>
+<tr>
+<td class="encabezado_list" style="text-align: left;">Nombre</td>
+<td><?php mostrar_valor_campo('nombre',359,$_REQUEST['iddoc']);?></td>
+</tr>
+<tr>
+<td class="encabezado_list" style="text-align: left;">Descripcion</td>
+<td><?php mostrar_valor_campo('des_formato',359,$_REQUEST['iddoc']);?></td>
+</tr>
+<tr>
+<td class="encabezado_list" style="text-align: left;">Caracterizacion</td>
+<td><?php mostrar_valor_campo('caracterizacion',359,$_REQUEST['iddoc']);?></td>
+</tr>
+</tbody>
+</table>
+<p><?php mostrar_estado_proceso(359,$_REQUEST['iddoc']);?></p></td></tr><?php include_once("../librerias/footer_nuevo.php"); ?>
