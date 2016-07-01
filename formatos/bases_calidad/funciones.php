93a9e3dfea5ad07c815afafa436239deed069698
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-07-01 19:00:34 +0000

    [desarrollo bases de calidad - req 1264][ruben.pulgarin]desarrollo bases de calidad - req 1264 desarrollo bases de calidad - req 1264

--- formatos/bases_calidad/funciones.php
+++ formatos/bases_calidad/funciones.php
@@ -26,7 +26,7 @@ function bases_calidad_ocultar_tipo($idformato,$iddoc){
         <script>
             $(document).ready(function(){
                 
-                $('[name="tipo_base_calidad"][value="1253"]').hide();
+                $('[name="tipo_base_calidad"][value="1253"]').parent().hide();
                 
                 
                 var cantidad=parseInt('<?php echo(count($tipos_existentes)); ?>');
