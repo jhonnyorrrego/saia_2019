b3f5b815f6ca80251e3ef7d2890f003e342009ef
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-07-01 21:55:40 +0000

    [desarrollo bases de calidad - req 12648][ruben.pulgarin]desarrollo bases de calidad - req 12648 desarrollo bases de calidad - req 12648

--- formatos/proceso/previo_mostrar_proceso.php
+++ formatos/proceso/previo_mostrar_proceso.php
@@ -176,7 +176,15 @@ $ok=@$_REQUEST["editar"];
        echo(estilo_bootstrap());
        //<div data-toggle="tooltip" class="btn btn-mini kenlace_saia pull-right" titulo="Editar  Tarea" enlace="pantallas/tareas_listado/editar_tareas_listado.php?idtareas_listado=411" id="editar_tarea_411" conector="iframe" data-original-title="Editar  Tarea" onclick=" ">	    <i class="icon-pencil"></i>	  </div>
     ?>
-    <tr><td><a title="Administración del arbol de calidad." href="../proceso/previo_mostrar_proceso.php?editar=1" target="detalles"><span class="phpmaker">EDITAR MAPA</span></a></td></tr>
+        <tr>
+            <td>
+                <a class="btn btn-mini pull-right" title="Administración del arbol de calidad." href="../proceso/previo_mostrar_proceso.php?editar=1"  onclick=" ">	    
+                    <i class="icon-pencil"></i>	 
+                </a>
+                
+                <a title="Administración del arbol de calidad." href="../proceso/previo_mostrar_proceso.php?editar=1" target="detalles"><span class="phpmaker">EDITAR MAPA</span></a>
+            </td>
+        </tr>
     <?php
     }
     ?>
