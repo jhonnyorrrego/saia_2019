6d43fe729821f1f2fd1617b22b81b88664c486fe
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-07-01 19:03:30 +0000

    [desarrollo bases de calidad - req 1264][ruben.pulgarin]desarrollo bases de calidad - req 1264 desarrollo bases de calidad - req 1264

--- formatos/bases_calidad/funciones.php
+++ formatos/bases_calidad/funciones.php
@@ -17,7 +17,7 @@ function bases_calidad_ocultar_tipo($idformato,$iddoc){
     
     $datos=busca_filtro_tabla("","ft_bases_calidad a, documento b","b.iddocumento=a.documento_iddocumento AND lower(b.estado)='aprobado'","",$conn);
     
-    $tipos_existentes=json_decode(extrae_campo($datos,'tipo_base_calidad'));
+    $tipos_existentes=json_encode(extrae_campo($datos,'tipo_base_calidad'));
     
     print_r($tipos_existentes);
     
