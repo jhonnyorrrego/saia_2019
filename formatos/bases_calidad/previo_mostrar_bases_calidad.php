5b9a49f2de7406e70d6c0d199c701c259494ba96
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-07-01 15:38:49 +0000

    [desarrollo bases de calidad - req 12648][ruben.pulgarin]desarrollo bases de calidad - req 12648 desarrollo bases de calidad - req 12648

--- formatos/bases_calidad/previo_mostrar_bases_calidad.php
+++ formatos/bases_calidad/previo_mostrar_bases_calidad.php
@@ -1,6 +1,16 @@
 <?php
-
-print_r($_REQUEST);
+$max_salida=6; // Previene algun posible ciclo infinito limitando a 10 los ../
+$ruta_db_superior=$ruta="";
+while($max_salida>0){
+  if(is_file($ruta."db.php")){
+    $ruta_db_superior=$ruta; //Preserva la ruta superior encontrada
+  }
+  $ruta.="../";
+  $max_salida--;
+}
+include_once($ruta_db_superior."db.php");
+include_once($ruta_db_superior."librerias_saia.php");
+echo(estilo_bootstrap());
 
 if(@$_REQUEST['id']){
     
