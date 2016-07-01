2b8fc4723d0220d4b51f2d34d98df9a0182d19ed
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-07-01 16:35:38 +0000

    [desarrollo bases de calidad - req 12648][ruben.pulgarin]desarrollo bases de calidad - req 12648 desarrollo bases de calidad - req 12648

--- formatos/bases_calidad/previo_mostrar_bases_calidad.php
+++ formatos/bases_calidad/previo_mostrar_bases_calidad.php
@@ -32,7 +32,7 @@ if(@$_REQUEST['iddoc']){
                 border-top-right-radius: 5px;
                 border-top-left-radius: 5px;
             }
-            #version_estado span{
+            .version_estado span{
                 font-weight:bold;
             }
         </style>
