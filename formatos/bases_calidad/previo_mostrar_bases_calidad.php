cfe69e3fa58aa7bd081a47f380f57d95aeb0403f
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-07-01 16:12:01 +0000

    [desarrollo bases de calidad - req 12648][ruben.pulgarin]desarrollo bases de calidad - req 12648 desarrollo bases de calidad - req 12648

--- formatos/bases_calidad/previo_mostrar_bases_calidad.php
+++ formatos/bases_calidad/previo_mostrar_bases_calidad.php
@@ -12,13 +12,8 @@ include_once($ruta_db_superior."db.php");
 include_once($ruta_db_superior."librerias_saia.php");
 echo(estilo_bootstrap());
 
-if(@$_REQUEST['id']){
-    
-    $keys=explode('-',$_REQUEST['id']);
-    $idformato=$keys[0];
-    $idft=$keys[1];
-    $idft_bases_calidad=$keys[2];
-    $iddoc=$keys[3];
+if(@$_REQUEST['iddoc']){
+    $iddoc=$_REQUEST['iddoc'];
     
     $datos=busca_filtro_tabla("","ft_bases_calidad a, documento b","b.iddocumento=a.documento_iddocumento AND lower(b.estado)='aprobado' AND a.documento_iddocumento=".$iddoc,"",$conn);
     $serie_seleccionada=busca_filtro_tabla("","serie","estado=1 and idserie=".$datos[0]['tipo_base_calidad'],"",$conn);
