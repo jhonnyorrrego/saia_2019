1bde3536a7e62ad6a7d80a7baa7106ad2a62cfb3
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-07-01 16:11:47 +0000

    [desarrollo bases de calidad - req 12648][ruben.pulgarin]desarrollo bases de calidad - req 12648 desarrollo bases de calidad - req 12648

--- formatos/arboles/arbolcalidad_macro.php
+++ formatos/arboles/arbolcalidad_macro.php
@@ -103,7 +103,7 @@ for($i=0;$i<$dependencia["numcampos"];$i++){
         if(bases_calidad[0]=='bcp'){
             alert('es padre bases calidad');
         }else if(bases_calidad[0]=='bc'){
-             conexion="../bases_calidad/previo_mostrar_bases_calidad.php?id="+bases_calidad[1];
+             conexion="../bases_calidad/previo_mostrar_bases_calidad.php?iddoc="+bases_calidad[1];
              window.parent.open(conexion,"detalles"); 
         }else{
             accion="mostrar";
