42a1932dbfeb29cd2cb229834d8b93344bdbc850
hernando.trejos <hernando.trejos@cerok.com>
2016-07-01 00:10:59 +0000

    [responsive][hernando.trejos]Se adiciona un boton que permita hacer el resize del centro

--- index_actualizacion.php
+++ index_actualizacion.php
@@ -33,7 +33,7 @@ include_once($ruta_db_superior."pantallas/lib/mobile_detect.php");
 $detect = new Mobile_Detect;
 if ( $detect->isMobile() ) {
 	$_SESSION["tipo_dispositivo"]="movil";
-	print_r($_SESSION["tipo_dispositivo"]);
+	print_r($_SESSION);
 }
 /*************actualizacion de fin de año ********/
 /*
