b5ad77e7453f64196e82b8aa37901673f34dc82f
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-07-05 21:54:06 +0000

    [mostrar y editar Mapa de Proceso en base al formato bases calidad  req 12648][ruben.pulgarin]mostrar y editar Mapa de Proceso en base al formato bases calidad  req 12648 mostrar y editar Mapa de Proceso en base al formato bases calidad  req 12648

--- formatos/proceso/traer_cordenadas_proceso.php
+++ formatos/proceso/traer_cordenadas_proceso.php
@@ -13,7 +13,7 @@ include_once($ruta_db_superior."db.php");
 
 if(@$_REQUEST['idft_proceso']){
     
-    $cordenadas=busca_filtro_tabla("","","","",$conn);
+    $cordenadas=busca_filtro_tabla("","ft_proceso","idft_proceso=".$_REQUEST['idft_proceso'],"",$conn);
     
     
 }
