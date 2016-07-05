f994da1990a9d3610c0eb7650e9066e809b67552
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-07-05 19:07:36 +0000

    [campo anexo, para anexar el Mapa de Proceso req 12648][ruben.pulgarin]campo anexo, para anexar el Mapa de Proceso req 12648 campo anexo, para anexar el Mapa de Proceso req 12648, desarrollado solo para mostrarse cuando se seleccione la opcion Mapa de Proceso

--- formatos/bases_calidad/funciones.php
+++ formatos/bases_calidad/funciones.php
@@ -44,6 +44,12 @@ function mostrar_ocultar_anexo_bases_calidad(){
      global $conn,$ruta_db_superior;  
     
      $serie_mapa_proceso=busca_filtro_tabla("idserie","serie","lower(nombre)='mapa de proceso'","",$conn);
+     $idserie_mapa_proceso="";
+     if($serie_mapa_proceso['numcampos']){
+         $idserie_mapa_proceso=$serie_mapa_proceso[0]['idserie'];
+     }
+     
+     
      ?>
      <script>
          $(document).ready(function(){
