c6064dea865b622865c5ac4e18e3a6333b990f0d
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-07-05 22:06:59 +0000

    [mostrar y editar Mapa de Proceso en base al formato bases calidad  req 12648][ruben.pulgarin]mostrar y editar Mapa de Proceso en base al formato bases calidad  req 12648 mostrar y editar Mapa de Proceso en base al formato bases calidad  req 12648

--- formatos/proceso/previo_mostrar_proceso.php
+++ formatos/proceso/previo_mostrar_proceso.php
@@ -291,14 +291,14 @@ $ok=@$_REQUEST["editar"];
                                     });
                                 }else{
                                     $('#cropbox').Jcrop({
-                                        	setSelect:[0,0,0,0],
+                                        	setSelect:[],
                                     });
                                 }
                             }
                     });                     
                 }else{
                                     $('#cropbox').Jcrop({
-                                        	setSelect:[0,0,0,0],
+                                        	setSelect:[],
                                     });                    
                 }
               });
