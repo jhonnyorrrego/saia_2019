defb44745ac3b2503df5d46cc53adf09e6689f9f
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-07-05 14:55:53 +0000

    [correccion tildes en reporte mis tareas componente 207][ruben.pulgarin]correccion tildes en reporte mis tareas componente 207 correccion tildes en reporte mis tareas componente 207

--- pantallas/tareas/librerias.php
+++ pantallas/tareas/librerias.php
@@ -100,9 +100,7 @@ if($documento_iddocumento!="documento_iddocumento"){
 
 function mostrar_texto_codificado($texto){
     global $conn,$ruta_db_superior;
-    
     $texto=html_entity_decode($texto);
-    
     return($texto);
 }
 
