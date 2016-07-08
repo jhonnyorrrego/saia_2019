616a71a3ee8350a14437f1f6c2e1e30c402d2fe0
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-07-08 16:44:17 +0000

    [correcciones de Control de documentos (Solicitud de adicin, modificacin, eliminacin de documentos, reportes de estado maestro y obsoletos) GOBERNACION - RELEASE1 req 12648 ][ruben.pulgarin]correcciones de Control de documentos (Solicitud de adicin, modificacin, eliminacin de documentos, reportes de estado maestro y obsoletos) GOBERNACION - RELEASE1 req 12648  correcciones de Control de documentos (Solicitud de adicin, modificacin, eliminacin de documentos, reportes de estado maestro y obsoletos) GOBERNACION - RELEASE1 req 12648

--- versionamiento/versionar_documentos.php
+++ versionamiento/versionar_documentos.php
@@ -134,6 +134,8 @@ if(!$datos_documento){
 					}else{
 						notificaciones("<b>Se creo la versi&oacute;n ".$datos_documento["version"]." del documento</b>","success",8500);						
 						$documentos     = obtener_anexos_paginas_documento($datos_documento);
+						print_r($documentos);die();
+						
 						$copia_archivos = copiar_anexos_paginas_documento($datos_documento,$documentos, $iddocumento_version);				
 																
 						if(!$copia_archivos){
