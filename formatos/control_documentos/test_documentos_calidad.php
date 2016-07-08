071385c4be1de18357c86a1ed30106a0fc87b865
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-07-08 21:19:57 +0000

    [adaptacion versionamiento bases calidad (mision, vision, mapa de proceso, etc) req 12648][ruben.pulgarin]adaptacion versionamiento bases calidad (mision, vision, mapa de proceso, etc) req 12648 adaptacion versionamiento bases calidad (mision, vision, mapa de proceso, etc) req 12648

--- formatos/control_documentos/test_documentos_calidad.php
+++ formatos/control_documentos/test_documentos_calidad.php
@@ -98,8 +98,10 @@ function arbol_bases_calidad(&$arbol,$formato){
 	
 	
 	//print_r($seleccionado);
-			
-    armar_ramas_arbol($arbol,1, $idserie_seleccionado[0]['nombre'], $formato[0]['idformato']."|",$nocheckbox);				
+	if($seleccionado['numcampos']){
+	    armar_ramas_arbol($arbol,1, $idserie_seleccionado[0]['nombre'], $formato[0]['idformato']."|",$nocheckbox);	
+	}		
+    			
 	
 }
 
