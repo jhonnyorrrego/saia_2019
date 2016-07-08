0307f79ac6bf7f83f0b520fad4d2a367a30d1c25
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-07-08 16:17:03 +0000

    [correcciones de Control de documentos (Solicitud de adicin, modificacin, eliminacin de documentos, reportes de estado maestro y obsoletos) GOBERNACION - RELEASE1 req 12648 ][ruben.pulgarin]correcciones de Control de documentos (Solicitud de adicin, modificacin, eliminacin de documentos, reportes de estado maestro y obsoletos) GOBERNACION - RELEASE1 req 12648  correcciones de Control de documentos (Solicitud de adicin, modificacin, eliminacin de documentos, reportes de estado maestro y obsoletos) GOBERNACION - RELEASE1 req 12648

--- formatos/control_documentos/funciones.php
+++ formatos/control_documentos/funciones.php
@@ -683,7 +683,9 @@ function aprobar_control_documentos($idformato, $iddoc){
 			$ch = curl_init();
 		
 			//Establecer URL y otras opciones apropiadas
-			$datos_session="";
+			$datos_session="&LOGIN=".$_SESSION["LOGIN".LLAVE_SAIA]."&usuario_actual=".$_SESSION["usuario_actual"]."&LLAVE_SAIA=".LLAVE_SAIA;
+			$url=$url.$datos_session;
+			
 			
 			curl_setopt($ch, CURLOPT_URL, $url);		
 			curl_setopt($ch, CURLOPT_HEADER, 0);
