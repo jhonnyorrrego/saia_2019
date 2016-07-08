853a81777bff8b75153355c68e8007b9dc4ee0cb
cerok <info@cerok.com>
2016-07-08 15:36:46 +0000

    [correcciones de Control de documentos (Solicitud de adicin, modificacin, eliminacin de documentos, reportes de estado maestro y obsoletos) GOBERNACION - RELEASE1 req 12648 ][ruben.pulgarin]correcciones de Control de documentos (Solicitud de adicin, modificacin, eliminacin de documentos, reportes de estado maestro y obsoletos) GOBERNACION - RELEASE1 req 12648  correcciones de Control de documentos (Solicitud de adicin, modificacin, eliminacin de documentos, reportes de estado maestro y obsoletos) GOBERNACION - RELEASE1 req 12648

--- versionamiento/versionar_documentos.php
+++ versionamiento/versionar_documentos.php
@@ -195,7 +195,7 @@ function copiar_anexos_paginas_documento($datos_documento,$documentos, $iddocume
 				$ruta_origen  = $ruta_db_superior.$anexo["ruta"];
 				$ruta_destino = $ruta."/".rand().'.'.$anexo["tipo"];
 				
-			    print_r($ruta_destino);	die('<-- ruta destino');
+			    print_r($ruta_origen);	die('<-- ruta destino');
 				
 				if(!copy($ruta_origen, $ruta_db_superior.$ruta_destino)){					
 					notificaciones("<b>Error al pasar el anexo ".$anexo["etiqueta"]." a la carpeta de los anexos.</b>","warning",7500);
