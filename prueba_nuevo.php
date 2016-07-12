c25b735fcb2cc5122c75a91439ecd9a43d2d514d
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-07-12 13:27:31 +0000

    [exportando formatos calidad req 12889][ruben.pulgarin]exportando formatos calidad req 12889 exportando formatos calidad req 12889

--- prueba_nuevo.php
+++ prueba_nuevo.php
@@ -2,7 +2,7 @@
 
 include_once('db.php');
 
-$idbusqueda=74;
+$idbusqueda=75;
 $consulta=busca_filtro_tabla("","busqueda","idbusqueda=".$idbusqueda,"",$conn);
 $busqueda=$consulta;
 for($i=0;$i<count($busqueda[0]);$i++){
