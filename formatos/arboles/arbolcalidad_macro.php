c80093560cc812a82f74512fc0e4df47b5d4c596
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-07-12 22:35:45 +0000

    [correccion mostrar mapa proceso al ingresar][ruben.pulgarin]correccion mostrar mapa proceso al ingresar correccion mostrar mapa proceso al ingresar

--- formatos/arboles/arbolcalidad_macro.php
+++ formatos/arboles/arbolcalidad_macro.php
@@ -148,10 +148,12 @@ for($i=0;$i<$dependencia["numcampos"];$i++){
       	tree_calidad.selectItem(item,true,false);
       <?php } ?>
       
-     
+         <?php 
+         $iddoc_mapa_proceso=busca_filtro_tabla("idformato","formato","nombre_tabla LIKE'ft_proceso'","",$conn);
+         ?>
      
          if( parseInt($('#ejecutar_evento_mapa_proceso').val())==1 ){
-             tree_calidad.selectItem(194,true,false); /*por defecto Mapa de proceso*/
+             tree_calidad.selectItem(parseInt('<?php echo($iddoc_mapa_proceso[0]['idformato']); ?>'),true,false); /*por defecto Mapa de proceso*/
             $('#ejecutar_evento_mapa_proceso').val(0);
          }
       }    
