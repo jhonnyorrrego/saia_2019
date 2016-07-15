1bed6b17b3486795018e0964f6f8edecab6bb94d
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-07-15 14:42:46 +0000

    [adaptacion nuevos graficos req: 12889][ruben.pulgarin]adaptacion nuevos graficos req: 12889 adaptacion nuevos graficos req: 12889

--- formatos/indicadores_calidad/prueba_graficos.php
+++ formatos/indicadores_calidad/prueba_graficos.php
@@ -1,3 +1,4 @@
+    <script src="echarts.min.js"></script>
 <?php
         
 
@@ -5,9 +6,52 @@
 
 function generar_grafico_barra($idcontenedor,$titulos,$valores){
     
-    
-    
-    
+    ?>
+        <script type="text/javascript">
+ 		    var myChart = echarts.init(document.getElementById('main'));
+
+            var option = {
+                color: ['#3398DB'],
+                tooltip : {
+                    trigger: 'axis',
+                    axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+                        type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+                    }
+                },
+                grid: {
+                    left: '3%',
+                    right: '4%',
+                    bottom: '3%',
+                    containLabel: true
+                },
+                xAxis : [
+                    {
+                        type : 'category',
+                        data : ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+                        axisTick: {
+                            alignWithLabel: true
+                        }
+                    }
+                ],
+                yAxis : [
+                    {
+                        type : 'value'
+                    }
+                ],
+                series : [
+                    {
+                        name:'直接访问',
+                        type:'bar',
+                        barWidth: '60%',
+                        data:[10, 52, 200, 334, 390, 330, 220]
+                    }
+                ]
+            };
+            
+            myChart.setOption(option);
+
+        </script>
+    <?php
 }
 
 
@@ -30,50 +74,4 @@ function generar_grafico_barra($idcontenedor,$titulos,$valores){
 
     <center>
     <div id="main" style="width: 600px;height:400px;"></div>
-    <script src="echarts.min.js"></script>
-    <script type="text/javascript">
- 		var myChart = echarts.init(document.getElementById('main'));
-        
-//app.title = '坐标轴刻度与标签对齐';
 
-var option = {
-    color: ['#3398DB'],
-    tooltip : {
-        trigger: 'axis',
-        axisPointer : {            // 坐标轴指示器，坐标轴触发有效
-            type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-        }
-    },
-    grid: {
-        left: '3%',
-        right: '4%',
-        bottom: '3%',
-        containLabel: true
-    },
-    xAxis : [
-        {
-            type : 'category',
-            data : ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-            axisTick: {
-                alignWithLabel: true
-            }
-        }
-    ],
-    yAxis : [
-        {
-            type : 'value'
-        }
-    ],
-    series : [
-        {
-            name:'直接访问',
-            type:'bar',
-            barWidth: '60%',
-            data:[10, 52, 200, 334, 390, 330, 220]
-        }
-    ]
-};
-        // Load data into the ECharts instance 
-        myChart.setOption(option);
-
-    </script>
