ec1105100fa6cbaa6d21530ef69c7164dbd75c61
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-07-15 16:03:32 +0000

    [adaptacion nuevos graficos req: 12889][ruben.pulgarin]adaptacion nuevos graficos req: 12889 adaptacion nuevos graficos req: 12889

--- formatos/indicadores_calidad/prueba_graficos.php
+++ formatos/indicadores_calidad/prueba_graficos.php
@@ -45,7 +45,7 @@ function generar_grafico_barra($color,$idcontenedor,$nombres,$valores,$titulo_gr
                 yAxis : [
                     {
                         nameRotate:11,
-                        nameGap:-50,
+                        nameGap:-100,
                         name:'hola mundo', //
                         type : 'value'
                     }
