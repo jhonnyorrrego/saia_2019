36977862b1e540aaa2bd94f44d168eaf4efe10fc
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-07-18 19:54:38 +0000

    [nuevos graficos REQ: 12889][ruben.pulgarin]nuevos graficos REQ: 12889 nuevos graficos REQ: 12889

--- formatos/indicadores_calidad/prueba_graficos.php
+++ formatos/indicadores_calidad/prueba_graficos.php
@@ -139,6 +139,7 @@ function generar_grafico_torta($configuracion_grafico){
 	            
 			case 'barras':
 			    $configuracion_grafico=array();
+			    $configuracion_grafico['imagen']=1;
 			    $configuracion_grafico['titulo_grafico']='Mi Grafico';
 			    $configuracion_grafico['contenedores']=array('contenedor_grafico_pc','imagen_grafico_pc');
 			    $configuracion_grafico['nombres']=array('titulo 5','titulo 10','titulo 15','titulo 20','titulo 25','titulo 30');
