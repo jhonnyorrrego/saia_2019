a8e2f92d7f51c1ec31754cf2a22ae4f43d072412
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-07-18 21:54:44 +0000

    [nuevos graficos REQ: 12889][ruben.pulgarin]nuevos graficos REQ: 12889 nuevos graficos REQ: 12889

--- formatos/indicadores_calidad/prueba_graficos.php
+++ formatos/indicadores_calidad/prueba_graficos.php
@@ -345,7 +345,6 @@ function generar_grafico_linea($configuracion_grafico){
     $configuracion_grafico['nombres']=array('titulo 5','titulo 10','titulo 15','titulo 20','titulo 25','titulo 30');
     $configuracion_grafico['valores']=array(array(5,10,15,20,25,30));
     $configuracion_grafico['colores']=array('#00FF51','#D923D0','#00FF51','#D923D0','#00FF51','#D923D0');
-    //$configuracion_grafico['color_grafico']='#B1B109';
     generar_grafico_barra($configuracion_grafico);
     
     
@@ -360,8 +359,8 @@ function generar_grafico_linea($configuracion_grafico){
     $configuracion_grafico['subtitulo_grafico']='Mi Subtitulo';    
     $configuracion_grafico['nombres']=array('titulo 5','titulo 10','titulo 15','titulo 20','titulo 25','titulo 30');
     $configuracion_grafico['valores']=array(array(5,10,15,20,25,30),array(50,100,150,200,250,300));
+     //$configuracion_grafico['color_saia']=1;
     $configuracion_grafico['colores']=array('#00FF51','#D923D0','#00FF51','#D923D0','#00FF51','#D923D0');
-    //$configuracion_grafico['color_grafico']='#B1B109';
     generar_grafico_linea($configuracion_grafico);
     
     
