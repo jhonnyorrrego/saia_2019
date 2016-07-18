2cb253b8d426aa78dbc697e7d76d93a5b2bdfa36
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-07-18 22:58:58 +0000

    [nuevos graficos REQ: 12889][ruben.pulgarin]nuevos graficos REQ: 12889 nuevos graficos REQ: 12889

--- formatos/indicadores_calidad/funciones.php
+++ formatos/indicadores_calidad/funciones.php
@@ -190,6 +190,7 @@ if ($formulas["numcampos"]) {
                 <tr><td colspan="5">
                      <script src="build/dist/echarts.js"></script>
                     <div id="contenedor_grafico_pc" style="width: 700px;height:240px;"></div>
+                    <br/>
                     <div id="contenedor_grafico_rs" style="width: 700px;height:240px;"></div>
                 </td> </tr>
 			');
