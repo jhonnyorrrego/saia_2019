8b475b03aab2d749d543b4f2041c3210f4a41edb
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-07-18 19:24:53 +0000

    [nuevos graficos REQ: 12889][ruben.pulgarin]nuevos graficos REQ: 12889 nuevos graficos REQ: 12889

--- formatos/indicadores_calidad/prueba_graficos.php
+++ formatos/indicadores_calidad/prueba_graficos.php
@@ -35,11 +35,13 @@ function generar_grafico_torta($color_grafico,$contenedores,$nombres,$valores,$t
         
        $data_nombres=array();
        for($i=0;$i<count($nombres['nombres']);$i++){
-            $data_nombres[$i]['value']=$nombres['nombres'][$i];
-            $data_nombres[$i]['textStyle']['color']=$nombres['colores'][$i];
-            $data_nombres[$i]['textStyle']['fontWeight']='bold';           
+            $data_nombres[$i]['name']=$nombres['name'][$i];
+            $data_nombres[$i]['value']=$nombres['value'][$i];
+            //$data_nombres[$i]['textStyle']['color']=$nombres['colores'][$i];
+            //$data_nombres[$i]['textStyle']['fontWeight']='bold';           
        }        
         $data_nombres=json_encode($data_nombres);
+        echo($data_nombres);die();   
        // echo($titulos);die();
     ?>
         <script type="text/javascript">
