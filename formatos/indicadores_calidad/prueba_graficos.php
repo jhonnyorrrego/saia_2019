72db93320815d04cc407abe859474b18a9063635
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-07-18 21:17:50 +0000

    [nuevos graficos REQ: 12889][ruben.pulgarin]nuevos graficos REQ: 12889 nuevos graficos REQ: 12889

--- formatos/indicadores_calidad/prueba_graficos.php
+++ formatos/indicadores_calidad/prueba_graficos.php
@@ -276,32 +276,12 @@ function generar_grafico_linea($configuracion_grafico){
                         name:'最高气温',
                         type:'line',
                         data:[11, 11, 15, 13, 12, 13, 10],
-                        markPoint : {
-                            data : [
-                                {type : 'max', name: '最大值'},
-                                {type : 'min', name: '最小值'}
-                            ]
-                        },
-                        markLine : {
-                            data : [
-                                {type : 'average', name: '平均值'}
-                            ]
-                        }
+
                     },
                     {
                         name:'最低气温',
                         type:'line',
                         data:[1, -2, 2, 5, 3, 2, 0],
-                        markPoint : {
-                            data : [
-                                {name : '周最低', value : -2, xAxis: 1, yAxis: -1.5}
-                            ]
-                        },
-                        markLine : {
-                            data : [
-                                {type : 'average', name : '平均值'}
-                            ]
-                        }
                     }
                 ]
             };
