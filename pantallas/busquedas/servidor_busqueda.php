2c28731780b21339095dca3a8d16bfd394ab3d1e
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-07-18 12:41:55 +0000

    [correccion contador buzones REQ: 13007][ruben.pulgarin]correccion contador buzones REQ: 13007 correccion contador buzones REQ: 13007

--- pantallas/busquedas/servidor_busqueda.php
+++ pantallas/busquedas/servidor_busqueda.php
@@ -252,20 +252,14 @@ if(@$_REQUEST["idbusqueda_temporal"]){
 	}
 }
 if(!@$_REQUEST["cantidad_total"]){
-    $consulta_conteo = "SELECT COUNT(1) AS cant FROM " . $tablas_consulta . " WHERE " . $condicion . $ordenar_consulta;
-    if(MOTOR == 'SqlServer' || MOTOR == 'MSSql'){
-        $consulta_conteo = "WITH conteo AS (SELECT " . $campos_consulta . " FROM " . $tablas_consulta . " WHERE " . $condicion . $ordenar_consulta.") SELECT COUNT(*) as cant FROM conteo";
-    } else if(strpos(strtolower($campos_consulta), "sum(") !== false || strpos(strtolower($campos_consulta), "avg(") !== false) {
-        $consulta_conteo = "SELECT COUNT(1) AS cant FROM (SELECT " . $campos_consulta . " FROM " . $tablas_consulta . " WHERE " . $condicion . $ordenar_consulta.") as cant";
-    }
-    $conteo_filas = $conn->Ejecutar_sql($consulta_conteo);
-    $result=phpmkr_fetch_array($conteo_filas);
-    $result[0]=array();
-    $result[0]['cant']=$result['cant'];
-    $result["numcampos"]=$result['cant'];
-    $_REQUEST["cantidad_total"]=$result["numcampos"];    
-
-} else {
+	$result = ejecuta_filtro_tabla("SELECT COUNT(*) AS cant FROM ".$tablas_consulta." WHERE ".$condicion.$ordenar_consulta,$conn);
+	if($result["numcampos"]>1){
+		$_REQUEST["cantidad_total"]=$result["numcampos"];
+	}else{
+		$_REQUEST["cantidad_total"]=$result[0]["cant"];
+	}
+}
+else{
 	$result["numcampos"]=@$_REQUEST["cantidad_total"];
 	$result[0]['cant']=@$_REQUEST["cantidad_total"];
 }
