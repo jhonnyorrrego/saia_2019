134c2714b327c4ac671e770d2a91c5923486d636
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-07-18 20:30:52 +0000

    [nuevos graficos REQ: 12889][ruben.pulgarin]nuevos graficos REQ: 12889 nuevos graficos REQ: 12889

--- formatos/indicadores_calidad/prueba_graficos.php
+++ formatos/indicadores_calidad/prueba_graficos.php
@@ -134,7 +134,10 @@ function generar_grafico_barra($configuracion_grafico){
                     x:'center'
                 },
                 tooltip : {
-                    trigger: 'axis'
+                    trigger: 'axis',
+                    axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+                        type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+                    }                    
                 },
                 /*toolbox: {
                     show : true,
