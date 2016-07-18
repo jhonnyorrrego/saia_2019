807f832898dafb2c9309f1e4a3294be590a741bb
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-07-18 19:31:20 +0000

    [nuevos graficos REQ: 12889][ruben.pulgarin]nuevos graficos REQ: 12889 nuevos graficos REQ: 12889

--- formatos/indicadores_calidad/prueba_graficos.php
+++ formatos/indicadores_calidad/prueba_graficos.php
@@ -98,13 +98,7 @@ function generar_grafico_torta($color_grafico,$contenedores,$nombres,$valores,$t
                         type:'pie',
                         radius : '55%',
                         center: ['50%', '60%'],
-                        data:[
-                            {value:335, name:'直接访问'},
-                            {value:310, name:'邮件营销'},
-                            {value:234, name:'联盟广告'},
-                            {value:135, name:'视频广告'},
-                            {value:1548, name:'搜索引擎'}
-                        ]
+                        data:<?php echo($data_nombres); ?>
                     }
                 ]
             };
