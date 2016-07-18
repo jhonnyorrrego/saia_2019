0ebde7b0203011a14500ff229aade8569a4d4a7d
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-07-18 20:15:07 +0000

    [nuevos graficos REQ: 12889][ruben.pulgarin]nuevos graficos REQ: 12889 nuevos graficos REQ: 12889

--- formatos/indicadores_calidad/prueba_graficos.php
+++ formatos/indicadores_calidad/prueba_graficos.php
@@ -128,35 +128,74 @@ function generar_grafico_barra($configuracion_grafico){
  		    var myChart = ec.init(document.getElementById('<?php echo($configuracion_grafico['contenedores'][0]); ?>'));
 
             var option = {
-                <?php echo($generar_imagen); ?>
                 title : {
-                    text: '<?php echo($configuracion_grafico['titulo_grafico']); ?>',
-                    subtext: '<?php echo($configuracion_grafico['subtitulo_grafico']); ?>',
-                    x:'center'
+                    text: '某地区蒸发量和降水量',
+                    subtext: '纯属虚构'
                 },
                 tooltip : {
-                    trigger: 'item',
-                    formatter: "{a} <br/>{b} : {c} ({d}%)"
+                    trigger: 'axis'
                 },
                 legend: {
-                    orient : 'vertical',
-                    x : 'left',
-                    data:<?php echo(json_encode($configuracion_grafico['nombres'])); ?>
+                    data:['蒸发量','降水量']
                 },
                 toolbox: {
                     show : true,
+                    feature : {
+                        mark : {show: true},
+                        dataView : {show: true, readOnly: false},
+                        magicType : {show: true, type: ['line', 'bar']},
+                        restore : {show: true},
+                        saveAsImage : {show: true}
+                    }
                 },
                 calculable : true,
+                xAxis : [
+                    {
+                        type : 'category',
+                        data : ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月']
+                    }
+                ],
+                yAxis : [
+                    {
+                        type : 'value'
+                    }
+                ],
                 series : [
                     {
-                        name:'Valores',
-                        type:'pie',
-                        radius : '55%',
-                        center: ['50%', '60%'],
-                        data:<?php echo($data_nombres); ?>
+                        name:'蒸发量',
+                        type:'bar',
+                        data:[2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3],
+                        markPoint : {
+                            data : [
+                                {type : 'max', name: '最大值'},
+                                {type : 'min', name: '最小值'}
+                            ]
+                        },
+                        markLine : {
+                            data : [
+                                {type : 'average', name: '平均值'}
+                            ]
+                        }
+                    },
+                    {
+                        name:'降水量',
+                        type:'bar',
+                        data:[2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3],
+                        markPoint : {
+                            data : [
+                                {name : '年最高', value : 182.2, xAxis: 7, yAxis: 183, symbolSize:18},
+                                {name : '年最低', value : 2.3, xAxis: 11, yAxis: 3}
+                            ]
+                        },
+                        markLine : {
+                            data : [
+                                {type : 'average', name : '平均值'}
+                            ]
+                        }
                     }
                 ]
             };
+                    
                     
             myChart.setOption(option);
             } //fin function ec
