2d341cd790a275a4cc470840bc95fe5c4063d73e
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-07-18 20:11:55 +0000

    [nuevos graficos REQ: 12889][ruben.pulgarin]nuevos graficos REQ: 12889 nuevos graficos REQ: 12889

--- formatos/indicadores_calidad/prueba_graficos.php
+++ formatos/indicadores_calidad/prueba_graficos.php
@@ -24,7 +24,6 @@ echo( librerias_jquery('1.7') );
 function generar_grafico_torta($configuracion_grafico){
     global $conn;
 
-        
         if($color_grafico==''){
             $color_saia=busca_filtro_tabla("","configuracion","nombre='color_encabezado_list'","",$conn);  
             $color_grafico=$color_saia[0]['valor'];
@@ -35,7 +34,6 @@ function generar_grafico_torta($configuracion_grafico){
         if($configuracion_grafico['imagen']){
             $generar_imagen='renderAsImage:true,';
         }
-        
         
         //PARSEO NOMBRES Y VALORES
        $data_nombres=array();
@@ -96,6 +94,88 @@ function generar_grafico_torta($configuracion_grafico){
         </script>
     <?php
 }
+
+
+
+function generar_grafico_barra($configuracion_grafico){
+    global $conn;
+
+        if($color_grafico==''){
+            $color_saia=busca_filtro_tabla("","configuracion","nombre='color_encabezado_list'","",$conn);  
+            $color_grafico=$color_saia[0]['valor'];
+        }
+        
+        //PARSEO renderAsImage
+        $generar_imagen='';
+        if($configuracion_grafico['imagen']){
+            $generar_imagen='renderAsImage:true,';
+        }
+        
+        //PARSEO NOMBRES Y VALORES
+       $data_nombres=array();
+       for($i=0;$i<count($configuracion_grafico['nombres']);$i++){
+            $data_nombres[$i]['value']=$configuracion_grafico['valores'][$i];
+            $data_nombres[$i]['name']=$configuracion_grafico['nombres'][$i];
+           // $data_nombres[$i]['textStyle']['color']=$nombres['colores'][$i];
+           // $data_nombres[$i]['textStyle']['fontWeight']='bold';           
+       }        
+        $data_nombres=json_encode($data_nombres);
+    ?>
+        <script type="text/javascript">
+        
+            require.config({
+              paths: {
+                 echarts: 'build/dist'
+              }
+            });
+            require(['echarts','echarts/chart/pie'],// require the specific chart type        
+            function (ec) {
+ 		    var myChart = ec.init(document.getElementById('<?php echo($configuracion_grafico['contenedores'][0]); ?>'));
+
+            var option = {
+                <?php echo($generar_imagen); ?>
+                title : {
+                    text: '<?php echo($configuracion_grafico['titulo_grafico']); ?>',
+                    subtext: '<?php echo($configuracion_grafico['subtitulo_grafico']); ?>',
+                    x:'center'
+                },
+                tooltip : {
+                    trigger: 'item',
+                    formatter: "{a} <br/>{b} : {c} ({d}%)"
+                },
+                legend: {
+                    orient : 'vertical',
+                    x : 'left',
+                    data:<?php echo(json_encode($configuracion_grafico['nombres'])); ?>
+                },
+                toolbox: {
+                    show : true,
+                },
+                calculable : true,
+                series : [
+                    {
+                        name:'Valores',
+                        type:'pie',
+                        radius : '55%',
+                        center: ['50%', '60%'],
+                        data:<?php echo($data_nombres); ?>
+                    }
+                ]
+            };
+                    
+            myChart.setOption(option);
+            } //fin function ec
+            
+            );
+        </script>
+    <?php
+}
+
+
+
+
+
+
 
 
 
