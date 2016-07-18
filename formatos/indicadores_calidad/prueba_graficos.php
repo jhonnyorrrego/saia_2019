9d6d254a2c6c1c8f1d5ba3659737255095d6947c
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-07-18 16:12:25 +0000

    [nuevos graficos REQ: 12889][ruben.pulgarin]nuevos graficos REQ: 12889 nuevos graficos REQ: 12889

--- formatos/indicadores_calidad/prueba_graficos.php
+++ formatos/indicadores_calidad/prueba_graficos.php
@@ -46,58 +46,40 @@ function generar_grafico_torta($color_grafico,$contenedores,$nombres,$valores,$t
  		    var myChart = echarts.init(document.getElementById('<?php echo($contenedores[0]); ?>'));
 
             var option = {
-                renderAsImage:true,
-                title: {text: '<?php echo($titulo_grafico); ?>', x:'center'},
-                color: ['<?php echo($color_grafico); ?>'],
+                title : {
+                    text: '某站点用户访问来源',
+                    subtext: '纯属虚构',
+                    x:'center'
+                },
                 tooltip : {
-                    trigger: 'axis',
-                    axisPointer : {            // 坐标轴指示器，坐标轴触发有效
-                        type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-                    }
+                    trigger: 'item',
+                    formatter: "{a} <br/>{b} : {c} ({d}%)"
                 },
-                grid: {
-                   // left: '3%',
-                   // right: '4%',
-                    //bottom: '3%',
-                    containLabel: true
+                legend: {
+                    orient: 'vertical',
+                    left: 'left',
+                    data: ['直接访问','邮件营销','联盟广告','视频广告','搜索引擎']
                 },
-                xAxis : [
-                    {
-                        
-                        nameTextStyle:{
-                          color: '#000000',
-                          fontWeight:'bold'
-                        },
-                        nameLocation:'middle',
-                        nameGap:21,                        
-                        name:'<?php echo($titulox); ?>',
-                        type : 'category',
-                        data: <?php echo($data_nombres); ?>,
-                       
-                        axisTick: {
-                            alignWithLabel: true
-                        }
-                    }
-                ],
-                yAxis : [
-                    {
-                        nameTextStyle:{
-                          color: '#000000',
-                          fontWeight:'bold'
-                        },
-                        nameLocation:'middle',
-                        nameGap:30,
-                        name:'<?php echo($tituloy); ?>', //
-                        type : 'value'
-                    }
-                ],
                 series : [
                     {
-                        name:'Valor',
-                        type:'bar',
-                        barWidth: '50%',
-                        data:<?php echo($valores); ?>
-                        
+                        name: '访问来源',
+                        type: 'pie',
+                        radius : '55%',
+                        center: ['50%', '60%'],
+                        data:[
+                            {value:335, name:'直接访问'},
+                            {value:310, name:'邮件营销'},
+                            {value:234, name:'联盟广告'},
+                            {value:135, name:'视频广告'},
+                            {value:1548, name:'搜索引擎'}
+                        ],
+                        itemStyle: {
+                            emphasis: {
+                                shadowBlur: 10,
+                                shadowOffsetX: 0,
+                                shadowColor: 'rgba(0, 0, 0, 0.5)'
+                            }
+                        }
                     }
                 ]
             };
