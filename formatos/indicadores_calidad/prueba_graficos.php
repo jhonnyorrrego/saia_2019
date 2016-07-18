a1d61b19836beb596f375bd2a54a569667cc6f89
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-07-18 20:42:47 +0000

    [nuevos graficos REQ: 12889][ruben.pulgarin]nuevos graficos REQ: 12889 nuevos graficos REQ: 12889

--- formatos/indicadores_calidad/prueba_graficos.php
+++ formatos/indicadores_calidad/prueba_graficos.php
@@ -236,6 +236,7 @@ function generar_grafico_barra($configuracion_grafico){
     $configuracion_grafico['contenedor']='contenedor_grafico_pc';
     $configuracion_grafico['titulo_grafico']='Mi Grafico';
     $configuracion_grafico['subtitulo_grafico']='Mi Subtitulo';
+    $configuracion_grafico['titulox']='Eje X';
     $configuracion_grafico['nombres']=array('titulo 5','titulo 10','titulo 15','titulo 20','titulo 25','titulo 30');
     $configuracion_grafico['valores']=array(5,10,15,20,25,30);
     $configuracion_grafico['colores']=array('#00FF51','#D923D0','#00FF51','#D923D0','#00FF51','#D923D0');
