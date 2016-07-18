7d8dc9af08d61230f59a0a26b74a0e11c2ae91b4
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-07-18 20:28:54 +0000

    [nuevos graficos REQ: 12889][ruben.pulgarin]nuevos graficos REQ: 12889 nuevos graficos REQ: 12889

--- formatos/indicadores_calidad/prueba_graficos.php
+++ formatos/indicadores_calidad/prueba_graficos.php
@@ -157,7 +157,7 @@ function generar_grafico_barra($configuracion_grafico){
                     {
                         name:'Valores',
                         type:'bar',
-                        barWidth: 20,
+                        barWidth: 50,
                         data:<?php echo(json_encode($configuracion_grafico['valores'])); ?>
                     },
                    /* {
