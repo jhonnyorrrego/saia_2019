698a3092fc15221b334bde7f1989607fc17489d9
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-07-18 18:57:41 +0000

    [nuevos graficos REQ: 12889][ruben.pulgarin]nuevos graficos REQ: 12889 nuevos graficos REQ: 12889

--- formatos/indicadores_calidad/prueba_graficos.php
+++ formatos/indicadores_calidad/prueba_graficos.php
@@ -51,10 +51,10 @@ function generar_grafico_torta($color_grafico,$contenedores,$nombres,$valores,$t
  		    var myChart = ec.init(document.getElementById('<?php echo($contenedores[0]); ?>'));
 
             var option = {
-                
+  
                 title : {
-                    text: 'GRAFICO TORTA',
-                    subtext: 'Subtitulo Grafico Torta',
+                    text: '某站点用户访问来源',
+                    subtext: '纯属虚构',
                     x:'center'
                 },
                 tooltip : {
@@ -62,34 +62,49 @@ function generar_grafico_torta($color_grafico,$contenedores,$nombres,$valores,$t
                     formatter: "{a} <br/>{b} : {c} ({d}%)"
                 },
                 legend: {
-                    orient: 'vertical',
-                    left: 'left',
-                    data: ['Opcion 1','Opcion 2','Opcion 3','Opcion 4','Opcion 5']
+                    orient : 'vertical',
+                    x : 'left',
+                    data:['直接访问','邮件营销','联盟广告','视频广告','搜索引擎']
                 },
+                toolbox: {
+                    show : true,
+                    feature : {
+                        mark : {show: true},
+                        dataView : {show: true, readOnly: false},
+                        magicType : {
+                            show: true, 
+                            type: ['pie', 'funnel'],
+                            option: {
+                                funnel: {
+                                    x: '25%',
+                                    width: '50%',
+                                    funnelAlign: 'left',
+                                    max: 1548
+                                }
+                            }
+                        },
+                        restore : {show: true},
+                        saveAsImage : {show: true}
+                    }
+                },
+                calculable : true,
                 series : [
                     {
-                        name: 'Valor',
-                        type: 'pie',
-                        selectedMode: 'single',
+                        name:'访问来源',
+                        type:'pie',
                         radius : '55%',
                         center: ['50%', '60%'],
                         data:[
-                            {value:335, name:'Opcion 1'},
-                            {value:310, name:'Opcion 2'},
-                            {value:234, name:'Opcion 3'},
-                            {value:135, name:'Opcion 4'},
-                            {value:1548, name:'Opcion 5'}
-                        ],
-                        itemStyle: {
-                            emphasis: {
-                                shadowBlur: 10,
-                                shadowOffsetX: 0,
-                                shadowColor: 'rgba(0, 0, 0, 0.5)'
-                            }
-                        }
+                            {value:335, name:'直接访问'},
+                            {value:310, name:'邮件营销'},
+                            {value:234, name:'联盟广告'},
+                            {value:135, name:'视频广告'},
+                            {value:1548, name:'搜索引擎'}
+                        ]
                     }
                 ]
             };
+                    
             
             myChart.setOption(option);
             
