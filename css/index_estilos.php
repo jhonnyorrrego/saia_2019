d0d220048d05cc7c4da19558da35bf24d1f43602
cerok <info@cerok.com>
2016-07-19 14:54:20 +0000

    [Interfaz movil][hernando.trejos]Se modifica el if anidado por el caso y se adiciona el tema index

--- css/index_estilos.php
+++ css/index_estilos.php
@@ -172,7 +172,7 @@ function index_estilos($tipo_tema) {
     if($tipo_tema=="temas_movil"){
     	$tema_index .= '<style type="text/css">
 		body { overflow-x:hidden; margin-left: 0px; margin-top: 0px; margin-right: 0px; margin-bottom: 0px; background-color: #e7e7e7; font-family: Verdana, Geneva, sans-serif; font-size: 10px; font-weight: normal; }
-		#loginForm { margin: auto; width: 700px; height: 180px; }
+		
 		.footer_login { font-weight: bold; background-image: url('.$ruta_db_superior.'imagenes/login/footerbkg.png); background-repeat: repeat-x; background-position: left top; height: 25px; width: 100%; padding-top: 0px; padding-bottom: 0px; text-align: right; color: #FFF; position: fixed; bottom: 0px; }
 		.footer_login_text, .footer_login_text * { color:#FFF; font-size:11px; font-weight:bold; }
 		.blueTexts { font-family: Verdana, Geneva, sans-serif; font-size: 9px; font-weight: bold; color: #036; text-decoration: none; }
