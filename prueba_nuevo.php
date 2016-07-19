5aa23616df3c27e91f783f065d25a071e5acd835
john.rodriguez <john.rodriguez@cerok.com>
2016-07-19 18:22:37 +0000

    Commit automatico editor saia. Cambios locales 2016-07-19 13:22:36

--- prueba_nuevo.php
+++ prueba_nuevo.php
@@ -1,75 +1,62 @@
 <?php
-
-phpinfo();
-
-die();
-include_once('db.php');
-
-$idbusqueda=46;
-$consulta=busca_filtro_tabla("","busqueda","idbusqueda=".$idbusqueda,"",$conn);
-$busqueda=$consulta;
-for($i=0;$i<count($busqueda[0]);$i++){
-	unset($busqueda[0][$i]);
+$max_salida=6; // Previene algun posible ciclo infinito limitando a 10 los ../
+$ruta_db_superior=$ruta="";
+while($max_salida>0){
+  if(is_file($ruta."db.php")){
+    $ruta_db_superior=$ruta; //Preserva la ruta superior encontrada
+  }
+  $ruta.="../";
+  $max_salida--;
 }
-unset($busqueda[0]['idbusqueda']);
-$tabla="busqueda";
-$fieldList=array();
-		
-$strsql = "INSERT INTO ".$tabla." (";
-$strsql .= implode(",", array_keys($busqueda[0]));			
-$strsql .= ") VALUES ('";			
-$strsql .= implode("','", array_values($busqueda[0]));			
-$strsql .= "')";
-		
-echo($strsql); //imprimo busqueda
+include('db.php');
+include_once("librerias_saia.php");
+include_once($ruta_db_superior."formatos/librerias/funciones_generales.php");
 
-echo('<br><br><br>');
+$respuesta=enviar_correo(11654);
 
-$consulta_componente=busca_filtro_tabla("","busqueda_componente","busqueda_idbusqueda=".$idbusqueda,"",$conn);
-$busqueda_componente=$consulta_componente;
-
-for($j=0;$j<$busqueda_componente['numcampos'];$j++){
-	for($i=0;$i<count($busqueda_componente[$j]);$i++){
-		unset($busqueda_componente[$j][$i]);
-	}
-	unset($busqueda_componente[$j]['idbusqueda_componente']);
-	$tabla="busqueda_componente";
-	$fieldList=array();
-			
-	$strsql = "INSERT INTO ".$tabla." (";
-	$strsql .= implode(",", array_keys($busqueda_componente[$j]));			
-	$strsql .= ") VALUES ('";			
-	$strsql .= implode("','", array_values($busqueda_componente[$j]));			
-	$strsql .= "')";
-			
-	echo($strsql); //imprimo busqueda componente
-	echo('<br><br>');
-	$sql_condicion='&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>';
+function enviar_correo($iddoc){
+	global $conn,$ruta_db_superior;
 	
-	$consulta_condicion=busca_filtro_tabla("","busqueda_condicion","fk_busqueda_componente=".$consulta_componente[$j]['idbusqueda_componente'],"",$conn);
 	
-	if(!$consulta_condicion['numcampos']){
-		$consulta_condicion=busca_filtro_tabla("","busqueda_condicion","busqueda_idbusqueda=".$idbusqueda,"",$conn);
-	}
-	if($consulta_condicion['numcampos']){
-		
-		for($x=0;$x<count($consulta_condicion[0]);$x++){
-			unset($consulta_condicion[0][$x]);
-		}
-		unset($consulta_condicion[0]['idbusqueda_condicion']);
-		$tabla="busqueda_condicion";
-		$fieldList=array();
-					
-		$strsql = "INSERT INTO ".$tabla." (";
-			$strsql .= implode(",", array_keys($consulta_condicion[0]));			
-			$strsql .= ") VALUES ('";			
-			$strsql .= implode("','", array_values($consulta_condicion[0]));			
-			$strsql .= "')";
-					
-		$sql_condicion.=$strsql.'</b>';
-		echo($sql_condicion); //imprimo busqueda condicion
-		echo('<br><br><br>');		
-	}	
+	$consulta=busca_filtro_tabla("","documento","iddocumento=".$iddoc,"",$conn);
+	$nombre_funcionario=busca_filtro_tabla("","funcionario a, dependencia_cargo b","a.funcionario_codigo=".$consulta[0]["ejecutor"],"",$conn);
+	
+	$datos_documento=busca_filtro_tabla("","documento","iddocumento=".$iddoc,"",$conn);
+	print_r($datos_documento[0]['pdf'].'<br />');
+	
+	$ruta=crear_pdf_documento_tcpdf($datos_documento[0]);
+	print_r("Ruta curl".$ruta.'<br />');
+	
+	$datos_documento=busca_filtro_tabla("","documento","iddocumento=".$iddoc,"",$conn);
+	
+	//$ruta_nueva=str_replace("_0_", "_".$datos_documento[0]['numero']."_", $datos_documento[0]['pdf']);
+	//print_r("Ruta nueva".$ruta_nueva.'<br />');
+	
+	phpmkr_query("UPDATE documento SET pdf='" . $ruta . "' WHERE iddocumento=".$iddoc);
+	$datos_documento=busca_filtro_tabla("","documento","iddocumento=".$iddoc,"",$conn);
+	
+	print_r("Ruta actualizada".$datos_documento[0]['pdf'].'<br />');
+	
+	die();
+	$nombre=$nombre_funcionario[0]['nombres']." ".$nombre_funcionario[0]['apellidos'];
+	
+	
+	$permiso=busca_filtro_tabla("a.nombre,b.motivo_permiso","serie a,ft_solicitud_permiso b","a.idserie=b.motivo_permiso AND b.documento_iddocumento=".$iddoc,"",$conn);
+	
+	$motivo=html_entity_decode($permiso[0]['nombre']);
+	$numero_radicado=$datos_documento[0]['numero'];
+	
+	$anexos = array($datos_documento[0]['pdf']);
+
+	$destinos = array('1088303313');
+	
+	$asunto="Solicitud de permiso aprobada"." - ".$nombre." - ".strip_tags($motivo);
+
+	$mensaje="La solicitud de permiso No. ".$numero_radicado." ha sido aprobada";
+	
+	//$resp=enviar_mensaje("","codigo",$destinos,$asunto,$mensaje,$anexos,"",$iddoc);	
 }
-die();
+
+
+		
 ?>
