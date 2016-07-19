dc3de4d72256157f821bd23b7b6bda9ca242d277
john.rodriguez <john.rodriguez@cerok.com>
2016-07-19 16:15:36 +0000

    [SQL injection][john.rodriguez]Implementan de seguridad  Se reduce la vulnerabilidad del archivo

--- perfiladd.php
+++ perfiladd.php
@@ -15,7 +15,20 @@ $x_nombre = Null;
 ?>
 <?php include ("phpmkrfn.php") ?>
 <?php
+$sKey = @$_GET["key"];
 
+if (array_key_exists("form_info", $_POST)) {
+    include_once ($ruta_db_superior . "pantallas/lib/librerias_cripto.php");
+    $data = json_decode($_POST["form_info"], true);
+    unset($_REQUEST);
+    unset($_POST);
+    for($i = 0; $i < count($data); $i ++) {
+        $_REQUEST[decrypt_blowfish($data[$i]["name"], LLAVE_SAIA_CRYPTO)] = decrypt_blowfish($data[$i]["value"], LLAVE_SAIA_CRYPTO);
+        $_POST[decrypt_blowfish($data[$i]["name"], LLAVE_SAIA_CRYPTO)] = decrypt_blowfish($data[$i]["value"], LLAVE_SAIA_CRYPTO);
+    }
+    print_r($_REQUEST);
+    die();
+}
 // Get action
 $sAction = @$_POST["a_add"];
 if (($sAction == "") || ((is_null($sAction)))) {
@@ -107,9 +120,29 @@ echo(estilo_bootstrap());
 </span></td>
 	</tr>
 </table>
+<input type="hidden" name="form_info" id="form_info" value="">
 <p>
-<input type="submit" name="Action" value="Adicionar"  class='btn btn-primary'>
+<input type="submit" name="Action" value="Adicionar"  id="continuar" class='btn btn-primary'>
 </form>
+
+<script type="text/javascript">
+$("#continuar").click(function(){
+	var salida = false;
+  		$.ajax({
+            type:'POST',
+            async: false,
+            url: "<?php echo $ruta_db_superior;?>formatos/librerias/encript_data.php",
+            data: {datos:JSON.stringify($('#perfiladd').serializeArray(), null)},
+            success: function(data) {
+            	$("#form_info").empty().val(data);
+            	//console.log($("#form_info").val());
+            	salida = true;
+         	}
+  		});  
+    return salida;
+  });
+
+</script>
 <?php include ("footer.php") ?>
 <?php
 
