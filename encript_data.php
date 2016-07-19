c1812f30e74b08d470bb86d5b023fd94d5c7dd53
john.rodriguez <john.rodriguez@cerok.com>
2016-07-19 15:25:10 +0000

    [Creacion del archivo encript_data][john.rodriguez]Para la conexion de los otros archivos

---
+++ encript_data.php
@@ -0,0 +1,25 @@
+<?php
+$max_salida = 10;
+$ruta_db_superior = $ruta = "";
+while ( $max_salida > 0 ) {
+    if (is_file($ruta . "db.php")) {
+        $ruta_db_superior = $ruta;
+    }
+    $ruta .= "../";
+    $max_salida --;
+}
+
+include_once ($ruta_db_superior . "db.php");
+include_once ($ruta_db_superior . "pantallas/lib/librerias_cripto.php");
+
+$data = array();
+if (isset($_POST["datos"])) {
+    $info = json_decode($_POST["datos"], true);
+    for($i = 0; $i < count($info); $i ++) {
+        $data[$i]["name"] = encrypt_blowfish($info[$i]["name"], LLAVE_SAIA_CRYPTO);
+        $data[$i]["value"] = encrypt_blowfish($info[$i]["value"], LLAVE_SAIA_CRYPTO);
+    }
+}
+echo json_encode($data);
+
+?>
