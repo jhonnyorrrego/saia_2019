a656cb664e926a8042e5217b2c46997e4c451dba
john.rodriguez <john.rodriguez@cerok.com>
2016-07-19 18:50:54 +0000

    [SQL injection][john.rodriguez]Incremento de seguridad Se modifica el formato con el fin de reducir la vulnerabilidad a ataques.

--- serieadd.php
+++ serieadd.php
@@ -364,7 +364,7 @@ $(document).ready(function(){
 	<tr class="ocultar">
 		<td class="encabezado" title="Cantidad de d&iacute;as para dar tr&aacute;mite y respuesta al documento"><span class="phpmaker" style="color: #FFFFFF;">TIEMPO DE RESPUESTA (D&Iacute;AS)</span></td>
 		<td bgcolor="#F5F5F5"><span class="phpmaker">
-<?php if (!(!is_null($x_dias_entrega)) || ($x_dias_entrega == "")) { $x_dias_entrega = 8;} // Set default value ?>
+<?php if (!(!is_null($x_dias_entrega)) || ($x_dias_entrega == "")) { $x_dias_entrega = 8;}  ?>
 <input type="text" name="x_dias_entrega" id="x_dias_entrega" size="30" value="<?php echo htmlspecialchars(@$x_dias_entrega) ?>">
 </span></td>
 	</tr>
