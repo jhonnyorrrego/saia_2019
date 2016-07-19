5274c74d644278153ee6a3f2843afe40ab24cae4
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-07-19 13:21:52 +0000

    [nuevos graficos REQ: 12889][ruben.pulgarin]nuevos graficos REQ: 12889 nuevos graficos REQ: 12889

--- formatos/indicadores_calidad/guardar_grafico.php
+++ formatos/indicadores_calidad/guardar_grafico.php
@@ -9,8 +9,7 @@ while($max_salida>0){
   $max_salida--;
 }
 include_once ($ruta_db_superior."db.php");
-$datos_img1=0;
-$datos_img2=0;
+$exito=0;
 if($_REQUEST['iddoc']){
 	$ruta_grafico=$ruta_db_superior."temporal_".usuario_actual("login")."/".$_REQUEST['iddoc']."/";
 	
@@ -36,8 +35,7 @@ if($_REQUEST['iddoc']){
 	if(!file_exists($ruta_grafico)){
 		crear_destino($ruta_grafico);
 	}
-	
-	if(@$_REQUEST['guardar_imagen']==1){
+
 	if($_REQUEST['img']){
 		$datoimg=explode(";",$_REQUEST['img']);
 		$decode=explode(",",$datoimg[1]);
@@ -48,26 +46,11 @@ if($_REQUEST['iddoc']){
 		fclose($archivo);
 		file_put_contents($ruta_grafico.$grafico_total_evaluacion, $datos);
 		if(file_exists($ruta_grafico.$grafico_total_evaluacion)){
-			$datos_img1=1;
+			$exito=1;
 		}
-	}
-	}
-	if(@$_REQUEST['guardar_imagen']==2){	
-	if($_REQUEST['img2']){
-		$datoimg=explode(";",$_REQUEST['img2']);
-		$decode=explode(",",$datoimg[1]);
-		$datos=base64_decode($decode[1]);
-		
-		$grafico_competencias="competencias.png";
-		$archivo = fopen($ruta_grafico.$grafico_competencias, "w+");	 //crea el archivo
-		fclose($archivo);
-		file_put_contents($ruta_grafico.$grafico_competencias, $datos);
-		if(file_exists($ruta_grafico.$grafico_competencias)){
-			$datos_img2=1;
-		}
-	}
 	}
 	
+
 }
 if($datos_img1==1){
 	echo 1;
@@ -76,23 +59,5 @@ if($datos_img1==1){
 }else{
 	echo 2;
 }
-function borrar_archivos_graficos($dir, $deleteRootToo){ 
-	if(!$dh = @opendir($dir)){ 
-  	return; 
-  } 
-  while (false !== ($obj = readdir($dh))){ 
-    if($obj == '.' || $obj == '..'){ 
-       continue; 
-    } 
-    if(!@unlink($dir . '/' . $obj)) {   
-       borrar_archivos_graficos($dir.'/'.$obj, true); 
-    }  
-  }
-  closedir($dh); 
-  //para borrar la carpeta raiz tambien
-  if($deleteRootToo){ 
-    @rmdir($dir); 
-  }
-  return; 
-}
+
 ?>
