c4961ecd0c020517618962e6c84563faee38e88e
hernando.trejos <hernando.trejos@cerok.com>
2016-07-21 00:47:40 +0000

    [Interfaz movil][hernando.trejos]Se adiciona el width al 80% para los label del formulario de bootstrap

--- css/index_estilos.php
+++ css/index_estilos.php
@@ -180,6 +180,7 @@ function index_estilos($tipo_tema) {
 		#contenedor_tabla{width: 25%; padding:10px; vertical-align:bottom}
 		#texto_pequenio{font-size:10px;font-weight:bold;}
 		hr {margin: 2px 0;border: 0;border-top: 1px solid rgb(77, 167, 226);border-bottom: 1px solid rgb(84, 167, 233);}
+		.form-horizontal .control-label { width: 80%; }
 		</style>';
     }
 return $tema_index;
