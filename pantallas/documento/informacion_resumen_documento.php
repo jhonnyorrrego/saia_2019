087fa585879beeb219efdd5aea9da589e2db5469
hernando.trejos <hernando.trejos@cerok.com>
2016-07-22 21:27:49 +0000

    [Interfaz movil][hernando.trejos]Se ajusta para el funcionamiento de dispositivos moviles

--- pantallas/documento/informacion_resumen_documento.php
+++ pantallas/documento/informacion_resumen_documento.php
@@ -421,7 +421,7 @@ function click_funcion(div){
     //iniciar_tooltip();
   } 
   $(".eliminar_pagina").removeClass('abrir_highslide');
-	$(".eliminar_adjunto_menu").removeClass('abrir_highslide');
+  $(".eliminar_adjunto_menu").removeClass('abrir_highslide');
   $(".eliminar_adjunto_menu").attr('id',"adjuntos_documento");
   $(".adicionar_anexos").attr('id',"adjuntos_documento");
   var browserType;
@@ -438,7 +438,7 @@ function click_funcion(div){
   tree2.setOnLoadingStart(cargando);
   tree2.setOnLoadingEnd(fin_cargando);
   tree2.setOnClickHandler(onNodeSelect);
-  tree2.loadXML("<?php echo($ruta_db_superior);?>formatos/arboles/test_formatos_documento2.php?id=<?php echo($iddoc2);?>");
+  tree2.loadXML("<?php echo($ruta_db_superior);?>formatos/arboles/test_forma_documento2.php?id=<?php echo($iddoc2);?>");
   function redireccion_ruta(iframe_destino,ruta_enlace){
     if(iframe_destino==''){
       window.location=ruta_enlace;
@@ -477,7 +477,7 @@ function click_funcion(div){
     llave=tree2.getParentId(nodeId);
 
     tree2.closeAllItems(tree2.getParentId(nodeId))
-    //tree2.openItem(nodeId);
+    tree2.openItem(nodeId);
     tree2.openItem(tree2.getParentId(nodeId));
     conexion="<?php echo($ruta_db_superior); ?>formatos/arboles/parsear_accion_arbol.php?id="+nodeId+"&accion="+accion+"&llave="+llave;
     window.parent.open(conexion,"detalles");
