a117a42cdbc32bb357ce3dffc4b770814201ddf8
hernando.trejos <hernando.trejos@cerok.com>
2016-07-22 20:58:31 +0000

    [Interfaz movil][hernando.trejos]Se ajusta para el funcionamiento de dispositivos moviles

--- pantallas/documento/informacion_resumen_documento.php
+++ pantallas/documento/informacion_resumen_documento.php
@@ -477,10 +477,10 @@ function click_funcion(div){
     llave=tree2.getParentId(nodeId);
 
     tree2.closeAllItems(tree2.getParentId(nodeId))
-    tree2.openItem(nodeId);
+    //tree2.openItem(nodeId);
     tree2.openItem(tree2.getParentId(nodeId));
     conexion="<?php echo($ruta_db_superior); ?>formatos/arboles/parsear_accion_arbol.php?id="+nodeId+"&accion="+accion+"&llave="+llave;
-    //window.parent.open(conexion,"detalles");
+    window.parent.open(conexion,"detalles");
     }
     function fin_cargando(){
         if (browserType == "gecko" )
