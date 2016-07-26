18343a7c18ed229fcefab9d513fe0652c41e35e3
john.rodriguez <john.rodriguez@cerok.com>
2016-07-26 15:41:01 +0000

    [correccin de codigo][john.rodriguez]Se corrige el cdigo por fallas del sql injection

--- class_transferencia.php
+++ class_transferencia.php
@@ -1797,9 +1797,10 @@ function transferencias_pendientes($serie)
 */   
 function radicar_plantilla(){ 
    global $conn,$sql,$ruta_db_superior;
-   if (array_key_exists("form_info", $_POST)) {
-       print_r($_POST);
+   print_r($_POST);
       die();
+   if (array_key_exists("form_info", $_POST)) {
+       
       $data = json_decode($_POST["form_info"], true);
       unset($_REQUEST);
       unset($_POST);
