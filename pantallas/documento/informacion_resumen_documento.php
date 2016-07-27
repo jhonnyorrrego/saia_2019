2643d9ef5ca0ddbfa804912d680f54ad1fa846a1
hernando.trejos <hernando.trejos@cerok.com>
2016-07-27 15:55:41 +0000

    [Interfaz movil][hernando.trejos]Se modifica para que seleccione pero no redireccione

--- pantallas/documento/informacion_resumen_documento.php
+++ pantallas/documento/informacion_resumen_documento.php
@@ -466,7 +466,7 @@ function click_funcion(div){
     else{
     	cargar_cantidades_documento(datos[3]);
 	    conexion="<?php echo($ruta_db_superior); ?>formatos/arboles/parsear_accion_arbol.php?id="+nodeId+"&accion=mostrar&llave="+llave+"&enlace_adicionar_formato=1";
-	    window.parent.frames["detalles"].location=conexion;
+	    redireccion_detalles(conexion);
     }
 	}
 	function seleccion_accion(accion,id){
@@ -478,14 +478,15 @@ function click_funcion(div){
       return;
     }
     llave=tree2.getParentId(nodeId);
-
     tree2.closeAllItems(tree2.getParentId(nodeId))
     tree2.openItem(nodeId);
     tree2.openItem(tree2.getParentId(nodeId));
     console.log(no_seleccionar);    
     conexion="<?php echo($ruta_db_superior); ?>formatos/arboles/parsear_accion_arbol.php?id="+nodeId+"&accion="+accion+"&llave="+llave;
-    window.parent.open(conexion,"detalles");
-
+    redireccion_detalles(conexion);
+    }
+    function redireccion_detalles(conexion){
+        window.parent.open(conexion,"detalles");    
     }
     function fin_cargando(){
         if (browserType == "gecko" )
