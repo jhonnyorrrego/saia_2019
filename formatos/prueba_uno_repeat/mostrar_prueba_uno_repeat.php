1f8c2529594dabd8f6947203e6ceff273f45daeb
john.rodriguez <john.rodriguez@cerok.com>
2016-07-27 20:27:30 +0000

    [prueba][john.rodriguez]prueba funcionalidad mostrar

--- formatos/prueba_uno_repeat/mostrar_prueba_uno_repeat.php
+++ formatos/prueba_uno_repeat/mostrar_prueba_uno_repeat.php
@@ -1,1 +1,3 @@
 <?php include_once("../librerias/funciones_generales.php"); ini_set("display_errors",true);?><script type="text/javascript" src="../../js/jquery.js"></script><?php include_once("../librerias/header_nuevo.php"); ?><?php include_once("../../class_transferencia.php"); ?><tr><td></td></tr><?php include_once("../librerias/footer_nuevo.php"); ?>
+
+n bl,nkbjk
