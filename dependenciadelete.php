13759466549f67610f131c2a7fbe44178730f0c2
john.rodriguez <john.rodriguez@cerok.com>
2016-07-27 22:42:34 +0000

    [correcciones del sql injection][john.rodriguez]Se modifica el cdigo por errores al pasar el sql injection

--- dependenciadelete.php
+++ dependenciadelete.php
@@ -16,7 +16,7 @@ $sKey = @$_GET["key"];
 if (array_key_exists("form_info", $_POST)) {
     include_once ($ruta_db_superior . "pantallas/lib/librerias_cripto.php");
     $data = json_decode($_POST["form_info"], true);
-    print_r($data);
+    print_r($_POST["form_info"]);
     die();
     unset($_REQUEST);
     unset($_POST);
