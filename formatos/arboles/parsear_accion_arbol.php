e2b1bbed187a93c3293e46f8bce17ed69483afc1
john.rodriguez <john.rodriguez@cerok.com>
2016-07-27 22:20:01 +0000

    [modificacion sql injection][john.rodriguez]Se agrega el codigo del sql injection

--- formatos/arboles/parsear_accion_arbol.php
+++ formatos/arboles/parsear_accion_arbol.php
@@ -12,6 +12,8 @@ $formato["numcampos"]=0;
 
 if(@$_REQUEST["id"]){
   $datos=parsea_idformato($_REQUEST["id"]);
+  print_r($datos);
+  die();
   $formato=busca_filtro_tabla("","formato","idformato=".$datos[0],"",$conn);  
   //print_r($datos);
   if(!$datos[2] && $datos[3]=="mostrar"){
