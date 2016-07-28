aa1c237bc619d15da3738ce31337a757edf0b28a
john.rodriguez <john.rodriguez@cerok.com>
2016-07-28 13:20:13 +0000

    [prueba injection][john.rodriguez]correccion del sql injection

--- class_transferencia.php
+++ class_transferencia.php
@@ -1800,7 +1800,7 @@ function radicar_plantilla(){
   /* if (array_key_exists("form_info", $_POST)) {
       $data = json_decode($_POST["form_info"], true);
       print_r($_POST);
-    die();
+        die();
       unset($_REQUEST);
       unset($_POST);
       for($i = 0; $i < count($data); $i ++) {
