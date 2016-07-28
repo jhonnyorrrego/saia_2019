932124c19cc88c13281c359dabe079638f35b957
john.rodriguez <john.rodriguez@cerok.com>
2016-07-28 13:15:30 +0000

    [prueba injection][john.rodriguez]correccion del sql injection

--- class_transferencia.php
+++ class_transferencia.php
@@ -1797,20 +1797,19 @@ function transferencias_pendientes($serie)
 */   
 function radicar_plantilla(){ 
    global $conn,$sql,$ruta_db_superior;
-   if (array_key_exists("form_info", $_POST)) {
+  /* if (array_key_exists("form_info", $_POST)) {
       $data = json_decode($_POST["form_info"], true);
-     // print_r($data);
-    //die();
-      //unset($_REQUEST);
-      //unset($_POST);
+      print_r($_POST);
+    die();
+      unset($_REQUEST);
+      unset($_POST);
       for($i = 0; $i < count($data); $i ++) {
-          //$_REQUEST[decrypt_blowfish($data[$i]["name"], LLAVE_SAIA_CRYPTO)] = decrypt_blowfish($data[$i]["value"], LLAVE_SAIA_CRYPTO);
-          //$_POST[decrypt_blowfish($data[$i]["name"], LLAVE_SAIA_CRYPTO)] = decrypt_blowfish($data[$i]["value"], LLAVE_SAIA_CRYPTO);
+          $_REQUEST[decrypt_blowfish($data[$i]["name"], LLAVE_SAIA_CRYPTO)] = decrypt_blowfish($data[$i]["value"], LLAVE_SAIA_CRYPTO);
+          $_POST[decrypt_blowfish($data[$i]["name"], LLAVE_SAIA_CRYPTO)] = decrypt_blowfish($data[$i]["value"], LLAVE_SAIA_CRYPTO);
       }
       
-   }
-   //print_r($_POST);
-    //die();
+   }*/
+   
    $valores=array();
    $plantilla="";
    $idformato=0;
