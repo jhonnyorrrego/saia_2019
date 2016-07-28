66ab320d34de680b66bf287974cda13d78f39d2f
john.rodriguez <john.rodriguez@cerok.com>
2016-07-28 13:12:15 +0000

    [prueba injection][john.rodriguez]correccion del sql injection

--- class_transferencia.php
+++ class_transferencia.php
@@ -2393,9 +2393,9 @@ function guardar_documento($iddoc,$tipo=0)
      llama_funcion_accion($iddoc,$idformato,"adicionar","ANTERIOR");
 
     $sql="INSERT INTO ".strtolower($_REQUEST["tabla"])."(".implode(",",$campos).") VALUES (".implode(",",$valores).")";
-  		/*if(usuario_actual("login")=="cerok"){  			
+  		if(usuario_actual("login")=="cerok"){  			
   			print_r($sql);die();
-  		}*/
+  		}
      phpmkr_query($sql,$conn);  
     //echo ($sql);
    $insertado=phpmkr_insert_id();
