56b582935a364e029b889044f9472ddf3b15a84c
john.rodriguez <john.rodriguez@cerok.com>
2016-08-02 13:03:59 +0000

    [Correccin del sql injection][john.rodriguez]prueba correccion

--- class_transferencia.php
+++ class_transferencia.php
@@ -1798,9 +1798,9 @@ function transferencias_pendientes($serie)
 function radicar_plantilla(){ 
    global $conn,$sql,$ruta_db_superior;
     if (array_key_exists("form_info", $_POST)) {
-     
+     // include_once ($ruta_db_superior . "pantallas/lib/librerias_cripto.php");
       $data = json_decode($_POST["form_info"], true);
-      print_r($_POST);
+      print_r($data);
       die();
       unset($_REQUEST);
       unset($_POST);
