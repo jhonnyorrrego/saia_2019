125065e4d3fc8ac342577295680a86cf35c5a28a
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-08-04 19:29:23 +0000

    [correccion adicionar_seleccionados, padding][ruben.pulgarin]correccion adicionar_seleccionados, padding correccion adicionar_seleccionados, padding

--- pantallas/lib/main.js
+++ pantallas/lib/main.js
@@ -15,6 +15,7 @@ $('.adicionar_seleccionados').live('click',function(){
     $('#resultado_pantalla_'+idregistro).addClass("alert");
     $('#resultado_pantalla_'+idregistro).addClass("alert-info"); 
     $('.alert-info').css("padding","0px");
+    alert('pasa');
   }    
 }); 
 $(".eliminar_seleccionado").live('click',function(){
