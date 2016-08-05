f02e9e05ddf1b5dfcb8df4bc22b4d6baa4dfc53a
hernando.trejos <hernando.trejos@cerok.com>
2016-08-05 16:44:35 +0000

    [Reparacion SQLInjection][hernando.trejos]Se modifica la redireccion del arbol al lado izquierdo

--- ordenar.php
+++ ordenar.php
@@ -100,11 +100,7 @@ $(document).ready(function(){
 <?php
 if($formato["numcampos"]){ ?>
 <script type="text/javascript">
-<?php if(usuario_actual('login')=='cerok'){ ?>     
-llamado_pantalla("formatos/arboles/arbolformato_documento.php","idformato=<?php echo($formato[0]["idformato"]);?>&iddoc=<?php echo($iddocumento); ?>&alto_pantalla="+alto,$("#izquierdo_saia"),"arbol_formato");
-<?php }else{ ?>
-llamado_pantalla("pantallas/documento/informacion_resumen_documento.php","idformato=<?php echo($formato[0]["idformato"]);?>&iddoc=<?php echo($iddocumento); ?>&alto_pantalla="+(alto-1),$("#izquierdo_saia"),"arbol_formato");
-<?php } ?>	
+llamado_pantalla("pantallas/documento/informacion_resumen_documento.php","form_info=<?php echo($llave);?>&alto_pantalla="+(alto-1),$("#izquierdo_saia"),"arbol_formato");
 llamado_pantalla("","",$("#contenedor_saia"),'detalles');
 hs.graphicsDir = '<?php echo($ruta_db_superior);?>anexosdigitales/highslide-4.0.10/highslide/graphics/';
 hs.outlineType = 'rounded-white';
