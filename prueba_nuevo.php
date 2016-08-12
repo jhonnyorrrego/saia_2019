194d8adfb56933c0c498dab5eb1db06073e8eaec
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-08-12 14:15:58 +0000

    [Pruebas][ruben.pulgarin]Creando funcion para generar json en versiones

--- prueba_nuevo.php
+++ prueba_nuevo.php
@@ -9,73 +9,21 @@ while($max_salida>0){
   $max_salida--;
 }
 include('db.php');
-include_once("librerias_saia.php");
-include_once($ruta_db_superior."formatos/librerias/funciones_generales.php");
-
- llena_serie(0,'modulo','');
-
-function llena_serie($serie,$tabla,$padre=''){
-global $conn;
-  $papas=busca_filtro_tabla("*",$tabla,"cod_padre=".$serie,"nombre ASC",$conn);
-
-if($papas["numcampos"])
-{ 
-  for($i=0; $i<$papas["numcampos"]; $i++)
-  {$hijos = busca_filtro_tabla("count(*)",$tabla,"cod_padre=".$papas[$i]["id$tabla"],"",$conn);
-   $hijos_seleccionados = busca_filtro_tabla("count(*)",$tabla,"cod_padre=".$papas[$i]["id$tabla"],"",$conn);
-   
-   // echo utf8_encode(html_entity_decode(($papas[$i]["etiqueta"])))." (".$papas[$i]["nombre"].") (".$papas[$i]["idmodulo"].") ---> PADRE: (".$padre.")";
-    echo ''.$papas[$i]["idmodulo"].'  '.utf8_encode(html_entity_decode($papas[$i]["etiqueta"])).'';
-    $padre='';
-    if($hijos[0][0]){
-        $padre=$papas[$i]["nombre"];  
-    }
-    echo('<br>');
-    llena_serie($papas[$i]["id$tabla"],'modulo',$padre);
-  }     
-}
-return;
-}
 
 
 
 
-die();
-
-$formatos=busca_filtro_tabla("idformato,etiqueta","formato","cod_padre IS NULL OR cod_padre='' ","etiqueta ASC",$conn);
-
-for($i=0;$i<$formatos['numcampos'];$i++){
-	echo('<p><strong>'.($i+1).') '.ucwords(strtolower($formatos[$i]['etiqueta'])).' ('.$formatos[$i]['idformato'].')</strong></p>');
-	$hijos=tiene_hijos($formatos[$i]['idformato']);
-	if($hijos['hijos']){
-		$lista_hijos=lista_hijos($hijos['cuales']);
-	}
-	//print_r($hijos);
-	
-	
-}
-function tiene_hijos($idformato){
-	global $conn;
-
-	$hijos=busca_filtro_tabla("idformato","formato","cod_padre=".$idformato,"",$conn);
-	
-	$retorno=array();
-	$retorno['hijos']=0;
-	if($hijos['numcampos']){
-		$retorno['hijos']=1;
-		$retorno['cuales']=implode(',',extrae_campo($hijos,'idformato'));
-	}
-	return($retorno);
-}
-function lista_hijos($cuales){
-	global $conn;
-	
-	$hijos=busca_filtro_tabla("etiqueta,idformato","formato","idformato IN(".$cuales.")","etiqueta ASC",$conn);
-	if($hijos['numcampos']){
-		for($i=0;$i<$hijos['numcampos'];$i++){
-			echo('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - '.ucwords(strtolower($hijos[$i]['etiqueta'])).' ('.$hijos[$i]['idformato'].')<br/>');
-		}
-	}
+function generar_version_json($iddoc){
+    global $conn;
+    
+    
+    $formato=busca_filtro_tabla('','formato a, documento b','lower(b.plantilla)=lower(a.nombre) AND iddocumento='.$iddoc,'',conn);
+    
+    $json=array();
+    
+    $ft=obtener_info_version();
+    
+    
 }
 
 
