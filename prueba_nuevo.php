2c554d20e2c0fc26589533b5beab58c7a2d038d9
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-09-05 21:56:03 +0000

    [Informacin del documento][ruben.pulgarin]desarrollo dias habiles festivos colombia

--- prueba_nuevo.php
+++ prueba_nuevo.php
@@ -38,8 +38,8 @@ function esCambioAnio($fecha,$dias){
         $part1_date1=date_create($anioini.'-'.$mesini.'-'.$diaini);
         $part1_date2=date_create($anioini.'-12-31');
         $diff=date_diff($part1_date1,$part1_date2);
-        //$retorno['diferencia_part1']=$diff->format("%a");
-
+        $retorno['diferencia_part1']=$diff->format("%a");
+        
         $part1_date1=$anioini.'-'.$mesini.'-'.$diaini;
         $festivos2 = new CalendarCol(date('Y'));
         $cantidad_festivos_part1=0;
