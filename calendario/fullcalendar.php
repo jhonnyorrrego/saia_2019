41e9876d257ab822347c2b3be17fffc3025d6014
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-09-06 21:48:27 +0000

    [Tareas][ruben.pulgarin]traslado desarrollo tareas desde sole

--- calendario/fullcalendar.php
+++ calendario/fullcalendar.php
@@ -266,7 +266,7 @@ else{
 
     $calendarios_tareas=array('calendario_tareas_responsable','calendario_tareas_coparticipante','calendario_tareas_seguidor','calendario_tareas_planeador');
     print_r('aqui:');
-    print_r($configuracion[0]['nombre']);die();
+    print_r($configuracion);die();
     
     if(in_array($configuracion[0]['nombre'],$calendarios_tareas)){ ?>
 	
