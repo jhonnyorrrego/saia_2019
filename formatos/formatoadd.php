d7b933390a7d346d2f6ac6dc08dfc73b3497df66
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-09-08 22:45:08 +0000

    [Informacin del documento][ruben.pulgarin]creando campo estado_documento al crear el formato

--- formatos/formatoadd.php
+++ formatos/formatoadd.php
@@ -810,6 +810,9 @@ function AddData($conn) {
 		phpmkr_query($strsql, $conn) or die("Falla al Ejecutar la busqueda " . phpmkr_error() . ' SQL:' . $strsql);
 	}
 	if($idformato && !$fieldList["item"]) {
+    	$sqlcd="INSERT INTO campos_formato (formato_idformato, nombre, etiqueta, tipo_dato, longitud, obligatoriedad, predeterminado, acciones, ayuda, banderas, etiqueta_html) VALUES (" . $idformato . ",'estado_documento', 'ESTADO DEL DOCUMENTO', 'VARCHAR', 255, 0,'', 'a','', '', 'hidden')";
+	   	phpmkr_query($sqlcd, $conn) or die("Falla al Ejecutar la busqueda " . phpmkr_error() . ' SQL:' . $strsql);
+	    
 		$strsql = "INSERT INTO campos_formato (formato_idformato, nombre, etiqueta, tipo_dato, longitud, obligatoriedad, predeterminado, acciones, ayuda, banderas, etiqueta_html) VALUES (" . $idformato . ",'serie_idserie', 'SERIE DOCUMENTAL', 'INT', 11, 1," . $fieldList["serie_idserie"] . ", 'a'," . $fieldList["etiqueta"] . ", 'fk', 'hidden')";
 		guardar_traza($strsql, "ft_" . $x_nombre);
 		phpmkr_query($strsql, $conn) or die("Falla al Ejecutar la busqueda " . phpmkr_error() . ' SQL:' . $strsql);
