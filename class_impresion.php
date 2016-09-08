876c3c0fa68d5725b06ae98aeca27fefa3636135
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-09-08 19:19:47 +0000

    [Informacin del documento][ruben.pulgarin]desarrollo contar paginas digitalizadas y paginas del pdf

--- class_impresion.php
+++ class_impresion.php
@@ -407,7 +407,7 @@ class Imprime_Pdf {
 
   function configurar_pagina($datos) {
       
-      print_r($datos);die();
+     
 
     if (isset($datos["imprimir_paginas"]) && $datos["imprimir_paginas"]) {
       $this->imprimir_paginas = $datos["imprimir_paginas"];
