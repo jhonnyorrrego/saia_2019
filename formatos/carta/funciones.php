e2e1c3c00f2bddaca7f7c30a7f543b54db98126a
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-09-08 21:20:21 +0000

    [Informacin del documento][ruben.pulgarin]desarrollo hash_file codigo qr

--- formatos/carta/funciones.php
+++ formatos/carta/funciones.php
@@ -54,7 +54,8 @@ function generar_codigo_qr_carta($idformato,$iddoc){
 	if($imagen==false){
 	  alerta("Error al tratar de crear el codigo qr");
 	}else{
-	  $sql_documento_qr="INSERT INTO documento_verificacion(documento_iddocumento,funcionario_idfuncionario,fecha,ruta_qr,verificacion) VALUES (".$iddoc.",".usuario_actual('idfuncionario').",".fecha_db_almacenar(date("Y-m-d H:i:s"),'Y-m-d H:i:s').",'".$imagen."','vacio')";
+	  $codigo_hash=obtener_codigo_hash_archivo($imagen,'crc32'); 
+	  $sql_documento_qr="INSERT INTO documento_verificacion(documento_iddocumento,funcionario_idfuncionario,fecha,ruta_qr,verificacion,codigo_hash) VALUES (".$iddoc.",".usuario_actual('idfuncionario').",".fecha_db_almacenar(date("Y-m-d H:i:s"),'Y-m-d H:i:s').",'".$imagen."','vacio','".$codigo_hash."')";
 	  phpmkr_query($sql_documento_qr);
 	}
 }
