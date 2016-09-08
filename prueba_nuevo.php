dae22408caa186d8651c9d8c148ab315fe9f842d
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-09-08 16:49:20 +0000

    [Informacin del documento][ruben.pulgarin]desarrollo contar paginas digitalizadas y paginas del pdf

--- prueba_nuevo.php
+++ prueba_nuevo.php
@@ -27,8 +27,14 @@ function calcular_paginas_documento($iddoc){
     
     //PAGINAS DEL PDF
     $banderas_formato=busca_filtro_tabla("a.banderas","formato a, documento b","lower(b.plantilla)=lower(a.nombre) AND b.iddocumento=".$iddoc,"",$conn);
+    $vector_banderas=explode(',',$banderas_formato[0]['banderas']);
     
-    print_r($banderas_formato);
+    if(in_array('cp',$vector_banderas)){
+        echo('contar paginas pdf');
+    }
+    
+    
+   
     
     
     
