55ef13939fab8b15c1bfff843afd790eb3bd8d8c
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-09-14 20:15:46 +0000

    [Admin. Flujos][ruben.pulgarin]desarrollo crear ruta tomado de un campo de un formato

--- bpmn/paso/editar_actividades_paso.php
+++ bpmn/paso/editar_actividades_paso.php
@@ -267,8 +267,8 @@ $paso=busca_filtro_tabla("","paso","idpaso=".$x_paso_idpaso,"",$conn);
                               <?php
                                 if($x_llave_entidad==-2){
                                     ?>
-                                    alert('entro: <?php echo($x_formato_anterior); ?>');
-                                    tree4.openItem( <?php echo($x_formato_anterior); ?> ); //ARBOL: expande nodo hasta el item indicado
+                                    
+                                    //tree4.openItem( <?php echo($x_formato_anterior); ?> ); //ARBOL: expande nodo hasta el item indicado
 		                            tree4.setCheck( <?php echo($x_formato_anterior); ?>,1 ); //ARBOL: check item indicado
                                     <?php
                                 }
