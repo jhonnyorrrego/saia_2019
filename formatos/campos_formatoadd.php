568f471bf984770403c809b437ba08b320dea914
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-09-14 21:38:48 +0000

    [Admin. Flujos][ruben.pulgarin]desarrollo crear ruta tomado de un campo de un formato

--- formatos/campos_formatoadd.php
+++ formatos/campos_formatoadd.php
@@ -341,10 +341,10 @@ $datos_formato=busca_filtro_tabla("item","formato","idformato=$x_formato_idforma
 		<td class="encabezado"><span class="phpmaker" style="color: #FFFFFF;">Campo Funcionario?</span></td>
 		<td bgcolor="#F5F5F5">
       <span class="phpmaker">
-        <input type="radio" name="x_banderas[]"  <?php if(in_array("u",$x_banderas)){echo("CHECKED"); } ?> value="ffc">funcionario_codigo
-        <input type="radio" name="x_banderas[]"  <?php if(in_array("u",$x_banderas)){echo("CHECKED"); } ?> value="fdc">iddependencia_cargo
-        <input type="radio" name="x_banderas[]"  <?php if(in_array("u",$x_banderas)){echo("CHECKED"); } ?> value="fid">idfuncionario        
-        <input type="radio" name="x_banderas[]"  <?php if(in_array("u",$x_banderas)){echo("CHECKED"); } ?> value="fc">cargo
+        <input type="radio" name="x_banderas[]"  <?php if(in_array("ffc",$x_banderas)){echo("CHECKED"); } ?> value="ffc">funcionario_codigo
+        <input type="radio" name="x_banderas[]"  <?php if(in_array("fdc",$x_banderas)){echo("CHECKED"); } ?> value="fdc">iddependencia_cargo
+        <input type="radio" name="x_banderas[]"  <?php if(in_array("fid",$x_banderas)){echo("CHECKED"); } ?> value="fid">idfuncionario        
+        <input type="radio" name="x_banderas[]"  <?php if(in_array("fc",$x_banderas)){echo("CHECKED"); } ?> value="fc">cargo
       </span>
     </td>
 	</tr>	
