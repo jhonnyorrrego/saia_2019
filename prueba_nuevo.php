60cb8a9c5bb565b34f81036c70a616adae25286a
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-09-14 14:24:08 +0000

    [Admin. Flujos][ruben.pulgarin]correccion pasos anteriores

--- prueba_nuevo.php
+++ prueba_nuevo.php
@@ -9,24 +9,13 @@ while($max_salida>0){
   $max_salida--;
 }
 include($ruta_db_superior.'db.php');
-include_once($ruta_db_superior."librerias_saia.php");
-include_once("pantallas/lib/librerias_cripto.php");
-//include_once($ruta_db_superior."workflow/libreria_paso.php");
-//$numero_usuarios=encrypt_blowfish(55,LLAVE_SAIA_CRYPTO);
-$numero_usuarios=decrypt_blowfish('a3171917621ac77ec05609d8207d0dfb',LLAVE_SAIA_CRYPTO);
-echo($numero_usuarios);die();
-    /*
-	 * cerok 			 1
-	 * radicador_salida	 2
-	 * mensajero 		 9
-	 * radicador_web 	 111222333
-  */
- 	$funcionarios=busca_filtro_tabla("","funcionario a","a.estado=1 AND lower(a.login) NOT IN ('cerok','radicador_salida','mensajero','radicador_web')","",$conn);
-	$reemplazos=busca_filtro_tabla("","reemplazo_saia b","b.estado=1","",$conn);
-	$funcionarios_activos=$funcionarios['numcampos'];
-	$reemplazos_activos=$reemplazos['numcampos'];
-	$cupos_usados=$funcionarios_activos+$reemplazos_activos;   
-	echo($cupos_usados);
+include_once($ruta_db_superior."workflow/libreria_paso.php");
+
+$idpaso=91;
+
+print_r(listado_pasos_anteriores_admin($idpaso));
+
+
 
 
 ?>
