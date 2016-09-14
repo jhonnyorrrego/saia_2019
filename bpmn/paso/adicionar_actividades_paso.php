a5602f6ad25ef7e1fc3938eb9409a06235aa8936
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-09-14 16:18:29 +0000

    [Admin. Flujos][ruben.pulgarin]desarrollo crear ruta tomado de un campo de un formato

--- bpmn/paso/adicionar_actividades_paso.php
+++ bpmn/paso/adicionar_actividades_paso.php
@@ -377,6 +377,8 @@ $paso=busca_filtro_tabla("","paso","idpaso=".$_REQUEST["idpaso"],"",$conn);
     
     if(tipo_actividad==1 && ( tipo_accion==3 || tipo_accion==7 ) && valor_llave.value=-2){
         $('#contenedor_formatos_anteriores').show();
+    }else{
+        $('#contenedor_formatos_anteriores').hide();
     }
     
     
