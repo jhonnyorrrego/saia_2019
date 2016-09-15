bef940b04f09ad4a09406f085fe650382151adcf
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-09-15 22:52:48 +0000

    [Admin. Flujos][ruben.pulgarin]desarrollo crear ruta tomado de un campo de un formato

--- workflow/libreria_paso.php
+++ workflow/libreria_paso.php
@@ -215,53 +215,16 @@ for($i=0;$i<@$listado_acciones_paso["numcampos"];$i++){
 	if(($accion=='confirmar' || $accion=='aprobar') && $listado_acciones_paso[$i]['llave_entidad']==-2){
 	    
 	                
-	                $datos_apaso_actividad=busca_filtro_tabla("","paso_actividad","idpaso_actividad=".$listado_acciones_paso[$i]['idpaso_actividad'],"",$conn);
+	    $datos_apaso_actividad=busca_filtro_tabla("","paso_actividad","idpaso_actividad=".$listado_acciones_paso[$i]['idpaso_actividad'],"",$conn);
 	                
-	                
-	                $condicion=generar_condicion_funcionario_tomado_campo($datos_apaso_actividad[0]["fk_campos_formato"],$datos_apaso_actividad[0]["formato_anterior"],$iddocumento);
-	                print_r($condicion);die();
+	   $condicion=generar_condicion_funcionario_tomado_campo($datos_apaso_actividad[0]["fk_campos_formato"],$datos_apaso_actividad[0]["formato_anterior"],$iddocumento);
+	   $funcionario=busca_filtro_tabla("","vfuncionario_dc",$condicion." AND estado_dc=1 AND estado=1","",$conn);
+                               
+	   
+	                print_r($funcionario);die();
 	                
                     $datos_formato_ruta=busca_filtro_tabla("b.nombre,b.banderas,a.nombre_tabla","formato a,campos_formato b","b.idcampos_formato=".$datos_apaso_actividad[0]["fk_campos_formato"]."  AND a.idformato=b.formato_idformato AND a.idformato=".$datos_apaso_actividad[0]["formato_anterior"],"",$conn);  
-                   
-                    if($datos_formato_ruta['numcampos']){
-                        $consulta_valor_campo=busca_filtro_tabla($datos_formato_ruta[0]['nombre'],$datos_formato_ruta[0]['nombre_tabla'],"documento_iddocumento=".$iddocumento,"",$conn);
-                         
-                        $valor_campo_ruta=$consulta_valor_campo[0][$datos_formato_ruta[0]['nombre']];
-                      
-                        if($valor_campo_ruta){
-                            $vector_banderas=explode(',',$datos_formato_ruta[0]['banderas']);
-                            $vector_banderas_validar=array('ffc','fdc','fid','fc');//funcionario_codigo,iddependencia_cargo,idfuncionario,idcargo
-                            $bandera_validar='';
-                            for($i=0;$i<count($vector_banderas_validar);$i++){
-                                if(in_array($vector_banderas_validar[$i],$vector_banderas)){
-                                    $bandera_validar=$vector_banderas_validar[$i];
-                                    $i=count($vector_banderas_validar); //corto el ciclo
-                                }
-                            }
-                            
-                            if($bandera_validar!=''){
-                                switch($bandera_validar){
-                                    case 'ffc': //funcionario_codigo
-                                        $condicion="funcionario_codigo='".$valor_campo_ruta."'";
-                                        break;
-                                    case 'fdc': //iddependencia_cargo
-                                        $condicion="iddependencia_cargo='".$valor_campo_ruta."'";
-                                        break;
-                                    case 'fid': //idfuncionario
-                                        $condicion="idfuncionario='".$valor_campo_ruta."'";
-                                        break;
-                                    case 'cargo': //idcargo
-                                        $condicion="idcargo='".$valor_campo_ruta."'";
-                                        break;                                        
-                                }
-                                $funcionario=busca_filtro_tabla("","vfuncionario_dc",$condicion." AND estado_dc=1 AND estado=1","",$conn);
-                               
-                            }
-                            
-                        }
-                        
-                        
-                    } //fin $datos_formato_ruta['numcampos']	    
+            
 	    
 	   
 	   	
