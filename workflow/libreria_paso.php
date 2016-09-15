29bed5762edbc1e41b7cd9163fcdc6e4a0b3a61c
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-09-15 22:49:58 +0000

    [Admin. Flujos][ruben.pulgarin]desarrollo crear ruta tomado de un campo de un formato

--- workflow/libreria_paso.php
+++ workflow/libreria_paso.php
@@ -219,7 +219,7 @@ for($i=0;$i<@$listado_acciones_paso["numcampos"];$i++){
 	                
 	                
 	                $condicion=generar_condicion_funcionario_tomado_campo($datos_apaso_actividad[0]["fk_campos_formato"],$datos_apaso_actividad[0]["formato_anterior"],$iddocumento);
-	                print_r($condicion.'<--aqui');die();
+	                print_r($condicion);die();
 	                
                     $datos_formato_ruta=busca_filtro_tabla("b.nombre,b.banderas,a.nombre_tabla","formato a,campos_formato b","b.idcampos_formato=".$datos_apaso_actividad[0]["fk_campos_formato"]."  AND a.idformato=b.formato_idformato AND a.idformato=".$datos_apaso_actividad[0]["formato_anterior"],"",$conn);  
                    
@@ -1362,7 +1362,7 @@ function generar_condicion_funcionario_tomado_campo($fk_campos_formato,$formato_
 
 	$condicion=0;
     $datos_formato_ruta=busca_filtro_tabla("b.nombre,b.banderas,a.nombre_tabla","formato a,campos_formato b","b.idcampos_formato=".fk_campos_formato."  AND a.idformato=b.formato_idformato AND a.idformato=".$formato_anterior,"",$conn);  
-                   
+      print_r($datos_formato_ruta);  die();          
     if($datos_formato_ruta['numcampos']){
 		$consulta_valor_campo=busca_filtro_tabla($datos_formato_ruta[0]['nombre'],$datos_formato_ruta[0]['nombre_tabla'],"documento_iddocumento=".$iddoc,"",$conn);
                          
