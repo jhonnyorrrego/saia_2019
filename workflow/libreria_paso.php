2c8f4f22b34a487671edd80d3df43ad99a41dcec
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-09-15 23:00:22 +0000

    [Admin. Flujos][ruben.pulgarin]desarrollo crear ruta tomado de un campo de un formato

--- workflow/libreria_paso.php
+++ workflow/libreria_paso.php
@@ -505,7 +505,7 @@ function validar_ruta_documento_flujo($iddoc,$pasos_evaluar,$paso_anterior,$acci
                     $funcionario=busca_filtro_tabla("","vfuncionario_dc",$condicion." AND estado_dc=1 AND estado=1","",$conn);
                     
                     
-                    print_r($funcionario);die();
+                   
                   }else{
                       $funcionario=busca_filtro_tabla("","vfuncionario_dc","idcargo=".$dato_paso_ruta[0]["llave_entidad"]." AND estado_dc=1 AND estado=1","",$conn);
                   } //fin llave_entidad -2
