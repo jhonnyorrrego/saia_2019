6e40f6b58c4d253085f1f606f6c5510dee3e3f41
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-09-15 22:17:30 +0000

    [Admin. Flujos][ruben.pulgarin]desarrollo crear ruta tomado de un campo de un formato

--- workflow/libreria_paso.php
+++ workflow/libreria_paso.php
@@ -136,9 +136,11 @@ if($accion!="" && $tipo_terminacion==1){
 
 //La condicion c.estado_paso_documento>4 es para verificar que el paso no este terminado o cerrado
   if($iddocumento){
-       print_r($accion);die();
+      
     //error("LISATDO ACCIONES 1");
     $listado_acciones_paso=busca_filtro_tabla("B.idpaso_actividad, A.idaccion, C.idpaso_documento, B.entidad_identidad, B.llave_entidad, C.diagram_iddiagram_instance, B.paso_idpaso,C.documento_iddocumento, B.formato_idformato", "accion A, paso_actividad B, paso_documento C","A.idaccion=B.accion_idaccion AND B.paso_idpaso=C.paso_idpaso AND C.documento_iddocumento=".$iddocumento." AND A.nombre='".$accion."' AND C.estado_paso_documento>3 AND B.estado=1","B.orden",$conn);
+    
+     print_r($listado_acciones_paso);die();
   }               
 }
 else if($paso_documento && $idactividad){
