6515106b8cc0bcc5e7c1c7dab67ba87b41b647e0
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-09-19 21:53:25 +0000

    [Transferencia documental][ruben.pulgarin]desarrollo etiquetar expedientes a transferir segun dias habiles

--- formatos/transferencia_doc/funciones.php
+++ formatos/transferencia_doc/funciones.php
@@ -36,10 +36,10 @@ function guardar_expedientes_add($idformato,$iddoc){
 	echo($texto);
 }
 function expedientes_vinculados_funcion($idformato,$iddoc){
-	global $conn, $ruta_db_superior;	
-	$datos=busca_filtro_tabla("","ft_transferencia_doc A, documento B","A.documento_iddocumento=".$iddoc." and A.documento_iddocumento=B.iddocumento","",$conn);
+	global $conn, $ruta_db_superior;
 	
 	/*
+	$datos=busca_filtro_tabla("","ft_transferencia_doc A, documento B","A.documento_iddocumento=".$iddoc." and A.documento_iddocumento=B.iddocumento","",$conn);
 	$expedientes=busca_filtro_tabla("","expediente A","A.idexpediente in(".$datos[0]["expediente_vinculado"].")","",$conn);
 	if($expedientes["numcampos"]){
 		$texto.='<p>&nbsp;</p>
