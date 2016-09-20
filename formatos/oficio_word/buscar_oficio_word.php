6779155746252b6be2a6353e1a6135853b77dd0d
giovanni.montes <giovanni.montes@cerok.com>
2016-09-20 16:50:54 +0000

    Commit automatico editor saia. Cambios locales 2016-09-20 11:50:53

--- formatos/oficio_word/buscar_oficio_word.php
+++ formatos/oficio_word/buscar_oficio_word.php
@@ -1,30 +1,7 @@
-<html><title>.:BUSCAR NUEVO OFICIO:.</title><head><script type="text/javascript" src="../../js/jquery.fcbkcomplete.js"></script><link rel="stylesheet" type="text/css" href="../../css/style_fcbkcomplete.css"/></head><body bgcolor="#F5F5F5"><form name="formulario_formatos" id="formulario_formatos" method="post" action="../librerias/funciones_buscador.php" enctype="multipart/form-data"><table width="100%" cellspacing="1" cellpadding="4" border="0"><tr><td colspan="4" class="encabezado_list">B&Uacute;SQUEDA NUEVO OFICIO</td></tr><tr>
+<html><title>.:BUSCAR OFICIO WORD:.</title><head><script type="text/javascript" src="../../js/jquery.fcbkcomplete.js"></script><link rel="stylesheet" type="text/css" href="../../css/style_fcbkcomplete.css"/></head><body bgcolor="#F5F5F5"><form name="formulario_formatos" id="formulario_formatos" method="post" action="../librerias/funciones_buscador.php" enctype="multipart/form-data"><table width="100%" cellspacing="1" cellpadding="4" border="0"><tr><td colspan="4" class="encabezado_list">B&Uacute;SQUEDA OFICIO WORD</td></tr><tr>
                    <td class="encabezado" width="20%" title="">ENLACE_PLANTILLA</td>
                    <td bgcolor="#F5F5F5"><label></label><input type="hidden" name="enlace_plantilla" value=""></td>
-                  </tr><tr><td class="encabezado">&nbsp;<select name="condicion_destinos" id="condicion_destinos"><option value="AND">Y</option><option value="OR">O</option></td>
-                     <td class="encabezado" width="20%" title="">DESTINOS</td><td class="encabezado">&nbsp;<select name="compara_destinos" id="compara_destinos"> <option value="or">Alguno</option><option value="and">Todos</option></select></td>
-                     <td bgcolor="#F5F5F5"><select multiple  maxlength="2000"   id="destinos" name="destinos"  ></select></td>
-                    </tr>
-                    <script>
-                     $(document).ready(function() 
-                      {
-                      $("#destinos").fcbkcomplete({
-                        complete_text:"Nombre o identificacion",
-                        newel: true
-                      });
-                      });
-                     </script><tr><td class="encabezado">&nbsp;<select name="condicion_asunto" id="condicion_asunto"><option value="AND">Y</option><option value="OR">O</option></td>
-                     <td class="encabezado" width="20%" title="">ASUNTO</td><td class="encabezado">&nbsp;<select name="compara_asunto" id="compara_asunto"> <option value="or">Alguno</option><option value="and">Todos</option></select></td>
-                     <td bgcolor="#F5F5F5"><select multiple id="asunto" name="asunto"></select><script>
-                     $(document).ready(function() 
-                      {
-                      $("#asunto").fcbkcomplete({
-                        complete_text:"Presione enter para agregar una palabra.",
-                        newel: true
-                      });
-                      });
-                     </script></td>
-                    </tr><tr><td class="encabezado">&nbsp;<select name="condicion_anexo_word" id="condicion_anexo_word"><option value="AND">Y</option><option value="OR">O</option></td>
+                  </tr><tr><td class="encabezado">&nbsp;<select name="condicion_anexo_word" id="condicion_anexo_word"><option value="AND">Y</option><option value="OR">O</option></td>
                      <td class="encabezado" width="20%" title="">ANEXO WORD</td><td class="encabezado">&nbsp;<select name="compara_anexo_word" id="compara_anexo_word"> <option value="or">Alguno</option><option value="and">Todos</option></select></td>
                      <td bgcolor="#F5F5F5"><select multiple id="anexo_word" name="anexo_word"></select><script>
                      $(document).ready(function() 
@@ -35,7 +12,7 @@
                       });
                       });
                      </script></td>
-                    </tr><?php submit_formato(149);?></table><?php if(@$_REQUEST["campo__retorno"]){ ?>
+                    </tr><?php submit_formato(400);?></table><?php if(@$_REQUEST["campo__retorno"]){ ?>
                 <input type="hidden" name="campo__retorno" value="<?php echo($_REQUEST["campo__retorno"]); ?>">
               <?php }
                if(@$_REQUEST["formulario__retorno"]){ ?>
