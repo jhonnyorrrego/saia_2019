a5d17cf90f40e455be3334811ca3e547a6699e33
Giovanni Montes <giovanni.montes@cerok.com>
2016-09-20 18:09:54 -0500

    incluir codigo qr en el word

--- pantallas/lib/PhpWord/numero_radicado_word.php
+++ pantallas/lib/PhpWord/numero_radicado_word.php
@@ -1,11 +1,9 @@
 <?php
 
-//	POSTERIOR AL APROBAR, se debe definir la variable ruta_db_superior desde donde se hace el llamado.
-
-
-include_once($ruta_db_superior."pantallas/lib/PhpWord/funciones_include.php");
-//require_once($ruta_db_superior.'pantallas/lib/PHPWord/src/PhpWord/Autoloader.php');
-require_once($ruta_db_superior.'pantallas/lib/PhpWord/Autoloader.php');
+// POSTERIOR AL APROBAR, se debe definir la variable ruta_db_superior desde donde se hace el llamado.
+include_once ($ruta_db_superior . "pantallas/lib/PhpWord/funciones_include.php");
+// require_once($ruta_db_superior.'pantallas/lib/PHPWord/src/PhpWord/Autoloader.php');
+require_once ($ruta_db_superior . 'pantallas/lib/PhpWord/Autoloader.php');
 date_default_timezone_set('UTC');
 
 /**
@@ -24,74 +22,98 @@ define('IS_INDEX', SCRIPT_FILENAME == 'index');
 Autoloader::register();
 Settings::loadConfig();
 
-
 // Return to the caller script when runs by CLI
-if (CLI) {
-    return;
+if(CLI) {
+	return;
 }
 
 /**
  * Write documents
  *
- * @param \PhpOffice\PhpWord\PhpWord $phpWord
- * @param string $filename
- * @param array $writers
+ * @param \PhpOffice\PhpWord\PhpWord $phpWord        	
+ * @param string $filename        	
+ * @param array $writers        	
  *
  * @return string
  */
 
-//ini_set('display_errors',true);
+// ini_set('display_errors',true);
 
-
-if(@$iddoc){
-	$_REQUEST["iddoc"]=$iddoc;	
+if(@$iddoc) {
+	$_REQUEST["iddoc"] = $iddoc;
 }
 
-$ruta_procesar='';
-if(@$_REQUEST["iddoc"]){
-	$anexo=busca_filtro_tabla("d.ruta","documento a, formato b, campos_formato c, anexos d","lower(a.plantilla)=b.nombre AND b.idformato=c.formato_idformato AND c.nombre='anexo_word' AND c.idcampos_formato=d.campos_formato AND a.iddocumento=".$_REQUEST["iddoc"]." AND d.documento_iddocumento=".$_REQUEST["iddoc"],"",$conn) ;
-}
-$ruta_docx='';
-if(@$anexo['numcampos']){
-	$ruta_anexo=explode('anexos',$anexo[0]["ruta"]);
-	$ruta_imagen=$ruta_db_superior.$ruta_anexo[0].'firma_temp/';
-	$ruta_docx=$ruta_db_superior.$ruta_anexo[0].'docx/';	
+$ruta_procesar = '';
+if(@$_REQUEST["iddoc"]) {
+	$anexo = busca_filtro_tabla("d.ruta", "documento a, formato b, campos_formato c, anexos d", "lower(a.plantilla)=b.nombre AND b.idformato=c.formato_idformato AND c.nombre='anexo_word' AND c.idcampos_formato=d.campos_formato AND a.iddocumento=" . $_REQUEST["iddoc"] . " AND d.documento_iddocumento=" . $_REQUEST["iddoc"], "", $conn);
 }
 
+$ruta_docx = '';
 
-if(file_exists($ruta_docx.'documento_word.docx')){
+if(@$anexo['numcampos']) {
+	$ruta_anexo = explode('anexos', $anexo[0]["ruta"]);
+	$ruta_imagen = $ruta_db_superior . $ruta_anexo[0] . 'firma_temp/';
+	$ruta_docx = $ruta_db_superior . $ruta_anexo[0] . 'docx/';
+}
 
-	$templateProcessor = new \PhpOffice\PhpWord\TemplateProcessor($ruta_docx.'documento_word.docx');
+if(file_exists($ruta_docx . 'documento_word.docx')) {
 	
-	$campos_word=$templateProcessor->getVariables();
-
-	if(@$_REQUEST["iddoc"] && count($campos_word)){
-
-		$numero_radicado=busca_filtro_tabla("","documento","iddocumento=".$_REQUEST["iddoc"],"",$conn);
- 		$templateProcessor->setValue('formato_numero',$numero_radicado[0]['numero']);  
-
-		$directorio_out=$ruta_docx;
-		$archivo_out='documento_word';
+	$templateProcessor = new \PhpOffice\PhpWord\TemplateProcessor($ruta_docx . 'documento_word.docx');
+	
+	$campos_word = $templateProcessor->getVariables();
+	
+	if(@$_REQUEST["iddoc"] && count($campos_word)) {
 		
-		$extension_doc='.docx';
-
-		if(file_exists($directorio_out.$archivo_out.$extension_doc)){
-			unlink($directorio_out.$archivo_out.$extension_doc);
-			unlink($directorio_out.$archivo_out.'.pdf');
+		$numero_radicado = busca_filtro_tabla("", "documento", "iddocumento=" . $_REQUEST["iddoc"], "", $conn);
+		$templateProcessor->setValue('formato_numero', $numero_radicado[0]['numero']);
+		
+		$campo_qr_word = "codigo_qr";
+		if(in_array($campo_qr_word, $campos_word)) {
+			$src = $ruta_db_superior . obtener_codigo_qr($_REQUEST["iddoc"]);
+			
+			$img2 = array(
+					array(
+							'img' => htmlspecialchars($src),
+							'size' => array(
+									170,
+									100
+							)
+					)
+			);
+			$templateProcessor->setImg($campo_qr_word, $img2);
 		}
-		$marca_agua=mostrar_estado_documento($_REQUEST['iddoc']);
-		$templateProcessor->setTextWatermark($marca_agua);
-		$templateProcessor->saveAs($directorio_out.$archivo_out.$extension_doc);		
+		$directorio_out = $ruta_docx;
+		$archivo_out = 'documento_word';
 		
-		if(file_exists($directorio_out.$archivo_out.$extension_doc)){
-		  $comando='export HOME=/tmp && libreoffice5.1 --headless --convert-to pdf:writer_pdf_Export --outdir '.$directorio_out.' '.$directorio_out.$archivo_out.$extension_doc;
-		  $var=shell_exec($comando); 
-		} 				
-					
-			 
- 
-	}// fin si existe iddoc y el word tiene campos del formato
-
-} //fin si existe word
+		$extension_doc = '.docx';
+		
+		if(file_exists($directorio_out . $archivo_out . $extension_doc)) {
+			unlink($directorio_out . $archivo_out . $extension_doc);
+			unlink($directorio_out . $archivo_out . '.pdf');
+		}
+		$marca_agua = mostrar_estado_documento($_REQUEST['iddoc']);
+		$templateProcessor->setTextWatermark($marca_agua);
+		$templateProcessor->saveAs($directorio_out . $archivo_out . $extension_doc);
+		
+		if(file_exists($directorio_out . $archivo_out . $extension_doc)) {
+			$comando = 'export HOME=/tmp && libreoffice5.1 --headless --convert-to pdf:writer_pdf_Export --outdir ' . $directorio_out . ' ' . $directorio_out . $archivo_out . $extension_doc;
+			$var = shell_exec($comando);
+		}
+	} // fin si existe iddoc y el word tiene campos del formato
+}
+ // fin si existe word
+function obtener_codigo_qr($iddoc, $campos_word) {
+	global $conn, $ruta_db_superior;
+	$codigo_qr = busca_filtro_tabla("", "documento_verificacion", "documento_iddocumento=" . $iddoc, "", $conn);
+	
+	if(!$codigo_qr['numcampos']) {
+		include_once ($ruta_db_superior . "pantallas/qr/librerias.php");
+		generar_codigo_qr($idformato, $iddoc);
+		
+		$codigo_qr = busca_filtro_tabla("", "documento_verificacion", "documento_iddocumento=" . $iddoc, "", $conn);
+	}
+	
+	return $codigo_qr[0]['ruta_qr'];
+}
 
 ?>
--- pantallas/qr/librerias.php
+++ pantallas/qr/librerias.php
@@ -1,87 +1,87 @@
 <?php
-$max_salida=6; // Previene algun posible ciclo infinito limitando a 10 los ../
-$ruta_db_superior=$ruta="";
-while($max_salida>0){
-  if(is_file($ruta."db.php")){
-    $ruta_db_superior=$ruta; //Preserva la ruta superior encontrada
-  }
-  $ruta.="../";
-  $max_salida--;
+$max_salida = 6; // Previene algun posible ciclo infinito limitando a 10 los ../
+$ruta_db_superior = $ruta = "";
+while($max_salida > 0) {
+	if(is_file($ruta . "db.php")) {
+		$ruta_db_superior = $ruta; // Preserva la ruta superior encontrada
+	}
+	$ruta .= "../";
+	$max_salida--;
 }
-include_once($ruta_db_superior."pantallas/documento/librerias.php");
+include_once ($ruta_db_superior . "pantallas/documento/librerias.php");
 
-
-function mostrar_codigo_qr($idformato, $iddoc){
-	global $conn,$ruta_db_superior;
-	$codigo_qr=busca_filtro_tabla("","documento_verificacion","documento_iddocumento=".$iddoc,"", $conn);	
-	if($codigo_qr['numcampos']){
-		echo("<img src='".PROTOCOLO_CONEXION.RUTA_PDF."/".$codigo_qr[0]['ruta_qr']."'>");	
-	}
-}
-function  generar_codigo_qr($idformato,$iddoc,$idfunc=0){
-  global $conn,$ruta_db_superior;
-	include_once($ruta_db_superior."formatos/librerias/funciones_generales.php");
-	include_once($ruta_db_superior."pantallas/lib/librerias_cripto.php");
-	
-  $codigo_qr = busca_filtro_tabla("ruta_qr, iddocumento_verificacion","documento_verificacion","documento_iddocumento=".$iddoc,"", $conn);	
-  $datos = busca_filtro_tabla("A.fecha,A.estado,A.numero","documento A","A.iddocumento=".$iddoc,"",$conn);  
-  $idfun="";
-	if($_REQUEST['tipo']==5){
-		$idfun=$_REQUEST['idfunc'];
-	}else{
-		$idfun=usuario_actual('idfuncionario');
-	}
-	
-	if($idfunc){
-		$idfun=$idfunc;
-	}
-	
-	$fecha=date_parse($datos[0]['fecha']);
-	$datos_qr="";
-	$cadena="id=".$iddoc;
-	$codificada=encrypt_blowfish($cadena,LLAVE_SAIA_CRYPTO);
-	//$datos_qr=PROTOCOLO_CONEXION.RUTA_PDF."/informacion_documento.php?key_cripto=".$codificada;
-	//$datos_qr=PROTOCOLO_CONEXION.RUTA_PDF."/info.php?".$cadena;
-	$datos_qr=RUTA_INFO_QR."?key_cripto=".$codificada;
-	
-	//$datos_qr="Radicado No: ".$datos[0]["numero"]."\n";
-	//$datos_qr.="Fecha: ".$fecha["day"]." de ".mes($fecha["month"])." del ".$fecha["year"]." a las ".$fecha["hour"].":".$fecha["minute"]."\n";
-	//$firmas = busca_filtro_tabla("CONCAT(B.nombres,CONCAT(' ',B.apellidos)) AS nombre","buzon_salida A, funcionario B","A.origen=B.funcionario_codigo AND (A.nombre LIKE 'APROBADO' OR A.nombre LIKE 'REVISADO')AND A.archivo_idarchivo=".$iddoc,"", $conn);				
-	//$datos_qr.="Firman: \n";
-	for($i=0; $i<$firmas['numcampos']; $i++){
-	  //$datos_qr .= utf8_encode(html_entity_decode($firmas[$i]['nombre']))." \n";    
-	}      
-	$ruta=RUTA_QR.$datos[0]['estado'].'/'.date('Y-m').'/'.$iddoc.'/qr/';
-	$imagen=generar_qr($ruta,$datos_qr); 	
-
-	if($imagen == false){
-	  alerta("Error al tratar de crear el codigo qr");
-	}
-	else{
-	  $codigo_hash=obtener_codigo_hash_archivo($imagen,'crc32'); 
-	  $sql_documento_qr="INSERT INTO documento_verificacion(documento_iddocumento,funcionario_idfuncionario,fecha,ruta_qr,verificacion,codigo_hash) VALUES (".$iddoc.",".$idfun.",".fecha_db_almacenar(date("Y-m-d H:m:s"),'Y-m-d H:i:S').",'".$imagen."','vacio','".$codigo_hash."')";	  	  
-	  phpmkr_query($sql_documento_qr);	  	 
+function mostrar_codigo_qr($idformato, $iddoc) {
+	global $conn, $ruta_db_superior;
+	$codigo_qr = busca_filtro_tabla("", "documento_verificacion", "documento_iddocumento=" . $iddoc, "", $conn);
+	if($codigo_qr['numcampos']) {
+		echo ("<img src='" . PROTOCOLO_CONEXION . RUTA_PDF . "/" . $codigo_qr[0]['ruta_qr'] . "'>");
 	}
 }
 
-function generar_qr($filename,$datos,$matrixPointSize = 2,$errorCorrectionLevel = 'L'){      
+function generar_codigo_qr($idformato, $iddoc, $idfunc = 0) {
+	global $conn, $ruta_db_superior;
+	include_once ($ruta_db_superior . "formatos/librerias/funciones_generales.php");
+	include_once ($ruta_db_superior . "pantallas/lib/librerias_cripto.php");
+	include_once ($ruta_db_superior . "pantallas/lib/librerias_archivo.php");
+	
+	$codigo_qr = busca_filtro_tabla("ruta_qr, iddocumento_verificacion", "documento_verificacion", "documento_iddocumento=" . $iddoc, "", $conn);
+	$datos = busca_filtro_tabla("A.fecha,A.estado,A.numero", "documento A", "A.iddocumento=" . $iddoc, "", $conn);
+	$idfun = "";
+	if($_REQUEST['tipo'] == 5) {
+		$idfun = $_REQUEST['idfunc'];
+	} else {
+		$idfun = usuario_actual('idfuncionario');
+	}
+	
+	if($idfunc) {
+		$idfun = $idfunc;
+	}
+	
+	$fecha = date_parse($datos[0]['fecha']);
+	$datos_qr = "";
+	$cadena = "id=" . $iddoc;
+	$codificada = encrypt_blowfish($cadena, LLAVE_SAIA_CRYPTO);
+	// $datos_qr=PROTOCOLO_CONEXION.RUTA_PDF."/informacion_documento.php?key_cripto=".$codificada;
+	// $datos_qr=PROTOCOLO_CONEXION.RUTA_PDF."/info.php?".$cadena;
+	$datos_qr = RUTA_INFO_QR . "?key_cripto=" . $codificada;
+	
+	// $datos_qr="Radicado No: ".$datos[0]["numero"]."\n";
+	// $datos_qr.="Fecha: ".$fecha["day"]." de ".mes($fecha["month"])." del ".$fecha["year"]." a las ".$fecha["hour"].":".$fecha["minute"]."\n";
+	// $firmas = busca_filtro_tabla("CONCAT(B.nombres,CONCAT(' ',B.apellidos)) AS nombre","buzon_salida A, funcionario B","A.origen=B.funcionario_codigo AND (A.nombre LIKE 'APROBADO' OR A.nombre LIKE 'REVISADO')AND A.archivo_idarchivo=".$iddoc,"", $conn);
+	// $datos_qr.="Firman: \n";
+	for($i = 0; $i < $firmas['numcampos']; $i++) {
+		// $datos_qr .= utf8_encode(html_entity_decode($firmas[$i]['nombre']))." \n";
+	}
+	
+	$formato_ruta = aplicar_plantilla_ruta_documento($iddoc);
+	$ruta = RUTA_QR . $formato_ruta . '/qr/';
+	$imagen = generar_qr($ruta, $datos_qr);
+	
+	if($imagen == false) {
+		alerta("Error al tratar de crear el codigo qr");
+	} else {
+		$codigo_hash = obtener_codigo_hash_archivo($imagen, 'crc32');
+		$sql_documento_qr = "INSERT INTO documento_verificacion(documento_iddocumento,funcionario_idfuncionario,fecha,ruta_qr,verificacion,codigo_hash) VALUES (" . $iddoc . "," . $idfun . "," . fecha_db_almacenar(date("Y-m-d H:m:s"), 'Y-m-d H:i:S') . ",'" . $imagen . "','vacio','" . $codigo_hash . "')";
+		phpmkr_query($sql_documento_qr);
+	}
+}
+
+function generar_qr($filename, $datos, $matrixPointSize = 2, $errorCorrectionLevel = 'L') {
 	global $ruta_db_superior;
-	include_once ($ruta_db_superior."phpqrcode/qrlib.php");    
-	if ($datos){        
-		if(trim($datos) == ''){          
+	include_once ($ruta_db_superior . "phpqrcode/qrlib.php");
+	if($datos) {
+		if(trim($datos) == '') {
 			return false;
-		}
-		else{                              
-			crear_destino($ruta_db_superior.$filename);
-			$filename .= 'qr'.date('Y_m_d_H_m_s').'.png'; 
-			if(file_exists($ruta_db_superior.$filename)){
+		} else {
+			crear_destino($ruta_db_superior . $filename);
+			$filename .= 'qr' . date('Y_m_d_H_m_s') . '.png';
+			if(file_exists($ruta_db_superior . $filename)) {
 			}
-			QRcode::png($datos,$ruta_db_superior.$filename, $errorCorrectionLevel, $matrixPointSize, 0);      
+			QRcode::png($datos, $ruta_db_superior . $filename, $errorCorrectionLevel, $matrixPointSize, 0);
 			return $filename;
-		}  
-	}
-	else{          
+		}
+	} else {
 		return false;
-	}     
+	}
 }
 ?>
