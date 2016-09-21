9974f35a6295a56448f2f91abe75e7ebf2eae2b5
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-09-21 22:36:20 +0000

    [Transferencia documental][ruben.pulgarin]desarrollo etiquetar expedientes a transferir segun dias habiles

--- pantallas/expediente/preparar_transferencia_documental.php
+++ pantallas/expediente/preparar_transferencia_documental.php
@@ -51,6 +51,6 @@ if($expedientes['numcampos']){
         } //fin if $datos_serie numcampos
     } //fin for expedientes
 } //fin if expedientes numcampos
-return($exito);
+echo($exito);
 } //fin if ejecutar
 ?>
