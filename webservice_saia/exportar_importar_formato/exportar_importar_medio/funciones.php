cb3374ccb3ade580be78cce5d3b5f5cc43ece65f
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-09-22 23:08:36 +0000

    [Creacion de formatos][ruben.pulgarin]correccion exportar importar formatos

--- webservice_saia/exportar_importar_formato/exportar_importar_medio/funciones.php
+++ webservice_saia/exportar_importar_formato/exportar_importar_medio/funciones.php
@@ -1,5 +1,5 @@
 <?php
-//@session_start();
+@session_start();
 
 $max_salida=10; // Previene algun posible ciclo infinito limitando a 10 los ../
 $ruta_db_superior=$ruta="";
@@ -11,7 +11,7 @@ while($max_salida>0){
 	$max_salida--;
 }
 
-include_once($ruta_db_superior."webservice_saia/exportar_importar_formato/exportar_importar_medio/define_remoto_medio.php");
+//include_once($ruta_db_superior."webservice_saia/exportar_importar_formato/exportar_importar_medio/define_remoto_medio.php");
 /*
 if(!defined("SERVIDOR_IMPORTAR")){
 	define("SERVIDOR_IMPORTAR",'http://75.101.166.85/saia_demos/saia_nucleo/saia/webservice_saia/exportar_importar_formato/importar_formato/receptor_importar.php');
