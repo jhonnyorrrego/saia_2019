20917b25ebaa7c268bf0b145c04eef2ea2d01d8b
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-09-22 23:06:03 +0000

    [Creacion de formatos][ruben.pulgarin]correccion exportar importar formatos

--- webservice_saia/exportar_importar_formato/exportar_importar_medio/exportar_importar_medio.php
+++ webservice_saia/exportar_importar_formato/exportar_importar_medio/exportar_importar_medio.php
@@ -10,7 +10,7 @@ while($max_salida>0){
 }
 
   require_once($ruta_db_superior.'webservice_saia/exportar_importar_formato/exportar_importar_medio/lib/nusoap.php');
-  include_once("define_remoto_medio.php");
+  require_once("define_remoto_medio.php");
   include_once('funciones.php');
  
   $URL = "www.test.com";
