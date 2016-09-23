5dba1b42c1c82eea18ebb863f94be27e66e6ec5a
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-09-23 13:30:17 +0000

    [Creacion de formatos][ruben.pulgarin]correccion define remoto

--- webservice_saia/exportar_importar_formato/exportar_importar_medio/define_remoto.php
+++ webservice_saia/exportar_importar_formato/exportar_importar_medio/define_remoto.php
@@ -1,8 +1,8 @@
 <?php
-/*
+
 if(!defined("SERVIDOR_IMPORTAR")){
 	define("SERVIDOR_IMPORTAR","http://75.101.166.85/saia_release1/saia/webservice_saia/exportar_importar_formato/importar_formato/receptor_importar.php");
-}*/
+}
 
 
 ?> 
