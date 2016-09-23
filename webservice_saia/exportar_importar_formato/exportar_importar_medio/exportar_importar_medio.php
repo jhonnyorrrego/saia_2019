40f6f211eaf9f39e6a9effbecfe3d9041e0d9d28
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-09-23 13:30:32 +0000

    [Creacion de formatos][ruben.pulgarin]correccion define remoto

--- webservice_saia/exportar_importar_formato/exportar_importar_medio/exportar_importar_medio.php
+++ webservice_saia/exportar_importar_formato/exportar_importar_medio/exportar_importar_medio.php
@@ -11,7 +11,8 @@ while($max_salida>0){
 
   require_once($ruta_db_superior.'webservice_saia/exportar_importar_formato/exportar_importar_medio/lib/nusoap.php');
  // require_once("define_remoto_medio.php");
- // include_once($ruta_db_superior.'webservice_saia/exportar_importar_formato/exportar_importar_medio/define_remoto.php');
+  include_once('define_remoto.php');
+  print_r(SERVIDOR_IMPORTAR);
   include_once('funciones.php');
   
  
