023769ce9cff9778c1c30bc6e8ca808453f57b9f
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-09-26 16:33:50 +0000

    [Solicitud de Elaboracin, Modificacin, Eliminacin de Documentos de Calidad][ruben.pulgarin]correccion versionamiento calidad M1

--- class_impresion.php
+++ class_impresion.php
@@ -2,6 +2,8 @@
 set_time_limit(0);
 
 
+print_r($_REQUEST);die();
+
 if(!@$_SESSION["LOGIN" . $_REQUEST["llave_saia"]] && $_REQUEST["conexion_remota"]) {
 	@session_start();
 	$_SESSION["LOGIN" . $_REQUEST["llave_saia"]] = $_REQUEST["conexion_usuario"];
