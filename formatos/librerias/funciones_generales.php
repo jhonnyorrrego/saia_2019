08b9f56dbc9934b20c2610126952919e25446030
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-09-26 16:32:36 +0000

    [Solicitud de Elaboracin, Modificacin, Eliminacin de Documentos de Calidad][ruben.pulgarin]correccion versionamiento calidad M1

--- formatos/librerias/funciones_generales.php
+++ formatos/librerias/funciones_generales.php
@@ -3525,7 +3525,7 @@ function crear_pdf_documento_tcpdf($datos_documento, $datos_ejecutor = null) {
 	
 		// Capturar la URL y pasarla al navegador
 		$responce = curl_exec($ch);
-		
+		print_r($responce);
 		
 		// Cerrar el recurso cURL y liberar recursos del sistema
 		curl_close($ch);
