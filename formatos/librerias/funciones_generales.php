0248664300b9d6f6f6e3a8d8cfd46134b26673da
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-09-26 15:37:12 +0000

    [Solicitud de Elaboracin, Modificacin, Eliminacin de Documentos de Calidad][ruben.pulgarin]correccion versionamiento calidad M1

--- formatos/librerias/funciones_generales.php
+++ formatos/librerias/funciones_generales.php
@@ -3516,7 +3516,7 @@ function crear_pdf_documento_tcpdf($datos_documento, $datos_ejecutor = null) {
 		
 		// Establecer URL y otras opciones apropiadas
 		$url = "http://" . RUTA_PDF_LOCAL . "/class_impresion.php?iddoc=" . $datos_documento['iddocumento'];
-		$datos_session = "&LOGIN=" . $_SESSION["LOGIN" . LLAVE_SAIA] . "&usuario_actual=" . $_SESSION["usuario_actual"] . "&LLAVE_SAIA=" . LLAVE_SAIA;
+		$datos_session = "&LOGIN=" . $_SESSION["LOGIN" . LLAVE_SAIA] . "&usuario_actual=" . $_SESSION["usuario_actual"] . "&llave_saia=" . LLAVE_SAIA;
 		$url = $url . $datos_session;
 		print_r($url);die();
 		curl_setopt($ch, CURLOPT_URL, $url);
