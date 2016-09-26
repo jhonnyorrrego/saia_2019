3ca2e63079f6f5313fac7c25476ff2ccf141c6bc
giovanni.montes <giovanni.montes@cerok.com>
2016-09-26 19:09:40 +0000

    [debug][giovanni.montes]debug

--- pantallas/lib/PhpWord/exportar_word.php
+++ pantallas/lib/PhpWord/exportar_word.php
@@ -394,10 +394,10 @@ if($ruta_procesar!=''){
 		$archivo_out='documento_word';
 		$extension_doc='.docx';
 		$templateProcessor->saveAs($directorio_out.$archivo_out.$extension_doc);
-		print_r($directorio_out.$archivo_out.$extension_doc);die("KAPUT");
 		if(file_exists($directorio_out.$archivo_out.$extension_doc)){
 		  $comando='export HOME=/tmp && libreoffice5.1 --headless --convert-to pdf:writer_pdf_Export --outdir '.$directorio_out.' '.$directorio_out.$archivo_out.$extension_doc;
 		  $var=shell_exec($comando); 
+		print_r($var);die("KAPUT");
 		} 
 		if(@$anexo['numcampos']){ //elimina las imagenes de la carpeta
 			
