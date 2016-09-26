edaf7fbf12a52e575d924ecdd2a69337bcaa451d
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-09-26 15:02:50 +0000

    Commit automatico editor saia. Cambios locales 2016-09-26 10:02:50

--- formatos/proceso/adicionar_proceso.php
+++ formatos/proceso/adicionar_proceso.php
@@ -8,7 +8,7 @@
 	$('#formulario_formatos').validate();
 	
 });
-</script> </head><body bgcolor="#F5F5F5"><?php llama_funcion_accion(@$_REQUEST["iddoc"],@$_REQUEST["idformato"],"ingresar","ANTERIOR");?><form name="formulario_formatos" id="formulario_formatos" method="post" action="../../class_transferencia.php" enctype="multipart/form-data"><table width="100%" cellspacing="1" cellpadding="4"><tr><td colspan="2" class="encabezado_list">MAPA DE PROCESOS</td></tr><input type="hidden" name="fecha_aprobacion_rie" value="<?php echo(validar_valor_campo(4118)); ?>"><input type="hidden" name="fecha_revision_riesg" value="<?php echo(validar_valor_campo(4119)); ?>"><tr>
+</script> </head><body bgcolor="#F5F5F5"><?php llama_funcion_accion(@$_REQUEST["iddoc"],@$_REQUEST["idformato"],"ingresar","ANTERIOR");?><form name="formulario_formatos" id="formulario_formatos" method="post" action="../../class_transferencia.php" enctype="multipart/form-data"><table width="100%" cellspacing="1" cellpadding="4"><tr><td colspan="2" class="encabezado_list">MAPA DE PROCESOS</td></tr><input type="hidden" name="estado_documento" value="<?php echo(validar_valor_campo(4863)); ?>"><input type="hidden" name="fecha_aprobacion_rie" value="<?php echo(validar_valor_campo(4118)); ?>"><input type="hidden" name="fecha_revision_riesg" value="<?php echo(validar_valor_campo(4119)); ?>"><tr>
                      <td class="encabezado" width="20%" title="">DEPENDENCIA DEL CREADOR DEL DOCUMENTO*</td>
                      <?php buscar_dependencia(194,2012);?></tr><tr>
                      <td class="encabezado" width="20%" title="Hace referencia al Codigo del Proceso (Campos Alfa Numericos)">C&Oacute;DIGO</td>
