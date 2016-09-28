9cca574fa4dc48e9ac3f552c49408a4e928070bf
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-09-28 21:08:20 +0000

    [Planes de Mejoramiento][ruben.pulgarin]correccion idformato quemados

--- formatos/seguimiento_plan_mejoramiento/previo_mostrar_seguimiento_plan_mejoramiento.php
+++ formatos/seguimiento_plan_mejoramiento/previo_mostrar_seguimiento_plan_mejoramiento.php
@@ -1,1 +1,10 @@
-<?php include_once('../librerias/estilo_formulario.php'); include_once('../librerias/funciones_formatos_generales.php');?><?php listado_hijos_formato(5,$_REQUEST["iddoc"]); ?>
+<?php
+
+
+
+
+$idformato_seguimiento_plan_mejoramiento=busca_filtro_tabla("idformato","formato","nombre='seguimiento_plan_mejoramiento'","",$conn);
+
+?>
+
+<?php include_once('../librerias/estilo_formulario.php'); include_once('../librerias/funciones_formatos_generales.php');?><?php listado_hijos_formato($idformato_seguimiento_plan_mejoramiento[0]['idformato'],$_REQUEST["iddoc"]); ?>
