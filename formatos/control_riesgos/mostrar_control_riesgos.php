497c8d24d04b3f977e271632236907d208871af1
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-09-28 15:27:29 +0000

    Commit automatico editor saia. Cambios locales 2016-09-28 10:27:29

--- formatos/control_riesgos/mostrar_control_riesgos.php
+++ formatos/control_riesgos/mostrar_control_riesgos.php
@@ -1,4 +1,4 @@
-<?php include_once("../carta/funciones.php"); ?><?php include_once("../pqr/funciones.php"); ?><?php include_once("../experiencia_laboral/funciones.php"); ?><?php include_once("../riesgos_proceso/../riesgos_proceso/funciones.php"); ?><?php include_once("funciones.php"); ?><?php include_once("../librerias/funciones_generales.php"); ?><script type="text/javascript" src="../../js/jquery.js"></script><?php include_once("../librerias/header_nuevo.php"); ?><?php include_once("../../class_transferencia.php"); ?><tr><td><p>&nbsp;<?php botones_valoracion_riesgos(394,$_REQUEST['iddoc']);?></p>
+<?php include_once("../carta/funciones.php"); ?><?php include_once("../riesgos_proceso/../riesgos_proceso/funciones.php"); ?><?php include_once("funciones.php"); ?><?php include_once("../librerias/funciones_generales.php"); ?><script type="text/javascript" src="../../js/jquery.js"></script><?php include_once("../librerias/header_nuevo.php"); ?><?php include_once("../../class_transferencia.php"); ?><tr><td><p>&nbsp;<?php botones_valoracion_riesgos(394,$_REQUEST['iddoc']);?></p>
 <table style="border-collapse: collapse; width: 100%;" border="1">
 <tbody>
 <tr>
