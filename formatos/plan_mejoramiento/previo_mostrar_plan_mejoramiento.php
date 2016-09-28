8929118aef7480aa5f74494d87b78ab97b4eb6cc
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-09-28 14:07:20 +0000

    [Planes de Mejoramiento(listado)][ruben.pulgarin]cambio idformato previo_mostrar

--- formatos/plan_mejoramiento/previo_mostrar_plan_mejoramiento.php
+++ formatos/plan_mejoramiento/previo_mostrar_plan_mejoramiento.php
@@ -1,2 +1,2 @@
 <?php include_once('../librerias/estilo_formulario.php'); include_once('../librerias/funciones_formatos_generales.php');?><p></p>
-<p>&nbsp;</p><?php listado_hijos_formato(1,$_REQUEST["iddoc"]); ?>
+<p>&nbsp;</p><?php listado_hijos_formato(379,$_REQUEST["iddoc"]); ?>
