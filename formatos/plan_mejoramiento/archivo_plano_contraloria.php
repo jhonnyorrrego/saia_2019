a8aad12c3dbb661b7e6adea7c4e7e6e8b29d9d32
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-09-28 17:02:53 +0000

    [Planes de Mejoramiento][ruben.pulgarin]correccion idformato quemados

--- formatos/plan_mejoramiento/archivo_plano_contraloria.php
+++ formatos/plan_mejoramiento/archivo_plano_contraloria.php
@@ -23,24 +23,27 @@ if($_REQUEST["iddoc"]){
     }*/
     $texto.='(N) Numero,(C) Clase De Observacion,(C) Descripcion Observacion Formulada Por La Cgr,(C) Areas Ciclos O Procesos Involucrados,(C) Acciones De Mejoramiento,(C) Responsable Del Mejoramiento,(F) Tiempo Programado Para El Cumplimiento De Las Acciones De Mejoramiento,(C) Mecanismo De Seguimiento Interno Adoptado Por La Entidad (Actividad),(C) Mecanismo De Seguimiento Interno Adoptado Por La Entidad (Tiempo),(C) Responsable Del Seguimiento Por La Entidad,(C) Indicador De Accion De Cumplimiento,(C) Observaciones'."\n";
     $numcols=count($nom_col);
+    
+    $idformato_hallazgo_plan_mejoramiento=busca_filtro_tabla("idformato","formato","nombre='hallazgo_plan_mejoramiento'","",$conn);
+    
     for($i=0;$i<$datos["numcampos"];$i++){
       for($j=1;$j<($numcols-1);$j+=2){
         if($sExport =="csv"){
           if($nom_col[$j]=="tiempo_seguimiento" || $nom_col[$j]=="tiempo_cumplimiento"){
-            $valor=mostrar_valor_campo($nom_col[$j],2,$datos[$i]["hallazgo_iddoc"],1);
+            $valor=mostrar_valor_campo($nom_col[$j],$idformato_hallazgo_plan_mejoramiento[0]['idformato'],$datos[$i]["hallazgo_iddoc"],1);
             if($valor=='null'){
               $valor='"ND"';
             }
             $texto1=str_replace('-','/',$valor);
           }  
           else if($nom_col[$j]=="secretarias"){
-            $valor1=str_replace("&nbsp;","",htmlspecialchars_decode(str_replace(",",";",mostrar_valor_campo($nom_col[$j],2,$datos[$i]["hallazgo_iddoc"],1))));
+            $valor1=str_replace("&nbsp;","",htmlspecialchars_decode(str_replace(",",";",mostrar_valor_campo($nom_col[$j],$idformato_hallazgo_plan_mejoramiento[0]['idformato'],$datos[$i]["hallazgo_iddoc"],1))));
             $valor1=trim(strip_tags(html_entity_decode($valor1)));
             $valor1=str_replace("\n","",$valor1);
             $valor1=str_replace("'",'"',$valor1);
             $valor1=str_replace('"','""',$valor1);
             $j+=2;
-            $valor2=str_replace("&nbsp;","",htmlspecialchars_decode(str_replace(",",";",mostrar_valor_campo($nom_col[$j],2,$datos[$i]["hallazgo_iddoc"],1))));
+            $valor2=str_replace("&nbsp;","",htmlspecialchars_decode(str_replace(",",";",mostrar_valor_campo($nom_col[$j],$idformato_hallazgo_plan_mejoramiento[0]['idformato'],$datos[$i]["hallazgo_iddoc"],1))));
             $valor2=trim(strip_tags(html_entity_decode($valor2)));
             $valor2=str_replace("\n","",$valor2);
             $valor2=str_replace("'",'"',$valor2);
@@ -48,7 +51,7 @@ if($_REQUEST["iddoc"]){
             $texto1='"'.$valor1.",".$valor2.'"';
           }
           else{
-            $texto1=str_replace("&nbsp;","",htmlspecialchars_decode(str_replace(",",";",mostrar_valor_campo($nom_col[$j],2,$datos[$i]["hallazgo_iddoc"],1))));
+            $texto1=str_replace("&nbsp;","",htmlspecialchars_decode(str_replace(",",";",mostrar_valor_campo($nom_col[$j],$idformato_hallazgo_plan_mejoramiento[0]['idformato'],$datos[$i]["hallazgo_iddoc"],1))));
             $texto1=trim(strip_tags(html_entity_decode($texto1)));
             $texto1=str_replace("\n","",$texto1);
             $texto1=str_replace("'",'"',$texto1);
