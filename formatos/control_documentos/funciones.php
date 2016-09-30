098b165acf02971671a476ee36bdd234ed5a2735
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-09-30 21:22:43 +0000

    [Solicitud de Elaboracin, Modificacin, Eliminacin de Documentos de Calidad][ruben.pulgarin]Organizando la ruta

--- formatos/control_documentos/funciones.php
+++ formatos/control_documentos/funciones.php
@@ -187,12 +187,12 @@ function ruta_aprobacion_control_documentos($idformato, $iddoc){
 		$radicador_salida=busca_filtro_tabla("origen","buzon_entrada","archivo_idarchivo=".$iddoc,"idtransferencia desc",$conn);
 	    array_push($ruta,array("funcionario"=>$radicador_salida[0][0],"tipo_firma"=>0)); 
 	    //phpmkr_query("update buzon_entrada set activo=0 where archivo_idarchivo=".$iddoc." and nombre='POR_APROBAR'");
-	    insertar_ruta_aprobacion_documento($ruta,$iddoc);	    
+	    insertar_ruta_cierre_solicitud_calidad($ruta,$iddoc);	    
 	}
 }
 
 
-function insertar_ruta_cierre($ruta,$iddoc)
+function insertar_ruta_cierre_solicitud_calidad($ruta,$iddoc)
 {global $conn;
 for($i=0;$i<count($ruta)-1;$i++)
 {if(!isset($ruta[$i]["tipo_firma"]))
