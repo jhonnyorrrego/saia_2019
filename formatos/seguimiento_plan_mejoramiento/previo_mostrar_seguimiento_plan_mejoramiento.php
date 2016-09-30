8a7060583b2578478f0c866a6467c3e4262502b6
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-09-30 15:25:46 +0000

    [Calidad][ruben.pulgarin]cambio del nombre y tabla de los siguientes formatos programas_calidad,acciones_riesgo, hallazgo_plan_mejoramiento,seguimiento_plan_mejoramiento

--- formatos/seguimiento_plan_mejoramiento/previo_mostrar_seguimiento_plan_mejoramiento.php
+++ formatos/seguimiento_plan_mejoramiento/previo_mostrar_seguimiento_plan_mejoramiento.php
@@ -1,9 +1,18 @@
 <?php
+$max_salida = 6;
+$ruta_db_superior = $ruta = "";
+while ($max_salida > 0) {
+  if (is_file($ruta . "db.php")) {
+    $ruta_db_superior = $ruta;    
+  }
+  $ruta .= "../";
+  $max_salida--;
+}
+include_once($ruta_db_superior."db.php");
 
 
 
-
-$idformato_seguimiento_plan_mejoramiento=busca_filtro_tabla("idformato","formato","nombre='seguimiento_plan_mejoramiento'","",$conn);
+$idformato_seguimiento_plan_mejoramiento=busca_filtro_tabla("idformato","formato","nombre='seguimiento'","",$conn);
 
 ?>
 
