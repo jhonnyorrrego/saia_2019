be627d56520337eadd97f75e8d96972b30fd49d5
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-03 13:56:08 +0000

    [Rutas][ruben.pulgarin]correccion consulta radicador salida

--- formatos/librerias/rutaadd.php
+++ formatos/librerias/rutaadd.php
@@ -941,12 +941,13 @@ function AddData($conn)
   if($_POST["add"]=="T"){
     $rs=busca_filtro_tabla("A.*,valor","configuracion A","nombre like 'radicador_salida'","",$conn);
     if($rs["numcampos"]>0){
-      $func=busca_cargo_funcionario(3,$rs[0]["valor"],"",$conn);
-      
+      $func=busca_cargo_funcionario(6,$rs[0]["valor"],"",$conn);
+      /*
       if(!$func["numcampos"]){
-       // validar_usuario_radicador_salida();
-      }
-      $func=busca_cargo_funcionario(3,$rs[0]["valor"],"",$conn);
+       //validar_usuario_radicador_salida();
+       //$func=busca_cargo_funcionario(3,$rs[0]["valor"],"",$conn);
+      }*/
+      
       
       if($func["numcampos"]>0&&$func[0]["estado"]){
         $destino=agregar_destino_ruta($destino,1,$func[0]["funcionario_codigo"],1,"APROBADO",$obligatorio);  
