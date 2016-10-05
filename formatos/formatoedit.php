7870d11225d5e820d51ceb3cf2184b43ef58ecf5
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-05 23:08:12 +0000

    [GIT][ruben.pulgarin]correccion ignorar archivos nucleo

--- formatos/formatoedit.php
+++ formatos/formatoedit.php
@@ -782,7 +782,7 @@ detalles_mostrar_".$x_nombre.".php";
       $data='*';
     }
     
-	$fp = fopen($x_nombre . "/.gitignore", 'W');
+	$fp = fopen($x_nombre . "/.gitignore", 'w+');
     fwrite($fp,$data);
     fclose($fp);
 	chmod($x_nombre . "/.gitignore",PERMISOS_ARCHIVOS);		
