a84e9be3ac882393517ca2bb84c91e8af06a09f6
Cero K <info@cerok.com>
2016-10-07 19:34:42 +0000

    retirando archivos de versionamiento

--- formatos/respuesta_pqrsf/mostrar_respuesta_pqrsf.php
+++
@@ -1,3 +0,0 @@
-<?php include_once("../carta/funciones.php"); ?><?php include_once("funciones.php"); ?><?php include_once("../correo_saia/funciones.php"); ?><?php include_once("../librerias/funciones_generales.php"); ?><script type="text/javascript" src="../../js/jquery.js"></script><?php include_once("../librerias/header_nuevo.php"); ?><?php include_once("../../class_transferencia.php"); ?><tr><td><p><?php mostrar_informacion_pqrsf_padre(307,$_REQUEST['iddoc']);?></p>
-<p>&nbsp;</p>
-<p><?php mostrar_estado_proceso(307,$_REQUEST['iddoc']);?></p></td></tr><?php include_once("../librerias/footer_nuevo.php"); ?>
