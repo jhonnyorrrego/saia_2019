c0b3baa66ecb2db37ba163574388a10b0a3530c2
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-11 20:34:39 +0000

    [Expediente][ruben.pulgarin]desarrollo cambio de responsable expediente

--- pantallas/expediente/detalles_expediente.php
+++ pantallas/expediente/detalles_expediente.php
@@ -95,7 +95,7 @@ $expediente=busca_filtro_tabla("a.*,".fecha_db_obtener("a.fecha","Y-m-d")." AS f
   	<?php if($expediente[0]["propietario"]){
   		$nombres=busca_filtro_tabla("","funcionario A","A.funcionario_codigo=".$expediente[0]["propietario"],"",$conn);
   		echo(ucwords(strtolower($nombres[0]["nombres"]." ".$nombres[0]["apellidos"])));
-  		$cadena_cambiar_propietario="&nbsp; &nbsp; &nbsp; <button class='btn btn-mini btn-default'><i class='icon-user'  title='Recurrencia'></i></button>";
+  		$cadena_cambiar_propietario="&nbsp; &nbsp; &nbsp; <button class='btn btn-mini btn-default'><i class='icon-user'  title='Cambiar Responsable'></i></button>";
   		echo($cadena_cambiar_propietario);
   	}else{
 			echo("<span style='color:red'>Expediente creado por el sistema</span>");
