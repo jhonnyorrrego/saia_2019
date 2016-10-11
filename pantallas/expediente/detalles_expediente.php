95d969e8dde502b30489b6076925ecd990263664
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-11 22:41:15 +0000

    [Expediente][ruben.pulgarin]desarrollo cambio de responsable expediente

--- pantallas/expediente/detalles_expediente.php
+++ pantallas/expediente/detalles_expediente.php
@@ -103,11 +103,17 @@ $expediente=busca_filtro_tabla("a.*,".fecha_db_obtener("a.fecha","Y-m-d")." AS f
   		    <button class='btn btn-mini btn-default cambiar_responsable_expediente'>
   		        <i class='icon-user' title='Cambiar Responsable'></i>
   		    </button>
+ 		<script type="text/javascript" src="<?php echo $ruta_db_superior;?>anexosdigitales/highslide-4.0.10/highslide/highslide-with-html.js"></script>
+		 <link rel="stylesheet" type="text/css" href="<?php echo $ruta_db_superior;?>anexosdigitales/highslide-4.0.10/highslide/highslide.css" />
+		 <script type='text/javascript'>
+		   hs.graphicsDir = '<?php echo $ruta_db_superior;?>anexosdigitales/highslide-4.0.10/highslide/graphics/';
+		   hs.outlineType = 'rounded-white';
+		</script> 		    
   		    <script>
   		        $(document).ready(function(){
   		            $('.cambiar_responsable_expediente').click(function(){
                          var enlace='<?php echo($ruta_db_superior); ?>pantallas/expediente/cambiar_responsable_expediente.php?idexpediente=<?php echo($idexpediente); ?>';
-        top.hs.htmlExpand(this, { objectType: 'iframe',width: 500, height: 200,contentId:'cuerpo_paso', 		preserveContent:false, src:enlace,outlineType: 'rounded-white',wrapperClassName:'highslide-wrapper drag-header'});
+                        hs.htmlExpand(this, { objectType: 'iframe',width: 300, height: 200,contentId:'cuerpo_paso', 		preserveContent:false, src:enlace,outlineType: 'rounded-white',wrapperClassName:'highslide-wrapper drag-header'});
   		            });
   		        });
   		    </script>
