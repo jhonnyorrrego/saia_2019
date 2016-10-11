7d688d40d78dfed622b37bf6103c26d974038a3a
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-11 13:51:29 +0000

    [Expediente][ruben.pulgarin]correccion usuario y ver documentos pero no acceder

--- pantallas/expediente/librerias.php
+++ pantallas/expediente/librerias.php
@@ -589,7 +589,7 @@ function origen_documento_expediente($doc,$numero,$origen="",$tipo_radicado="",$
     //SE VALIDA SI EL USUARIO ESTA INVOLUCRADO CON EL DOCUMENTO (TRANSFERENCIA) 
     $involucrado=validar_relacion_documento_expediente($doc);
     if(!$involucrado['numcampos']){
-        $enlace=str_replace("class=[\"\'][^\"\']+[\"\']","",$enlace);
+        $enlace=preg_match("class=[\"\'][^\"\']+[\"\']","",$enlace);
     }        
     return ($enlace);
 }
