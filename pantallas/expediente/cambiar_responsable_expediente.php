9132f4390c4c85695d961965bbdfab51b7cd543e
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-11 22:42:54 +0000

    [Expediente][ruben.pulgarin]desarrollo cambio de responsable expediente

--- pantallas/expediente/cambiar_responsable_expediente.php
+++ pantallas/expediente/cambiar_responsable_expediente.php
@@ -82,7 +82,7 @@ $(document).ready(function(){
           }else{
                   var x_valor=$(this).val();
                   delay(function(){
-                          $("#ul_completar").load( "cambiar_responsable_expediente.php", { num_radicado: x_valor });
+                          $("#ul_completar").load( "<?php echo($ruta_db_superior); ?>pantallas/expediente/autocompletar_funcionario_responsable_expediente.php", { num_radicado: x_valor });
                   },500);
           }
   });
