ff2d15a26dd9a35e93c3a2725b090e6027f9d067
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-11 21:58:39 +0000

    [Expediente][ruben.pulgarin]desarrollo cambio de responsable expediente

--- pantallas/expediente/cambiar_responsable_expediente.php
+++ pantallas/expediente/cambiar_responsable_expediente.php
@@ -1,46 +1,30 @@
 <?php
-$max_salida=10; // Previene algun posible ciclo infinito limitando a 10 los ../
+$max_salida=6; // Previene algun posible ciclo infinito limitando a 10 los ../
 $ruta_db_superior=$ruta="";
-while($max_salida>0){
-	if(is_file($ruta."db.php")){
-		$ruta_db_superior=$ruta; //Preserva la ruta superior encontrada
-	}
-	$ruta.="../";
-	$max_salida--;
+while($max_salida>0)
+{
+if(is_file($ruta."db.php"))
+{
+$ruta_db_superior=$ruta; //Preserva la ruta superior encontrada
+}
+$ruta.="../";
+$max_salida--;
 }
 include_once($ruta_db_superior."db.php");
-include_once($ruta_db_superior."librerias_saia.php");
-echo(librerias_jquery("1.7"));
-echo(estilo_bootstrap());
 
+if(isset($_REQUEST['num_radicado'])){
+	$datos=busca_filtro_tabla("funcionario_codigo,concat(nombres,' ',apellidos) as nombre_funcionario,cargo","vfuncionario_dc","estado_dc=1 AND (nombres LIKE '%".$_REQUEST['num_radicado']."%' OR apellidos LIKE '%".$_REQUEST['num_radicado']."%')","nombres,apellidos",$conn);		
+	$html="<ul>";
+	if($datos['numcampos']){
+		for($i=0;$i<$datos['numcampos'];$i++){
+			$descripcion=$datos[$i]['nombre_funcionario']." (".$datos[$i]['cargo'].")";
+			$html.="<li onclick=\"cargar_datos(".$datos[$i]['funcionario_codigo'].",'".$descripcion."')\">".$descripcion."</li>";
+		}
+	}else{
+		$html.="<li onclick=\"cargar_datos(0)\">NO hay Radicados con el Numero solicitado</li>";
+	}
+	$html.="</ul>";
+	echo $html;
+}
 
-
-  		                /*
-  		                var idexpediente='<?php echo($idexpediente); ?>';
-                        $.ajax({
-                            type:'POST',
-                            dataType: 'json',
-                            url: "ejecutar_acciones.php",
-                            data: {
-                                idexpediente:idexpediente,
-                                ejecutar_expediente:'cambiar_responsable_expediente'
-                            },
-                            success: function(datos){
-
-                            }
-                        }); 
-                        
-                        */
 ?>
-
-
-<table class="table table-bordered">
-  <tr>
-    <td width="40%" class="prettyprint">
-      <b>Funcionario:</b>
-    </td>
-    <td>
-       <?php echo($expediente[0]["nombre"]);?>
-    </td>    
-  </tr>
- </table> 
