ec8861fd1bf8724414525da97dcd4bf702fa6b83
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-11 14:21:29 +0000

    [Expediente][ruben.pulgarin]correccion usuario y ver documentos pero no acceder

--- pantallas/expediente/librerias.php
+++ pantallas/expediente/librerias.php
@@ -589,7 +589,7 @@ function origen_documento_expediente($doc,$numero,$origen="",$tipo_radicado="",$
     //SE VALIDA SI EL USUARIO ESTA INVOLUCRADO CON EL DOCUMENTO (TRANSFERENCIA) 
     $involucrado=validar_relacion_documento_expediente($doc);
     if(!$involucrado['numcampos']){
-        $enlace=preg_replace("/class=[\"\'][^\"\']+[\"\']/","class='link' llave=".$doc,$enlace,1);
+        $enlace=preg_replace("/class=[\"\'][^\'\"]*kenlace_saia[^\'\"]*[\"\']/","class='link' llave=".$doc,$enlace,1);
     }        
     return ($enlace);
 }
@@ -599,7 +599,7 @@ function fecha_creacion_documento_expediente($fecha0,$plantilla=Null,$doc=Null){
     //SE VALIDA SI EL USUARIO ESTA INVOLUCRADO CON EL DOCUMENTO (TRANSFERENCIA) 
     $involucrado=validar_relacion_documento_expediente($doc);  
     if(!$involucrado['numcampos']){
-       $enlace=preg_replace("/class=[\"\'][^\"\']+[\"\']/","class='link' llave=".$doc,$enlace,1);
+       $enlace=preg_replace("/class=[\"\'][^\'\"]*kenlace_saia[^\'\"]*[\"\']/","class='link' llave=".$doc,$enlace,1);
     } 
     
     return($enlace);
