01df5fcc5c576f8dc6819e1bef28ce1539d4801e
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-13 16:28:08 +0000

    [Comunicacin Interna][ruben.pulgarin]correccion al generar pdf

--- class_impresion.php
+++ class_impresion.php
@@ -355,7 +355,7 @@ class Imprime_Pdf {
 			$contenido = preg_replace('#onclick="(.*?)"#is', '', $contenido);
 			
 			if(usuario_actual('login') == 'cerok') {
-			//	 print_r($contenido);die();
+				 print_r($contenido);die();
 			}
 			
 			$this->pdf->writeHTML(stripslashes($contenido), true, false, false, false, '');
