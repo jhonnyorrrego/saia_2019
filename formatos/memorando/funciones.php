ac1f7122d28d46a24ab26d427d5666433b4709b0
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-13 23:35:41 +0000

    [QR][ruben.pulgarin]correccion qr

--- formatos/memorando/funciones.php
+++ formatos/memorando/funciones.php
@@ -226,7 +226,7 @@ function mostrar_qr_interna($idformato,$iddoc){
 			$extension=explode(".",$codigo_qr[0]['ruta_qr']);
 			$img='&nbsp;<img src="http://'.RUTA_PDF.'/'.$codigo_qr[0]['ruta_qr'].'" />';
 		}else{
-		    print_r('aqui1: '.$conn);
+		    print_r('aqui1: '.$conn);die();
 			generar_codigo_qr_interna($idformato,$iddoc);
 			
 			$codigo_qr=busca_filtro_tabla("","documento_verificacion","documento_iddocumento=".$iddoc,"", $conn);
