18bdfdc47f0db2b7b341462cc501203c1d9470de
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-13 19:09:58 +0000

    [QR][ruben.pulgarin]correccion qr

--- sql.php
+++ sql.php
@@ -289,7 +289,7 @@ la matriz con los valores del resultado se obtiene por medio de la función Resu
   function Ejecutar_Sql_MySql($sql)
     {$this->filas=0;
      if($sql && $sql<>"" && $this->Conn->conn){
-         $this->res=mysqli_query($this->Conn->conn,$sql) or die("ERROR SQL ".mysqli_error($this->Conn->conn)." en ".$_SERVER["PHP_SELF"]." ->".$sql);// or error("Error al Ejecutar:  $sql --- ".mysql_error());
+         $this->res=mysqli_query($this->Conn->conn,$sql) or die("ERROR SQL ".mysqli_error($this->Conn->conn)." en ".$_SERVER["PHP_SELF"]." ->".$sql);// or error//("Error al Ejecutar:  $sql --- ".mysql_error());
         if($this->res){
          if(strpos(strtolower($sql),"insert")!==false)
             $this->ultimo_insert=$this->Ultimo_Insert_Mysql();
@@ -303,7 +303,7 @@ la matriz con los valores del resultado se obtiene por medio de la función Resu
 
           $this->consulta=trim($sql);
           //$fin=strpos($this->consulta," ");
-          //$accion=substr($this->consulta,0,$fin);
+          //$accion=substr($this->consulta,0,$fin); 
         }
         return($this->res);
       }
