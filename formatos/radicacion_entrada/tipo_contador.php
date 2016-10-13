999896c09f914744d71eec9c66b1381d98f80bd8
jheison.vargas <jheison.vargas@cerok.com>
2016-10-13 19:28:15 +0000

    [Radicar entrada][jheison.vargas]se crea el archivo tipo_contador.php

---
+++ formatos/radicacion_entrada/tipo_contador.php
@@ -0,0 +1,16 @@
+<?php
+$max_salida=6; // Previene algun posible ciclo infinito limitando a 10 los ../
+$ruta_db_superior=$ruta="";
+while($max_salida>0)
+{
+if(is_file($ruta."db.php"))
+{
+$ruta_db_superior=$ruta; //Preserva la ruta superior encontrada
+}
+$ruta.="../";
+$max_salida--;
+}
+
+print_r($_REQUEST);die();
+
+?>
