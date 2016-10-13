2536f57d487cd107d81df6f5b2a993ca55bec786
jheison.vargas <jheison.vargas@cerok.com>
2016-10-13 23:12:43 +0000

    [Radicar entrada][jheison.vargas]Modificacin funcin tipo_radicado_radicacion

--- formatos/radicacion_entrada/funciones.php
+++ formatos/radicacion_entrada/funciones.php
@@ -299,10 +299,11 @@ function tipo_radicado_radicacion($idformato,$iddoc){
     ?>
         <script>
             $(document).ready(function(){
-                $('#fecha_oficio_entrada').addClass('required');
-                $('#serie_idserie').removeClass('required');
+                
                 
                 $('[name="tipo_origen"]').click(function(){
+                    $('#fecha_oficio_entrada').addClass('required');
+                $('#serie_idserie').removeClass('required');
                     var tipo=$(this).val();
                     if(tipo==1){
                         $('[name="tipo_radicado"]').val('radicacion_entrada');
