23871537321f7d1fb5caa8587b35a51cf259c609
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-13 23:36:03 +0000

    [QR][ruben.pulgarin]correccion qr

--- formatos/memorando/funciones.php
+++ formatos/memorando/funciones.php
@@ -226,9 +226,7 @@ function mostrar_qr_interna($idformato,$iddoc){
 			$extension=explode(".",$codigo_qr[0]['ruta_qr']);
 			$img='&nbsp;<img src="http://'.RUTA_PDF.'/'.$codigo_qr[0]['ruta_qr'].'" />';
 		}else{
-		    print_r('aqui1: '.$conn);die();
 			generar_codigo_qr_interna($idformato,$iddoc);
-			
 			$codigo_qr=busca_filtro_tabla("","documento_verificacion","documento_iddocumento=".$iddoc,"", $conn);
 			$extension=explode(".",$codigo_qr[0]['ruta_qr']);
 			$img='&nbsp;<img src="http://'.RUTA_PDF.'/'.$codigo_qr[0]['ruta_qr'].'" />';	
@@ -240,7 +238,6 @@ function mostrar_qr_interna($idformato,$iddoc){
 function generar_codigo_qr_interna($idformato,$iddoc){
   global $conn,$ruta_db_superior;	
 	include_once($ruta_db_superior."pantallas/lib/librerias_fechas.php");
-
   $codigo_qr=busca_filtro_tabla("ruta_qr, iddocumento_verificacion","documento_verificacion","documento_iddocumento=".$iddoc,"", $conn);
   $datos=busca_filtro_tabla("A.fecha,A.estado, A.numero","documento A","A.iddocumento=".$iddoc,"",$conn);	
 	$fecha=mostrar_fecha_saia($datos[0]['fecha']);
@@ -253,18 +250,12 @@ function generar_codigo_qr_interna($idformato,$iddoc){
 	}
 	$ruta=RUTA_QR.$datos[0]['estado'].'/'.date('Y-m').'/'.$iddoc.'/qr/';	
 	
-	
-
 	$imagen=generar_qr_interna($ruta,$datos_qr);
 	
-
-
 	if($imagen==false){
 	  alerta("Error al tratar de crear el codigo qr");
 	}else{
-	   
 	   $codigo_hash=obtener_codigo_hash_archivo($imagen,'crc32'); 
-	   	
 	  $sql_documento_qr="INSERT INTO documento_verificacion(documento_iddocumento,funcionario_idfuncionario,fecha,ruta_qr,verificacion,codigo_hash) VALUES (".$iddoc.",".usuario_actual('idfuncionario').",".fecha_db_almacenar(date("Y-m-d H:i:s"),'Y-m-d H:i:s').",'".$imagen."','vacio','".$codigo_hash."')";
 	  phpmkr_query($sql_documento_qr);
 	}
