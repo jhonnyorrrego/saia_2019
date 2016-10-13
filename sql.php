6c2f9ab9219d1e463d5d30d7cab95359f645ff6b
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-13 20:51:46 +0000

    [qr][ruben.pulgarin]error qr

--- sql.php
+++ sql.php
@@ -293,7 +293,7 @@ la matriz con los valores del resultado se obtiene por medio de la función Resu
         $this->filas=0;
      if($sql && $sql<>"" && $this->Conn->conn){
 
-         $this->res=mysqli_query($this->Conn->conn,$sql) or die($this->Conn->conn." <-> ERROR SQL  en ".$_SERVER["PHP_SELF"]." ->".$sql);// or error//("Error al Ejecutar:  $sql --- ".mysql_error());
+         $this->res=mysqli_query($this->Conn->conn,$sql) or die("conn: ".$this->Conn->conn." <-> ERROR SQL  en ".$_SERVER["PHP_SELF"]." ->".$sql);// or error//("Error al Ejecutar:  $sql --- ".mysql_error());
          
          if( $this->res===false ){
              
