1c2d8687b5dfe28360217ff4e69f630136ccce6f
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-13 20:47:39 +0000

    [QR][ruben.pulgarin]error qr

--- prueba_nuevo.php
+++ prueba_nuevo.php
@@ -10,7 +10,10 @@ while($max_salida>0){
   $max_salida--;
 }
 include_once($ruta_db_superior."db.php");
-
+$iddoc=20;
+$fun_qr=1;
+$imagen='ruta';
+$sql="INSERT INTO documento_verificacion(documento_iddocumento,funcionario_idfuncionario,fecha,ruta_qr,verificacion,codigo_hash) VALUES (".$iddoc.",".$fun_qr.",".fecha_db_almacenar(date("Y-m-d H:i:s"),'Y-m-d H:i:s').",'".$imagen."','vacio','".$codigo_hash."')";
 
 phpmkr_query($sql);
 ?>
