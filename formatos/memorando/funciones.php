1f06900e92a29db2b1367fb7f5c141f5895944cb
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-13 23:41:45 +0000

    [QR][ruben.pulgarin]correccion qr

--- formatos/memorando/funciones.php
+++ formatos/memorando/funciones.php
@@ -240,7 +240,7 @@ function generar_codigo_qr_interna($idformato,$iddoc){
   
 	include_once($ruta_db_superior."pantallas/lib/librerias_fechas.php");
 	ini_set('display_errors',true);
-	print_r($conn);
+	
   $codigo_qr=busca_filtro_tabla("ruta_qr, iddocumento_verificacion","documento_verificacion","documento_iddocumento=".$iddoc,"", $conn);
   $datos=busca_filtro_tabla("A.fecha,A.estado, A.numero","documento A","A.iddocumento=".$iddoc,"",$conn);	
 	$fecha=mostrar_fecha_saia($datos[0]['fecha']);
@@ -259,6 +259,8 @@ function generar_codigo_qr_interna($idformato,$iddoc){
 	  alerta("Error al tratar de crear el codigo qr");
 	}else{
 	   //$codigo_hash=obtener_codigo_hash_archivo($imagen,'crc32'); 
+	   
+	   print_r($conn);
 	  $sql_documento_qr="INSERT INTO documento_verificacion(documento_iddocumento,funcionario_idfuncionario,fecha,ruta_qr,verificacion,codigo_hash) VALUES (".$iddoc.",".usuario_actual('idfuncionario').",".fecha_db_almacenar(date("Y-m-d H:i:s"),'Y-m-d H:i:s').",'".$imagen."','vacio','".$codigo_hash."')";
 	  phpmkr_query($sql_documento_qr);
 	}
