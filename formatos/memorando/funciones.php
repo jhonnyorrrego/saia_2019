a52cc7d5d3748862f1347c242079e2e4508cc0d9
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-13 23:49:38 +0000

    [QR][ruben.pulgarin]correccion qr

--- formatos/memorando/funciones.php
+++ formatos/memorando/funciones.php
@@ -260,12 +260,12 @@ function generar_codigo_qr_interna($idformato,$iddoc){
 	}else{
 	   $codigo_hash=obtener_codigo_hash_archivo($imagen,'crc32'); 
 	   
-	   print_r($conn);
+	   
 	  $sql_documento_qr="INSERT INTO documento_verificacion(documento_iddocumento,funcionario_idfuncionario,fecha,ruta_qr,verificacion,codigo_hash) VALUES (".$iddoc.",1,".fecha_db_almacenar(date("Y-m-d H:i:s"),'Y-m-d H:i:s').",'".$imagen."','vacio','".$codigo_hash."')";
 	  print_r($sql_documento_qr);
-	  
-	  phpmkr_query($sql_documento_qr);
 	  print_r($conn);
+	  phpmkr_query($sql_documento_qr);
+	 
 	}
 }
 
