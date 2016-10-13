0f691000c8e73c0e0dfc8e3db3ff64fb837f24bc
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-13 16:55:35 +0000

    [Comunicacin Interna][ruben.pulgarin]error en qr

--- formatos/memorando/funciones.php
+++ formatos/memorando/funciones.php
@@ -249,7 +249,8 @@ function generar_codigo_qr_interna($idformato,$iddoc){
 	for($i=0; $i<$firmas['numcampos']; $i++){
 	  $datos_qr .= $firmas[$i]['nombre']." \n";
 	}
-	$ruta=RUTA_QR.$datos[0]['estado'].'/'.date('Y-m-d').'/'.$iddoc.'/qr/';	
+	$formato_ruta = aplicar_plantilla_ruta_documento($iddoc);
+	$ruta=RUTA_QR.$formato_ruta . '/qr/';
 	
 	$imagen=generar_qr_interna($ruta,$datos_qr);
 	
