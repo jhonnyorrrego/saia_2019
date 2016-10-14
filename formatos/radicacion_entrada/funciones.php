e5f539d1733336531d78623ab02cd362d940b1b9
jheison.vargas <jheison.vargas@cerok.com>
2016-10-14 17:51:51 +0000

    [Radicar entrada][jheison.vargas]Creacion funcion mostrar_item_destino_radicacion

--- formatos/radicacion_entrada/funciones.php
+++ formatos/radicacion_entrada/funciones.php
@@ -451,7 +451,7 @@ function mostrar_item_destino_radicacion($idformato,$iddoc){
     	            <td><textarea name='observaciones[]'></textarea></td>
     	        </tr>
     	    </table>
-    	    </form>
+    	    </form><br/><br/>
     	    <input type='submit' value='Aplicar Despacho Físico'/>
     	    ";
     	    
