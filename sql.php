54527ebc5106031078eaca291ce9f4acea76f3e9
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-14 15:27:10 +0000

    [qr][ruben.pulgarin]qr

--- sql.php
+++ sql.php
@@ -291,22 +291,12 @@ la matriz con los valores del resultado se obtiene por medio de la función Resu
         
        // $sql=addslashes($sql);
 
-         if(strpos($sql,'INSERT INTO documento_verificacion')!==false){
-             
-            
-            print_r ($this->Conn); 
-             print_r ('<------------------->');
-         }
+
        
         $this->filas=0;
      if($sql && $sql<>"" && $this->Conn->conn){
          $this->res=mysqli_query($this->Conn->conn,$sql); // or die("ERROR SQL ".mysqli_error($this->Conn->conn)." en ".$_SERVER["PHP_SELF"]." ->".$sql);// or error//("Error al Ejecutar:  $sql --- ".mysql_error());
-         
-         if(strpos($sql,'INSERT INTO documento_verificacion')!==false){
-             print_r ($sql);
-             print_r ('<------------------->');
-            print_r ($this->Conn); 
-         }
+
          
         if($this->res){
          if(strpos(strtolower($sql),"insert")!==false)
