9a34cdd83688ba7815473a54a6ff1a45c44ec030
jheison.vargas <jheison.vargas@cerok.com>
2016-10-14 16:44:51 +0000

    [Radicar entrada][jheison.vargas]Creacion funcion ingresar_item_destino_radicacion

--- formatos/radicacion_entrada/funciones.php
+++ formatos/radicacion_entrada/funciones.php
@@ -432,9 +432,9 @@ function mostrar_item_destino_radicacion($idformato,$iddoc){
     	for ($i=0; $i < $datos['numcampos']; $i++) {
     	    $origen=busca_filtro_tabla("a.nombres, a.apellidos","funcionario a","a.funcionario_codigo=".$datos[$i]['nombre_origen'],"",$conn);
     	    if($datos[$i]['tipo_destino']==1){
-    	        $destino=busca_filtro_tabla("b.nombre, b.apellidos","datos_ejecutor a, ejecutor b","b.idejecutor=a.ejecutor_idejecutor AND a.iddatos_ejecutor=".$datos[$i]['nombre_destino'],"",$conn);
+    	        $destino=busca_filtro_tabla("","datos_ejecutor a, ejecutor b","b.idejecutor=a.ejecutor_idejecutor AND a.iddatos_ejecutor=".$datos[$i]['nombre_destino'],"",$conn);
     	    }else{
-    	        $destino=busca_filtro_tabla("nombres, apellidos","vfuncionario_dc","iddependencia_cargo=".$datos[$i]['nombre_destino'],"",$conn);
+    	        $destino=busca_filtro_tabla("","vfuncionario_dc","iddependencia_cargo=".$datos[$i]['nombre_destino'],"",$conn);
     	    }
     	}
 	}
