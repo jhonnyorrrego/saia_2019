831bb3fdb5b3f132f9aaacaeacceb96255173416
jheison.vargas <jheison.vargas@cerok.com>
2016-10-14 16:59:29 +0000

    [Radicar entrada][jheison.vargas]Creacion funcion mostrar_item_destino_radicacion

--- formatos/radicacion_entrada/funciones.php
+++ formatos/radicacion_entrada/funciones.php
@@ -409,8 +409,9 @@ function ingresar_item_destino_radicacion($idformato,$iddoc){//posterior al adic
 		    $destino=explode(",",$padre[0]["$campo"]);
 		    for($i=0; $i < (count($destino)); $i++){
 		        $cadena='INSERT INTO ft_destino_radicacion (ft_radicacion_entrada,nombre_destino, nombre_origen, tipo_origen, tipo_destino) VALUES ('.$padre[0]['idft_radicacion_entrada'].','.$destino[$i].', '.$padre[0]['ejecutor'].', '.$padre[0]['tipo_origen'].', '.$padre[0]['tipo_destino'].')';
-		        phpmkr_query($cadena);
+		        print_r($cadena);
 		    }
+		    die();
 	}
 }
 
