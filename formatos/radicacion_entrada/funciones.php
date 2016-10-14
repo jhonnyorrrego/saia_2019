21ec4ca2d9d2c1f930edb31e976f49253fec3444
jheison.vargas <jheison.vargas@cerok.com>
2016-10-14 15:29:35 +0000

    [Radicar entrada][jheison.vargas]Creacion funcion ingresar_item_destino_radicacion

--- formatos/radicacion_entrada/funciones.php
+++ formatos/radicacion_entrada/funciones.php
@@ -424,7 +424,7 @@ function mostrar_item_destino_radicacion($idformato,$iddoc){
 	global $conn,$ruta_db_superior;
 	$padre=busca_filtro_tabla("","ft_radicacion_entrada A, documento B ","A.documento_iddocumento=B.iddocumento AND B.estado<>'ELIMINADO' AND B.iddocumento=".$iddoc,"",$conn);  //nombre tabla padre
 	$datos=busca_filtro_tabla("","ft_destino_radicacion","ft_radicacion_entrada=".$padre[0]["idft_radicacion_entrada"],"",$conn);	
-	$tabla='<form id="item_prerequisitos" action="actualizar_item_prerequisitos.php"><table class="table-bordered adicionar_campo" style="width: 95%; font-size:10px; text-align:center;" border="1">
+	$tabla='<form id="item_prerequisitos" action="actualizar_item_destino_radicacion.php"><table class="table-bordered adicionar_campo" style="width: 95%; font-size:10px; text-align:center;" border="1">
 	<tr>
     	<th>VERIFICACIÓN</th>
    		<th>REQUERIDO</th>
