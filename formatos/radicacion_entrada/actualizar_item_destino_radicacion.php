573685f29d9185afac8616047408820ef684a95d
jheison.vargas <jheison.vargas@cerok.com>
2016-10-14 18:03:37 +0000

    [Radicar entrada][jheison.vargas]crear archivo actualizar_item_destino_radicacion.php

--- formatos/radicacion_entrada/actualizar_item_destino_radicacion.php
+++ formatos/radicacion_entrada/actualizar_item_destino_radicacion.php
@@ -12,11 +12,11 @@ $max_salida--;
 }
 
 include_once($ruta_db_superior."db.php");
-
+print_r($_REQUEST);
 $id=$_REQUEST['id'];
 $observacion=$_REQUEST['observaciones'];
 
 for ($i=0; $i < count($id) ; $i++) { 
 	$sql="UPDATE ft_destino_radicacion SET observaciones={$observacion[$i]} WHERE idft_destino_radicacion={$id[$i]}";
-	phpmkr_query($sql);
+	//phpmkr_query($sql);
 }
