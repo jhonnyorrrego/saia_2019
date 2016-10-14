0a3dc12b00678fc9d5e2dc7f1ae66bd3505cd175
jheison.vargas <jheison.vargas@cerok.com>
2016-10-14 14:50:19 +0000

    [Radicar entrada][jheison.vargas]Creacion funcion ingresar_item_destino_radicacion

--- formatos/radicacion_entrada/funciones.php
+++ formatos/radicacion_entrada/funciones.php
@@ -399,7 +399,7 @@ function tipo_radicado_radicacion($idformato,$iddoc){//en el adicionar
 function ingresar_item_destino_radicacion($idformato,$iddoc){//posterior al adicionar
 	global $conn,$ruta_db_superior;
 	$padre=busca_filtro_tabla("","ft_radicacion_entrada A, documento B ","A.documento_iddocumento=B.iddocumento AND B.estado<>'ELIMINADO' AND B.iddocumento=".$iddoc,"",$conn);  //nombre tabla padre
-	$item=busca_filtro_tabla("","ft_destino_radicacion","ft_radicacion_entrada=".$padre[0]["idft_cliente"],"",$conn);
+	$item=busca_filtro_tabla("","ft_destino_radicacion","ft_radicacion_entrada=".$padre[0]["idft_radicacion_entrada"],"",$conn);
 	if ($item['numcampos']==0) {
 		
 	
