7593045948c80f14ec71804a502441753dff2dcd
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-14 13:26:42 +0000

    [QR][ruben.pulgarin]ERROR QR

--- sql.php
+++ sql.php
@@ -289,16 +289,16 @@ la matriz con los valores del resultado se obtiene por medio de la función Resu
   function Ejecutar_Sql_MySql($sql)
     {
         
-        $sql=addslashes($sql);
+       // $sql=addslashes($sql);
         $this->filas=0;
      if($sql && $sql<>"" && $this->Conn->conn){
          $this->res=mysqli_query($this->Conn->conn,$sql); // or die("ERROR SQL ".mysqli_error($this->Conn->conn)." en ".$_SERVER["PHP_SELF"]." ->".$sql);// or error//("Error al Ejecutar:  $sql --- ".mysql_error());
-         
+         /*
          if(strpos($sql,'INSERT INTO')!==false){
              print_r ($sql);
              print_r ('<------------------->');
             print_r ($this->Conn); 
-         }
+         }*/
          
         if($this->res){
          if(strpos(strtolower($sql),"insert")!==false)
