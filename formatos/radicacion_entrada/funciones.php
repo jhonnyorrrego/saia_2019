509c5c68c263869ac575f0025324c39d5b215ace
jheison.vargas <jheison.vargas@cerok.com>
2016-10-14 17:27:20 +0000

    [Radicar entrada][jheison.vargas]Creacion funcion mostrar_item_destino_radicacion

--- formatos/radicacion_entrada/funciones.php
+++ formatos/radicacion_entrada/funciones.php
@@ -436,7 +436,7 @@ function mostrar_item_destino_radicacion($idformato,$iddoc){
     	    if($datos[$i]['tipo_destino']==1){
     	        $destino=busca_filtro_tabla("b.nombre, a.cargo","datos_ejecutor a, ejecutor b","b.idejecutor=a.ejecutor_idejecutor AND a.iddatos_ejecutor=".$datos[$i]['nombre_destino'],"",$conn);
     	    }else{
-    	        $destino=busca_filtro_tabla("concat(nombres,' ',apellidos) AS nombre","vfuncionario_dc","iddependencia_cargo=".$datos[$i]['nombre_destino'],"",$conn);
+    	        $destino=busca_filtro_tabla("concat(nombres,' ',apellidos) AS nombre, cargo","vfuncionario_dc","iddependencia_cargo=".$datos[$i]['nombre_destino'],"",$conn);
     	    }
     	    
     	    $tabla.="
