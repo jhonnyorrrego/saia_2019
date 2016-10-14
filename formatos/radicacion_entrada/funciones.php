846b0ad37b565009c9c0a5a9ba56bfd5a9940996
jheison.vargas <jheison.vargas@cerok.com>
2016-10-14 20:52:35 +0000

    [Radicar entrada][jheison.vargas]crear funcion mostrar_destino_radicacion

--- formatos/radicacion_entrada/funciones.php
+++ formatos/radicacion_entrada/funciones.php
@@ -534,7 +534,14 @@ function campos_adicionales_radicacion($idformato,$iddoc){
 	global $conn,$ruta_db_superior;
     
     $datos=busca_filtro_tabla("","ft_radicacion_entrada","documento_iddocumento=".$iddoc,"",$conn);
-    
+    $tabla='<table class="table-bordered adicionar_campo" style="width: 95%; font-size:10px; text-align:center;" border="1">';
+    if($datos[0]['tipo_destino']==1){
+    	        $destino=busca_filtro_tabla("b.nombre","datos_ejecutor a, ejecutor b","b.idejecutor=a.ejecutor_idejecutor AND a.iddatos_ejecutor IN(".$datos[0]['persona_natural_dest'].")","",$conn);
+    	        
+    	    }else{
+    	        $destino=busca_filtro_tabla("concat(nombres,' ',apellidos) AS nombre","vfuncionario_dc","iddependencia_cargo IN(".$datos[0]['destino'].")","",$conn);
+    	        
+    	    }
 }
 
 ?>
