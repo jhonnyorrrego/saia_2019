5aa9f9cbaf3180865b2e9f1fa361daa7e6608e4b
jheison.vargas <jheison.vargas@cerok.com>
2016-10-18 13:42:15 +0000

    [Radicar entrada][jheison.vargas]Modificacion mostrar_item_destino_radicacion

--- formatos/radicacion_entrada/funciones.php
+++ formatos/radicacion_entrada/funciones.php
@@ -499,8 +499,8 @@ function mostrar_item_destino_radicacion($idformato,$iddoc){
     	        <tr>
     	            <td>".$origen[0]['nombres']." ".$origen[0]['apellidos']."</td>
     	            <td>".$destino[0]['nombre']."</td>
-    	            <td>".$destino[0]['cargo']."</td>
-    	            <td>".$ubicacion."</td>
+    	            <td text-align:center;>".$destino[0]['cargo']."</td>
+    	            <td text-align:center;>".$ubicacion."</td>
     	            <td>".$datos[$i]['observacion_destino']."</td>
     	        </tr>
     	    ";
