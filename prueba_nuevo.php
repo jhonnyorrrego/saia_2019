364cc6ee8b225a46d1d08544ea1c3f8d6215b679
jheison.vargas <jheison.vargas@cerok.com>
2016-10-19 15:59:48 +0000

    [2. RADICACION][jheison.vargas]prueba

--- prueba_nuevo.php
+++ prueba_nuevo.php
@@ -1,5 +1,4 @@
 <?php
-
 header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); // date in the past
 header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); // always modified
 header("Cache-Control: no-store, no-cache, must-revalidate"); // HTTP/1.1 
