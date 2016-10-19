7358ece5ac9cea9a6c5ac2518f984750ce72d334
jheison.vargas <jheison.vargas@cerok.com>
2016-10-19 13:44:54 +0000

    [2. RADICACION][jheison.vargas]Crear radicacion rapida

--- formatos/radicacion_entrada/radicacion_rapida.php
+++ formatos/radicacion_entrada/radicacion_rapida.php
@@ -1,3 +1,25 @@
+<?php
+$max_salida=10; // Previene algun posible ciclo infinito limitando a 10 los ../
+$ruta_db_superior=$ruta="";
+while($max_salida>0){
+	if(is_file($ruta."db.php")){
+		$ruta_db_superior=$ruta; //Preserva la ruta superior encontrada
+	}
+	$ruta.="../";
+	$max_salida--;
+}
+include_once($ruta_db_superior."db.php");
+include_once($ruta_db_superior."calendario/calendario.php");
+include_once($ruta_db_superior."librerias_saia.php");
+echo(librerias_html5());
+echo(librerias_jquery("1.7"));
+echo(librerias_arboles());
+echo(estilo_bootstrap()); 
+?>    
+
+
+
+
 <html>
 <head>
 <meta http-equiv="content-type" content="text/html; charset=utf-8">
