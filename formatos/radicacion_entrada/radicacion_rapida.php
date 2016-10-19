c3904c83771d42ffbbc04961042bac4455dd13f3
jheison.vargas <jheison.vargas@cerok.com>
2016-10-19 14:02:48 +0000

    [2. RADICACION][jheison.vargas]Crear radicacion rapida

--- formatos/radicacion_entrada/radicacion_rapida.php
+++ formatos/radicacion_entrada/radicacion_rapida.php
@@ -17,89 +17,6 @@ echo(librerias_arboles());
 echo(estilo_bootstrap()); 
 
 
-function arbol(){
-include_once("db.php");
-include_once("formatos/librerias/estilo_formulario.php");
-$adicional=Null;
-if(@$_REQUEST["idcategoria_formato"]){
-	$adicional="?idcategoria_formato=".$_REQUEST["idcategoria_formato"];
-}
-?><link rel="STYLESHEET" type="text/css" href="css/dhtmlXTree.css">
-	<script type="text/javascript" src="js/dhtmlXCommon.js"></script>
-	<script type="text/javascript" src="js/dhtmlXTree.js"></script>
-	<script type="text/javascript" src="js/dhtmlXTree_xw.js"></script> 
-	<script type="text/javascript" src="asset/js/jquery.min.js"></script>
-	<script type="text/javascript" src="asset/js/main.js"></script>
-  Buscar :<br />
-  <input type="text" id="stext_3" width="200px" size="20">      
-      <a href="javascript:void(0)" onclick="tree_equipos.findItem(document.getElementById('stext_3').value,1)">
-      <img src="botones/general/anterior.png" border="0px" alt="Anterior"></a>
-      <a href="javascript:void(0)" onclick="tree_equipos.findItem(document.getElementById('stext_3').value,0,1)">
-      <img src="botones/general/buscar.png" border="0px" alt="Buscar"></a>
-      <a href="javascript:void(0)" onclick="tree_equipos.findItem(document.getElementById('stext_3').value)">
-      <img src="botones/general/siguiente.png" border="0px" alt="Siguiente"></a>  
-	<br><div id="esperando_serie">
-    <img src="imagenes/cargando.gif"></div>
-	<div id="treeboxbox_tree_equipos" style="height:89%;width:95%" ></div>
-	<script type="text/javascript">
-  <!--		
-  	$(document).ready(function  () {
-			top.setTitulo("Ingreso de documentos");  
-	});	
-  	
-      var browserType;
-      
-      if (document.layers) {browserType = "nn4"}
-      if (document.all) {browserType = "ie"}
-      if (window.navigator.userAgent.toLowerCase().match("gecko")) {
-         browserType= "gecko"
-      }
-			tree_equipos=new dhtmlXTreeObject("treeboxbox_tree_equipos","100%","100%",0);
-			tree_equipos.setImagePath("imgs/");
-			tree_equipos.enableIEImageFix(true);
-			tree_equipos.setOnClickHandler(onNodeSelect);  
-			tree_equipos.setOnLoadingStart(cargando_serie);
-      tree_equipos.setOnLoadingEnd(fin_cargando_serie);
-      tree_equipos.setXMLAutoLoading("test_categoria.php<?php echo $adicional; ?>");
-      //tree_equipos.enableSmartXMLParsing(true);
-			tree_equipos.loadXML("test_categoria.php<?php echo $adicional; ?>");
-	    function onNodeSelect(nodeId){
-	     if(nodeId.indexOf('#',0)==-1){
-            window.parent.previsualizar.location='parsear_categoria.php?id='+nodeId;
-	     }
-      }
-      function fin_cargando_serie() {
-        if (browserType == "gecko" )
-           document.poppedLayer =
-               eval('document.getElementById("esperando_serie")');
-        else if (browserType == "ie")
-           document.poppedLayer =
-              eval('document.getElementById("esperando_serie")');
-        else
-           document.poppedLayer =
-              eval('document.layers["esperando_serie"]');
-        document.poppedLayer.style.visibility = "hidden";
-        tree_equipos.openAllItems(0);
-      }
-
-      function cargando_serie() {
-        if (browserType == "gecko" )
-           document.poppedLayer =
-               eval('document.getElementById("esperando_serie")');
-        else if (browserType == "ie")
-           document.poppedLayer =
-              eval('document.getElementById("esperando_serie")');
-        else
-           document.poppedLayer =
-               eval('document.layers["esperando_serie"]');
-        document.poppedLayer.style.visibility = "visible";
-      }                            	
-	--> 		
-	</script>
-	}
-
-
-
 
 ?>    
 
@@ -231,8 +148,84 @@ if(@$_REQUEST["idcategoria_formato"]){
                         <td style="font-size:8pt;" class="encabezado_list" colspan="2" align="center">Seleccione el formato a radicar</td>
                     </tr>
                     <tr>
-                        <td colspan="2"><?php 
-	        echo arbol("radicacion","tipo_radicacion","formatos/radicacion_entrada/text_radicacion_rapida.php?idcategoria_formato=1","","","","","check"); 
+                        <td colspan="2">include_once("db.php");<?php
+include_once("formatos/librerias/estilo_formulario.php");
+$adicional=Null;
+if(@$_REQUEST["idcategoria_formato"]){
+	$adicional="?idcategoria_formato=".$_REQUEST["idcategoria_formato"];
+}
+?><link rel="STYLESHEET" type="text/css" href="css/dhtmlXTree.css">
+	<script type="text/javascript" src="js/dhtmlXCommon.js"></script>
+	<script type="text/javascript" src="js/dhtmlXTree.js"></script>
+	<script type="text/javascript" src="js/dhtmlXTree_xw.js"></script> 
+	<script type="text/javascript" src="asset/js/jquery.min.js"></script>
+	<script type="text/javascript" src="asset/js/main.js"></script>
+  Buscar :<br />
+  <input type="text" id="stext_3" width="200px" size="20">      
+      <a href="javascript:void(0)" onclick="tree_equipos.findItem(document.getElementById('stext_3').value,1)">
+      <img src="botones/general/anterior.png" border="0px" alt="Anterior"></a>
+      <a href="javascript:void(0)" onclick="tree_equipos.findItem(document.getElementById('stext_3').value,0,1)">
+      <img src="botones/general/buscar.png" border="0px" alt="Buscar"></a>
+      <a href="javascript:void(0)" onclick="tree_equipos.findItem(document.getElementById('stext_3').value)">
+      <img src="botones/general/siguiente.png" border="0px" alt="Siguiente"></a>  
+	<br><div id="esperando_serie">
+    <img src="imagenes/cargando.gif"></div>
+	<div id="treeboxbox_tree_equipos" style="height:89%;width:95%" ></div>
+	<script type="text/javascript">
+  <!--		
+  	$(document).ready(function  () {
+			top.setTitulo("Ingreso de documentos");  
+	});	
+  	
+      var browserType;
+      
+      if (document.layers) {browserType = "nn4"}
+      if (document.all) {browserType = "ie"}
+      if (window.navigator.userAgent.toLowerCase().match("gecko")) {
+         browserType= "gecko"
+      }
+			tree_equipos=new dhtmlXTreeObject("treeboxbox_tree_equipos","100%","100%",0);
+			tree_equipos.setImagePath("imgs/");
+			tree_equipos.enableIEImageFix(true);
+			tree_equipos.setOnClickHandler(onNodeSelect);  
+			tree_equipos.setOnLoadingStart(cargando_serie);
+      tree_equipos.setOnLoadingEnd(fin_cargando_serie);
+      tree_equipos.setXMLAutoLoading("test_categoria.php<?php echo $adicional; ?>");
+      //tree_equipos.enableSmartXMLParsing(true);
+			tree_equipos.loadXML("test_categoria.php<?php echo $adicional; ?>");
+	    function onNodeSelect(nodeId){
+	     if(nodeId.indexOf('#',0)==-1){
+            window.parent.previsualizar.location='parsear_categoria.php?id='+nodeId;
+	     }
+      }
+      function fin_cargando_serie() {
+        if (browserType == "gecko" )
+           document.poppedLayer =
+               eval('document.getElementById("esperando_serie")');
+        else if (browserType == "ie")
+           document.poppedLayer =
+              eval('document.getElementById("esperando_serie")');
+        else
+           document.poppedLayer =
+              eval('document.layers["esperando_serie"]');
+        document.poppedLayer.style.visibility = "hidden";
+        tree_equipos.openAllItems(0);
+      }
+
+      function cargando_serie() {
+        if (browserType == "gecko" )
+           document.poppedLayer =
+               eval('document.getElementById("esperando_serie")');
+        else if (browserType == "ie")
+           document.poppedLayer =
+              eval('document.getElementById("esperando_serie")');
+        else
+           document.poppedLayer =
+               eval('document.layers["esperando_serie"]');
+        document.poppedLayer.style.visibility = "visible";
+      }                            	
+	--> 		
+	</script>
 	        ?></td>
                     </tr>
                     <tr>
