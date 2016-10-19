741daa13497e8f57e7ca9663e28dead8689eec3c
jheison.vargas <jheison.vargas@cerok.com>
2016-10-19 14:04:36 +0000

    [2. RADICACION][jheison.vargas]Crear radicacion rapida

--- formatos/radicacion_entrada/radicacion_rapida.php
+++ formatos/radicacion_entrada/radicacion_rapida.php
@@ -154,23 +154,7 @@ $adicional=Null;
 if(@$_REQUEST["idcategoria_formato"]){
 	$adicional="?idcategoria_formato=".$_REQUEST["idcategoria_formato"];
 }
-?><link rel="STYLESHEET" type="text/css" href="css/dhtmlXTree.css">
-	<script type="text/javascript" src="js/dhtmlXCommon.js"></script>
-	<script type="text/javascript" src="js/dhtmlXTree.js"></script>
-	<script type="text/javascript" src="js/dhtmlXTree_xw.js"></script> 
-	<script type="text/javascript" src="asset/js/jquery.min.js"></script>
-	<script type="text/javascript" src="asset/js/main.js"></script>
-  Buscar :<br />
-  <input type="text" id="stext_3" width="200px" size="20">      
-      <a href="javascript:void(0)" onclick="tree_equipos.findItem(document.getElementById('stext_3').value,1)">
-      <img src="botones/general/anterior.png" border="0px" alt="Anterior"></a>
-      <a href="javascript:void(0)" onclick="tree_equipos.findItem(document.getElementById('stext_3').value,0,1)">
-      <img src="botones/general/buscar.png" border="0px" alt="Buscar"></a>
-      <a href="javascript:void(0)" onclick="tree_equipos.findItem(document.getElementById('stext_3').value)">
-      <img src="botones/general/siguiente.png" border="0px" alt="Siguiente"></a>  
-	<br><div id="esperando_serie">
-    <img src="imagenes/cargando.gif"></div>
-	<div id="treeboxbox_tree_equipos" style="height:89%;width:95%" ></div>
+?>
 	<script type="text/javascript">
   <!--		
   	$(document).ready(function  () {
