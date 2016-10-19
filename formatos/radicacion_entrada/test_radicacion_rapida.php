dda55ee5156d45bb7848f1c280e9797f1000126d
jheison.vargas <jheison.vargas@cerok.com>
2016-10-19 15:03:06 +0000

    [2. RADICACION][jheison.vargas]rbol radicacion rapida

--- formatos/radicacion_entrada/test_radicacion_rapida.php
+++ formatos/radicacion_entrada/test_radicacion_rapida.php
@@ -1,19 +1,6 @@
 <?php 
-$max_salida=6; // Previene algun posible ciclo infinito limitando a 10 los ../
-$ruta_db_superior=$ruta="";
-while($max_salida>0)
-{
-if(is_file($ruta."db.php"))
-{
-$ruta_db_superior=$ruta; //Preserva la ruta superior encontrada
-}
-$ruta.="../";
-$max_salida--;
-}
 
 
-include_once($ruta_db_superior."db.php");
-include_once($ruta_db_superior."formatos/librerias/estilo_formulario.php");
 
 header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); // date in the past
 header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); // always modified
