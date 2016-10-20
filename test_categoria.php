69d7322bffac0cd6d64a644438151f994f40a8a0
jheison.vargas <jheison.vargas@cerok.com>
2016-10-20 14:12:59 +0000

    [2. RADICACION][jheison.vargas]rbol radicacion rapida

--- test_categoria.php
+++ test_categoria.php
@@ -6,6 +6,8 @@ header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache"); // HTTP/1.0
 $id = @$_GET["id"];
 $idcategoria=Null;
+print_r($_REQUEST);die();
+
 if(@$_REQUEST["idcategoria_formato"]){
 	$idcategoria=$_REQUEST["idcategoria_formato"];
 }
