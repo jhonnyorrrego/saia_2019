65a0785df6642bca2e054b6f798bbf1a441df4c9
jheison.vargas <jheison.vargas@cerok.com>
2016-10-24 18:32:26 +0000

    [Radicar entrada][jheison.vargas]Cambiar arbol de series segun usuario

--- test_serie_funcionario2.php
+++ test_serie_funcionario2.php
@@ -104,8 +104,9 @@ if(isset($_REQUEST["orden"]))
 else
   $orden="nombre";
 if($serie=="NULL"){
-  $papas=busca_filtro_tabla("*",$tabla,"(cod_padre IS NULL OR cod_padre=0) $activo","$orden ASC",$conn);
-  
+  $papas=busca_filtro_tabla("*",$tabla,"(cod_padre IS NULL OR cod_padre=0) $activo $condicion","$orden ASC",$conn);
+}elseif(){
+    
 }else
   $papas=busca_filtro_tabla("*",$tabla,"cod_padre=".$serie.$activo.$condicion,"$orden ASC",$conn); 
 
