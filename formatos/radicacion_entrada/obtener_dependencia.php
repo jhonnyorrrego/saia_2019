c0f6802841e4f9f16ce5ca9ec849cf6e7acf79a7
jheison.vargas <jheison.vargas@cerok.com>
2016-10-24 15:57:58 +0000

    [Radicar entrada][jheison.vargas]Obtener dependencia del creador

--- formatos/radicacion_entrada/obtener_dependencia.php
+++ formatos/radicacion_entrada/obtener_dependencia.php
@@ -17,3 +17,5 @@ include_once($ruta_db_superior."formatos/librerias_funciones_generales.php");
 $datos=busca_filtro_tabla('iddependencia','vfuncionario_dc','iddependencia_cargo='.$_REQUEST['iddependencia_cargo'],'',conn);
 
 $datos=json_encode($datos);
+
+echo($datos);
