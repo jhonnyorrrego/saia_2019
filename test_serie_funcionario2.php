21b9e621a60d66eeca9ca035f04c8906ced4143b
jheison.vargas <jheison.vargas@cerok.com>
2016-10-24 15:30:38 +0000

    [Radicar entrada][jheison.vargas]Cambiar arbol de series segun usuario

--- test_serie_funcionario2.php
+++ test_serie_funcionario2.php
@@ -39,7 +39,7 @@ if($id and $id<>"" && @$_REQUEST["uid"] && $id[0]=="d"){
         //print_r($_REQUEST);
         $dependencia=str_replace("d","",$id);
         echo("<tree id=\"".$id."\">\n");
-        llena_serie($dependencia);
+        llena_serie($dependencia,$condicion);
         llena_series_asignadas($dependencia);
         echo("</tree>\n");
 	    die();
