1fa1eb19b11b192bc9db05b14555e295a5b4cee1
jheison.vargas <jheison.vargas@cerok.com>
2016-10-24 13:03:00 +0000

    [Radicar entrada][jheison.vargas]Cambiar arbol de series segun usuario

--- test_serie_funcionario2.php
+++ test_serie_funcionario2.php
@@ -120,13 +120,13 @@ if($papas["numcampos"]){
       echo(" child=\"1\">\n");
     else
       echo(" child=\"0\">\n");
-    //if(!$_REQUEST["id"] && $tabla!='dependencia'){
+    if(!$_REQUEST["id"] && $tabla!='dependencia'){
     	llena_serie($papas[$i]["id$tabla"]);
-		//}else{
-			//if(!$_REQUEST["admin"]){
+		}else{
+			if(!$_REQUEST["admin"]){
 				llena_series_asignadas($papas[$i]["id$tabla"]);
-			//}
-		//}
+			}
+		}
     echo("</item>\n");
   }     
 }
