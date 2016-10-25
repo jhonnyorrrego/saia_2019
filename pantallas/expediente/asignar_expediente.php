99b68d1f4a5b9da6a3aab95edf53cfbae17be829
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-25 21:47:58 +0000

    [Expediente][ruben.pulgarin]desarrollo asignar permiso incluidos hijos

--- pantallas/expediente/asignar_expediente.php
+++ pantallas/expediente/asignar_expediente.php
@@ -144,8 +144,8 @@ $table.="</table>";
     ?>
     <div id="sub_entidad" class="arbol_saia">
     </div>
+    <hr/>
 		Asignar Permiso A:* <input type="hidden" name="propietario" value="<?php echo $propietario[0]["idfuncionario"];?>"> <input type='hidden' id='idfuncionario' size='50' name='idfuncionario' value="<?php echo implode(",", $idfuncionarios);?>"> <input type='text' id='buscar_radicado' size='50' name='buscar_radicado'> <div id='ul_completar' class='ac_results'></div>
-		<hr/>
 		<?php
 			echo $table;
 		?>
