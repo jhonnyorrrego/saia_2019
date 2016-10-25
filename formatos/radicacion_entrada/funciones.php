6787f07b3fe1e50efa71233d4dc932b0aa3b012f
jheison.vargas <jheison.vargas@cerok.com>
2016-10-25 15:24:02 +0000

    [Radicar entrada][jheison.vargas]Cambiar arbol de series segun usuario

--- formatos/radicacion_entrada/funciones.php
+++ formatos/radicacion_entrada/funciones.php
@@ -346,7 +346,7 @@ function tipo_radicado_radicacion($idformato,$iddoc){//en el adicionar
                             setTimeout(function(){ 
                                 tree_serie_idserie.deleteChildItems(0); 
                                 tree_serie_idserie.loadXML("../../test_serie_funcionario2.php?tabla=dependencia&admin=1&dependencia="+datos+"&sin_padre=1");
-                            }, 2000);
+                            }, 1000);
                             
                             
                         }
