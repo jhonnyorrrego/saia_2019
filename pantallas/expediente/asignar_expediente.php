8d83dc54990e1de875e2ed36df456fe48874d18b
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-25 21:21:02 +0000

    [Expediente][ruben.pulgarin]desarrollo asignar permiso incluidos hijos

--- pantallas/expediente/asignar_expediente.php
+++ pantallas/expediente/asignar_expediente.php
@@ -82,8 +82,8 @@ $table.="</table>";
       tree2.setOnLoadingStart(cargando_expediente);
       tree2.setOnLoadingEnd(fin_cargando_expediente);
       
-			tree2.setXMLAutoLoading("test_expediente.php?doc=617&accion=1&permiso_editar=1");
-			tree2.loadXML("test_expediente.php?doc=<?php echo($iddoc); ?>&accion=1&permiso_editar=1");
+			tree2.setXMLAutoLoading("<?php echo($ruta_db_superior);?>test_expediente.php?doc=617&accion=1&permiso_editar=1");
+			tree2.loadXML("<?php echo($ruta_db_superior);?>test_expediente.php?doc=<?php echo($iddoc); ?>&accion=1&permiso_editar=1");
 			
 			function fin_cargando_expediente() {
         if (browserType == "gecko" )
