59a6d059359e0f48c4990cf56ab3bb83d36cee40
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-25 22:10:54 +0000

    [Expediente][ruben.pulgarin]desarrollo asignar permiso incluidos hijos

--- pantallas/expediente/asignar_expediente.php
+++ pantallas/expediente/asignar_expediente.php
@@ -69,9 +69,10 @@ $table.="</table>";
              $(document).ready(function(){
                 $('[name="todos_ninguno"]').click(function(){
                     var valor=$(this).attr('value');
-                    alert(valor);
+                    
                     if(valor=='todos'){
                         tree3.openAllItems();
+                        
                     }else if(valor=='ninguno'){
                         
                     }
