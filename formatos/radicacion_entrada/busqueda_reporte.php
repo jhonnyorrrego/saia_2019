d005eb8cbcc2f577e2042ba8e48f770e4f6a68cf
jheison.vargas <jheison.vargas@cerok.com>
2016-10-26 22:16:32 +0000

    [Radicar entrada][jheison.vargas]Bsqueda avanzada

--- formatos/radicacion_entrada/busqueda_reporte.php
+++ formatos/radicacion_entrada/busqueda_reporte.php
@@ -22,7 +22,7 @@ global $conn;
   <body>
  
     <div class="container master-container">
-        <legend>Filtrar seguimientos</legend>  
+        <legend>Filtrar despachos</legend>  
 		<div class="container master-container">
        <form accept-charset="UTF-8" id="kformulario_saia"  method="post" >  
         <div class="control-group">
