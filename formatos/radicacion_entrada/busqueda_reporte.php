b37240249394cdb4d604ba885b5807ff6622ebe1
jheison.vargas <jheison.vargas@cerok.com>
2016-10-26 23:37:00 +0000

    [Radicar entrada][jheison.vargas]Bsqueda avanzada

--- formatos/radicacion_entrada/busqueda_reporte.php
+++ formatos/radicacion_entrada/busqueda_reporte.php
@@ -108,7 +108,7 @@ global $conn;
 				$(document).ready(function(){
 					$("#filtro_mensajeros").live("change",function(){
                         var mensajero_filtro=$(this).val();
-                        $('#variable_busqueda').attr("value",mensajero_filtro);
+                        $('#variable_busqueda').val('mensajero_filtro');
                         
                     });	
 				});
