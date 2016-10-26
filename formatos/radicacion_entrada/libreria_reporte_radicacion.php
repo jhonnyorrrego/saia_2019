df70feeb44aecb55f378acf18dd5529a45935497
cerok <info@cerok.com>
2016-10-26 18:15:02 +0000

    [Radicar entrada][jheison.vargas]Funciones del reporte

--- formatos/radicacion_entrada/libreria_reporte_radicacion.php
+++ formatos/radicacion_entrada/libreria_reporte_radicacion.php
@@ -100,6 +100,6 @@ function condicion_adicional(){
     if($cargo[0]['cargo']=="mensajero"){
         $condicion="AND B.mensajero_encargado=".$cargo[0]['iddependencia_cargo'];
     }  
-    return $cargo[0]['cargo'];
+    return $cargo['numcampos'];
 }
 
