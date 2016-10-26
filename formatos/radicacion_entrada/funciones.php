f63897128d9c0d6d9af4a12127533f204639828f
alejandro.carvajal <alejandro.carvajal@cerok.com>
2016-10-26 15:35:33 +0000

    [Entrada][alejandro.carvajal]Cambios arbol de serie

--- formatos/radicacion_entrada/funciones.php
+++ formatos/radicacion_entrada/funciones.php
@@ -321,7 +321,8 @@ function tipo_radicado_radicacion($idformato,$iddoc){//en el adicionar
     $funcionario_codigo=usuario_actual('funcionario_codigo');
     $cargo=busca_filtro_tabla("","vfuncionario_dc a","estado_dc=1 AND a.funcionario_codigo=".$funcionario_codigo,"",$conn);
 	$dependencia_principal=buscar_dependencias_principal($cargo[0]["iddependencia"]);
-die($dependencia_principal);
+echo($dependencia_principal);
+die("-------------------------**");
 	?>
         <script>
             $(document).ready(function(){
