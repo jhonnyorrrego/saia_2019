0086eb42d4a2189f7ae48605ea4dadbe3db251b9
alejandro.carvajal <alejandro.carvajal@cerok.com>
2016-10-26 13:59:20 +0000

    [Entrada][alejandro.carvajal]Busca todas las dependencias papas.

--- formatos/radicacion_entrada/ajax_serie.php
+++ formatos/radicacion_entrada/ajax_serie.php
@@ -15,6 +15,7 @@ include_once($ruta_db_superior."db.php");
 $retorno[]= $_REQUEST['iddependencia'];
 buscar_dependencias_principal($_REQUEST['iddependencia']);
 echo(json_encode($retorno));
+echo(json_encode(array_reverse($retorno)));
 function buscar_dependencias_principal($iddependencia){
     global $retorno;
 	$cod_dep=busca_filtro_tabla("cod_padre","dependencia","iddependencia=".$iddependencia,"",$conn);
