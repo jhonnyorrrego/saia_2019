352944e4e9e2c43e0dcec5aa5bfc485f91905a52
alejandro.carvajal <alejandro.carvajal@cerok.com>
2016-10-26 13:54:17 +0000

    [Entrada][alejandro.carvajal]Busca todas las series papas.

---
+++ formatos/radicacion_entrada/ajax_serie.php
@@ -0,0 +1,30 @@
+<?php
+$max_salida=6; // Previene algun posible ciclo infinito limitando a 10 los ../
+$ruta_db_superior=$ruta="";
+while($max_salida>0)
+{
+if(is_file($ruta."db.php"))
+{
+$ruta_db_superior=$ruta; //Preserva la ruta superior encontrada
+}
+$ruta.="../";
+$max_salida--;
+}
+
+include_once($ruta_db_superior."db.php");
+$retorno[]= $_REQUEST['iddependencia'];
+buscar_dependencias_principal($_REQUEST['iddependencia']);
+print_r($retorno);
+function buscar_dependencias_principal($iddependencia){
+    global $retorno;
+	$cod_dep=busca_filtro_tabla("cod_padre","dependencia","iddependencia=".$iddependencia,"",$conn);
+
+	if(!$cod_dep['numcampos']){
+		return(true);
+	}else{
+	    $retorno[]=$cod_dep[0]["cod_padre"];
+		$dep=buscar_dependencias_principal($cod_dep[0]["cod_padre"]);
+		return($dep);
+	}
+}
+?>
