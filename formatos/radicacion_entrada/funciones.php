b2c4b83044486dd607817b48427c63f67c63842c
cerok <info@cerok.com>
2016-10-26 16:24:26 +0000

    [Entrada][alejandro.carvajal]Cambios arbol de serie

--- formatos/radicacion_entrada/funciones.php
+++ formatos/radicacion_entrada/funciones.php
@@ -818,8 +818,8 @@ function serie_documental_radicacion($idformato,$iddoc){
 	$(document).ready(function(){
 	    var cargado=[];
 	    var dependencia_principal='<?php echo($dependencia_principal); ?>';
-	    console.log(dependencia_principal);
 	    cargado.push(dependencia_principal);
+	    console.log(cargado);
 	    //Busca las dependencias del rol actual para que no carguen duplicados en la recursion del abrol de series
 	    $.ajax({
 	        type:'POST',
