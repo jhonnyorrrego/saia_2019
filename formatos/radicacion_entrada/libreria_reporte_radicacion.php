8c2a93e021218870f1180eeb19c460a333b7e6ae
jheison.vargas <jheison.vargas@cerok.com>
2016-10-26 20:09:28 +0000

    [Radicar entrada][jheison.vargas]Funciones del reporte

--- formatos/radicacion_entrada/libreria_reporte_radicacion.php
+++ formatos/radicacion_entrada/libreria_reporte_radicacion.php
@@ -108,7 +108,7 @@ function condicion_adicional(){
     global $ruta_db_superior, $conn;
     
     $funcionario_codigo=usuario_actual('funcionario_codigo');
-    $cargo=busca_filtro_tabla("lower(cargo) AS cargo","vfuncionario_dc a","a.funcionario_codigo=".$funcionario_codigo,"",$conn);
+    $cargo=busca_filtro_tabla("lower(cargo) AS cargo, iddependencia_cargo","vfuncionario_dc a","a.funcionario_codigo=".$funcionario_codigo,"",$conn);
     if($cargo[0]['cargo']=="mensajero"){
         $condicion="AND B.mensajero_encargado=".$cargo[0]['iddependencia_cargo'];
     }  
