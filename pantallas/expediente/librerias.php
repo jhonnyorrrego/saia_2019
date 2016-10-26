3525cdfa06b1103de51643ccf4f77e1be13ab68c
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-26 15:06:35 +0000

    [Expediente][ruben.pulgarin]mostrar el nombre de la dependencia del responsable

--- pantallas/expediente/librerias.php
+++ pantallas/expediente/librerias.php
@@ -39,6 +39,16 @@ function permiso_funcionario_expediente($expediente,$entidad,$llave){
 	}
 	return("");
 }
+
+function mostrar_informacion_adicional_expediente($idexpediente){
+    global $conn; 
+    
+    $expediente_actual=busca_filtro_tabla("tomo_padre,tomo_no,serie_idserie,propietario","expediente","idexpediente=".$idexpediente,"",$conn);
+    $dependencia_propietario=busca_filtro_tabla("dependencia","vfuncionario_dc","estado_dc=1 AND funcionario_codigo=".$expediente_actual[0]['propietario'],"",$conn);
+    $nombre_dependencia_propietario=$dependencia_propietario[0]['dependencia'];  
+    return($nombre_dependencia_propietario);
+}
+
 function enlace_expediente($idexpediente,$nombre){
 	global $conn;    
 
