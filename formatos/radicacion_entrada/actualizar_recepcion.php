5b318510fce863903b7b990f3cfcd2f954bf6d93
cerok <info@cerok.com>
2016-10-27 13:16:40 +0000

    [Radicar entrada][jheison.vargas]Actualizar campo recepcin

--- formatos/radicacion_entrada/actualizar_recepcion.php
+++ formatos/radicacion_entrada/actualizar_recepcion.php
@@ -11,6 +11,10 @@ while($max_salida>0){
 
 include_once($ruta_db_superior."db.php");
 
+$datos=busca_filtro_tabla('','ft_destino_radicacion','idft_destino_radicacion='.$_REQUEST['idft_destino_radicacion'],'',conn);
+
+if($datos[0]['recepcion']==0){
 $sql="UPDATE ft_destino_radicacion SET recepcion={$_REQUEST['funcionario']} WHERE idft_destino_radicacion={$_REQUEST['idft_destino_radicacion']}";
 
 phpmkr_query($sql);
+}
