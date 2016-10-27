2e307f74459fd3e6f6b231517f90d50e121c690f
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-27 13:58:47 +0000

    [Expediente][ruben.pulgarin]nuevo desarrollo arbol entidad serie

--- test_dependencia_serie.php
+++ test_dependencia_serie.php
@@ -103,8 +103,8 @@ else
 if($papas["numcampos"]){ 
   for($i=0; $i<$papas["numcampos"]; $i++){
     $hijos = busca_filtro_tabla("count(*) AS cant",$tabla,"cod_padre=".$papas[$i]["id$tabla"].$activo.$condicion,"",$conn);
-    $hijos_entidad_serie = busca_filtro_tabla("serie_idserie","entidad_serie","estado=1 AND entidad_identidad='2' AND llave_entidad=".$papas[$i]["cod_padre"],"",$conn);
-    //print_r($hijos_entidad_serie);die();
+    $hijos_entidad_serie = busca_filtro_tabla("serie_idserie","entidad_serie","estado=1 AND entidad_identidad='2' AND llave_entidad=".$papas[$i]["id$tabla"],"",$conn);
+    print_r($hijos_entidad_serie);die();
     echo("<item style=\"font-family:verdana; font-size:7pt;color:blue;\" ");
     $cadena_codigo='';
     if(@$papas[$i]["codigo"]){
@@ -140,7 +140,7 @@ if($papas["numcampos"]){
     
     if(@$_REQUEST['uid']){
         if($hijos_entidad_serie['numcampos']){
-            llena_entidad_serie($papas[$i]["cod_padre"],implode(',',extrae_campo($hijos_entidad_serie,'serie_idserie')));
+            llena_entidad_serie($papas[$i]["id$tabla"],implode(',',extrae_campo($hijos_entidad_serie,'serie_idserie')));
         }
     	if(!$_REQUEST["id"]){
     	    llena_serie($papas[$i]["id$tabla"]);
