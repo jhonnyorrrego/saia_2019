bf8505e4d7fc656951efe1cdfd03de7d0123fe05
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-27 16:57:05 +0000

    [Expediente][ruben.pulgarin]nuevo desarrollo arbol entidad serie

--- test_dependencia_serie.php
+++ test_dependencia_serie.php
@@ -188,8 +188,8 @@ function llena_entidad_serie($iddependencia,$series){
 
 //SERIES SIN ASIGNAR (ssa)
 function series_sin_asignar(){
-	global $conn;
-	$series=busca_filtro_tabla("","serie a left join entidad_serie b ON a.idserie=b.serie_idserie AND b.entidad_identidad =2","b.serie_idserie IS NULL AND a.categoria<>3","nombre asc",$conn);
+	global $conn,$activo;
+	$series=busca_filtro_tabla("","serie a left join entidad_serie b ON a.idserie=b.serie_idserie AND b.entidad_identidad =2","b.serie_idserie IS NULL AND a.categoria<>3".$activo,"nombre asc",$conn);
 	for($i=0;$i<$series["numcampos"];$i++){
 		echo("<item style=\"font-family:verdana; font-size:7pt;\" text=\"".htmlspecialchars($series[$i]["nombre"])."(".$series[$i]["codigo"].")\" id=\"d"."-".$series[$i]["idserie"]."\" child=\"0\">\n");
 		echo("</item>\n");
