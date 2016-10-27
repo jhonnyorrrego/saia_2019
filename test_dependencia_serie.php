0f12318699c01017da85d846837d957ecd7ec571
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-27 20:52:27 +0000

    [Expediente][ruben.pulgarin]nuevo desarrollo arbol entidad serie

--- test_dependencia_serie.php
+++ test_dependencia_serie.php
@@ -165,9 +165,10 @@ if($papas["numcampos"]){
 	if(@$_REQUEST["arbol_series"]){		
 				
 	}		
-	else if($hijos[0]["cant"]!=0 && (@$_REQUEST["sin_padre"]) ){		
+	else if(@$_REQUEST["sin_padre"]){		
       echo(" nocheckbox=\"1\" ");		
 	}
+	
     if(in_array($papas[$i]["id$tabla"],$seleccionado)!==false){
         echo " checked=\"1\" ";  
     }
