354e4d455f784abf6fb5c4ca2079dea39d4a184a
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-28 15:14:36 +0000

    [Expediente][ruben.pulgarin]cargar codigo dependencia, codigo series y permitir ingresar consecutivo campo codigo numero

--- pantallas/expediente/editar_expediente.php
+++ pantallas/expediente/editar_expediente.php
@@ -435,8 +435,10 @@ if($dato_padre["numcampos"]){
       var codigo_serie=vector_text[vector_text.length-1].substring(0,vector_text[vector_text.length-1].length-2);
       	if(tree3.isItemChecked(nodeId)){ //checkqueado
       	    $('[name="codigo_numero_serie"]').val(codigo_serie);
+      	    $('[name="codigo_numero_serie"]').trigger('keyup');
       	}else{ //unchecked
       	    $('[name="codigo_numero_serie"]').val('');
+      	    $('[name="codigo_numero_serie"]').trigger('keyup');
       	}      
       
     }
