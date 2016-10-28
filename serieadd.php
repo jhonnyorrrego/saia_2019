cdfc9c959d071ad2be2db5a3f3e2ca9aa070cd33
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-28 23:05:31 +0000

    [Expediente][ruben.pulgarin]desarrollo nuevos cambios series

--- serieadd.php
+++ serieadd.php
@@ -308,14 +308,17 @@ $(document).ready(function(){
         		        case 1:
         		            $('#x_tipo1').attr('disabled','disabled');
         		            tree2.deleteItem('3-categoria-Otras categorias');
-		    tree2.deleteItem('1-categoria-Comunicaciones Oficiales');
-		    tree2.deleteItem('2-categoria-Produccion Documental');
+		                    tree2.deleteItem('1-categoria-Comunicaciones Oficiales');
+		                    tree2.deleteItem('2-categoria-Produccion Documental');
         		             $('#x_tipo2').click(); 
         		            break;
         		        case 2:
-
+                            tree2.deleteItem('3-categoria-Otras categorias');
+		                    tree2.deleteItem('1-categoria-Comunicaciones Oficiales');
+		                    tree2.deleteItem('2-categoria-Produccion Documental');
                             $('#x_tipo1').attr('disabled','disabled');
         		            $('#x_tipo2').attr('disabled','disabled');
+        		             $('#x_tipo3').click(); 
         		            break;
         		        case 3:
         		            
