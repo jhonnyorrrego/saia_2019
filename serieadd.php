b55d1c29c61431781f36e9c3e6f9031bf1be0486
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-28 19:53:39 +0000

    [Expediente][ruben.pulgarin]desarrollo nuevos cambios series

--- serieadd.php
+++ serieadd.php
@@ -289,8 +289,8 @@ $(document).ready(function(){
     //tree2.setOnClickHandler(onNodeSelect);
       tree2.setOnLoadingStart(cargando_serie);
       tree2.setOnLoadingEnd(fin_cargando_serie);
-      tree2.setXMLAutoLoading("test_serie.php?tabla=serie&admin=1&arbol_series=1");
-      tree2.loadXML("test_serie.php?tabla=serie&admin=1&arbol_series=1");
+      tree2.setXMLAutoLoading("test_serie.php?tabla=serie&admin=1&arbol_series=1&categoria=2");
+      tree2.loadXML("test_serie.php?tabla=serie&admin=1&arbol_series=1&categoria=2");
       
 	  tree2.setOnCheckHandler(onNodeSelect_tree2);
       function onNodeSelect_tree2(nodeId){
