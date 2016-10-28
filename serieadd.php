1ae69b397729e19e7e473a84914e80115843ee81
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-28 23:04:00 +0000

    [Expediente][ruben.pulgarin]desarrollo nuevos cambios series

--- serieadd.php
+++ serieadd.php
@@ -307,7 +307,7 @@ $(document).ready(function(){
         		    switch(parseInt('<?php echo($x_tipo); ?>')){
         		        case 1:
         		            $('#x_tipo1').attr('disabled','disabled');
-        		            
+        		             $('#x_tipo2').click(); 
         		            break;
         		        case 2:
 
@@ -318,7 +318,7 @@ $(document).ready(function(){
         		            
         		            break;
         		    }		            
-		           $('[name="x_tipo"]').click(); 
+		          
 			    });
 			</script>
 			
