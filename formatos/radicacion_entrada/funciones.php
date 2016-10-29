018f90efa2b4e8d449fc8ac0a2d0f3f0f041fcec
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-29 16:25:49 +0000

    [2. RADICACION][ruben.pulgarin]correccion arbol dependencia series formato radicacion entrada

--- formatos/radicacion_entrada/funciones.php
+++ formatos/radicacion_entrada/funciones.php
@@ -903,9 +903,9 @@ function serie_documental_radicacion($idformato,$iddoc){
         });
         
         $('#tipo_origen0').click(function(){
-            tree_serie_idserie.deleteChildItems(0); 
-            tree_serie_idserie.loadXML("<?php echo($ruta_db_superior); ?>test_serie_funcionario2.php?tabla=dependencia&admin=1&dependencia=38&sin_padre=1");
-            tree_serie_idserie.loadXML("<?php echo($ruta_db_superior); ?>test_serie_funcionario2.php?tabla=dependencia&admin=1&sin_padre=1&uid=1477409126024&id=d"+dependencia_principal);
+          //  tree_serie_idserie.deleteChildItems(0); 
+            //tree_serie_idserie.loadXML("<?php echo($ruta_db_superior); ?>test_serie_funcionario2.php?tabla=dependencia&admin=1&dependencia=38&sin_padre=1");
+            //tree_serie_idserie.loadXML("<?php echo($ruta_db_superior); ?>test_serie_funcionario2.php?tabla=dependencia&admin=1&sin_padre=1&uid=1477409126024&id=d"+dependencia_principal);
         });
         
 	});
