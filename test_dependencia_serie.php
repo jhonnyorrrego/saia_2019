e016bca698387fc5d55a6c8c6bacd4439428e94f
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-29 15:28:29 +0000

    [2. RADICACION][ruben.pulgarin]correccion arbol dependencia series formato radicacion entrada

--- test_dependencia_serie.php
+++ test_dependencia_serie.php
@@ -169,10 +169,7 @@ if($papas["numcampos"]){
     }
 	
     echo("text=\"".htmlspecialchars(($papas[$i]["nombre"])).$cadena_codigo." \" id=\"d".$papas[$i]["id$tabla"]."\"");
-	if(@$_REQUEST["arbol_series"]){		
-				
-	}		
-	else if(@$_REQUEST["sin_padre_dependencia"]){		
+    if(@$_REQUEST["sin_padre_dependencia"]){		
       echo(" nocheckbox=\"1\" ");		
 	}
 	
