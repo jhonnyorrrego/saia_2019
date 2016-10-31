86dd23fb2a2a295f58153a7a506d7c8a8eb71872
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-31 14:13:40 +0000

    [Expediente][ruben.pulgarin]prueba test

--- prueba_test_hernando.php
+++ prueba_test_hernando.php
@@ -23,8 +23,6 @@ if(@$_REQUEST['id'] && @$_REQUEST['uid']){
     die();
 }
 
-
-
 $arbol="<tree id=\"0\">\n";
 $arbol.=llena_padres();
 $arbol.="</tree>\n";
@@ -41,33 +39,9 @@ function llena_padres(){
     return($arbol_padres);
 }
 function llena_hijos($idpadre){
-        $numero_nodo_padre=explode('n',$idpadre);
-        $arbol_padres="<item style=\"font-family:verdana; font-size:7pt;\" text=\"Nodo ".$numero_nodo_padre[1].".".($i+1)."\" id=\"".$idpadre.".".($i+1)."\" child=\"1\">\n";
-        $arbol_padres.="</item>\n"; 
-        return($arbol_padres);
+    $numero_nodo_padre=explode('n',$idpadre);
+    $arbol_padres="<item style=\"font-family:verdana; font-size:7pt;\" text=\"Nodo ".$numero_nodo_padre[1].".".($i+1)."\" id=\"".$idpadre.".".($i+1)."\" child=\"1\">\n";
+    $arbol_padres.="</item>\n"; 
+    return($arbol_padres);
 }
-
-/*
-function llena_hijos($padre){
-    global $cantidad_hijos;
-    
-    $arbol_hijos="";
-    for($j=0;$j<$cantidad_hijos;$j++){
-        $arbol_hijos.="<item style=\"font-family:verdana; font-size:7pt;\" text=\"Hijo ".($padre+1).".".($j+1)."\" id=\"".($padre+1)."_hijo_".($j+1)."\" child=\"1\">\n";
-        $arbol_hijos.=llena_subhijos($j);
-        $arbol_hijos.="</item>\n";
-    }    
-    return($arbol_hijos);
-}
-function llena_subhijos($padre){
-    global $cantidad_subhijos;
-    
-    $arbol_hijos="";
-    for($j=0;$j<$cantidad_subhijos;$j++){
-        $arbol_hijos.="<item style=\"font-family:verdana; font-size:7pt;\" text=\"Subhijo ".($padre+1).".".($j+1)."\" id=\"".($padre+1)."_subhijo_".($j+1)."\" child=\"0\">\n";
-        $arbol_hijos.="</item>\n";
-    }    
-    return($arbol_hijos);
-}
-*/
 ?>
