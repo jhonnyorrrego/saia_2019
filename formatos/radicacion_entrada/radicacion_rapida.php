50f81093e7d884a3fc59f150d9b789220cd13431
jheison.vargas <jheison.vargas@cerok.com>
2016-10-31 16:44:39 +0000

    [Radicar entrada][jheison.vargas]Radicacion rapida

--- formatos/radicacion_entrada/radicacion_rapida.php
+++ formatos/radicacion_entrada/radicacion_rapida.php
@@ -183,11 +183,12 @@ if(@$_REQUEST["idcategoria_formato"]){
 			tree_equipos.setOnClickHandler(onNodeSelect);  
 			tree_equipos.setOnLoadingStart(cargando_serie);
             tree_equipos.setOnLoadingEnd(fin_cargando_serie);
-            tree_equipos.enableCheckBoxes(1);
-            tree_equipos.enableRadioButtons(true); 
+            
             tree_equipos.setXMLAutoLoading("<?php echo($ruta_db_superior); ?>test_categoria.php?tipo_radicado=radicacion_entrada<?php echo $adicional; ?>");
       //tree_equipos.enableSmartXMLParsing(true);
 			tree_equipos.loadXML("<?php echo($ruta_db_superior); ?>test_categoria.php?tipo_radicado=radicacion_entrada<?php echo $adicional; ?>");
+			tree_equipos.enableCheckBoxes(1);
+            tree_equipos.enableRadioButtons(true); 
 	    function onNodeSelect(nodeId){
 	     if(nodeId.indexOf('#',0)==-1){
             $('#generar_consecutivo').val(nodeId);
