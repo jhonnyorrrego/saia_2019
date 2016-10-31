81a3353ee68e00432d238fe8c8034e9679135f14
jheison.vargas <jheison.vargas@cerok.com>
2016-10-31 18:38:59 +0000

    [Radicar entrada][jheison.vargas]Radicacion rapida

--- formatos/radicacion_entrada/radicacion_rapida.php
+++ formatos/radicacion_entrada/radicacion_rapida.php
@@ -180,7 +180,8 @@ if(@$_REQUEST["idcategoria_formato"]){
 			tree_equipos=new dhtmlXTreeObject("treeboxbox_tree_equipos","100%","100%",0);
 			tree_equipos.setImagePath("<?php echo $ruta_db_superior;?>imgs/");
 			tree_equipos.enableIEImageFix(true);
-			tree_equipos.setOnClickHandler(onNodeSelect);
+			//tree_equipos.setOnClickHandler(onNodeSelect);
+			tree_equipos.setOnCheckHandler(onNodeSelect);
 			tree_equipos.enableCheckBoxes(1);
             tree_equipos.enableRadioButtons(true);
 			tree_equipos.setOnLoadingStart(cargando_serie);
