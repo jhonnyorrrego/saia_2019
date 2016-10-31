ab2e4c33116736ec20ec778ad28658daab27d00b
alejandro.carvajal <alejandro.carvajal@cerok.com>
2016-10-31 21:49:35 +0000

    [Reportes y Graficos][alejandro.carvajal]reporte mensajeros

--- formatos/radicacion_entrada/adicionales_js.php
+++ formatos/radicacion_entrada/adicionales_js.php
@@ -30,6 +30,8 @@ echo(librerias_notificaciones());
 			    if(checkbox.is(':checked')===true){
 			        mensajero=$(this).attr('mensajero');
 			        registros_seleccionados+=$(this).val()+",";
+			        console.log(mensajero);
+			        console.log(mensajero_temp);
 			        if(mensajero_temp){
 			            if(mensajero_temp!=mensajero){
 			                error=1;
