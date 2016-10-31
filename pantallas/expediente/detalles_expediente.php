bccb948c13c9bc863904f6cf0898bffec27b937b
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-31 23:09:04 +0000

    [Expediente][ruben.pulgarin]guardar historial abrir y cerrar expediente

--- pantallas/expediente/detalles_expediente.php
+++ pantallas/expediente/detalles_expediente.php
@@ -163,8 +163,7 @@ $expediente=busca_filtro_tabla("a.*,".fecha_db_obtener("a.fecha","Y-m-d")." AS f
 		$observaciones_abrir_cerrar='
 		    <tr>
 		       <td style="text-align:center" colspan="2">
-		           <textarea id="observaciones_abrir_cerrar" placeholder="Justificacion de reapertura...">
-		           </textarea>
+		           <textarea id="observaciones_abrir_cerrar"></textarea>
 		       </td>
 		    </tr>
 		
