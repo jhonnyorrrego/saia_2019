f211ce827bdd567342a8b3d54c761cffbad2dff1
jheison.vargas <jheison.vargas@cerok.com>
2016-10-31 14:18:34 +0000

    [Radicar entrada][jheison.vargas]Mensajero por defecto

--- formatos/radicacion_entrada/libreria_reporte_radicacion.php
+++ formatos/radicacion_entrada/libreria_reporte_radicacion.php
@@ -85,7 +85,7 @@ function mostrar_mensajeros_dependencia($idft_destino_radicacion){
     $select="<select class='mensajeros' data-idft='$idft_destino_radicacion' name='responsable_{$idft_destino_radicacion}' style='width:150px;'>";
     
     if($responsable['numcampos']==1){
-            return $responsable[0]['mensajero_ruta'];die();
+            //return $responsable[0]['mensajero_ruta'];die();
             $mensajero=busca_filtro_tabla("concat(nombres,' ',apellidos) AS nombre","vfuncionario_dc","iddependencia_cargo=".$responsable[0]['mensajero_ruta'],"",$conn);
             if($responsable[0]['mensajero_ruta']==$datos[0]['mensajero_encargado']){
                 $select.="<option value='".$responsable[0]['mensajero_ruta']."' selected>".$mensajero[0]['nombre']."</option>";
