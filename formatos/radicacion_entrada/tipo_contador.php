137025fe3d00170633d676a1be31886b2858d3f5
jheison.vargas <jheison.vargas@cerok.com>
2016-10-31 13:20:19 +0000

    [Radicar entrada][jheison.vargas]Cambio archivo tipo_contador

--- formatos/radicacion_entrada/tipo_contador.php
+++ formatos/radicacion_entrada/tipo_contador.php
@@ -22,7 +22,7 @@ if($_REQUEST['tipo_radicacion']==1){
     $tipo="I";
 }
 $fecha=date('Y-m-d');
-$idcontador=array(1=>$fecha."-".$contador[0]['consecutivo']."-".$tipo);
+$idcontador=array(0=>$fecha."-".$contador[0]['consecutivo']."-".$tipo);
 $idcontador=json_encode($idcontador);
 echo ($idcontador); 
  
