346d00a602229a6e1fac808e84e5d854893c7f55
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-31 21:42:18 +0000

    [Expediente][ruben.pulgarin]prueba test

--- prueba_cargar_arbol.php
+++ prueba_cargar_arbol.php
@@ -35,7 +35,7 @@ echo(librerias_notificaciones());
             var seleccionado = tree2.getAllChecked();
             //tree2.loadXML("prueba_test_hernando.php?seleccionado="+seleccionado);
             //tree2.setXMLAutoLoadingBehaviour("prueba_test_hernando.php?seleccionado="+seleccionado);
-            tree2.setXMLAutoLoading("prueba_test_hernando.php?seleccionado="+seleccionado);
+            tree2.setXMLAutoLoading("prueba_test_hernando.php?selecciondo=n1.1.1");
             //tree2.refreshItem(seleccionado);
             //tree2.refreshItems(seleccionado,"prueba_test_hernando.php?seleccionado="+seleccionado);
             //tree2.smartRefreshItem(seleccionado);
