c604a23b345eca4eca14f3d60919c2520bc5829a
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-10-31 14:21:35 +0000

    [Expediente][ruben.pulgarin]prueba test

--- prueba_cargar_arbol.php
+++ prueba_cargar_arbol.php
@@ -47,23 +47,8 @@ echo(librerias_notificaciones());
 			tree2.setXMLAutoLoading("prueba_test_hernando.php");
 			tree2.loadXML("prueba_test_hernando.php");
 			function onNodeSelect(nodeId){
-        var datos=nodeId.split("-");
-        var datos2=nodeId.split("sub");
-        if(datos[1] || datos2[1]){
-            var dato=datos[1];
-            if(datos2[1]){
-                dato=datos2[1];
+
             }
-           parent.serielist.location = "serieview.php?key=" + dato; 
-        }else{    
-            var datos=nodeId.split("d");
-            parent.serielist.location = "asignarserie_entidad.php?tipo_entidad=2&llave_entidad=" + datos[1];
-        }
-            
-            //asignarserie_entidad.php
-            
-        	//notificacion_saia("Esto es una dependencia","error","",2500);
-      }
       function fin_cargando_serie() {
         if (browserType == "gecko" )
            document.poppedLayer =
@@ -89,30 +74,6 @@ echo(librerias_notificaciones());
                eval('document.layers["esperando_serie"]');
         document.poppedLayer.style.display = "";
       }
-        function buscar_nodo(){
-       	$.ajax({
-       		type:'POST',
-       		url: "buscar_test_serie.php",
-       		dataType:"json",
-       		data: {
-       			nombre: $('#stext_serie_idserie').val(),
-       			 tabla: "serie"
-       		},
-       		success: function(data){
-       			$.each(data, function(i, item) {
-       				$.each(item, function(j, value) {
-       					tree2.openItem(value);
-       					if(j==item.length-1){
-       						tree2.selectItem(value);
-       						tree2.focusItem(value);
-       					}
-       				});
-       			});
-					}
-				});
-				tree2.findItem(htmlentities(document.getElementById('stext_serie_idserie').value));
-       }
-	--> 		
 	</script>
 	</body>
 </html>
