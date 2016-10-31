00057684fa6121f61e0b36187c286c91665190ec
jheison.vargas <jheison.vargas@cerok.com>
2016-10-31 17:02:14 +0000

    [Radicar entrada][jheison.vargas]Radicacion rapida

--- test_categoria.php
+++ test_categoria.php
@@ -79,10 +79,10 @@ if($papas["numcampos"])
 		
 		echo("text=\"".htmlspecialchars($papas[$i]["nombre"]).$estado." \" id=\"".$papas[$i]["idcategoria_formato"]."\"");
 		if(in_array($papas[$i]["idcategoria_formato"],$seleccionados)){
-			echo (" checked=\"1\" nocheckbox=\"1\" >");
+			echo (" checked=\"1\" >");
 		}
 		else{
-			echo ("nocheckbox=\"1\" >");
+			echo (" >");
 		}
 	}
     if($hijos[0][0]){
