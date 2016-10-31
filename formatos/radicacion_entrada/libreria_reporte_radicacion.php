fe3d9167c76a0a373a7f609b1a463b5d5df2f541
jheison.vargas <jheison.vargas@cerok.com>
2016-10-31 14:17:31 +0000

    [Radicar entrada][jheison.vargas]Mensajero por defecto

--- formatos/radicacion_entrada/libreria_reporte_radicacion.php
+++ formatos/radicacion_entrada/libreria_reporte_radicacion.php
@@ -86,6 +86,7 @@ function mostrar_mensajeros_dependencia($idft_destino_radicacion){
     
     if($responsable['numcampos']==1){
             return $responsable[0]['mensajero_ruta'];die();
+            $mensajero=busca_filtro_tabla("concat(nombres,' ',apellidos) AS nombre","vfuncionario_dc","iddependencia_cargo=".$responsable[0]['mensajero_ruta'].");
             if($responsable[0]['mensajero_ruta']==$datos[0]['mensajero_encargado']){
                 $select.="<option value='".$responsable[0]['mensajero_ruta']."' selected>".$mensajero[0]['nombre']."</option>";
             }else{
@@ -109,7 +110,7 @@ function mostrar_mensajeros_dependencia($idft_destino_radicacion){
         
         $select.="<option value=''>Seleccione</option>";
         for($i=0;$i<$responsable['numcampos'];$i++){
-            $mensajero=busca_filtro_tabla("concat(nombres,' ',apellidos) AS nombre","vfuncionario_dc","iddependencia_cargo={$responsable[$i]['mensajero_ruta']}");
+            $mensajero=busca_filtro_tabla("concat(nombres,' ',apellidos) AS nombre","vfuncionario_dc","iddependencia_cargo={$responsable[$i]['mensajero_ruta']}","",$conn);
             if($responsable[$i]['mensajero_ruta']==$datos[0]['mensajero_encargado']){
                 $select.="<option value='{$responsable[$i]['mensajero_ruta']}' selected>".$mensajero[0]['nombre']."</option>";
             }else{
