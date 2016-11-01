7c07406d140d8c56222c447cbb2d77287e414840
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-11-01 15:44:21 +0000

    [TRD][ruben.pulgarin]Correccion arbol TIPO DOCUMENTO formato radicacion_entrada

--- formatos/radicacion_entrada/funciones.php
+++ formatos/radicacion_entrada/funciones.php
@@ -858,9 +858,9 @@ function serie_documental_radicacion($idformato,$iddoc){
 	            var dependencia=tree_destino.getParentId(nodeId);
 	            var padre=tree_destino.getParentId(dependencia);
 	            alert(padre);
-	            /*if(!padre){
+	            if(padre==){
 	                padre=dependencia;
-	            }*/
+	            }
 	            
 	            padre=padre.replace("#","");
 	            dependencia=dependencia.replace("#","");
