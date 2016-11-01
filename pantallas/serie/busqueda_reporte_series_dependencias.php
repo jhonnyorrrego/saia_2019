0f0625d8eae3765966ad34fdb3b597544aa6b09a
jheison.vargas <jheison.vargas@cerok.com>
2016-11-01 21:42:50 +0000

    [TRD][jheison.vargas]Bsqueda series dependencias

--- pantallas/serie/busqueda_reporte_series_dependencias.php
+++ pantallas/serie/busqueda_reporte_series_dependencias.php
@@ -92,6 +92,20 @@ global $conn;
         
     <br>
     
+        <div class="container master-container">
+       <form accept-charset="UTF-8" id="kformulario_saia"  method="post" >  
+        <div class="control-group">
+          <label class="string required control-label" for="retencion_gestion">
+			<b>Archivo de central:</b>
+			<input type="hidden" name="bksaiacondicion_s@retencion_gestion" id="bksaiacondicion_s-retencion_gestion" value="=">
+          </label>
+          <div class="controls">
+            <input id="bqsaia_codigo" name="bqsaia_s@retencion_gestion" size="50" type="text">
+            <input type="hidden" name="bqsaiaenlace_s@retencion_gestion" id="bqsaiaenlace_s-retencion_gestion" value="y">
+          </div>
+        </div> 
+        
+    <br>
 
         <div class="row">
           <div class="control-group radio_buttons span4">
