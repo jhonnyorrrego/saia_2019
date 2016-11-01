2da13b6eb593c74435b9c65175084c492354875a
alejandro.carvajal <alejandro.carvajal@cerok.com>
2016-11-01 20:54:47 +0000

    [recepcion][alejandro.carvajal]guarda fecha recepcion

--- formatos/radicacion_entrada/actualizar_recepcion.php
+++ formatos/radicacion_entrada/actualizar_recepcion.php
@@ -14,7 +14,7 @@ include_once($ruta_db_superior."db.php");
 $datos=busca_filtro_tabla('','ft_destino_radicacion','idft_destino_radicacion='.$_REQUEST['idft_destino_radicacion'],'',conn);
 
 if($datos[0]['recepcion']==0){
-$sql="UPDATE ft_destino_radicacion SET recepcion={$_REQUEST['funcionario']}, recepcion_fecha=".fecha_db_almacenar(date("Y-m-d H:i:s"),"Y-m-d H:i:s")." WHERE idft_destino_radicacion={$_REQUEST['idft_destino_radicacion']}";
+$sql="UPDATE ft_destino_radicacion SET recepcion='".$_REQUEST['funcionario']."', recepcion_fecha=".fecha_db_almacenar(date("Y-m-d H:i:s"),"Y-m-d H:i:s")." WHERE idft_destino_radicacion={$_REQUEST['idft_destino_radicacion']}";
 
 phpmkr_query($sql);
 
