4b3d4a62626006dfd68e2fb1c00e5a0c4ff7d719
alejandro.carvajal <alejandro.carvajal@cerok.com>
2016-11-01 20:12:09 +0000

    [Entrada][alejandro.carvajal]generacion de radicado en los items

--- formatos/radicacion_entrada/actualizar_item_destino_radicacion.php
+++ formatos/radicacion_entrada/actualizar_item_destino_radicacion.php
@@ -26,8 +26,8 @@ phpmkr_query($sql1);
 $radicado=busca_filtro_tabla('b.numero, c.idft_destino_radicacion','ft_radicacion_entrada a,documento b,ft_destino_radicacion c','a.documento_iddocumento = b.iddocumento AND a.idft_radicacion_entrada = c.ft_radicacion_entrada AND a.documento_iddocumento='.$_REQUEST['iddoc'],'',conn);
 
 for($i=0;$i<$radicado['numcampos'];$i++){
-    $sql="UPDATE ft_destino_radicacion SET numero_item='".$radicado[$i]['numero'].".".$i+1."' WHERE idft_destino_radicacion=".$radicado[$i]['idft_destino_radicacion'];
-
+    $numero_item=$i+1;
+    $sql="UPDATE ft_destino_radicacion SET numero_item='".$radicado[$i]['numero'].".".$numero_item."' WHERE idft_destino_radicacion=".$radicado[$i]['idft_destino_radicacion'];
     phpmkr_query($sql);
 }
 echo('<script>window.history.back();</script>');
