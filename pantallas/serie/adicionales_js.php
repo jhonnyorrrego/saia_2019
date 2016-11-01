51ef81db63c220041c0c2fa4c7d3a777a86d7be6
jheison.vargas <jheison.vargas@cerok.com>
2016-11-01 20:48:39 +0000

    [TRD][jheison.vargas]Filtro reportes

---
+++ pantallas/serie/adicionales_js.php
@@ -0,0 +1,30 @@
+<?php
+$max_salida=10; // Previene algun posible ciclo infinito limitando a 10 los ../
+$ruta_db_superior=$ruta="";
+while($max_salida>0){
+	if(is_file($ruta."db.php")){
+		$ruta_db_superior=$ruta; //Preserva la ruta superior encontrada
+	}
+	$ruta.="../";
+	$max_salida--;
+}
+
+include_once($ruta_db_superior."db.php");
+include_once($ruta_db_superior."librerias_saia.php");
+echo(librerias_jquery('1.7'));
+echo(librerias_notificaciones());
+
+?>
+<script>
+    $(document).ready(function(){
+        
+        
+        $("#filtro_dependencias").live("change",function(){
+            var filtro_dependencias=$(this).val();
+            window.location.href = "<?php echo $ruta_db_superior;?>pantallas/busquedas/consulta_busqueda_reporte.php?idbusqueda_componente=269&variable_busqueda="+filtro_dependencias;
+            
+        });
+        
+        
+    });
+</script>
