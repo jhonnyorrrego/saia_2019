88eeb70428f4c2da6742ab9d8b1c4935b3f4b9cc
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-11-01 13:28:42 +0000

    [TRD][ruben.pulgarin]Correccion arbol TIPO DOCUMENTO formato radicacion_entrada

--- formatos/radicacion_entrada/funciones.php
+++ formatos/radicacion_entrada/funciones.php
@@ -822,8 +822,8 @@ function serie_documental_radicacion($idformato,$iddoc){
                 function cargar_arbol(){
                 setTimeout(function(){  
                     tree_serie_idserie.deleteChildItems(0); 
-                    tree_serie_idserie.loadXML("<?php echo($ruta_db_superior); ?>test_dependencia_serie.php?tabla=dependencia&admin=1&dependencia=38&mostrar_nodos=dsa&sin_padre_dependencia=1");
-                    tree_serie_idserie.loadXML("<?php echo($ruta_db_superior); ?>test_dependencia_serie.php?tabla=dependencia&sin_padre_dependencia=1&admin=1&mostrar_nodos=dsa&uid=1477409126024&id=d"+dependencia_principal);
+                    tree_serie_idserie.loadXML("<?php echo($ruta_db_superior); ?>test_dependencia_serie.php?tabla=dependencia&admin=1&dependencia=38&mostrar_nodos=dsa&sin_padre_dependencia=1&solo_dependencias=1");
+                    tree_serie_idserie.loadXML("<?php echo($ruta_db_superior); ?>test_dependencia_serie.php?tabla=dependencia&sin_padre_dependencia=1&admin=1&mostrar_nodos=dsa&uid=1477409126024&id=d"+dependencia_principal+"&solo_dependencias=1");
                 }, 1000);
                 }
 	    var cargado=[<?php echo($cargo); ?>];
