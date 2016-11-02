62fc310f3f73e461ce7f4d6c1eae47578fd8ead9
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-11-02 19:48:33 +0000

    [Transferencia documental][ruben.pulgarin]parametrizacion borde tabla

--- formatos/transferencia_doc/funciones.php
+++ formatos/transferencia_doc/funciones.php
@@ -130,7 +130,7 @@ function expedientes_vinculados_funcion($idformato,$iddoc){
             
         $texto.='    
           </tr>
-          <tr border="1">
+          <tr >
             <th '.$estilo_general.'>INICIAL</th>
             <th '.$estilo_general.'>FINAL</th>
             <th '.$estilo_general.'>CAJA</th>
@@ -150,7 +150,7 @@ function expedientes_vinculados_funcion($idformato,$iddoc){
 			if(is_object($expedientes[$i]["fecha_extrema_i"]))$expedientes[$i]["fecha_extrema_i"]=$expedientes[$i]["fecha_extrema_i"]->format('Y-m-d');
 			if(is_object($expedientes[$i]["fecha_extrema_f"]))$expedientes[$i]["fecha_extrema_f"]=$expedientes[$i]["fecha_extrema_f"]->format('Y-m-d');
 			
-			$texto.='<tr id="tr_'.$expedientes[$i]["idexpediente"].'" border="1">
+			$texto.='<tr id="tr_'.$expedientes[$i]["idexpediente"].'" >
 			<td style="text-align:center">'.($i+1).'</td>
 			<td>'.$expedientes[$i]["codigo_numero"].'</td>
 			<td>'.$expedientes[$i]["nombre"].'</td>
