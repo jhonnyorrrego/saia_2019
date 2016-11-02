9ca9f15f27493dbfccc842fc168f85be584297d1
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-11-02 15:36:02 +0000

    [Transferencia documental][ruben.pulgarin]no esta cambiando los expedientes al nuevo estado

--- formatos/transferencia_doc/funciones.php
+++ formatos/transferencia_doc/funciones.php
@@ -1,4 +1,17 @@
 <?php
+$max_salida = 10; // Previene algun posible ciclo infinito limitando a 10 los ../
+$ruta_db_superior = $ruta = "";
+while ($max_salida > 0) {
+  if (is_file($ruta . "db.php")) {
+    $ruta_db_superior = $ruta; //Preserva la ruta superior encontrada
+  }
+  $ruta.="../";
+  $max_salida--;
+}
+include_once($ruta_db_superior."db.php");
+
+
+
 function validacion_js_transferencia($idformato,$iddoc){
 	global $conn;
 	?>
