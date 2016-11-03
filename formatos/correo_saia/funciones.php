265cd933211eefdb3ccfa8f637c0aa0a09c04478
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-11-03 13:24:47 +0000

    [Email][ruben.pulgarin]correccion de y para

--- formatos/correo_saia/funciones.php
+++ formatos/correo_saia/funciones.php
@@ -21,7 +21,6 @@ function recibir_datos($idformato, $iddoc){
 	
 	
 	$datos_correo = json_decode($_REQUEST['datos_correo']);
-	print_r($datos_correo);
 	$asunto = $datos_correo->asunto;
 	$contenido = $datos_correo->contenido;
 	$de = $datos_correo->from;
