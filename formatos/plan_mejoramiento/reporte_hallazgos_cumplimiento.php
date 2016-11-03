6cc3e17a835f3bbc04d0d9508321d77b3b2cb58c
alejandro.carvajal <alejandro.carvajal@cerok.com>
2016-11-03 18:52:16 +0000

    [reporte][alejandro.carvajal]cambio reporte

--- formatos/plan_mejoramiento/reporte_hallazgos_cumplimiento.php
+++ formatos/plan_mejoramiento/reporte_hallazgos_cumplimiento.php
@@ -32,7 +32,7 @@ $style = "
        padding:10px; 
        text-align: left;	
        } 
-       .encabezado_list { 
+       .encabezado { 
        background-color:".$config[0]["valor"]."; 
        color:white ; 
        vertical-align:middle;
@@ -90,7 +90,7 @@ function reporte(){
 	
 	$tabla.='<table style="font-size:7pt;border-collapse:collapse; font-family:verdana;" border="1px">';
 	if($hallazgos["numcampos"]){
-		$tabla.='<tr class="encabezado_list">';
+		$tabla.='<tr class="encabezado">';
 		if($_REQUEST["idformato"]!=''){
 			$tabla.='<td style="width:3%"></td>';
 		}
