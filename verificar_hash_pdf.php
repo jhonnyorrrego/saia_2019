86f37178efa3361377d2e8436a161c4e5ddaf011
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-11-04 19:47:04 +0000

    [PDF][ruben.pulgarin]desarrollo validar hash de pdf

--- verificar_hash_pdf.php
+++ verificar_hash_pdf.php
@@ -9,6 +9,11 @@ while($max_salida>0){
     $max_salida--;
 }
 include_once($ruta_db_superior."db.php");
+include_once($ruta_db_superior."librerias_saia.php");
+echo(librerias_jquery('1.7'));
+echo( estilo_bootstrap() );
+
+
 
 
 
