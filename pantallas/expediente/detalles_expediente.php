a092c2c948c5059e4b39bc769ba45aeb1521622c
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-11-08 20:33:25 +0000

    [Expediente][ruben.pulgarin]desarrollo semaforo expediente cerrado

--- pantallas/expediente/detalles_expediente.php
+++ pantallas/expediente/detalles_expediente.php
@@ -264,7 +264,7 @@ if($expediente[0]["estado_cierre"]==2){  //si esta cerrado
         $cadena_inicial='Faltan ';
         $color='green';
         if($interval_mes<2 && $interval_anio==0){
-            $color='yellow';
+            $color='orange';
         }
         
         if($interval_pos_neg==1){
