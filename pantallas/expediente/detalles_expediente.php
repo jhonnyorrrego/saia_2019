ba71a339688ff194d81fdd8ae7ec9407294fc140
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-11-08 13:50:54 +0000

    [Expediente][ruben.pulgarin]desarrollo agrupador

--- pantallas/expediente/detalles_expediente.php
+++ pantallas/expediente/detalles_expediente.php
@@ -66,7 +66,9 @@ $expediente=busca_filtro_tabla("a.*,".fecha_db_obtener("a.fecha","Y-m-d")." AS f
   
   <?php
       if($expediente[0]["agrupador"]){
-          
+        
+        echo('</table></div>');
+        die();
       }
   
   ?>
