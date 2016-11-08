e19af7e5d803416798bac2ca1735779c4693701c
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-11-08 22:02:57 +0000

    [Expediente][ruben.pulgarin]desarrollo transferencia documental cajas

--- formatos/transferencia_doc/funciones.php
+++ formatos/transferencia_doc/funciones.php
@@ -51,7 +51,7 @@ function guardar_expedientes_add($idformato,$iddoc){
 		<input type='hidden' name='expediente_vinculado' id='expediente_vinculado' value='".$ids."'>
 		</td>";
 	}else if(@$_REQUEST['id_caja']){
-	    $ids_caja=@$_REQUEST['id_caja'];
+	    $ids_caja=$_REQUEST['id_caja'];
         $caja=busca_filtro_tabla("no_consecutivo","caja","idcaja IN(".$ids_caja.")","",$conn);
 		$etiquetas=extrae_campo($expedientes,"no_consecutivo","");
 		$texto.="<td><ul><li>".implode("</li><li>",$etiquetas)."</li></ul>
