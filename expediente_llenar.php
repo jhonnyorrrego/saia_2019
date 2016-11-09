a8616c0c7dbd6352b7057410b4a779ffc7765d06
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-11-09 18:43:14 +0000

    [Expediente][ruben.pulgarin]desarrollo fecha limite respuesta

--- expediente_llenar.php
+++ expediente_llenar.php
@@ -126,7 +126,8 @@ if(count($nombres_exp)){
     <label class="control-label" for="fecha_limite"> Fecha Limite de Respuesta
   </label>
   <div class="controls">
-      <input id="fecha_limite" name="fecha_limite" style="width:100px" type="text" value="0000-00-00" ><input type="button" onclick="document.getElementById('fecha_limite').value='0000-00-00'" />
+      <input type="button" onclick="document.getElementById('fecha_limite').value='0000-00-00'" value="Limpiar"/>
+      <input id="fecha_limite" name="fecha_limite" style="width:100px" type="text" value="0000-00-00" >
       <?php selector_fecha("fecha_limite","form1","Y-m-d",date("m"),date("Y"),"default.css","",""); ?>
   </div>
 </div>
