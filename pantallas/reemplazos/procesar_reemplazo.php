f92dc1c0fd3e761067d48cdf02a2a066554c2760
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-11-11 22:01:34 +0000

    [Expediente][ruben.pulgarin]desarrollo reemplazo expedientes

--- pantallas/reemplazos/procesar_reemplazo.php
+++ pantallas/reemplazos/procesar_reemplazo.php
@@ -313,8 +313,9 @@ function inactivar_reemplazo_expedientes($idreemplazo_saia){
     $idreemplazo_expedientes=$reemplazo_expedientes[0]['idreemplazo_expediente'];
     
     $sql3="UPDATE reemplazo_expediente SET estado=0 WHERE idreemplazo_expediente=".$idreemplazo_expedientes;
-    //phpmkr_query($sql3);
+    phpmkr_query($sql3);
     $sql4="UPDATE entidad_expediente SET  llave_entidad=".$idfuncionario_antiguo[0]['idfuncionario']." WHERE identidad_expediente IN(".$identidad_expediente.")";
+    phpmkr_query($sql4);
 }
 
 function insertar_reemplazo_expediente($idreemplazo_saia){
