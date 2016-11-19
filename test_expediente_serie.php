5ef3aa4c579664541e284584b07e71ffcd538d86
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-11-19 15:27:57 +0000

    [Expediente][ruben.pulgarin]desarrollo test_expediente_serie

--- test_expediente_serie.php
+++ test_expediente_serie.php
@@ -132,7 +132,7 @@ if($papas["numcampos"]){
 
 
 if(@$_REQUEST['uid'] || @$_REQUEST['id'] ){
-    
+    if($_REQUEST['id']==$id){
         $hijos_entidad_serie = busca_filtro_tabla("serie_idserie","expediente","idexpediente=".$_REQUEST['id'],"",$conn);
         
         if($hijos_entidad_serie['numcampos']){
@@ -143,7 +143,7 @@ if(@$_REQUEST['uid'] || @$_REQUEST['id'] ){
             
             llena_entidad_serie($_REQUEST['id'],$lista_entidad_series_filtrar);
         }
-      
+    }  
 }
 
 
