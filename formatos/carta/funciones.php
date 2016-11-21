1d3c5a90d5f00f86da5775ef825287954fe2cc25
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-11-21 20:15:14 +0000

    [Expediente][ruben.pulgarin]desarrollo incluir arbol expediente_serie al formato comunicacion externa

--- formatos/carta/funciones.php
+++ formatos/carta/funciones.php
@@ -767,7 +767,6 @@ function vincular_expediente_serie_carta($idformato,$iddoc){ //POSTERIOR AL APRO
     
     $datos=busca_filtro_tabla("expediente_serie,documento_iddocumento","ft_carta","documento_iddocumento=".$iddoc,"",$conn);
     $vinculado=busca_filtro_tabla("","expediente_doc","documento_iddocumento=".$datos[0]['documento_iddocumento']." AND expediente_idexpediente=".$datos[0]['expediente_serie'],"",$conn);
-    
     if(!$vinculado['numcampos']){
         $sql="INSERT INTO expediente_doc (expediente_idexpediente,documento_iddocumento,fecha) VALUES (".$datos[0]['expediente_serie'].",".$datos[0]['documento_iddocumento'].",".fecha_db_almacenar(date("Y-m-d H:i:s"),'Y-m-d H:i:s').")";
         phpmkr_query($sql);
