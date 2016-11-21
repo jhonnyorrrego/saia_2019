583eec5ed5fc777ace29f3a10db84eddd81aaf0e
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-11-21 20:14:56 +0000

    [Expediente][ruben.pulgarin]desarrollo incluir arbol expediente_serie al formato comunicacion interna

--- formatos/memorando/funciones.php
+++ formatos/memorando/funciones.php
@@ -300,6 +300,16 @@ function parsear_arbol_expediente_serie_memorando(){
     </script>
     <?php    
 }
-
+function vincular_expediente_serie_memorando($idformato,$iddoc){ //POSTERIOR AL APROBAR
+    global $conn,$ruta_db_superior;
+    
+    $datos=busca_filtro_tabla("expediente_serie,documento_iddocumento","ft_memorando","documento_iddocumento=".$iddoc,"",$conn);
+    $vinculado=busca_filtro_tabla("","expediente_doc","documento_iddocumento=".$datos[0]['documento_iddocumento']." AND expediente_idexpediente=".$datos[0]['expediente_serie'],"",$conn);
+    
+    if(!$vinculado['numcampos']){
+        $sql="INSERT INTO expediente_doc (expediente_idexpediente,documento_iddocumento,fecha) VALUES (".$datos[0]['expediente_serie'].",".$datos[0]['documento_iddocumento'].",".fecha_db_almacenar(date("Y-m-d H:i:s"),'Y-m-d H:i:s').")";
+        phpmkr_query($sql);
+    }    
+}
 
 ?>
