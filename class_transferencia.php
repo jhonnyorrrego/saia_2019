65019689d2ac7b597b5f689a78172b70d66895b0
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-11-22 17:06:03 +0000

    [Acciones del documento][ruben.pulgarin]correccion redireccionando documentos

--- class_transferencia.php
+++ class_transferencia.php
@@ -773,7 +773,7 @@ function aprobar($iddoc=0,$url="")
   }
   
   //pantallas/documento/informacion_resumen_documento.php?form_info=idformato=1&iddoc=851&alto_pantalla=634
-  echo abrir_url($ruta_db_superior.'pantallas/documento/informacion_resumen_documento.php?form_info=idformato='.$tipo_radicado[0]["idformato"].'&iddoc='.$iddoc,'centro');
+  echo abrir_url($ruta_db_superior.'pantallas/documento/informacion_resumen_documento.php?form_info=idformato='.$tipo_radicado[0]["idformato"].'&iddoc='.$iddoc,'formato_detalles');
   
 /*if(strpos($_SERVER["PHP_SELF"],"meses")<=0)
    {
