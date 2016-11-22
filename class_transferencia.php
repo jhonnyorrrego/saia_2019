58a4f9210f7146fe2f3dbb4bbf0b589352371891
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-11-22 19:41:17 +0000

    [Acciones del documento][ruben.pulgarin]correccion redireccionando documentos

--- class_transferencia.php
+++ class_transferencia.php
@@ -769,6 +769,7 @@ function aprobar($iddoc=0,$url="")
 	}
   llama_funcion_accion($iddoc,$tipo_radicado[0]["idformato"],"confirmar","POSTERIOR");
   if($aprobar_posterior){
+      print_r('por aqui entro');die();
       llama_funcion_accion($iddoc,$tipo_radicado[0]["idformato"],"aprobar","POSTERIOR");
   }
   
