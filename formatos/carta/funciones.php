5ea7779b7dd104653b7f4320aea37ad9b52e09aa
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-11-22 18:44:22 +0000

    [Acciones del documento][ruben.pulgarin]correccion redireccionando documentos

--- formatos/carta/funciones.php
+++ formatos/carta/funciones.php
@@ -766,6 +766,8 @@ function vincular_expediente_serie_carta($idformato,$iddoc){ //POSTERIOR AL APRO
     global $conn,$ruta_db_superior;
     
     $datos=busca_filtro_tabla("expediente_serie,documento_iddocumento","ft_carta","documento_iddocumento=".$iddoc,"",$conn);
+    print_r($datos);die();
+    
     $vinculado=busca_filtro_tabla("","expediente_doc","documento_iddocumento=".$datos[0]['documento_iddocumento']." AND expediente_idexpediente=".$datos[0]['expediente_serie'],"",$conn);
     if(!$vinculado['numcampos']){
         $sql="INSERT INTO expediente_doc (expediente_idexpediente,documento_iddocumento,fecha) VALUES (".$datos[0]['expediente_serie'].",".$datos[0]['documento_iddocumento'].",".fecha_db_almacenar(date("Y-m-d H:i:s"),'Y-m-d H:i:s').")";
