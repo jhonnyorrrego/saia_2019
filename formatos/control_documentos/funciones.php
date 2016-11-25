00afe717608fc98ae7f3b74ccf2a4a79662b49e9
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-11-25 14:03:53 +0000

    [M1][ruben.pulgarin]desarrollo versionamiento bases de calidad

--- formatos/control_documentos/funciones.php
+++ formatos/control_documentos/funciones.php
@@ -95,6 +95,7 @@ function validar_tipo_documento($idformato, $iddoc){
 		    }else{  //desarrollo bases de calidad
 		        $('#nombre_documento').parent().parent().hide(); $('#nombre_documento').removeClass('required');
 		        $('#listado_procesos').parent().parent().hide(); $('#listado_procesos').removeClass('required');
+		        $('#documento_calidad').parent().parent().hide(); $('#documento_calidad').removeClass('required');
 		       
 		        
 		    }
