4cb9577c20b8cf91c981732375080672d2a2700a
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-12-01 22:56:47 +0000

    [Expediente][ruben.pulgarin]desarrollo ver historial apertura y cierre

--- pantallas/expediente/detalles_expediente.php
+++ pantallas/expediente/detalles_expediente.php
@@ -48,6 +48,14 @@ $expediente=busca_filtro_tabla("a.*,".fecha_db_obtener("a.fecha","Y-m-d")." AS f
     }
     
 ?>    
+
+<script type="text/javascript" src="<?php echo $ruta_db_superior;?>anexosdigitales/highslide-4.0.10/highslide/highslide-with-html.js"></script>
+<link rel="stylesheet" type="text/css" href="<?php echo $ruta_db_superior;?>anexosdigitales/highslide-4.0.10/highslide/highslide.css" />
+<script type='text/javascript'>
+   hs.graphicsDir = '<?php echo $ruta_db_superior;?>anexosdigitales/highslide-4.0.10/highslide/graphics/';
+   hs.outlineType = 'rounded-white';
+</script> 
+
     
 <div class="container"> 
 <div data-toggle="collapse" data-target="#div_info_expediente" style="cursor:pointer;">
@@ -149,12 +157,6 @@ $expediente=busca_filtro_tabla("a.*,".fecha_db_obtener("a.fecha","Y-m-d")." AS f
   		    <button class='btn btn-mini btn-default cambiar_responsable_expediente'>
   		        <i class='icon-user' title='Cambiar Responsable'></i>
   		    </button>
- 		<script type="text/javascript" src="<?php echo $ruta_db_superior;?>anexosdigitales/highslide-4.0.10/highslide/highslide-with-html.js"></script>
-		 <link rel="stylesheet" type="text/css" href="<?php echo $ruta_db_superior;?>anexosdigitales/highslide-4.0.10/highslide/highslide.css" />
-		 <script type='text/javascript'>
-		   hs.graphicsDir = '<?php echo $ruta_db_superior;?>anexosdigitales/highslide-4.0.10/highslide/graphics/';
-		   hs.outlineType = 'rounded-white';
-		</script> 		    
   		    <script>
   		        $(document).ready(function(){
   		            $('.cambiar_responsable_expediente').click(function(){
@@ -233,8 +235,16 @@ $expediente=busca_filtro_tabla("a.*,".fecha_db_obtener("a.fecha","Y-m-d")." AS f
     			    <br>
     			    <br>
                       <button class='btn btn-mini btn-default '>
-  		                <i class='icon-globe' title='Ver Historial de Cambio'></i>
-  		              </button>    			    
+  		                <i class='icon-globe historial_abrir_cerrar' title='Ver Historial de Cambio'></i>
+  		              </button>    	
+              		    <script>
+              		        $(document).ready(function(){
+              		            $('.historial_abrir_cerrar').click(function(){
+                                     var enlace='<?php echo($ruta_db_superior); ?>pantallas/expediente/historial_abrir_cerrar.php?idexpediente=<?php echo($idexpediente); ?>';
+                                    hs.htmlExpand(this, { objectType: 'iframe',width: 400, height: 200,contentId:'cuerpo_paso', 		preserveContent:false, src:enlace,outlineType: 'rounded-white',wrapperClassName:'highslide-wrapper drag-header'});
+              		            });
+              		        });
+              		    </script>  		              
     			</td>
     		</tr>	
     	</table>
