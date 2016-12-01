99a809c118e173273004fc30837f689858b10035
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-12-01 23:01:27 +0000

    [Expediente][ruben.pulgarin]desarrollo ver historial apertura y cierre

--- pantallas/expediente/historial_abrir_cerrar.php
+++ pantallas/expediente/historial_abrir_cerrar.php
@@ -1,4 +1,4 @@
-<img src="http://www.sencico.gob.pe/info/sencicoweb/media/img483.jpg">
+<img src="http://www.sencico.gob.pe/info/sencicoweb/media/img483.jpg" width="200" height="200">
 
 <?php 
 die();
