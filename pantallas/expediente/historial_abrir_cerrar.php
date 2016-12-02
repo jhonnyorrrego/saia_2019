d5f109fade3b43492f095e8d8f61d3c7243469d1
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-12-02 13:21:47 +0000

    [Expediente][ruben.pulgarin]desarrollo ver historial apertura y cierre

--- pantallas/expediente/historial_abrir_cerrar.php
+++ pantallas/expediente/historial_abrir_cerrar.php
@@ -1,10 +1,31 @@
-<img src="http://www.sencico.gob.pe/info/sencicoweb/media/img483.jpg" width="300" height="150">
-
 <?php 
-die();
-
-
-
-
-
+$max_salida=10; // Previene algun posible ciclo infinito limitando a 10 los ../
+$ruta_db_superior=$ruta="";
+while($max_salida>0){
+	if(is_file($ruta."db.php")){
+		$ruta_db_superior=$ruta; //Preserva la ruta superior encontrada
+	}
+	$ruta.="../";
+	$max_salida--;
+}
+include_once($ruta_db_superior."db.php");
+include_once($ruta_db_superior."librerias_saia.php");
+echo(estilo_bootstrap()); 
 ?>
+<html>
+    <head>
+        <title>Historial de Apertura y Cierre de Expedientes</title>
+    </head>
+    <body>
+        <table class="table table-bordered">
+          <tr>
+            <td width="40%" class="prettyprint">
+              <b>Nombre del <?php echo($etiqueta_expediente); ?>:</b>
+            </td> 
+            <td>
+                
+            </td>
+        </tr>
+        </table>
+    </body>
+</html>
