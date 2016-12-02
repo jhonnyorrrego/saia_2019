485c2570141306e4991b3c7f063c1cb69a9b20d3
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-12-02 13:30:16 +0000

    [Expediente][ruben.pulgarin]desarrollo ver historial apertura y cierre

--- pantallas/expediente/historial_abrir_cerrar.php
+++ pantallas/expediente/historial_abrir_cerrar.php
@@ -16,13 +16,7 @@ echo(estilo_bootstrap());
     <head>
         <title>Historial de Apertura y Cierre de Expedientes</title>
     </head>
-    
     <body>
-        <style>
-            tr th{
-                text-align:center;
-            }
-        </style>
         <table class="table table-bordered">
             <tr>
                 <th class="prettyprint">
@@ -40,6 +34,5 @@ echo(estilo_bootstrap());
             </tr>
             
         </table>
-
     </body>
 </html>
