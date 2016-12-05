4b7f574ab351208edd6231bd572b00997eeed018
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-12-05 15:34:44 +0000

    [Planes de Mejoramiento][ruben.pulgarin]correccion no muestra plan de mejoramiento al generar pdf desde informe seguimiento

--- class_impresion.php
+++ class_impresion.php
@@ -352,7 +352,7 @@ class Imprime_Pdf {
     	
     		}
 		}
-		print_r($direccion);die();
+		//print_r($direccion);die();
 		foreach($direccion as $fila) {
 			
 			$fila .= "&font_size=" . $this->font_size;
@@ -373,7 +373,7 @@ class Imprime_Pdf {
 			
 		    
 	
-		print_r('inicio---->'.$contenido);die("<----fin");
+		//print_r('inicio---->'.$contenido);die("<----fin");
 			
 			$this->pdf->writeHTML(stripslashes($contenido), true, true, false, false, '');
 		}
