8381886c156db0da33dab101cb546df29c408c36
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-12-05 15:21:10 +0000

    [Planes de Mejoramiento][ruben.pulgarin]correccion no muestra plan de mejoramiento al generar pdf desde informe seguimiento

--- formatos/informe_contraloria/funciones.php
+++ formatos/informe_contraloria/funciones.php
@@ -82,6 +82,8 @@ function logo_contraloria() {
 
 function listar_hallazgo_informe($idformato, $iddoc, $condicion = "") {
 	global $conn,$ruta_db_superior;
+	
+	$texto='';
 	$formato = busca_filtro_tabla("", "formato A", "A.idformato=" . $idformato, "", $conn);
 	if ($formato["numcampos"]) {
 		$documento = busca_filtro_tabla("", $formato[0]["nombre_tabla"], "documento_iddocumento=" . $iddoc, "", $conn);
