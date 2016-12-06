13bb9217a9fc8abf629bd7d02ca793083f94590d
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-12-06 14:09:44 +0000

    [Admin. Flujos][ruben.pulgarin]correccion scroll en el flujo

--- bpmn/bpmn/funciones.php
+++ bpmn/bpmn/funciones.php
@@ -2,13 +2,13 @@
 $(document).ready(function(){
 	$(".habilitado").live("click",function(){
 		if($(this).hasClass("task") || $(this).hasClass("task_ok") || $(this).hasClass("task_iniciada") || $(this).hasClass("task_error")){
-			llamado_bpmn("<?php echo($ruta_db_superior);?>bpmn/tarea_usuario.php?idbpmn=<?php echo($_REQUEST['idbpmn'])?>&idfigura="+$(this).attr("data-activity-id")+"&vista_bpmn=<?php echo($_REQUEST['vista_bpmn']);?>&idbpmni=<?php echo($_REQUEST['idbpmni']);?>",590,600);
+			llamado_bpmn("<?php echo($ruta_db_superior);?>bpmn/tarea_usuario.php?idbpmn=<?php echo($_REQUEST['idbpmn'])?>&idfigura="+$(this).attr("data-activity-id")+"&vista_bpmn=<?php echo($_REQUEST['vista_bpmn']);?>&idbpmni=<?php echo($_REQUEST['idbpmni']);?>",590,400);
 		}
 		if($(this).hasClass("startevent")){
 
 		}
 		if($(this).hasClass("exclusivegateway")){
-			llamado_bpmn("<?php echo($ruta_db_superior);?>bpmn/condicional.php?idbpmn=<?php echo($_REQUEST['idbpmn'])?>&idcondicional="+$(this).attr("data-activity-id")+"&tipo_evento=condicional&vista_bpmn=<?php echo($_REQUEST['vista_bpmn']);?>&idbpmni=<?php echo($_REQUEST['idbpmni']);?>",590,600);
+			llamado_bpmn("<?php echo($ruta_db_superior);?>bpmn/condicional.php?idbpmn=<?php echo($_REQUEST['idbpmn'])?>&idcondicional="+$(this).attr("data-activity-id")+"&tipo_evento=condicional&vista_bpmn=<?php echo($_REQUEST['vista_bpmn']);?>&idbpmni=<?php echo($_REQUEST['idbpmni']);?>",590,300);
 		}	
 		if($(this).hasClass("intermediatethrowevent") || $(this).hasClass("endevent")){		
 
