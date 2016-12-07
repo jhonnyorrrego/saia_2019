c799a002d03a2c034d0036bdbbe3c5b276332517
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-12-07 23:16:56 +0000

    [Informacin del documento][ruben.pulgarin]correccion redireccionando documentos

--- class_transferencia.php
+++ class_transferencia.php
@@ -790,7 +790,7 @@ function aprobar($iddoc=0,$url="")
              window.parent.frames[0].location=vector[0]+'&'+vector[1]+'&seleccionar=".$datos_formato[0]["idformato"]."-".$tabla_padre[0]["nombre_tabla"]."-".$datos_formato[0]["nombre_tabla"]."-".$iddoc."';
             </script>";
           }else{
-              //echo abrir_url($ruta_db_superior.'pantallas/documento/informacion_resumen_documento.php?iddoc='.$iddoc,'formato_detalles');
+              /*//echo abrir_url($ruta_db_superior.'pantallas/documento/informacion_resumen_documento.php?iddoc='.$iddoc,'formato_detalles');
               $ruta1=$ruta_db_superior.'pantallas/documento/informacion_resumen_documento.php?iddoc='.$iddoc;
               $ruta2=$ruta_db_superior.'pantallas/documento/informacion_resumen_documento.php?iddoc='.$iddoc;
               echo('
@@ -806,7 +806,8 @@ function aprobar($iddoc=0,$url="")
                   </script>
               
               ');
-              die();
+              die();*/
+              echo abrir_url($ruta_db_superior.'pantallas/documento/informacion_resumen_documento.php?iddoc='.$iddoc,'arbol_formato');
               /*
               if( document.getElementById( "formato_detalles" ) ){ //arbol_formato
                   echo abrir_url($ruta_db_superior.'pantallas/documento/informacion_resumen_documento.php?iddoc='.$iddoc,'formato_detalles');
