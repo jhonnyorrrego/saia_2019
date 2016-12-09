d46646d66eed2fc383dde4a5b4630c6fd6f937a1
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-12-09 14:15:27 +0000

    [M1][ruben.pulgarin]correccion no muestra planes relacionados

--- formatos/indicadores_calidad/planes_relacionados.php
+++ formatos/indicadores_calidad/planes_relacionados.php
@@ -11,12 +11,13 @@ if(isset($_REQUEST["seguimiento_indicador"])&&$_REQUEST["seguimiento_indicador"]
    echo "<br /><br />No existen planes relacionados con el seguimiento.";
  elseif($planes["numcampos"]==1)
    {
-       redirecciona('../../ordenar.php?accion=mostrar&mostrar_formato=1&key='.$planes[0][0]);
        
-   /*echo "<script>
-          //window.parent.hs.close();
-          window.open('../../ordenar.php?accion=mostrar&mostrar_formato=1&key=".$planes[0][0]."','_blank');
-          </script>";  */
+       
+   echo "<script>
+          
+          parent.window.open('../../ordenar.php?accion=mostrar&mostrar_formato=1&key=".$planes[0][0]."','_blank');
+          window.parent.hs.close();
+          </script>";  
    }
  else
    {echo "<B><br /><br />PLANES DE MEJORAMIENTO RELACIONADOS CON EL SEGUIMIENTO</B><br /><br /><br />
