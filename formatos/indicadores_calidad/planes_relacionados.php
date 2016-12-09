f9e2298910f9b625f4e9fbbf7730a49be317ace2
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-12-09 14:16:39 +0000

    [M1][ruben.pulgarin]correccion no muestra planes relacionados

--- formatos/indicadores_calidad/planes_relacionados.php
+++ formatos/indicadores_calidad/planes_relacionados.php
@@ -7,20 +7,9 @@ if(isset($_REQUEST["seguimiento_indicador"])&&$_REQUEST["seguimiento_indicador"]
  else*/
    $target="centro";  
  $planes=busca_filtro_tabla("iddocumento,descripcion,numero","seguimiento_planes,documento","iddocumento=plan_mejoramiento and documento.estado<>'ELIMINADO' and idft_seguimiento_indicador=".$_REQUEST["seguimiento_indicador"],"",$conn);
- if($planes["numcampos"]==0)
+ if($planes["numcampos"]==0){
    echo "<br /><br />No existen planes relacionados con el seguimiento.";
- elseif($planes["numcampos"]==1)
-   {
-       
-       
-   echo "<script>
-          
-          parent.window.open('../../ordenar.php?accion=mostrar&mostrar_formato=1&key=".$planes[0][0]."','_blank');
-         parent.hs.close();
-          </script>";  
-   }
- else
-   {echo "<B><br /><br />PLANES DE MEJORAMIENTO RELACIONADOS CON EL SEGUIMIENTO</B><br /><br /><br />
+ }else{echo "<B><br /><br />PLANES DE MEJORAMIENTO RELACIONADOS CON EL SEGUIMIENTO</B><br /><br /><br />
           <table border=1 style='border-collapse:collapse' cellpadding=5 align='center'><tr class='encabezado_list'><td>NUMERO</td><td>DESCRIPCION</td><td></td></tr>";
     for($i=0;$i<$planes["numcampos"];$i++)
       echo "<tr><td align=center>".$planes[$i]["numero"]."</td><td>".$planes[$i]["descripcion"]."</td><td><a target='$target' href='../../ordenar.php?accion=mostrar&mostrar_formato=1&key=".$planes[$i]["iddocumento"]."'>Ver</a></td></tr>";      
