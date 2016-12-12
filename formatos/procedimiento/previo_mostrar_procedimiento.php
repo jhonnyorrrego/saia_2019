5ae31538133d8c355d0f6b87bb9481f45d4ce21c
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-12-12 19:37:12 +0000

    [Calidad][ruben.pulgarin]correccion idformato previo

--- formatos/procedimiento/previo_mostrar_procedimiento.php
+++ formatos/procedimiento/previo_mostrar_procedimiento.php
@@ -1,2 +1,2 @@
 <?php
-include_once('../librerias/estilo_formulario.php'); include_once('../librerias/funciones_formatos_generales.php');?><?php listado_hijos_formato(10,$_REQUEST["iddoc"]); ?>
+include_once('../librerias/estilo_formulario.php'); include_once('../librerias/funciones_formatos_generales.php');?><?php listado_hijos_formato(364,$_REQUEST["iddoc"]); ?>
