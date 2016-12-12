e4d725e84d349a28ea26b6e54fd7fc54ddf5cc4c
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-12-12 20:08:39 +0000

    [Formatos][ruben.pulgarin]correccion mostrar anexos

--- formatos/formato/funciones.php
+++ formatos/formato/funciones.php
@@ -23,3 +23,25 @@ function validar_campos_obligatorios_formato(){
 </script>
 <?php
 }
+
+function mostrar_anexos_soporte_formato($idformato,$iddoc){
+    global $conn, $ruta_db_superior;
+    
+    
+    $anexos=busca_filtro_tabla("","anexos","documento_iddocumento=".$iddoc,"",$conn);
+	if($anexos['numcampos']){
+		$tabla='<ul>';
+	    for($j=0;$j<$anexos['numcampos'];$j++){
+	        if($anexos[$j]['tipo']=='jpg' || $anexos[$j]['tipo']=='JPG' || $anexos[$j]['tipo']=='pdf' || $anexos[$j]['tipo']=='PDF' || $anexos[$j]['tipo']=='png'){
+	            $tabla.="<li><a href='".$ruta_db_superior.$anexos[$j]['ruta']."' target='_blank'>".$anexos[$j]['etiqueta']."</a></li>";
+	        }
+	        else{
+	            $tabla.='<li><a title="Descargar" href="'.$ruta_db_superior.'anexosdigitales/parsea_accion_archivo.php?idanexo='.$anexos[$j]['idanexos'].'&amp;accion=descargar" border="0px">'.$anexos[$j]['etiqueta'].'</a></li>';
+	        }
+							
+	    }
+		$tabla.='</ul>';
+		echo($tabla);
+	}
+    
+}
