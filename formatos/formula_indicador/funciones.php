9bfef8e3e453eb55fb650a1c76083bb1bad7a9a0
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-12-12 19:17:29 +0000

    [Formula del indicador][ruben.pulgarin]correccion mostrar

--- formatos/formula_indicador/funciones.php
+++ formatos/formula_indicador/funciones.php
@@ -1,4 +1,17 @@
 <?php
+$max_salida=6; // Previene algun posible ciclo infinito limitando a 10 los ../
+$ruta_db_superior=$ruta="";
+while($max_salida>0){
+  if(is_file($ruta."db.php")){
+    $ruta_db_superior=$ruta; //Preserva la ruta superior encontrada
+  }
+  $ruta.="../";
+  $max_salida--;
+}
+include_once($ruta_db_superior."librerias_saia.php");
+echo( librerias_jquery('1.7') );
+
+
 function validar_formula_llenar(){
 ?>
   <script>
