336b691692255b5052317eaa0e63d9a8ffcddf87
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-12-13 14:14:42 +0000

    [prueba][ruben.pulgarin]prueba

--- prueba_nuevo.php
+++ prueba_nuevo.php
@@ -1,7 +1,21 @@
 <?php
+$max_salida=10; // Previene algun posible ciclo infinito limitando a 10 los ../
+$ruta_db_superior=$ruta="";
+while($max_salida>0){
+	if(is_file($ruta."db.php")){
+		$ruta_db_superior=$ruta; //Preserva la ruta superior encontrada
+	}
+	$ruta.="../";
+	$max_salida--;
+}
+include_once($ruta_db_superior."db.php");
+include_once($ruta_db_superior."librerias_saia.php");
 
+echo(estilo_bootstrap());
 
-
+$boton = "<button class='btn btn-success' id='confirmar_cambios' >Aprobación de la Solicitud</button>";	
+echo($boton.'<br><br>');
+echo "<button class='btn btn-info dropdown-toggle' id='btn_editar' >Editar</button>";	
 
 die();
 phpinfo();
