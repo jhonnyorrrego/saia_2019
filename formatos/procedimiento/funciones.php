78b1ec93f3499e26ab0105fa6463a61735a1923f
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-12-13 14:08:58 +0000

    [Calidad][ruben.pulgarin]funcion para mostrar anexos

--- formatos/procedimiento/funciones.php
+++ formatos/procedimiento/funciones.php
@@ -1,6 +1,17 @@
 <?php
-include_once("../../db.php");
-include_once("../librerias/funciones_generales.php");
+$max_salida=6; // Previene algun posible ciclo infinito limitando a 10 los ../
+$ruta_db_superior=$ruta="";
+while($max_salida>0)
+{
+if(is_file($ruta."db.php"))
+{
+$ruta_db_superior=$ruta; //Preserva la ruta superior encontrada
+}
+$ruta.="../";
+$max_salida--;
+}
+include_once($ruta_db_superior."db.php");
+include_once($ruta_db_superior."formatos/librerias/funciones_generales.php");
 /*
 function listado_pasos_procedimiento($idformato,$iddoc){
 global $conn;
@@ -95,4 +106,28 @@ function vigencia_procedimiento($idformato,$iddoc){
   
   return($fecha[0]["vigencia"]);
 }
+
+//MOSTRAR
+function mostrar_anexos_anexos_procedimiento($idformato,$iddoc){
+
+	$anexos=busca_filtro_tabla("","anexos","documento_iddocumento=".$iddoc,"",$conn);
+	if($anexos['numcampos']){
+		$tabla='<ul>';
+	    for($j=0;$j<$anexos['numcampos'];$j++){
+	        if($anexos[$j]['tipo']=='jpg' || $anexos[$j]['tipo']=='JPG' || $anexos[$j]['tipo']=='pdf' || $anexos[$j]['tipo']=='PDF' || $anexos[$j]['tipo']=='png'){
+	            $tabla.="<li><a href='".$ruta_db_superior.$anexos[$j]['ruta']."' target='_blank'>".$anexos[$j]['etiqueta']."</a></li>";
+	        }
+	        else{
+	            $tabla.='<li><a title="Descargar" href="'.$ruta_db_superior.'anexosdigitales/parsea_accion_archivo.php?idanexo='.$anexos[$j]['idanexos'].'&amp;accion=descargar" border="0px">'.$anexos[$j]['etiqueta'].'</a></li>';
+	        }
+							
+	    }
+		$tabla.='</ul>';
+		echo($tabla);
+	}
+
+    
+}
+
+
 ?>
