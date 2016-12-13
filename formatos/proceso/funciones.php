f51dbdfceba4e9d2780ed4207b46ac6f49a98c6e
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-12-13 19:16:24 +0000

    [Calidad][ruben.pulgarin]correccion anexos proceso

--- formatos/proceso/funciones.php
+++ formatos/proceso/funciones.php
@@ -333,4 +333,25 @@ function historial_lider_responsable($idformato,$iddoc){
   }
   return true;
 }
+
+function mostrar_anexos_anexos_proceso($idformato,$iddoc){
+    global $conn, $ruta_db_superior;
+    
+	$anexos=busca_filtro_tabla("","anexos","documento_iddocumento=".$iddoc,"",$conn);
+	if($anexos['numcampos']){
+		$tabla='<ul>';
+	    for($j=0;$j<$anexos['numcampos'];$j++){
+	        if($anexos[$j]['tipo']=='jpg' || $anexos[$j]['tipo']=='JPG' || $anexos[$j]['tipo']=='pdf' || $anexos[$j]['tipo']=='PDF' || $anexos[$j]['tipo']=='png'){
+	            $tabla.="<li><a href='".$ruta_db_superior.$anexos[$j]['ruta']."' target='_blank'>".$anexos[$j]['etiqueta']."</a></li>";
+	        }
+	        else{
+	            $tabla.='<li><a title="Descargar" href="'.$ruta_db_superior.'anexosdigitales/parsea_accion_archivo.php?idanexo='.$anexos[$j]['idanexos'].'&amp;accion=descargar" border="0px">'.$anexos[$j]['etiqueta'].'</a></li>';
+	        }
+							
+	    }
+		$tabla.='</ul>';
+		echo($tabla);
+	}    
+}
+
 ?>
