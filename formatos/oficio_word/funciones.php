8163e4bbd0879e7cbbfa35a11dac9f966f1112e0
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-12-14 15:41:58 +0000

    [Oficio Word][ruben.pulgarin]correccion firmar word

--- formatos/oficio_word/funciones.php
+++ formatos/oficio_word/funciones.php
@@ -60,7 +60,6 @@ function generar_radicado_word($idformato, $iddoc) { // POSTERIOR AL APROBAR
 	
 	$busca_masivo=busca_filtro_tabla("","anexos a, campos_formato b","b.nombre='anexo_csv' AND a.campos_formato=b.idcampos_formato AND a.documento_iddocumento=".$iddoc,"",$conn);
 	
-	
 	if($busca_masivo['numcampos']){
 	    include_once ($ruta_db_superior . 'pantallas/lib/PhpWord/funciones_radicacion_word.php');
     	$radicar_word = new RadicadoWord($iddoc);
@@ -68,8 +67,6 @@ function generar_radicado_word($idformato, $iddoc) { // POSTERIOR AL APROBAR
 	}else{
 	    include_once($ruta_db_superior.'pantallas/lib/PhpWord/numero_radicado_word.php');
 	}
-	
-
 }
 
 function mostrar_mensaje_error_pdf($idformato, $iddoc) { // mostrar
