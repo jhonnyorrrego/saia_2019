41e165c333bb18f089b31a9506c99ec36b4da4f1
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-12-15 16:42:31 +0000

    [Riesgos][ruben.pulgarin]correccion no se muestra la ayuda de contexto estrategico

--- formatos/riesgos_proceso/previo_mostrar_riesgos_proceso.php
+++ formatos/riesgos_proceso/previo_mostrar_riesgos_proceso.php
@@ -111,7 +111,7 @@ $datos=explode("-",$_REQUEST["llave"]);
 			<?php
 				$idft_papa = busca_filtro_tabla("","ft_proceso","documento_iddocumento=".$_REQUEST['iddoc'],"",$conn);
 			?>
-        	<a  title="" href="<?php echo($ruta_db_superior);?>formatos/contexto_extrategico/adicionar_contexto_extrategico.php?anterior=<?php echo($_REQUEST['iddoc']);?>&padre=<?php echo($idft_papa[0]['idft_proceso']);?>&idformato=211" target="_self">CONTEXTO EXTRAT&Eacute;GICO</a>(<a class="highslide" onclick='return hs2.htmlExpand(this, { objectType: "iframe",width: 500, height: 700,preserveContent:true} )' href="ayuda_contexto_estrategico_riesgos.php"><b>ayuda</b></a>)
+        	<a  title="" href="<?php echo($ruta_db_superior);?>formatos/contexto_extrategico/adicionar_contexto_extrategico.php?anterior=<?php echo($_REQUEST['iddoc']);?>&padre=<?php echo($idft_papa[0]['idft_proceso']);?>&idformato=211" target="_self">CONTEXTO EXTRAT&Eacute;GICO</a>(<a class="highslide" onclick='return hs.htmlExpand(this, { objectType: "iframe",width: 500, height: 700,preserveContent:true} )' href="ayuda_contexto_estrategico_riesgos.php"><b>ayuda</b></a>)
         	<?php
 				$t=1;
 				if($t==0){
