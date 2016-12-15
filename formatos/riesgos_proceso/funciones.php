4aecb5f0b95c31b401cd6db0eac713ed506222e6
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-12-15 15:03:56 +0000

    [Riesgos][ruben.pulgarin]correccion redireccion riesgos

--- formatos/riesgos_proceso/funciones.php
+++ formatos/riesgos_proceso/funciones.php
@@ -484,6 +484,11 @@ function redireccionar_from_riesgos($idformato, $iddoc){
     
     $datos=busca_filtro_tabla("from_riesgos","ft_riesgos_proceso","documento_iddocumento=".$iddoc,"",$conn);
     if($datos[0]['from_riesgos']){
+        ?>   
+        <script>
+            parent.location.reload();
+        </script>
+        <?php
         redirecciona($ruta_db_superior."formatos/riesgos_proceso/mostrar_riesgos_proceso.php?iddoc=".$iddoc);
         die();
     }
