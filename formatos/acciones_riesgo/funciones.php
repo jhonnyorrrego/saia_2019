75534736bc4af6a92247968a787496c4aa713468
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-12-15 15:37:12 +0000

    [Riesgos][ruben.pulgarin]correccion select campo CONTROL

--- formatos/acciones_riesgo/funciones.php
+++ formatos/acciones_riesgo/funciones.php
@@ -51,7 +51,9 @@ function control_funcion($idformato,$iddoc){
 		$selected='';
 		if($accion[0]["acciones_control"]==$valoracion[$i]["idft_control_riesgos"])$selected='selected';
 		$cadena_descripcion=ucfirst(strip_tags(decodifica_encabezado(html_entity_decode($valoracion[$i]["descripcion_control"]))));
-		$cadena_descripcion=substr ( $cadena_descripcion , 0 , 60 ).'...';
+		if(strlen ( $cadena_descripcion )>60){
+		    $cadena_descripcion=substr ( $cadena_descripcion , 0 , 60 ).'...';
+		}
 		$select.='<option value="'.$valoracion[$i]["idft_control_riesgos"].'" '.$selected.'>'.$cadena_descripcion.'</option>';
 	}//string substr ( string $string , int $start [, int $length ] )
 	$select.='<option value="'.$valoracion[$i]["idft_control_riesgos"].'-1" '.$selected.'>Nuevo Control</option>';
