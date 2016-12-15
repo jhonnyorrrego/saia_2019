aca8c6e4dabfb2274776263b0c5932eeb7720a16
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-12-15 19:00:26 +0000

    [prueba][ruben.pulgarin]prueba

--- prueba_nuevo.php
+++ prueba_nuevo.php
@@ -1,22 +1,114 @@
-<?php
-$max_salida=10; // Previene algun posible ciclo infinito limitando a 10 los ../
-$ruta_db_superior=$ruta="";
-while($max_salida>0){
-	if(is_file($ruta."db.php")){
-		$ruta_db_superior=$ruta; //Preserva la ruta superior encontrada
-	}
-	$ruta.="../";
-	$max_salida--;
-}
-include_once($ruta_db_superior."db.php");
-
-generar_firma_word(400, 986);
-
-
-function generar_firma_word($idformato, $iddoc) { // POSTERIOR AL CONFIRMAR
-	global $ruta_db_superior, $conn;
-	
-	include_once ($ruta_db_superior . 'pantallas/lib/PhpWord/firmar_word.php');
-}
-
-?>
+<table border="1px" style="border-collapse:collapse; width:100%;font-size:7pt;">
+	<tr>									
+		<td style="background-color: #D2D3D5; text-align:center;" colspan="12">MATRIZ DE RIESGOS DEL PROCESO</td>
+	</tr>
+	<tr>
+		<td style="background-color: #D2D3D5; text-align:center;" colspan="12">MAPA DE RIESGOS</td>
+	</tr>
+	<tr>
+		<td colspan="12" style="background-color: #D2D3D5; text-align:center;">PROCESO: Proceso 1</td>
+	</tr>
+	<tr>
+		<td colspan="12" style="background-color: #D2D3D5; text-align:center;">
+			OBJETIVO: Si vas a utilizar un pasaje de Lorem Ipsum, necesit&aacute;s estar seguro de que no hay nada avergonzante escondido en el medio del texto.
+		</td>
+	</tr>
+	<tr height="80px">
+		<td style="background-color: #D2D3D5; text-align:center;" rowspan="2"><p>RIESGO</p></td>
+        <td style="background-color: #D2D3D5; text-align:center;" colspan="2"><p>CALIFICACI&Oacute;N</p></td>
+        <td style="background-color: #D2D3D5; text-align:center;" rowspan="2"><p>EVALUACION RIESGO</p></td>
+        <td style="background-color: #D2D3D5; text-align:center;" rowspan="2"><p>CONTROLES</p></td>
+        <td style="background-color: #D2D3D5; text-align:center;" colspan="2"><p>NUEVA<br>CALIFICACION</p></td>
+        <td style="background-color: #D2D3D5; text-align:center;" rowspan="2"><p>NUEVA EVALUACION</p></td>
+        <td style="background-color: #D2D3D5; text-align:center;" rowspan="2"><p>OPCIONES<br>MANEJO</p></td>
+        <td style="background-color: #D2D3D5; text-align:center;" rowspan="2"><p>ACCIONES</p></td>
+        <td style="background-color: #D2D3D5; text-align:center;" rowspan="2"><p>RESPONSABLE DE LA ACCI&Oacute;N</p></td>
+        <td style="background-color: #D2D3D5; text-align:center;" rowspan="2"><p>INDICADOR</p></td>											
+	</tr>
+	<tr height="80px">
+		<td style="background-color: #D2D3D5; text-align:center;"><p>Probabilidad</p></td>
+        <td style="background-color: #D2D3D5; text-align:center;"><p>Impacto</p></td>
+        <td style="background-color: #D2D3D5; text-align:center;"><p>Probabilidad</p></td>
+        <td style="background-color: #D2D3D5; text-align:center;"><p>Impacto</p></td>
+	</tr>
+	<tbody>
+								
+	<tr>
+		<td>1 - <p style="text-align: justify;">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt ex</td>
+		<td style="text-align: center;">(2)<br>Improbable</td><td style="text-align: center;">(1)<br>Insignificante</td><td style="text-align:center;background-color:green;">Zona de riesgo baja</td><td>1. Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur<br /><br />2. But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness.<br /><br /></td>
+		<td style="text-align:center;">(1)<br>Raro</td><td style="text-align:center;">(1)<br>Insignificante</td><td style="text-align:center;background-color:green;">Zona de riesgo baja</td><td style="text-align:center;">Asumir el riesgo</td><td><p>dsfsfsfds</p></td>
+		<td>- Carlos  Angulo Santamaria<br></td>
+		<td><p>fdsfdsfds</p></td>
+	</tr>
+	<tr>
+		<td>2 - Prueba creando riesgos</td>
+		<td style="text-align: center;">(1)<br>Raro</td>
+		<td style="text-align: center;">(1)<br>Insignificante</td>
+		<td style="text-align:center;background-color:green;">Zona de riesgo baja</td>
+		<td></td>
+		<td style="text-align:center;">(1)<br>Raro</td>
+		<td style="text-align:center;">(1)<br>Insignificante</td>
+		<td style="text-align:center;background-color:green;">Zona de riesgo baja</td>
+		<td style="text-align:center;">Asumir el riesgo</td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td>3 - prueba</td>
+		<td style="text-align: center;">(1)<br>Raro</td>
+		<td style="text-align: center;">(1)<br>Insignificante</td>
+		<td style="text-align:center;background-color:green;">Zona de riesgo baja</td>
+		<td></td>
+		<td style="text-align:center;">(1)<br>Raro</td>
+		<td style="text-align:center;">(1)<br>Insignificante</td>
+		<td style="text-align:center;background-color:green;">Zona de riesgo baja</td>
+		<td style="text-align:center;">Asumir el riesgo</td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td>4 - prueba riesgos</td>
+		<td style="text-align: center;">(1)<br>Raro</td>
+		<td style="text-align: center;">(1)<br>Insignificante</td>
+		<td style="text-align:center;background-color:green;">Zona de riesgo baja</td>
+		<td></td>
+		<td style="text-align:center;">(1)<br>Raro</td>
+		<td style="text-align:center;">(1)<br>Insignificante</td>
+		<td style="text-align:center;background-color:green;">Zona de riesgo baja</td>
+		<td style="text-align:center;">Asumir el riesgo</td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td>5 - prueba</td>
+		<td style="text-align: center;">(1)<br>Raro</td>
+		<td style="text-align: center;">(1)<br>Insignificante</td>
+		<td style="text-align:center;background-color:green;">Zona de riesgo baja</td>
+		<td></td>
+		<td style="text-align:center;">(1)<br>Raro</td>
+		<td style="text-align:center;">(1)<br>Insignificante</td>
+		<td style="text-align:center;background-color:green;">Zona de riesgo baja</td>
+		<td style="text-align:center;">Asumir el riesgo</td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td>7 - xcvxcvxc</td>
+		<td style="text-align: center;">(1)<br>Raro</td>
+		<td style="text-align: center;">(1)<br>Insignificante</td>
+		<td style="text-align:center;background-color:green;">Zona de riesgo baja</td>
+		<td></td>
+		<td style="text-align:center;">(1)<br>Raro</td>
+		<td style="text-align:center;">(1)<br>Insignificante</td>
+		<td style="text-align:center;background-color:green;">Zona de riesgo baja</td>
+		<td style="text-align:center;">Asumir el riesgo</td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	</tbody>
+</table>
