545c977cfa9e982a2fb5eb78aff8035a3ba48e9f
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-12-16 13:47:55 +0000

    [Firmas][ruben.pulgarin]Correccion generar firma blanco cuando el funcionario no tenga firma

--- formatos/librerias/mostrar_foto.php
+++ formatos/librerias/mostrar_foto.php
@@ -14,7 +14,12 @@ include_once($ruta_db_superior."db.php");
 global $conn;
 /*muestra una imagen con la firma del funcionario identificado con $_GET["codigo"]*/
 $func=busca_filtro_tabla("firma","funcionario","funcionario_codigo=".$_REQUEST["codigo"],"",$conn);
-
+if($func[0]['firma'] == "null" || $func[0]['firma'] == ""){//si no tiene firma (corrige error en pdf cuando el fun no tiene firma)
+	$fileHandle = fopen($ruta_db_superior.'firmas/blanco.jpg', "rb");
+    $fileContent = fread($fileHandle, filesize($ruta_db_superior.'firmas/blanco.jpg'));
+    fclose($fileHandle);
+    $func[0]["firma"] = $fileContent;
+}	
 header("Content-Type: image/jpeg");
 if(MOTOR=="Oracle"){
 	echo ($func[0]["firma"]);	
