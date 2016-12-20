31d2ec2b5aa83583e899ec61a38d6036dd24b3b4
ruben.pulgarin <ruben.pulgarin@cerok.com>
2016-12-20 18:50:14 +0000

    [2. RADICACION][ruben.pulgarin]correccion no muestra el ejecutor del documento

--- pantallas/documento/librerias.php
+++ pantallas/documento/librerias.php
@@ -196,7 +196,7 @@ if(!$ruta){
 		$ejecutor=busca_filtro_tabla("nombre","ejecutor","idejecutor=".$datos_ejecutor[0]["ejecutor_idejecutor"],"",$conn);
 	}
 	
-    return($datos_ejecutor);
+    return(print_r($datos_ejecutor));
 	
 	
 	if($ejecutor["numcampos"]&&$datos_ejecutor[0]["plantilla"]==""){
