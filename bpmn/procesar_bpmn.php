22ac34463b0ecef84dc1ac880824c6767dbb32d2
ruben.pulgarin <ruben.pulgarin@cerok.com>
2017-01-23 22:52:36 +0000

    [Admin. Flujos][ruben.pulgarin]correccion scroll digrama de flujo

--- bpmn/procesar_bpmn.php
+++ bpmn/procesar_bpmn.php
@@ -88,7 +88,7 @@ if(@$_REQUEST["idpaso_documento"]){
 		<?php } ?>		
 	</div>
 	</div>
-	<div class="row-fluid " id="diagram" style="position:relative; overflow: scroll;">
+	<div class="row-fluid " id="diagram" style="height:80%; position:relative; overflow: scroll;">
 	</div>
 	 
 </div>
