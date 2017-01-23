c798f17acf3cf01449df8423e3bfed530d32806a
ruben.pulgarin <ruben.pulgarin@cerok.com>
2017-01-23 22:26:25 +0000

    [Admin. Flujos][ruben.pulgarin]correccion scroll digrama de flujo

--- bpmn/procesar_bpmn.php
+++ bpmn/procesar_bpmn.php
@@ -113,7 +113,8 @@ if(@$_REQUEST["idpaso_documento"]){
   require(["bpmn/Bpmn", "dojo/domReady!"], function(Bpmn) {
     new Bpmn().renderUrl("<?php echo($ruta_db_superior.$bpmn->archivo); ?>", {
       diagramElement : "diagram",
-      overlayHtml : '<div class="elemento_bpmn_saia"></div>'
+      overlayHtml : '<div class="elemento_bpmn_saia"></div>',
+      height:100%
     }).then(function (bpmn){				
       bpmn.zoom(1.0);
       <?php $bpmn->imprimir_estados_tarea("bpmn"); ?>    
