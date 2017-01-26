a3c32def7a5c862429450a19f10c4ab14573db94
ruben.pulgarin <ruben.pulgarin@cerok.com>
2017-01-26 20:58:16 +0000

    [Tareas][ruben.pulgarin]correccion visual pantalla detalles

--- pantallas/tareas/mostrar_tareas.php
+++ pantallas/tareas/mostrar_tareas.php
@@ -80,7 +80,7 @@ if((@$_REQUEST["iddoc"] || @$_REQUEST["key"])&& !@$_REQUEST["idpaso_documento"])
 								switch ($busca_avances[$i]['estado']) {
 									case 0 :
 										$estado = "Pendiente";
-										$color="yellow";
+										$color="#BDBF1C";
 										break;
 									case 1 :
 										$estado = "Proceso";
