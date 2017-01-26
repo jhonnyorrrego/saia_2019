c5f7394a47ac4acd984d17bcdfad8ec6d7e1b33f
ruben.pulgarin <ruben.pulgarin@cerok.com>
2017-01-26 22:22:55 +0000

    [Categoria Formatos][ruben.pulgarin]desarrollo administrador de categorias

--- pantallas/categoria_formato/detalles_categoria_formato.php
+++ pantallas/categoria_formato/detalles_categoria_formato.php
@@ -8,8 +8,52 @@ while($max_salida>0){
 	$ruta.="../";
 	$max_salida--;
 }
-include_once($ruta_db_superior."db.php");
-include_once($ruta_db_superior."librerias_saia.php");
+
+
+if(@$_REQUEST['idcategoria_formato']){
+
+    include_once($ruta_db_superior."db.php");
+    include_once($ruta_db_superior."librerias_saia.php");
+    echo(estilo_bootstrap()); 
+    $idcategoria_formato=$_REQUEST['idcategoria_formato'];
+    $categoria_formato=busca_filtro_tabla("","categoria_formato","idcategoria_formato=".$idcategoria_formato,"",$conn);
+    
+    ?>
+    <table class="table">
+         <tr>
+            <td class="prettyprint">Fecha de creaci&oacute;n</td>
+            <td></td>
+        </tr>         
+        <tr>
+            <td class="prettyprint">Nombre</td>
+            <td></td>
+        </tr>
+         <tr>
+            <td class="prettyprint">Estado</td>
+            <td></td>
+        </tr>       
+         <tr>
+            <td class="prettyprint">Descripci&oacute;n</td>
+            <td></td>
+        </tr>          
+    </table>
+    
+    
+    <?php
+
+    
+    
+    
+
+
+
+    
+} //FIN IF REQUEST idcategoria_formato
+
+
+
+
+
 
 
 ?>
