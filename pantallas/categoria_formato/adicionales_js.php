94d0303341225dd7170ce0b9bb2c209e0300a607
ruben.pulgarin <ruben.pulgarin@cerok.com>
2017-01-26 22:31:51 +0000

    [Categoria Formatos][ruben.pulgarin]desarrollo administrador de categorias

---
+++ pantallas/categoria_formato/adicionales_js.php
@@ -0,0 +1,18 @@
+<?php
+$max_salida=10; // Previene algun posible ciclo infinito limitando a 10 los ../
+$ruta_db_superior=$ruta="";
+while($max_salida>0){
+	if(is_file($ruta."db.php")){
+		$ruta_db_superior=$ruta; //Preserva la ruta superior encontrada
+	}
+	$ruta.="../";
+	$max_salida--;
+}
+
+include_once($ruta_db_superior."db.php");
+include_once($ruta_db_superior."librerias_saia.php");
+echo(librerias_jquery('1.7'));
+
+
+
+?>
