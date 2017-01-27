fe1513c5b0814ea636862aa9b7b72d672bde2865
ruben.pulgarin <ruben.pulgarin@cerok.com>
2017-01-27 14:03:19 +0000

    [Categoria Formatos][ruben.pulgarin]desarrollo administrador de categorias

--- pantallas/categoria_formato/librerias.php
+++ pantallas/categoria_formato/librerias.php
@@ -48,7 +48,7 @@ if(@$_REQUEST['ejecutar_funcion']){
     
     function inactivate_categoria(){
         global $conn;
-        print_r($_REQUEST);die();
+        //print_r($_REQUEST);die();
         $sql="UPDATE categoria_formato SET estado=2 WHERE idcategoria_formato=".@$_REQUEST['categoria_formato'];
         phpmkr_query($sql);
         echo('<b>ATENCI&Oacute;N</b><br>La categoria se ha inactivado con exito!');
