4c36fa718c96001da61831a7555e3d26ef0e3b4a
Cero K <info@cerok.com>
2017-04-03 21:39:24 +0000

    Estilo bootstarp en radicación rápida

--- formatos/radicacion_entrada/radicacion_rapida.php
+++ formatos/radicacion_entrada/radicacion_rapida.php
@@ -8,137 +8,25 @@ while($max_salida>0){
 	$ruta.="../";
 	$max_salida--;
 }
+include_once($ruta_db_superior."db.php");
 include_once($ruta_db_superior."librerias_saia.php");
-echo(librerias_html5());
-echo(librerias_jquery("1.7"));
-echo(estilo_bootstrap());
-ini_set('display_errors',true);
 ?>    
-
-
 <html>
 <head>
 <meta http-equiv="content-type" content="text/html; charset=utf-8">
-
-     <style type="text/css">
-     <!--INPUT, TEXTAREA, SELECT, body {
-        font-family: Tahoma; 
-        font-size: 10px; 
-       } 
-       .phpmaker {
-       font-family: Verdana; 
-       font-size: 9px; 
-       } 
-       .encabezado {
-       background-color:#57B0DE; 
-       color:white ; 
-       padding:10px; 
-       text-align: left;	
-       } 
-       .encabezado_list { 
-       background-color:#57B0DE; 
-       color:white ; 
-       vertical-align:middle;
-       text-align: center;
-       font-weight: bold;	
-       }
-       table thead td {
-		    font-weight:bold;
-    		cursor:pointer;
-    		background-color:#57B0DE;
-    		text-align: center;
-        font-family: Verdana; 
-        font-size: 9px;
-        text-transform:Uppercase;
-        vertical-align:middle;    
-    	 }
-    	 table tbody td {	
-    		font-family: Verdana; 
-        font-size: 9px;
-    	 }
-    	 .ac_results {
-				padding: 0px;
-				border: 0px solid black;
-				background-color: white;
-				overflow: hidden;
-				z-index: 99999;
-			}
-    	 
-			.ac_results ul {
-				width: 100%;
-				list-style-position: outside;
-				list-style: none;
-				padding: 0;
-				margin: 0;
-			}
-			.ac_results li:hover {
-			background-color: A9E2F3;
-			}
-			
-			.ac_results li {
-				margin: 0px;
-				padding: 2px 5px;
-				cursor: default;
-				display: block;
-				font: menu;
-				font-size: 10px;
-				line-height:10px;
-				overflow: hidden;
-			}
-       -->
-       </style><style type="text/css" media="screen">
-	@import "../../css/title2note.css";
-	html, body {
-   height: 99%;
-   width:99%;
-   overflow: hidden;
-}
-#div_contenido {
-   height: 100%;
-   overflow: auto; 
-   width:100%;
-   position: relative;
-   z-index: 2;
-}
-</style>
+<?php
+echo(librerias_html5());
+echo(librerias_jquery("1.7"));
+echo(librerias_arboles());
+echo(estilo_bootstrap());
+?>
 </head>
 <body>
 <div id="div_contenido">
-<script src="../../js/jquery-1.7.min.js" type="text/javascript"></script><link rel="stylesheet" type="text/css" href="../../css/bootstrap.css"><link rel="stylesheet" type="text/css" href="../../css/bootstrap-responsive.css"><link rel="stylesheet" type="text/css" href="../../css/jasny-bootstrap.min.css"><link rel="stylesheet" type="text/css" href="../../css/jasny-bootstrap-responsive.min.css"><link rel="stylesheet" type="text/css" href="../../css/bootstrap_reescribir.css"><link rel="stylesheet" type="text/css" href="../../pantallas/lib/librerias_css.css"><link rel="stylesheet" type="text/css" href="../../css/bootstrap_iconos_segundarios.css"><style type="text/css">
-			.btn-primary{
-				  background-color: #0044cc;
-				  background-image: linear-gradient(to top, #0088cc,#0044cc);
-			}
-			.btn-primary:hover {
-			  background-color: #0044cc;
-			  background-image: linear-gradient(to bottom, #0088cc,#0044cc);
-			}
-			.encabezado_list { 
-			    background-color: #57B0DE;
-			}
-			textarea, input[type="text"], input[type="password"], input[type="datetime"], 
-			input[type="datetime-local"], input[type="date"], input[type="month"], input[type="time"], 
-			input[type="week"], input[type="number"], input[type="email"], input[type="url"], 
-			input[type="search"], input[type="tel"], input[type="color"], .uneditable-input { 
-			  border-color: #cccccc;
-			  box-shadow: inset 0 1px 1px #cccccc, 0 0 8px #cccccc;
-			}
-			textarea:focus, input[type="text"]:focus, input[type="password"]:focus, input[type="datetime"]:focus, 
-			input[type="datetime-local"]:focus, input[type="date"]:focus, input[type="month"]:focus, 
-			input[type="time"]:focus, input[type="week"]:focus, input[type="number"]:focus, input[type="email"]:focus, 
-			input[type="url"]:focus, input[type="search"]:focus, input[type="tel"]:focus, input[type="color"]:focus,
-			 .uneditable-input:focus {
-			  border-color: #cccccc;
-			  box-shadow: inset 0 1px 1px #cccccc, 0 0 8px #cccccc;
-			}
-			.label-info, .badge-info {
-			    background-color: #0B7BB6;
-			}
-			</style>
 			<form method="POST" action="<?php echo($ruta_db_superior); ?>colilla.php"><br/><br />
-                <table class="table-hover" style="font-size:10pt;border-collapse:collapse; width:40%; height: auto;" border="1" align="center">
+                <table class="table-bordered" border="1" align="center">
                     <tr>
-                        <td style="font-size:8pt;" class="encabezado_list" colspan="2" align="center">Seleccione Tipo de Radicación</td>
+                        <td class="encabezado_list" colspan="2" align="center">Seleccione Tipo de Radicación</td>
                     </tr>
                     <tr>
                         <td colspan="2">
@@ -146,24 +34,14 @@ ini_set('display_errors',true);
                             <input type="hidden" name="enlace" id="enlace" value="pantallas/buscador_principal.php?idbusqueda=7">
                             
                             <?php
-
-include_once($ruta_db_superior."db.php");
-include_once($ruta_db_superior."formatos/librerias/estilo_formulario.php");
-
 $adicional=Null;
 if(@$_REQUEST["idcategoria_formato"]){
 	$adicional="&idcategoria_formato=".$_REQUEST["idcategoria_formato"];
 }
-?><link rel="STYLESHEET" type="text/css" href="<?php echo($ruta_db_superior); ?>css/dhtmlXTree.css">
-	<script type="text/javascript" src="<?php echo($ruta_db_superior); ?>js/dhtmlXCommon.js"></script>
-	<script type="text/javascript" src="<?php echo($ruta_db_superior); ?>js/dhtmlXTree.js"></script>
-	<script type="text/javascript" src="<?php echo($ruta_db_superior); ?>js/dhtmlxtree_xw.js"></script> 
-	<script type="text/javascript" src="<?php echo($ruta_db_superior); ?>asset/js/jquery.min.js"></script>
-	<script type="text/javascript" src="<?php echo($ruta_db_superior); ?>asset/js/main.js"></script>
-    
-	<br><div id="esperando_serie">
+?>
+	<div id="esperando_serie">
     <img src="<?php echo($ruta_db_superior); ?>imagenes/cargando.gif"></div>
-	<div id="treeboxbox_tree_equipos" style="height:89%;width:95%" ></div>
+	<div id="treeboxbox_tree_equipos" class="arbol_saia" style="height:89%;width:95%" ></div>
 	<script type="text/javascript">
   <!--		
   	$(document).ready(function  () {
@@ -179,6 +57,7 @@ if(@$_REQUEST["idcategoria_formato"]){
       }
 			tree_equipos=new dhtmlXTreeObject("treeboxbox_tree_equipos","100%","100%",0);
 			tree_equipos.setImagePath("<?php echo $ruta_db_superior;?>imgs/");
+			tree_equipos.enableTreeImages("false");
 			tree_equipos.enableIEImageFix(true);
 			//tree_equipos.setOnClickHandler(onNodeSelect);
 			tree_equipos.setOnCheckHandler(onNodeSelect);
@@ -236,25 +115,13 @@ if(@$_REQUEST["idcategoria_formato"]){
 	--> 		
 	</script>
 	        </td>
-                    </tr>
-                    <tr><td style="font-size:8pt;"  align="center">Descripci&oacuten General</td>
-                        <td align="center"><input type="text"  id="descripcion_general" name="descripcion_general"/></td>
-                    </tr>
-                    <tr>
-                        <td colspan="2" align="center"><input class="btn btn-primary btn-mini" type="submit" value="Radicar" id="enviar" name="enviar"/></td>
-                    </tr>
-                </table>
-                <input type="hidden" name="target" value="_self">
-            </form>
-    <!--script>
-	$(document).ready(function(){
-		$("#enviar").click(function(){
-			var ingreso=confirm("Esta seguro de generar un nuevo radicado?");
-			if(ingreso){
-				form.submit();
-			}else{
-				return false;
-			}
-		});
-	});
-</script-->
+            </tr>
+            <tr><td style="font-size:8pt;"  align="center">Descripci&oacuten General</td>
+                <td align="center"><input type="text"  id="descripcion_general" name="descripcion_general"/></td>
+            </tr>
+            <tr>
+                <td colspan="2" align="center"><input class="btn btn-primary btn-mini" type="submit" value="Radicar" id="enviar" name="enviar"/></td>
+            </tr>
+        </table>
+        <input type="hidden" name="target" value="_self">
+    </form>
