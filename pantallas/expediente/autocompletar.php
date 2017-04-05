83cd1cc87040b9a321d9d3759758a84b2b5ba16e
john.rodriguez <john.rodriguez@cerok.com>
2017-04-05 18:48:39 +0000

    se retira guion del autocompletar de permisos de expedientes

--- pantallas/expediente/autocompletar.php
+++ pantallas/expediente/autocompletar.php
@@ -18,7 +18,7 @@ if (isset($_REQUEST['valor']) && $_REQUEST['opt'] == 1) {
 	
 	$_REQUEST['valor']=$_REQUEST['valor'];
 
-	$datos = busca_filtro_tabla("idfuncionario as id," . concatenar_cadena_sql(array("nombres", "' -'", "apellidos")) . " as descripcion", "funcionario f", "f.estado=1 and f.idfuncionario<>".$_REQUEST["propietario"].$parte." and (f.nombres like '%" . $_REQUEST["valor"] . "%' OR f.apellidos like '%" . $_REQUEST["valor"] . "%')", "", $conn);
+	$datos = busca_filtro_tabla("idfuncionario as id," . concatenar_cadena_sql(array("nombres", "' '", "apellidos")) . " as descripcion", "funcionario f", "f.estado=1 and f.idfuncionario<>".$_REQUEST["propietario"].$parte." and (f.nombres like '%" . $_REQUEST["valor"] . "%' OR f.apellidos like '%" . $_REQUEST["valor"] . "%')", "", $conn);
 	$html = "<ul>";
 	if ($datos['numcampos']) {
 		for ($i = 0; $i < $datos['numcampos']; $i++) {
