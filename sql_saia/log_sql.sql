49b43229025d166b2eeb48a08769581f3418865e
Cero K <info@cerok.com>
2017-04-10 16:42:08 +0000

    correccion log_sql update modulos desarrollo pantallas kaiten

--- sql_saia/log_sql.sql
+++ sql_saia/log_sql.sql
@@ -191,8 +191,8 @@ UPDATE  modulo SET  enlace_pantalla =  '1' WHERE idmodulo =1552;
 UPDATE  modulo SET  enlace_pantalla =  '1' WHERE idmodulo =1522;
 UPDATE  modulo SET  enlace_pantalla =  '1' WHERE  idmodulo =1141;
 UPDATE  modulo SET  enlace_pantalla =  '1' WHERE  idmodulo =1142;
-UPDATE  modulo SET  enlace =  'arboldependencia.php',enlace_pantalla =  '1' WHERE  idmodulo =16;
-UPDATE  modulo SET  enlace =  'arbolserie.php',enlace_pantalla =  '1' WHERE  idmodulo =14;
+UPDATE  modulo SET  enlace =  'dependencia.php',enlace_pantalla =  '1' WHERE  idmodulo =16;
+UPDATE  modulo SET  enlace =  'serielist.php',enlace_pantalla =  '1' WHERE  idmodulo =14;
 UPDATE  modulo SET  enlace ='calidad_macro.php?from_modulo_calidad=1',enlace_pantalla =  '1' WHERE  idmodulo =202;
 
 
