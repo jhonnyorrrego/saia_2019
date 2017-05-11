df10b6e22208fb2d019dbd05de5e5bcaaabc1542
Cero K <info@cerok.com>
2017-05-11 13:33:54 +0000

    correccion al finalizar la recogida obligaba a que tuviera un mensajero de entrega, se organiza

--- formatos/radicacion_entrada/actualizar_recepcion.php
+++ formatos/radicacion_entrada/actualizar_recepcion.php
@@ -11,7 +11,7 @@ while($max_salida>0){
 
 include_once($ruta_db_superior."db.php");
 
-$idft_funcionario=json_decode($_REQUEST['idft_funcionario'],1);
+$idft_funcionario=json_decode(@$_REQUEST['idft_funcionario'],1);
 for($i=0;$i<count($idft_funcionario);$i++){
 	$separacion_idft_funcionario=explode('|',$idft_funcionario[$i]['value']);
 	
@@ -26,7 +26,12 @@ for($i=0;$i<count($idft_funcionario);$i++){
 			$destino=busca_filtro_tabla("iddependencia","vfuncionario_dc","iddependencia_cargo=".$datos[0]['nombre_destino'],"",$conn);
 			$responsable=busca_filtro_tabla("mensajero_ruta","documento d,ft_ruta_distribucion a, ft_dependencias_ruta b, ft_funcionarios_ruta c","d.iddocumento=a.documento_iddocumento AND lower(d.estado)='aprobado' AND b.estado_dependencia=1 AND c.estado_mensajero=1 AND a.idft_ruta_distribucion=b.ft_ruta_distribucion AND a.idft_ruta_distribucion=c.ft_ruta_distribucion AND b.dependencia_asignada=".$destino[0]['iddependencia'],"",$conn);
 		}
-		$sql="UPDATE ft_destino_radicacion SET estado_recogida=1,estado_item=1,mensajero_encargado=".$responsable[0]['mensajero_ruta']." WHERE idft_destino_radicacion=".$separacion_idft_funcionario[0];	
+		$sql="UPDATE ft_destino_radicacion SET estado_recogida=1,estado_item=1 WHERE idft_destino_radicacion=".$separacion_idft_funcionario[0];	
+		
+		if(@$responsable[0]['mensajero_ruta'] && @$responsable[0]['mensajero_ruta']!=''){
+			$sql1="UPDATE ft_destino_radicacion SET mensajero_encargado=".$responsable[0]['mensajero_ruta']." WHERE idft_destino_radicacion=".$separacion_idft_funcionario[0];	
+			phpmkr_query($sql1);	 
+		}	
 	}else{
 		if($datos[0]['recepcion']==0){
 			$sql="UPDATE ft_destino_radicacion SET recepcion='".$separacion_idft_funcionario[1]."', recepcion_fecha=".fecha_db_almacenar(date("Y-m-d H:i:s"),"Y-m-d H:i:s").", estado_item=3 WHERE idft_destino_radicacion=".$separacion_idft_funcionario[0];		
