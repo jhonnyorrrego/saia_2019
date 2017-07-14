f85c959b8bd2effe9d091d3edbe34f7c31218713
Cero K <info@cerok.com>
2017-07-14 22:40:18 +0000

    desarrollo listar items con informacion en las novedades

--- formatos/novedad_despacho/funciones.php
+++ formatos/novedad_despacho/funciones.php
@@ -18,20 +18,131 @@ function cargar_items_radicacion($idformato,$iddoc){
 	$items_seleccionados=busca_filtro_tabla("iddestino_radicacion","ft_despacho_ingresados","documento_iddocumento=".$_REQUEST['anterior'],"",$conn);
 	$cadena_items_seleccionados=$items_seleccionados[0]['iddestino_radicacion'];
 	
-	$registros=busca_filtro_tabla("b.idft_destino_radicacion,b.numero_item","ft_destino_radicacion b, ft_radicacion_entrada a, documento d","a.idft_radicacion_entrada=b.ft_radicacion_entrada AND a.documento_iddocumento=d.iddocumento AND b.idft_destino_radicacion in(".$cadena_items_seleccionados.")","",$conn);
+	$registros=busca_filtro_tabla("b.idft_destino_radicacion,b.numero_item,b.estado_recogida,d.iddocumento,d.plantilla,b.numero_item,b.nombre_destino,b.destino_externo,b.origen_externo,b.tipo_origen,b.tipo_destino,b.nombre_origen,a.documento_iddocumento,a.descripcion,a.tipo_mensajeria","ft_destino_radicacion b, ft_radicacion_entrada a, documento d","a.idft_radicacion_entrada=b.ft_radicacion_entrada AND a.documento_iddocumento=d.iddocumento AND b.idft_destino_radicacion in(".$cadena_items_seleccionados.")","",$conn);
 	
-	$html='<td>';
-	$salto=0;
-    for ($i=0; $i < $registros['numcampos']; $i++) { 
-        $html.="<input type='checkbox' name='item_radicacion[]' value='".$registros[$i]['idft_destino_radicacion']."' />".$registros[$i]['numero_item']."";
-        if($salto==5){
-        	$salto=0;
-        	$html.="<br>";
+	$html="<td>
+		<table style='width:100%;border-collapse:collapse;'  border='1px'>
+		<tr style='font-weight:bold;text-align:center;'>
+			<td>
+				
+			</td>
+			<td>
+				TR&Aacute;MITE
+			</td>
+			<td>
+				TIPO
+			</td>
+			<td>
+				Rad. Item
+			</td>
+			<td>
+				FECHA DE RECIBO
+			</td>	
+			<td>
+				ORIGEN	
+			</td>
+			<td>
+				DESTINO	
+			</td>	
+			<td>
+				ASUNTO
+			</td>																			
+		</tr>
+	";
+    for ($i=0; $i < $registros['numcampos']; $i++) {
+			
+	    $array_concat=array("nombres","' '","apellidos");
+		$cadena_concat=concatenar_cadena_sql($array_concat);
+		$origen=busca_filtro_tabla($cadena_concat." AS nombre","vfuncionario_dc","funcionario_codigo=".$registros[$i]['nombre_origen'],"",$conn);
+
+			if($registros[$i]['tipo_origen']==1){
+				$origen=busca_filtro_tabla("nombre,ciudad,direccion","vejecutor a","a.iddatos_ejecutor=".$registros[$i]['nombre_origen'],"",$conn);
+				if(!$origen['numcampos']){
+					$origen=busca_filtro_tabla("nombre,ciudad,direccion","vejecutor a","a.iddatos_ejecutor=".$registros[$i]['origen_externo'],"",$conn);
+				}
+				$ciudad=busca_filtro_tabla("nombre","municipio","idmunicipio=".$origen[0]['ciudad'],"",$conn);
+            	$ubicacion_origen=$ciudad[0]['nombre'].' '.$origen[0]['direccion'];
+				
+			}else{
+				if($registros[$i]['tipo_origen']==2 && ($registros[$i]['tipo_mensajeria']==2 || $registros[$i]['tipo_mensajeria']==1)){
+	    			$array_concat=array("nombres","' '","apellidos");
+					$cadena_concat=concatenar_cadena_sql($array_concat);					
+					$origen=busca_filtro_tabla($cadena_concat." AS nombre,dependencia","vfuncionario_dc a","a.iddependencia_cargo=".$registros[$i]['nombre_origen'],"",$conn);
+					$ubicacion_origen=$origen[0]['dependencia'];
+				}
+			}		
+				
+		
+        $ubicacion="";
+		if($registros[$i]["tipo_destino"]==1){
+		    $destino=busca_filtro_tabla("b.nombre,a.direccion,a.ciudad","datos_ejecutor a, ejecutor b","b.idejecutor=a.ejecutor_idejecutor AND a.iddatos_ejecutor=".$registros[$i]['nombre_destino'],"",$conn);
+	        if(!$destino['numcampos']){
+	        	$destino=busca_filtro_tabla("b.nombre,a.direccion,a.ciudad","datos_ejecutor a, ejecutor b","b.idejecutor=a.ejecutor_idejecutor AND a.iddatos_ejecutor=".$registros[$i]['destino_externo'],"",$conn);
+	        }			
+			$ciudad=busca_filtro_tabla("nombre","municipio","idmunicipio=".$destino[0]['ciudad'],"",$conn);
+            $ubicacion=$ciudad[0]['nombre'].' '.$destino[0]['direccion'];
+		}elseif($registros[$i]["tipo_destino"]==2){
+	    	$array_concat=array("nombres","' '","apellidos");
+			$cadena_concat=concatenar_cadena_sql($array_concat);			
+		    $destino=busca_filtro_tabla($cadena_concat." AS nombre,dependencia","vfuncionario_dc","iddependencia_cargo=".$registros[$i]['nombre_destino'],"",$conn);
+		    $ubicacion=$destino[0]['dependencia'];
+		}
+		$texto.='<tr>';
+		$fecha_radicacion=busca_filtro_tabla(fecha_db_obtener("fecha","Y-m-d")." as fecha,tipo_radicado","documento","iddocumento=".$registros[$i]["documento_iddocumento"],"",$conn);
+        
+        $tipo_radicado="";
+        if($fecha_radicacion[0]['tipo_radicado']==1){
+            $tipo_radicado="E";
         }else{
-        	$salto++;
+            $tipo_radicado="I";
         }
+        
+    	$tipo_tramite='ENTREGA';
+    	if(($registros[$i]['tipo_mensajeria']==2 || $registros[$i]['tipo_mensajeria']==1) && ($registros[$i]['estado_recogida']==0 || $registros[$i]['estado_recogida']=='estado_recogida') ){
+    		$tipo_tramite='RECOGIDA';
+    	}    	
+		
+    	 $html.="
+    	 
+    	 <tr>
+    	 	<td style='text-align:center; width:5%'>
+    	 		<input type='checkbox' name='item_radicacion[]' value='".$registros[$i]['idft_destino_radicacion']."' />
+    	 	</td>
+    	 	<td style='text-align:center; width:5%'>
+    	 		".$tipo_radicado."
+    	 	</td>    	 
+    	 	<td style='text-align:center; width:10%'>
+    	 		".$tipo_tramite."
+    	 	</td>
+    	 	<td style='text-align:center; width:5%'>
+    	 		".$registros[$i]['numero_item']."
+    	 	</td>
+    	 	<td style='text-align:center; width:10%'>
+    	 		".$fecha_radicacion[0]["fecha"]."
+    	 	</td>
+    	 	<td style='text-align:left; width:21,66%;'>
+    	 		".$origen[0]['nombre'].'<br><b>Ubicacion:</b>'.$ubicacion_origen."
+    	 	</td>
+    	 	<td style='text-align:left; width:21,66%;'>
+    	 		".$destino[0]["nombre"].'<br><b>Ubicacion:</b>'.$ubicacion."
+    	 	</td>
+    	 	<td style='text-align:left; width:21,66%;'>
+    	 		".$registros[$i]["descripcion"]."
+    	 	</td>
+		</tr>
+		
+		";
+		
+
+		
+		
+		
+		
     }	
-    $html.='</td>';
+    $html.="
+    </table>
+    </td>
+    ";
     echo($html);
 }
 function mostrar_numero_item_novedad($idformato,$iddoc){
