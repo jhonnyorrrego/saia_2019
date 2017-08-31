7eaa2668365ffff138e579749028dbefe80d1f24
Usuario generico cerok <info@cerok.com>
2017-08-31 13:04:41 -0500

    se unifica desarrollo de Alejandro Carvajal para obterne el total de registros

--- pantallas/busquedas/servidor_busqueda.php
+++ pantallas/busquedas/servidor_busqueda.php
@@ -268,40 +268,32 @@ if(@$_REQUEST["idbusqueda_temporal"]){
 		}
 	}
 }
-if(!@$_REQUEST["cantidad_total"]){
-	$result = ejecuta_filtro_tabla("SELECT COUNT(*) AS cant FROM ".$tablas_consulta." WHERE ".$condicion.$ordenar_consulta,$conn);
-	if($result["numcampos"]>1){
-		$_REQUEST["cantidad_total"]=$result["numcampos"];
-	}else{
-		$_REQUEST["cantidad_total"]=$result[0]["cant"];
+
+if (!@$_REQUEST["cantidad_total"]) {
+	$consulta_conteo = "SELECT COUNT(1) AS cant FROM " . $tablas_consulta . " WHERE " . $condicion . $ordenar_consulta;
+	if (MOTOR == 'SqlServer' || MOTOR == 'MSSql') {
+		$consulta_conteo = "WITH conteo AS (SELECT " . $campos_consulta . " FROM " . $tablas_consulta . " WHERE " . $condicion . $ordenar_consulta . ") SELECT COUNT(*) as cant FROM conteo";
+		$conteo_filas = $conn->Ejecutar_sql($consulta_conteo);
+		$result = phpmkr_fetch_array($conteo_filas);
+		$result[0] = array();
+		$result[0]['cant'] = $result['cant'];
+		$result["numcampos"] = $result['cant'];
+	} else {
+		if (strpos(strtolower($campos_consulta), "sum(") !== false || strpos(strtolower($campos_consulta), "avg(") !== false) {
+			$consulta_conteo = "SELECT COUNT(1) AS cant FROM (SELECT " . $campos_consulta . " FROM " . $tablas_consulta . " WHERE " . $condicion . $ordenar_consulta . ") as cant";
+		}
+		$result = ejecuta_filtro_tabla($consulta_conteo, $conn);
 	}
-}
-else{
-	$result["numcampos"]=@$_REQUEST["cantidad_total"];
-	$result[0]['cant']=@$_REQUEST["cantidad_total"];
-}
-
-/*
-if(!@$_REQUEST["cantidad_total"]){ //DESARROLLO ALEJANDRO CARVAJAL
-    $consulta_conteo = "SELECT COUNT(1) AS cant FROM " . $tablas_consulta . " WHERE " . $condicion . $ordenar_consulta;
-    if(MOTOR == 'SqlServer' || MOTOR == 'MSSql'){
-        $consulta_conteo = "WITH conteo AS (SELECT " . $campos_consulta . " FROM " . $tablas_consulta . " WHERE " . $condicion . $ordenar_consulta.") SELECT COUNT(*) as cant FROM conteo";
-    } else if(strpos(strtolower($campos_consulta), "sum(") !== false || strpos(strtolower($campos_consulta), "avg(") !== false) {
-        $consulta_conteo = "SELECT COUNT(1) AS cant FROM (SELECT " . $campos_consulta . " FROM " . $tablas_consulta . " WHERE " . $condicion . $ordenar_consulta.") as cant";
-    }
-    $conteo_filas = $conn->Ejecutar_sql($consulta_conteo);
-    $result=phpmkr_fetch_array($conteo_filas);
-    $result[0]=array();
-    $result[0]['cant']=$result['cant'];
-    $result["numcampos"]=$result['cant'];
-    $_REQUEST["cantidad_total"]=$result["numcampos"];
-
+	if ($result["numcampos"] > 1) {
+		$_REQUEST["cantidad_total"] = $result["numcampos"];
+	} else {
+		$_REQUEST["cantidad_total"] = $result[0]["cant"];
+	}
 } else {
-	$result["numcampos"]=@$_REQUEST["cantidad_total"];
-	$result[0]['cant']=@$_REQUEST["cantidad_total"];
+	$result["numcampos"] = @$_REQUEST["cantidad_total"];
+	$result[0]['cant'] = @$_REQUEST["cantidad_total"];
 }
 
-*/
 $response=new stdClass();
 $response->cantidad_total = $result[0]['cant'];
 $response->exito=0;
