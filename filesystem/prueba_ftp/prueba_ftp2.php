3e78f4b03f99367369c051270b7cce6778e0da63
cerok <info@cerok.com>
2017-11-07 16:33:09 +0000

    Cambios almacenamiento ftp

--- filesystem/prueba_ftp/prueba_ftp2.php
+++ filesystem/prueba_ftp/prueba_ftp2.php
@@ -15,9 +15,9 @@ file_put_contents($arch_origen, date("Y-m-d H:i:s"));
 
 $opciones_ftp = array(
 		'port'     => 21,
-		'username' => 'digitalizacion_saia',
+		'username' => 'digitalizacion',
 		'password' => 'cerok_saia421_5',
-		'passive'  => true,
+		'passive'  => false,
 		'create'   => true, // Whether to create the remote directory if it does not exist
 		'mode'     => FTP_BINARY, // Or FTP_TEXT
 		'ssl'      => false,
@@ -27,7 +27,7 @@ $recurso='pruebas_ftp/prueba.txt';
 
 // Los valores enviados en la URL: usuario, clave, puerto sobreescriben los del array
 //$ruta_servidor = "ftp://user:password@54.84.23.212/saia_php7/saia/temporal";
-$ruta_servidor = "ftp://54.84.23.212/saia_php7/saia/temporal";
+$ruta_servidor = "ftp://34.195.147.42/prueba_virtual/prueba_ftp/datos_origen";
 
 $saia_almacenamiento = new SaiaStorage();
 $saia_almacenamiento->set_opciones_ftp($opciones_ftp);
@@ -35,12 +35,12 @@ $saia_almacenamiento->resolver_adaptador($ruta_servidor);
 
 
 //print_r($saia_almacenamiento);die();
-$numbytes = $saia_almacenamiento->almacenar_recurso($recurso, $arch_origen);
+//$numbytes = $saia_almacenamiento->almacenar_recurso($recurso, $arch_origen);
 
-echo "Bytes enviados" . $numbytes;
+echo "Bytes enviados" . $numbytes; 
 
 //Leer remoto
-
+  
 $directorio_remoto = dirname($recurso);
 
 //print_r($directorio_remoto);die();
@@ -48,22 +48,21 @@ $directorio_remoto = dirname($recurso);
 $filesystem = $saia_almacenamiento->get_filesystem();
 
 // Listar el contenido del directorio remoto
-print_r($filesystem->listKeys($directorio_remoto));
-
-// Traer el contenido del directorio remoto
-$contenido = $filesystem->read($recurso);
-
-print_r($contenido);
-
-echo "Guardando archivo local<br>";
-
-$adapter = new Local(dirname($arch_origen));
+$lista_remoto=$filesystem->listKeys($directorio_remoto);
+print_r($lista_remoto);
+$ruta_final= __DIR__ . "/datos_origen";
+$adapter = new Local($ruta_final);
 $fs_local = new Filesystem($adapter);
 
-$numbytes = $fs_local->write('remoto.txt', $contenido, true);
-//$content = $adapter->read(basename($arch_origen));
-//$numbytes = $filesystem->write($recurso, $content, true);
-echo "Bytes guardatos" . $numbytes;
-
-
+foreach($lista_remoto["keys"] AS $key=>$ruta_remota){
+	$contenido = $filesystem->read($ruta_remota);
+	$nombre_archivo=basename($ruta_remota);
+	$numbytes = $fs_local->write($nombre_archivo, $contenido, true);
+	//$content = $adapter->read(basename($arch_origen));
+	//$numbytes = $filesystem->write($recurso, $content, true);
+	echo "Bytes guardatos" . $numbytes."<br>";
+	
+	echo($ruta_remota."<hr>");
+	
+} 
 ?>
--- sql_saia/migraciones_my/migrations-db.php
+++ sql_saia/migraciones_my/migrations-db.php
@@ -1,9 +1,9 @@
 <?php
 return [
-    'dbname' => 'saia_elastic_local',
+    'dbname' => 'saia_mezcla_final',
     'user' => 'saia',
-    'password' => 'cerok_saia',
-    'host' => 'localhost',
+    'password' => 'cerok_saia421_5',
+    'host' => 'saia-laboratorio.ct00qljbq3lp.us-east-1.rds.amazonaws.com',
     'driver' => 'pdo_mysql',
 	'port' => '3306'
 ];
