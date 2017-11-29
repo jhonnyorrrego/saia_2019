2e544eb2d215f21869ed2f461c92340850770f36
Cero K <info@cerok.com>
2017-11-29 21:58:46 +0000

    optimizacion de consultas al momento de listar los formatos para adicionar

--- pantallas/formato/listar_proceso_formatos.php
+++ pantallas/formato/listar_proceso_formatos.php
@@ -13,7 +13,7 @@ include_once($ruta_db_superior."librerias_saia.php");
 include_once($ruta_db_superior."pantallas/lib/encabezado_componente.php");
 usuario_actual("login");
 
-$consulta=busca_filtro_tabla('','categoria_formato','cod_padre=2 and estado=1','nombre ASC',$conn);
+$consulta=busca_filtro_tabla('idcategoria_formato,nombre','categoria_formato','cod_padre=2 and estado=1','nombre ASC',$conn);
 $adicional="";
 $request=array();
 foreach(@$_REQUEST as $id => $value){
@@ -32,7 +32,7 @@ if(count($request)){
 	      	
 	      		$idcategoria_formato=$consulta[$i]['idcategoria_formato'];
 			  	$mostrar=0;
-				$cuantos_formatos=busca_filtro_tabla("","formato","mostrar=1 AND (cod_padre IS NULL OR cod_padre=0) AND (fk_categoria_formato like'".$idcategoria_formato."' OR   fk_categoria_formato like'%,".$idcategoria_formato."'  OR   fk_categoria_formato like'".$idcategoria_formato.",%' OR   fk_categoria_formato like'%,".$idcategoria_formato.",%') AND (fk_categoria_formato like'2' OR   fk_categoria_formato like'%,2'  OR   fk_categoria_formato like'2,%' OR   fk_categoria_formato like'%,2,%') ","etiqueta ASC",$conn);
+				$cuantos_formatos=busca_filtro_tabla("nombre,ruta_adicionar","formato","mostrar=1 AND (cod_padre IS NULL OR cod_padre=0) AND (fk_categoria_formato like'".$idcategoria_formato."' OR   fk_categoria_formato like'%,".$idcategoria_formato."'  OR   fk_categoria_formato like'".$idcategoria_formato.",%' OR   fk_categoria_formato like'%,".$idcategoria_formato.",%') AND (fk_categoria_formato like'2' OR   fk_categoria_formato like'%,2'  OR   fk_categoria_formato like'2,%' OR   fk_categoria_formato like'%,2,%') ","etiqueta ASC",$conn);
 				
 				if($cuantos_formatos['numcampos']==1){
 					$url=$ruta_db_superior.'formatos/'.$cuantos_formatos[0]['nombre'].'/'.$cuantos_formatos[0]['ruta_adicionar']."?1=1";
