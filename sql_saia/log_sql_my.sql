49395b9a5085e37195c3cd83d70e057b974d244d
Usuario generico cerok <info@cerok.com>
2018-01-19 09:57:37 -0500

    migraciones para mezcla con saia_expedientes

--- sql_saia/log_sql_my.sql
+++ sql_saia/log_sql_my.sql
@@ -1091,7 +1091,7 @@ where idbusqueda_componente=320;
 -- FIN DESARROLLO NUEVO REPORTE EXPEDIENTES 20171005
 
 -- --------------------------------------------------------------------
--- REPORTE DE PRESTAMO ANIDADO AL REPORTE DE ARCHIVO(ESPEDIENTES)
+-- REPORTE DE PRESTAMO ANIDADO AL REPORTE DE ARCHIVO(EXPEDIENTES)
 
 INSERT INTO modulo (idmodulo, pertenece_nucleo, nombre, tipo, imagen, etiqueta, enlace, enlace_mobil, destino, cod_padre, orden, ayuda, parametros, busqueda_idbusqueda, permiso_admin, busqueda, enlace_pantalla) VALUES
 (1666, 0, 'modulo_reporte_expedientes_prestamo', 'secundario', 'botones/principal/defaut.png', 'Prestamo', 'pantallas/busquedas/consulta_busqueda_reporte.php?idbusqueda_componente=203', NULL, '_self', 0, 5, '', '', 0, 0, '1', 0);
@@ -1140,9 +1140,9 @@ UPDATE busqueda_grafico_serie SET valor = 'count(b.destino)' WHERE idbusqueda_gr
 
 UPDATE busqueda_grafico_serie SET valor = 'count(b.destino)' WHERE idbusqueda_grafico_serie = 30;
 
--- --------------------------------------------------------------------
+-- INICIO MIGRACION Version20180118194327.php
 
-DESARROLLO EXPEDIENTES BY JHON SEBASTIAN 2017/11/01
+-- DESARROLLO EXPEDIENTES BY JHON SEBASTIAN 2017/11/01
 
 ALTER TABLE expediente add (
 indice_uno varchar(255),
@@ -1169,53 +1169,57 @@ CREATE TABLE `cf_material` (
 INSERT INTO `cf_material` (`idcf_material`, `nombre`, `valor`, `cod_padre`, `descripcion`, `tipo`, `categoria`, `estado`) VALUES
 (1, 'Carton', '1', NULL, NULL, NULL, NULL, 1),
 (2, 'Otro', '2', NULL, NULL, NULL, NULL, 1);
--- -----------------------------------------------------------------------
-DESARROLLO EXPEDIENTES BY JOHN HARRISON RODRIGUEZ BOTERO <john.rodriguez@cerok.com> 2017/11/01
 
+UPDATE busqueda SET cantidad_registros = '100', etiqueta = 'Inventario Documental', campos = 'a.fecha,a.nombre,a.descripcion,a.cod_arbol,a.estado_cierre,a.nombre_serie,a.propietario,a.fecha_extrema_i,a.fecha_extrema_f,a.no_unidad_conservacion,a.no_folios,a.no_carpeta,a.soporte,a.notas_transf,a.tomo_no,a.indice_uno,a.indice_dos,a.indice_tres' 
+WHERE busqueda.nombre='reporte_expediente_grid'; --idbusqueda = 115;
 
-ALTER TABLE expediente ADD indice_uno VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER agrupador, ADD indice_dos VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER indice_uno, ADD indice_tres VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER indice_dos;
+UPDATE busqueda_componente SET etiqueta = 'Inventario Documental', 
+info = 'CODIGO|{*cod_arbol*}|left|80|-|Nombre de la serie subserie o asuntos|{*descripcion*}|left|200|-|Fecha Extrema Inicial|{*fecha_extrema_i*}|left|150|-|Fecha Extrema Final|{*fecha_extrema_f*}|left|150|-|Unidad de conservacion|{*no_unidad_conservacion*}|left|180|-|Folios|{*no_folios*}|left|80|-|Soporte|{*soporte*}|left|80|-|Notas|{*notas_transf*}|left|180|' 
+WHERE busqueda_componente.nombre='reporte_expediente_grid_exp'; -- idbusqueda_componente = 320;
 
-UPDATE busqueda SET campos = 'a.fecha,a.nombre,a.descripcion,a.cod_arbol,a.estado_cierre,a.nombre_serie,a.propietario,a.fecha_extrema_i,a.fecha_extrema_f,a.no_unidad_conservacion,a.no_folios,a.no_carpeta,a.soporte,a.notas_transf,a.tomo_no,a.indice_uno,a.indice_dos,a.indice_tres' WHERE busqueda.idbusqueda = 115;
+UPDATE busqueda_condicion SET codigo_where = '1=1 and agrupador=0 {*filtro_cod_arbol*}' 
+WHERE busqueda_condicion.etiqueta_condicion='condicion_reporte_expediente_grid'; -- idbusqueda_condicion = 245;
 
-UPDATE busqueda SET etiqueta = 'Inventario Documental' WHERE busqueda.idbusqueda = 115;
+UPDATE busqueda_componente SET info = 'CODIGO|{*cod_arbol*}|left|80|-|Nombre expediente|{*nombre*}|left|200|-|Nombre de la serie subserie o asuntos|{*descripcion*}|left|200|-|Fecha Extrema Inicial|{*fecha_extrema_i*}|left|150|-|Fecha Extrema Final|{*fecha_extrema_f*}|left|150|-|Unidad de conservacion|{*no_unidad_conservacion*}|left|180|-|Folios|{*no_folios*}|left|80|-|Soporte|{*soporte*}|left|80|-|Notas|{*notas_transf*}|left|180|' 
+WHERE busqueda_componente.nombre='reporte_expediente_grid_exp'; -- idbusqueda_componente = 320;
 
-UPDATE busqueda_componente SET etiqueta = 'Inventario Documental' WHERE busqueda_componente.idbusqueda_componente = 320;
+UPDATE busqueda_componente SET info = 'CODIGO|{*cod_arbol*}|left|80|-|Nombre expediente|{*direcciona_nombre@idexpediente,nombre*}|left|200|-|RETENCI&Oacute;N|{*fecha_reten@idexpediente*}|left|200|-|Nombre de la serie subserie o asuntos|{*descripcion*}|left|200|-|Fecha Extrema Inicial|{*fecha_extrema_i*}|left|150|-|Fecha Extrema Final|{*fecha_extrema_f*}|left|150|-|Unidad de conservacion|{*no_unidad_conservacion*}|left|180|-|Folios|{*no_folios*}|left|80|-|Soporte|{*soporte*}|left|80|-|Notas|{*notas_transf*}|left|180|' 
+WHERE busqueda_componente.nombre='reporte_expediente_grid_exp'; -- idbusqueda_componente = 320;
 
-UPDATE busqueda_componente SET info = 'CODIGO|{*cod_arbol*}|left|80|-|Nombre de la serie subserie o asuntos|{*descripcion*}|left|200|-|Fecha Extrema Inicial|{*fecha_extrema_i*}|left|150|-|Fecha Extrema Final|{*fecha_extrema_f*}|left|150|-|Unidad de conservacion|{*no_unidad_conservacion*}|left|180|-|Folios|{*no_folios*}|left|80|-|Soporte|{*soporte*}|left|80|-|Notas|{*notas_transf*}|left|180|' WHERE busqueda_componente.idbusqueda_componente = 320;
+UPDATE busqueda_componente SET info = '<div title=\"Cajas\" data-load=\'{\"kConnector\":\"iframe\",\"url\":\"../pantallas/busquedas/consulta_busqueda_caja.php?idbusqueda_componente=323\",\"kTitle\":\"Cajas\",\"kWidth\":\"320px\"}\' class=\"items navigable\">\r\n<div class=\"head\"></div>\r\n<div class=\"label\">Cajas</div>\r\n<div class=\"tail\"></div>\r\n</div>' 
+WHERE busqueda_componente.nombre = 'cajas'; -- idbusqueda_componente = 160;
 
-UPDATE busqueda SET cantidad_registros = '100' WHERE busqueda.idbusqueda = 115;
+UPDATE busqueda_componente SET direccion = 'asc',ordenado_por='a.fecha' WHERE busqueda_componente.nombre='reporte_docs_expediente_grid_exp'; --idbusqueda_componente = 321;
 
-UPDATE busqueda_condicion SET codigo_where = '1=1 and agrupador=0 {*filtro_cod_arbol*}' WHERE busqueda_condicion.idbusqueda_condicion = 245;
+UPDATE busqueda_componente SET etiqueta = 'Indice de Expediente', info = 'expediente_idexpediente|{*expediente_idexpediente*}|left|200|-|documento_iddocumento|{*documento_iddocumento*}|left|200|-|fecha|{*fecha*}|left' 
+WHERE busqueda_componente.nombre='reporte_docs_expediente_grid_exp'; -- idbusqueda_componente = 321;
 
-UPDATE busqueda_componente SET info = 'CODIGO|{*cod_arbol*}|left|80|-|Nombre expediente|{*nombre*}|left|200|-|Nombre de la serie subserie o asuntos|{*descripcion*}|left|200|-|Fecha Extrema Inicial|{*fecha_extrema_i*}|left|150|-|Fecha Extrema Final|{*fecha_extrema_f*}|left|150|-|Unidad de conservacion|{*no_unidad_conservacion*}|left|180|-|Folios|{*no_folios*}|left|80|-|Soporte|{*soporte*}|left|80|-|Notas|{*notas_transf*}|left|180|' WHERE busqueda_componente.idbusqueda_componente = 320;
+UPDATE busqueda SET etiqueta = 'Indice de Expediente' WHERE busqueda.nombre='eporte_docs_expediente_grid'; -- idbusqueda = 116;
 
-UPDATE busqueda_componente SET info = 'CODIGO|{*cod_arbol*}|left|80|-|Nombre expediente|{*direcciona_nombre@idexpediente,nombre*}|left|200|-|RETENCI&Oacute;N|{*fecha_reten@idexpediente*}|left|200|-|Nombre de la serie subserie o asuntos|{*descripcion*}|left|200|-|Fecha Extrema Inicial|{*fecha_extrema_i*}|left|150|-|Fecha Extrema Final|{*fecha_extrema_f*}|left|150|-|Unidad de conservacion|{*no_unidad_conservacion*}|left|180|-|Folios|{*no_folios*}|left|80|-|Soporte|{*soporte*}|left|80|-|Notas|{*notas_transf*}|left|180|' WHERE busqueda_componente.idbusqueda_componente = 320;
+UPDATE busqueda SET campos = 'a.nombre,a.codigo_numero,a.nombre_serie,a.fecha_extrema_i,a.fecha_extrema_f,a.indice_uno,a.indice_dos,a.indice_tres,a.no_folios,a.no_unidad_conservacion,a.soporte,a.notas_transf'
+WHERE busqueda.nombre='reporte_expediente_grid'; -- idbusqueda = 115;
 
-UPDATE busqueda_componente SET info = '<div title=\"Cajas\" data-load=\'{\"kConnector\":\"iframe\",\"url\":\"../pantallas/busquedas/consulta_busqueda_caja.php?idbusqueda_componente=323\",\"kTitle\":\"Cajas\",\"kWidth\":\"320px\"}\' class=\"items navigable\">\r\n<div class=\"head\"></div>\r\n<div class=\"label\">Cajas</div>\r\n<div class=\"tail\"></div>\r\n</div>' WHERE busqueda_componente.idbusqueda_componente = 160;
+UPDATE busqueda_condicion SET codigo_where = '1=1 and agrupador=0 {*filtro_cod_arbol*}{*tipo_expediente*}' 
+WHERE busqueda_condicion.idbusqueda_condicion = 245;
+
+-- DESARROLLO EXPEDIENTES BY JOHN HARRISON RODRIGUEZ BOTERO <john.rodriguez@cerok.com> 2017/11/01
+
+ALTER TABLE expediente ADD indice_uno  VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER agrupador, 
+                       ADD indice_dos  VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER indice_uno, 
+                       ADD indice_tres VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER indice_dos;
 
 INSERT INTO busqueda_componente ( busqueda_idbusqueda, tipo, conector, url, etiqueta, nombre, orden, info, exportar, exportar_encabezado, encabezado_componente, estado, ancho, cargar, campos_adicionales, tablas_adicionales, ordenado_por, direccion, agrupado_por, busqueda_avanzada, acciones_seleccionados, modulo_idmodulo, menu_busqueda_superior, enlace_adicionar, encabezado_grillas) VALUES
 (48, 3, 2, 'pantallas/busquedas/consulta_busqueda_caja.php', 'Cajas', 'cajas1', 1, '<div id=\"resultado_pantalla_{*idcaja*}\" class=\"well\"><div class=\"btn btn-mini enlace_caja pull-right\" idregistro=\"{*idcaja*}\" title=\"{*no_consecutivo*}\"><i class=\"icon-info-sign\"></i></div>{*enlaces_adicionales_caja@idcaja,no_consecutivo*}<div class=\"btn btn-mini enlace_caja tooltip_saia pull-right\" idregistro=\"{*idcaja*}\" title=\"Asignar {*no_consecutivo*}\" enlace=\"pantallas/caja/asignar_caja.php?idcaja={*idcaja*}\"><i class=\"icon-lock\"></i></div><i class=\' icon-folder-open pull-left\'></i>{*enlace_caja@idcaja,no_consecutivo*}<br/><div class=\'descripcion_documento\'>{*obtener_descripcion_caja@fondo,seccion,subseccion,codigo*}</div></div>', NULL, NULL, NULL, 2, 320, 2, NULL, '', 'a.idcaja', 'desc', '', 'pantallas/caja/buscar_caja.php?idbusqueda_componente=161', NULL, NULL, 'barra_superior_busqueda', '', NULL);
 
-UPDATE busqueda_componente SET direccion = 'asc',ordenado_por='a.fecha' WHERE busqueda_componente.idbusqueda_componente = 321;
-
-UPDATE busqueda_componente SET info = 'Radicado|{*radicado_exp_doc@documento_iddocumento*}|left|200|-|Tipo de documento|{*tipo_doc@documento_iddocumento*}|left|200|-|Descripcion|{*descripcion_doc@documento_iddocumento*}|left|200|-|Fecha|{*fecha*}|left' WHERE busqueda_componente.idbusqueda_componente = 321;
-
-UPDATE busqueda SET etiqueta = 'Indice de Expediente' WHERE busqueda.idbusqueda = 116;
-
-UPDATE busqueda_componente SET etiqueta = 'Indice de Expediente' WHERE busqueda_componente.idbusqueda_componente = 321;
-
 CREATE OR REPLACE VIEW vexpediente_serie AS select a.propietario AS propietario,c.nombre AS nombre_serie,a.fecha AS fecha,a.nombre AS nombre,a.codigo_numero AS codigo_numero,a.descripcion AS descripcion,a.cod_arbol AS cod_arbol,a.cod_padre AS cod_padre,a.estado_archivo AS estado_archivo,a.fk_idcaja AS fk_idcaja,a.estado_cierre AS estado_cierre,a.idexpediente AS idexpediente,b.entidad_identidad AS identidad_exp,b.llave_entidad AS llave_exp,d.entidad_identidad AS identidad_ser,d.llave_entidad AS llave_ser,d.estado AS estado_entidad_serie,a.prox_estado_archivo AS prox_estado_archivo,a.fecha_extrema_i AS fecha_extrema_i,a.fecha_extrema_f AS fecha_extrema_f,a.no_unidad_conservacion AS no_unidad_conservacion,a.no_folios AS no_folios,a.no_carpeta AS no_carpeta,a.soporte AS soporte,a.notas_transf AS notas_transf,a.tomo_no AS tomo_no,a.agrupador AS agrupador,a.indice_uno AS indice_uno,a.indice_dos AS indice_dos,a.indice_tres AS indice_tres from (((expediente a left join entidad_expediente b on((a.idexpediente = b.expediente_idexpediente))) left join serie c on((a.serie_idserie = c.idserie))) left join entidad_serie d on((c.idserie = d.serie_idserie)))
 
-UPDATE busqueda_condicion SET codigo_where = '1=1 and agrupador=0 {*filtro_cod_arbol*}{*tipo_expediente*}' WHERE busqueda_condicion.idbusqueda_condicion = 245;
+UPDATE busqueda_componente SET info = 'CODIGO|{*codigo_numero*}|left|80|-|NOMBRE DEL EXPEDIENTE|{*direcciona_nombre@idexpediente,nombre*}|left|200|-|SERIE|{*nombre_serie*}|left|200|-|FECHA INICIAL|{*fecha_extrema_i*}|center|80|-|FECHA FINAL|{*fecha_extrema_f*}|center|80|-|INDICE 1|{*indice_uno*}|left|130|-|INDICE 2|{*indice_dos*}|left|130|-|INDICE 3|{*indice_tres*}|left|130|-|FOLIOS|{*no_folios*}|center|80|-|UNID CONSERV|{*no_unidad_conservacion*}|left|80|SOPORTE 3|{*soporte*}|left|80|-|NOTAS|{*notas_transf*}|left|200|' 
+WHERE busqueda_componente.idbusqueda_componente = 320;
 
-UPDATE busqueda SET campos = 'a.nombre,a.codigo_numero,a.nombre_serie,a.fecha_extrema_i,a.fecha_extrema_f,a.indice_uno,a.indice_dos,a.indice_tres,a.no_folios,a.no_unidad_conservacion,a.soporte,a.notas_transf' WHERE busqueda.idbusqueda = 115;
+UPDATE busqueda_componente SET info = 'Radicado|{*radicado_exp_doc@documento_iddocumento*}|left|80|-|Tipo de documento|{*tipo_doc@documento_iddocumento*}|left|180|-|Descripcion|{*descripcion_doc@documento_iddocumento*}|left|220|-|Fecha|{*fecha*}|center' 
+WHERE busqueda_componente.idbusqueda_componente = 321;
 
-UPDATE busqueda_componente SET info = 'CODIGO|{*codigo_numero*}|left|80|-|NOMBRE DEL EXPEDIENTE|{*direcciona_nombre@idexpediente,nombre*}|left|200|-|SERIE|{*nombre_serie*}|left|200|-|FECHA INICIAL|{*fecha_extrema_i*}|center|80|-|FECHA FINAL|{*fecha_extrema_f*}|center|80|-|INDICE 1|{*indice_uno*}|left|130|-|INDICE 2|{*indice_dos*}|left|130|-|INDICE 3|{*indice_tres*}|left|130|-|FOLIOS|{*no_folios*}|center|80|-|UNID CONSERV|{*no_unidad_conservacion*}|left|80|SOPORTE 3|{*soporte*}|left|80|-|NOTAS|{*notas_transf*}|left|200|' WHERE busqueda_componente.idbusqueda_componente = 320;
-
-UPDATE busqueda_componente SET info = 'Radicado|{*radicado_exp_doc@documento_iddocumento*}|left|80|-|Tipo de documento|{*tipo_doc@documento_iddocumento*}|left|180|-|Descripcion|{*descripcion_doc@documento_iddocumento*}|left|220|-|Fecha|{*fecha*}|center' WHERE busqueda_componente.idbusqueda_componente = 321;
-
--- -----------------------------------------------------------------------
-DESARROLLO EXPEDIENTES BY Hernando Trejos <hernando.trejos@cerok.com> 2017/11/01
+-- DESARROLLO EXPEDIENTES BY Hernando Trejos <hernando.trejos@cerok.com> 2017/11/01
 
 ALTER TABLE `expediente` ADD `dependencia_iddependencia` INT NOT NULL AFTER `serie_idserie`;
 
@@ -1223,10 +1227,14 @@ ALTER TABLE `entidad_expediente` CHANGE `fecha` `fecha` DATETIME NULL DEFAULT NU
 
 ALTER TABLE `caja` ADD `dependencia_iddependencia` INT NOT NULL AFTER `serie_idserie`;
 
-UPDATE `busqueda_componente` SET `campos_adicionales` = 'b.documento_iddocumento, b.idft_solicitud_prestamo, b.serie_idserie, b.estado_documento, b.anexos, b.estado_devolucion, b.documento_archivo, b.observaciones, b.nombre_solicita, b.fecha, b.funcionario_prestamo, b.funcionario_devoluci, b.fecha_prestamo_rep, b.fecha_devolucion_rep, b.transferencia_presta'WHERE `busqueda_componente`.`idbusqueda_componente` = 203;
+UPDATE busqueda_componente SET campos_adicionales = 'b.documento_iddocumento, b.idft_solicitud_prestamo, b.serie_idserie, b.estado_documento, b.anexos, b.documento_archivo, b.observaciones, b.nombre_solicita, b.fecha, b.fecha_prestamo_rep, b.fecha_devolucion_rep, b.transferencia_presta,c.idft_item_prestamo_exp, c.fecha_prestamo, c.fecha_devolucion, c.estado_prestamo, c.funcionario_prestamo, c.observacion_prestamo,c.fecha_devolucion,c.observacion_devolver,c.funcionario_devoluci,c.fk_expediente'
+WHERE busqueda_componente.nombre='reporte_solicitud_prestamo'; -- idbusqueda_componente` = 203;
 
--- -----------------------------------------------------------------------
-DESARROLLO EXPEDIENTES BY Ruben Pulgarin <ruben.pulgarin@cerok.com> 2017/11/01
+-- FIN MIGRACION Version20180118194327.php
+
+-- INICIO MIGRACION Version20180118223002.php
+
+-- DESARROLLO EXPEDIENTES BY Ruben Pulgarin <ruben.pulgarin@cerok.com> 2017/11/01
 
 UPDATE campos_formato SET etiqueta_html = 'checkbox' WHERE campos_formato.idcampos_formato = 5205;	
 
@@ -1252,7 +1260,6 @@ INSERT INTO funciones_formato_accion (idfunciones_formato_accion, idfunciones_fo
 (307, 958, 3, 412, 'POSTERIOR', 1, 1);
 
 UPDATE campos_formato SET etiqueta_html = 'checkbox' WHERE campos_formato.idcampos_formato = 3995;	
-	
 
 INSERT INTO busqueda (idbusqueda, nombre, etiqueta, estado, ancho, campos, llave, tablas, ruta_libreria, ruta_libreria_pantalla, cantidad_registros, tiempo_refrescar, ruta_visualizacion, tipo_busqueda, badge_cantidades) VALUES
 (54, 'reporte_solicitud_prestamo', 'Prestamo', 1, 200, 'a.fecha,a.ejecutor', 'a.iddocumento', 'documento a', 'formatos/solicitud_prestamo/librerias.php', 'formatos/solicitud_prestamo/funciones_js.php', 30, 500, 'pantallas/busquedas/consulta_busqueda_reporte.php', 2, NULL);
@@ -1263,14 +1270,14 @@ INSERT INTO busqueda_componente (idbusqueda_componente, busqueda_idbusqueda, tip
 INSERT INTO busqueda_condicion (idbusqueda_condicion, busqueda_idbusqueda, fk_busqueda_componente, codigo_where, etiqueta_condicion) VALUES
 (170, NULL, 203, 'b.idft_solicitud_prestamo=c.ft_solicitud_prestamo AND a.iddocumento=b.documento_iddocumento and lower(a.estado) not in(''eliminado'',''anulado'',''activo'')', 'condicion_reporte_reserva_documentos');
 
+UPDATE busqueda SET ruta_libreria = 'pantallas/expediente/funciones_reporte_grid.php,pantallas/expediente/librerias.php' WHERE busqueda.idbusqueda = 115;
 
 UPDATE busqueda_condicion SET codigo_where = '1=1 and agrupador=0 {*filtro_cod_arbol*}{*tipo_expediente*} AND {*expedientes_asignados*}' WHERE busqueda_condicion.idbusqueda_condicion = 245;
-
-UPDATE busqueda SET ruta_libreria = 'pantallas/expediente/funciones_reporte_grid.php,pantallas/expediente/librerias.php' WHERE busqueda.idbusqueda = 115;
 
 INSERT INTO modulo (idmodulo, pertenece_nucleo, nombre, tipo, imagen, etiqueta, enlace, enlace_mobil, destino, cod_padre, orden, ayuda, parametros, busqueda_idbusqueda, permiso_admin, busqueda, enlace_pantalla) VALUES
 (1669, 0, 'permiso_armin_archivo', 'secundario', 'botones/principal/defaut.png', 'Administraci&oacute;n de Archivo', '#', NULL, '_self', 45, 2, '', '', 0, 0, '', 0);
 
+-- FIN MIGRACION Version20180118223002.php
 
 -- ------------------------------------------------------------------------
 -- DESARROLLO OPCIONES PRINCIPALES DE SAIA BAJO PERMISO <andres.agudelo>
