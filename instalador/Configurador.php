98787a592cec8676964c2130934a4e726360013c
Usuario generico cerok <info@cerok.com>
2018-02-09 16:27:16 -0500

    error clase redefinida

--- instalador/Configurador.php
+++ instalador/Configurador.php
@@ -1,4 +1,6 @@
 <?php
+namespace Saia\Composer;
+
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
