a817c49e09a077fb8123c1d486e30d0e428825b3
Usuario generico cerok <info@cerok.com>
2018-02-14 11:48:10 -0500

    se incluyen validaciones en la captura de datos

--- instalador/Configurador.php
+++ instalador/Configurador.php
@@ -4,21 +4,14 @@ namespace Saia\Composer;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
+use Symfony\Component\Console\Question\Question;
 
 class Configuracion extends Command {
 
     protected $dependencies = array();
 
-    private $parametros = array(
-        "dbengine" => "Motor (MySql, Oracle, SqlServer, MSSql, Postgres)",
-        "dbuser" => "Usuario bdd",
-        "dbpass" => "Clave bdd",
-        "dbhost" => "Servidor",
-        "dbname" => "Instancia bdd", // INSTANCIA
-        "dbschema" => "Esquema (o bdd si es MySql)", // ESQUEMA
-        "tablespace" => "Tablespace (o bdd si es MySql)", // TABLESPACE
-        "dbport" => "Puerto"
-    );
+    private $parametros;
 
     /**
      * Relacion con los drivers de doctrine
@@ -28,45 +21,112 @@ class Configuracion extends Command {
      * pdo_sqlsrv: A Microsoft SQL Server driver that uses pdo_sqlsrv PDO Note that this driver caused problems in our tests. Prefer the sqlsrv driver if possible.
      */
     protected $motores = array(
-        "MySql"     => "pdo_mysql",
-        "Oracle"    => "pdo_oci",
+        "MySql" => "pdo_mysql",
+        "Oracle" => "pdo_oci",
         "SqlServer" => "pdo_sqlsrv",
-        "MSSql"     => "pdo_sqlsrv",
-        "Postgres"  => "pdo_pgsql"
+        "MSSql" => "pdo_sqlsrv",
+        "Postgres" => "pdo_pgsql"
     );
 
     private $valores = array();
 
     protected function execute(InputInterface $input, OutputInterface $output) {
-        $dialog = $this->getHelperSet()->get('dialog');
+        $helper = $this->getHelperSet()->get('question');
         $output->writeln('<info>Por favor ingrese los valores solicitados</info>');
         $motor = "";
         foreach ($this->parametros as $key => $pregunta) {
-            if($motor == "MySql" && in_array($key, array("dbschema","tablespace"))) {
+            if ($motor == "MySql" && in_array($key, array(
+                "dbschema",
+                "tablespace"
+            ))) {
                 $this->valores[$key] = $this->valores["dbname"];
                 continue;
             }
-            $un_valor = $dialog->ask($output, '<question>' . $pregunta . ': </question>');
+            $un_valor = $helper->ask($input, $output, $pregunta);
 
-            if($key == "dbengine") {
-                if(in_array($un_valor, array_keys($this->motores))) {
-                    $this->valores[$key] = $this->motores[$un_valor];
+            if ($un_valor) {
+                if ($key == "dbengine") {
                     $motor = $un_valor;
-                } else {
-                    $output->writeln(sprintf('<error>El valor para Motor debe ser: %s</error>', $pregunta, implode(", ", array_keys($this->motores))));
-                    return 1;
+                    // $this->valores[$key] = $this->motores[$un_valor];
+                    $this->valores["driver"] = $this->motores[$un_valor];
                 }
-            } else if ($un_valor) {
                 $this->valores[$key] = $un_valor;
-                // $output->writeln(sprintf('<info>Package %s was succesfully registered</info>', $package));
             } else {
-                $output->writeln(sprintf('<error>Debe ingresar un valor para </error>', $pregunta));
+                $output->writeln(sprintf('<error>Debe ingresar un valor: %s</error>', $pregunta->getQuestion()));
             }
         }
+        var_dump($this->valores);
     }
 
     protected function configure() {
         $this->setName('configurar')->setDescription("Obtener parámetros de configuración");
+        $this->parametros = array();
+        $preguntaMotor = new ChoiceQuestion('Seleccione el Motor (Por defecto MySql)', array(
+            "MySql",
+            "Oracle",
+            "SqlServer",
+            "MSSql",
+            "Postgres"
+        ), 0);
+
+        $validar_vacio = function ($value) {
+            if (trim($value) == '') {
+                throw new \Exception('El valor no puede ser vacio');
+            }
+
+            return $value;
+        };
+        $validar_url = function ($value) {
+            if (trim($value) == '') {
+                throw new \Exception('El valor no puede ser vacio');
+            }
+            $url = $value;
+            if(substr($value, 0, strlen("http")) !== "http") {
+                $url = "http://" . $value;
+            }
+
+            if (filter_var($url, FILTER_VALIDATE_URL) === false) {
+                throw new \Exception('Url incorrecta: ' . $url);
+            }
+            return $value;
+        };
+        $normalizer = function ($value) {
+            return trim($value);
+        };
+        $preguntaMotor->setErrorMessage('El motor de base de datos %s no es válido.');
+        $preguntaUsuario = new Question("Usuario bdd: ");
+        $preguntaPw = new Question('Clave bdd: ');
+        $preguntaPw->setHidden(true);
+        // $preguntaPw->setValidator($validar_vacio);
+        $preguntaServidorBdd = new Question("Servidor bdd: ");
+        $preguntaServidorWeb = new Question("Dominio/IP instalación: ");
+        $preguntaBdd = new Question("Instancia bdd: ");
+        $preguntaEsquema = new Question("Esquema (o bdd si es MySql): ");
+        $preguntaTableSpace = new Question("Tablespace (o bdd si es MySql): ");
+        $preguntaPuerto = new Question("Puerto bdd: ");
+        // $preguntaPw->setHiddenFallback(false);
+        $this->parametros["dbengine"] = $preguntaMotor;
+        $this->parametros["dbuser"] = $preguntaUsuario;
+        $this->parametros["dbpass"] = $preguntaPw;
+        $this->parametros["dbhost"] = $preguntaServidorBdd;
+        $this->parametros["dbname"] = $preguntaBdd; // INSTANCIA
+        $this->parametros["dbschema"] = $preguntaEsquema; // ESQUEMA
+        $this->parametros["tablespace"] = $preguntaTableSpace; // TABLESPACE
+        $this->parametros["dbport"] = $preguntaPuerto;
+        // Establecer los validadores
+        foreach ($this->parametros as $key => $pregunta) {
+            if ($pregunta instanceof ChoiceQuestion) {
+                continue;
+            }
+            $pregunta->setValidator($validar_vacio);
+            $pregunta->setNormalizer($normalizer);
+            $pregunta->setMaxAttempts(3);
+        }
+
+        $preguntaServidorWeb->setValidator($validar_url);
+        $preguntaServidorWeb->setNormalizer($normalizer);
+        $preguntaServidorWeb->setMaxAttempts(3);
+        $this->parametros["urlsaia"] = $preguntaServidorWeb;
     }
 
     public function get_valores() {
