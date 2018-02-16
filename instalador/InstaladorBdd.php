ce77bbfd24db80f78e812bf151a47fe8d6df2477
Usuario generico cerok <info@cerok.com>
2018-02-16 11:13:40 -0500

    incluir vistas y procedimientos

--- instalador/InstaladorBdd.php
+++ instalador/InstaladorBdd.php
@@ -5,32 +5,35 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Doctrine\Common\ClassLoader;
-
 use Doctrine\DBAL\DriverManager;
 use Doctrine\DBAL\Configuration;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Tools\Setup;
+use Doctrine\DBAL\Schema\View;
 
 class ImportDbCommand extends Command {
 
     /*
-    driver: The built-in driver implementation to use. The following drivers are currently available:
-
-    pdo_mysql: A MySQL driver that uses the pdo_mysql PDO extension.
-    drizzle_pdo_mysql: A Drizzle driver that uses pdo_mysql PDO extension.
-    mysqli: A MySQL driver that uses the mysqli extension.
-    pdo_sqlite: An SQLite driver that uses the pdo_sqlite PDO extension.
-    pdo_pgsql: A PostgreSQL driver that uses the pdo_pgsql PDO extension.
-    pdo_oci: An Oracle driver that uses the pdo_oci PDO extension. Note that this driver caused problems in our tests. Prefer the oci8 driver if possible.
-    pdo_sqlsrv: A Microsoft SQL Server driver that uses pdo_sqlsrv PDO Note that this driver caused problems in our tests. Prefer the sqlsrv driver if possible.
-    sqlsrv: A Microsoft SQL Server driver that uses the sqlsrv PHP extension.
-    oci8: An Oracle driver that uses the oci8 PHP extension.
-    sqlanywhere: A SAP Sybase SQL Anywhere driver that uses the sqlanywhere PHP extension.
-    */
-
+     * driver: The built-in driver implementation to use. The following drivers are currently available:
+     * pdo_mysql: A MySQL driver that uses the pdo_mysql PDO extension.
+     * drizzle_pdo_mysql: A Drizzle driver that uses pdo_mysql PDO extension.
+     * mysqli: A MySQL driver that uses the mysqli extension.
+     * pdo_sqlite: An SQLite driver that uses the pdo_sqlite PDO extension.
+     * pdo_pgsql: A PostgreSQL driver that uses the pdo_pgsql PDO extension.
+     * pdo_oci: An Oracle driver that uses the pdo_oci PDO extension. Note that this driver caused problems in our tests. Prefer the oci8 driver if possible.
+     * pdo_sqlsrv: A Microsoft SQL Server driver that uses pdo_sqlsrv PDO Note that this driver caused problems in our tests. Prefer the sqlsrv driver if possible.
+     * sqlsrv: A Microsoft SQL Server driver that uses the sqlsrv PHP extension.
+     * oci8: An Oracle driver that uses the oci8 PHP extension.
+     * sqlanywhere: A SAP Sybase SQL Anywhere driver that uses the sqlanywhere PHP extension.
+     */
     protected $install_dir;
+
     protected $configuracion;
+
+    protected $connectionParams;
+
+    protected $connection;
 
     public function __construct($vendor_dir, $configuracion) {
         parent::__construct();
@@ -40,65 +43,69 @@ class ImportDbCommand extends Command {
 
     protected function execute(InputInterface $input, OutputInterface $output) {
 
-        //$output->writeln('<info>Directorio: ' . $this->install_dir . '</info>');
-        //echo $this->vendor_dir . PHP_EOL;
-        if(empty($this->configuracion->get_valores()) ) {
+        // $output->writeln('<info>Directorio: ' . $this->install_dir . '</info>');
+        // echo $this->vendor_dir . PHP_EOL;
+        if (empty($this->configuracion->get_valores())) {
             $output->writeln('<info>Primero debe ejecutar la tarea "configurar"</info>');
             return 1;
         }
 
         $valores = $this->configuracion->get_valores();
-        //echo __DIR__ . PHP_EOL;
-        //require_once realpath(__DIR__ . '/../vendor/doctrine/common/lib/Doctrine/Common/ClassLoader.php');
-        require_once (__DIR__  . '/../vendor/autoload.php');
-        //require '../vendor/doctrine/common/lib/Doctrine/Common/ClassLoader.php';
+        // echo __DIR__ . PHP_EOL;
+        // require_once realpath(__DIR__ . '/../vendor/doctrine/common/lib/Doctrine/Common/ClassLoader.php');
+        require_once (__DIR__ . '/../vendor/autoload.php');
+        // require '../vendor/doctrine/common/lib/Doctrine/Common/ClassLoader.php';
         $config = new Configuration();
-        $connectionParams = array(
+        $this->connectionParams = array(
             'dbname' => $valores["dbname"],
             'user' => $valores["dbuser"],
             'password' => $valores["dbpass"],
             'host' => $valores["dbhost"],
-            'driver' => $valores["driver"],
+            'driver' => $valores["driver"]
         );
 
         date_default_timezone_set("America/Bogota");
 
         $output->writeln('<info>Creando las tablas en base de datos</info>');
-        $this->crearBaseDeDatos($connectionParams);
+        $this->crearBaseDeDatos();
         $output->writeln('');
-
         $output->writeln('<info>Creando tablas adicionales</info>');
-        $this->crearTablasAdicionales($connectionParams);
+        $this->crearTablasAdicionales();
+        $output->writeln('');
+        $output->writeln('<info>Creando configuración inicial</info>');
+        $this->insertarValores();
         $output->writeln('');
 
-        $output->writeln('<info>Ejecutando la configuración final</info>');
-        $this->insertarValores($connectionParams);
+        $output->writeln('<info>Creando vistas y procedimientos</info>');
+        $this->crearVistas();
+        $this->crearProcedimientos();
         $output->writeln('');
         $output->writeln('<info>FIN DE LA INSTALACION</info>');
-        //$databases = $sm->listDatabases();
+        $output->writeln('');
 
-        //print_r($sm->listTableNames());
+        // $databases = $sm->listDatabases();
 
+        // print_r($sm->listTableNames());
 
-        /*$sql = "SELECT login FROM funcionario";
-        $stmt = $conn->query($sql); // Simple, but has several drawbacks
-
-        while ($row = $stmt->fetch()) {
-            $output->writeln(sprintf('<info>Usuario: %s<info>',$row["login"]));
-        }*/
-
+        /*
+         * $sql = "SELECT login FROM funcionario";
+         * $stmt = $conn->query($sql); // Simple, but has several drawbacks
+         * while ($row = $stmt->fetch()) {
+         * $output->writeln(sprintf('<info>Usuario: %s<info>',$row["login"]));
+         * }
+         */
     }
 
-    private function compararBasesDeDatos($connectionParams) {
+    private function compararBasesDeDatos() {
         $config = new Configuration();
         $connectionParams2 = array(
             'dbname' => 'saia_aguas',
             'user' => 'saia',
             'password' => 'cerok_saia',
             'host' => 'localhost',
-            'driver' => 'pdo_mysql',
+            'driver' => 'pdo_mysql'
         );
-        $conn = DriverManager::getConnection($connectionParams, $config);
+        $conn = DriverManager::getConnection($this->connectionParams, $config);
         $conn2 = DriverManager::getConnection($connectionParams2, $config);
 
         $sm = $conn->getSchemaManager();
@@ -112,77 +119,75 @@ class ImportDbCommand extends Command {
 
         $fromSchema = $sm->createSchema();
         $toSchema = $sm2->createSchema();
-        //$toSchema = new Schema();
+        // $toSchema = new Schema();
 
-        //$toSchema->dropTable('evento');
+        // $toSchema->dropTable('evento');
 
-        //$sql = $fromSchema->getMigrateToSql($toSchema, $platf1);
-        //$sql = $fromSchema->getMigrateFromSql($toSchema, $platf1);
+        // $sql = $fromSchema->getMigrateToSql($toSchema, $platf1);
+        // $sql = $fromSchema->getMigrateFromSql($toSchema, $platf1);
 
         $comparator = new \Doctrine\DBAL\Schema\Comparator();
         $schemaDiff = $comparator->compare($toSchema, $fromSchema);
 
         $queries = $schemaDiff->toSql($platf1); // queries to get from one to another schema.
-        //$saveQueries = $schemaDiff->toSaveSql($myPlatform);
+                                                // $saveQueries = $schemaDiff->toSaveSql($myPlatform);
 
         print_r($queries);
-
     }
 
     private function registrar_mapeo_enum(&$platform) {
-        $types = ['enum' => 'string'];
+        $types = [
+            'enum' => 'string'
+        ];
         foreach ($types as $type_key => $type_value) {
             if (!$platform->hasDoctrineTypeMappingFor($type_key)) {
                 $platform->registerDoctrineTypeMapping($type_key, $type_value);
             }
         }
-
     }
 
     protected function configure() {
         $this->setName('crearbdd')->setDescription("Crear la base de datos destino");
     }
 
-    private function crearBaseDeDatos($dbParams) {
-        //$config = new Configuration();
+    private function crearBaseDeDatos() {
+        // $config = new Configuration();
         $isDevMode = true;
-        //$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/orm"), $isDevMode);
-        $config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/orm/Saia"), $isDevMode, null, null, false);
-        //$config->addEntityNamespace('', 'Saia\\');
-        //var_dump($config);die();
-        $entityManager = EntityManager::create($dbParams, $config);
+        // $config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/orm"), $isDevMode);
+        $config = Setup::createAnnotationMetadataConfiguration(array(
+            __DIR__ . "/orm/Saia"
+        ), $isDevMode, null, null, false);
+        // $config->addEntityNamespace('', 'Saia\\');
+        // var_dump($config);die();
+        $entityManager = EntityManager::create($this->connectionParams, $config);
         $tool = new \Doctrine\ORM\Tools\SchemaTool($entityManager);
         $classes = $entityManager->getMetadataFactory()->getAllMetadata();
-        /*$classes = array(
-            $entityManager->getClassMetadata('Saia\\Documento'),
-            $entityManager->getClassMetadata('Saia\Funcionario')
-        );*/
-        //$tool
+        /*
+         * $classes = array(
+         * $entityManager->getClassMetadata('Saia\\Documento'),
+         * $entityManager->getClassMetadata('Saia\Funcionario')
+         * );
+         */
+        // $tool
         $tool->createSchema($classes);
-        //var_dump($classes);
+        // var_dump($classes);
     }
 
     /**
      * Para crear las tablas que no tienen PK
-     * @param array $dbParams
      */
-    private function crearTablasAdicionales($dbParams) {
-
-        $config = new Configuration();
-
-        $conn = DriverManager::getConnection($dbParams, $config);
-
-
+    private function crearTablasAdicionales() {
+        $conn = $this->obtenerConexion();
         $sm = $conn->getSchemaManager();
 
         $platf1 = $conn->getDatabasePlatform();
-        if($dbParams['driver'] == 'pdo_mysql') {
+        if ($dbParams['driver'] == 'pdo_mysql') {
             $this->registrar_mapeo_enum($platf1);
         }
 
         $schema = $sm->createSchema();
 
-        //$schema->dropTable('evento');
+        // $schema->dropTable('evento');
 
         if (!$schema->hasTable("version_pivote_anexo")) {
             $table = $schema->createTable("version_pivote_anexo");
@@ -195,21 +200,18 @@ class ImportDbCommand extends Command {
                 "notnull" => false
             ]);
         }
-
     }
 
-    private function insertarValores($dbParams) {
-        $config = new Configuration();
+    private function insertarValores() {
+        $conn = $this->obtenerConexion();
 
-        $conn = DriverManager::getConnection($dbParams, $config);
-
-        if($dbParams['driver'] == 'pdo_mysql') {
+        if ($this->connectionParams['driver'] == 'pdo_mysql') {
             $platf1 = $conn->getDatabasePlatform();
             $this->registrar_mapeo_enum($platf1);
         }
 
-        //$conn->beginTransaction();
-        include_once('carga_datos.php');
+        // $conn->beginTransaction();
+        include_once ('carga_datos.php');
         $this->ejecutar_insert($conn, 'configuracion', $configuracion);
         $this->ejecutar_insert($conn, 'funcionario', $funcionario);
         $this->ejecutar_insert($conn, 'dependencia', $dependencia);
@@ -227,9 +229,9 @@ class ImportDbCommand extends Command {
         $this->ejecutar_insert($conn, 'categoria_formato', $categoria_formato);
 
         $valores = $this->configuracion->get_valores();
-        if(!empty($valores["urlsaia"])) {
+        if (!empty($valores["urlsaia"])) {
             $partes_url = parse_url($valores["urlsaia"]);
-            if(@$partes_url["host"]){
+            if (@$partes_url["host"]) {
                 $valores = [
                     [
                         "idconfiguracion" => 1,
@@ -247,11 +249,68 @@ class ImportDbCommand extends Command {
         return true;
     }
 
-    private function ejecutar_insert($conn, $tabla, $valores) {
+    private function crearVistas() {
+        $conn = $this->obtenerConexion();
+
+        include ('vistas.php');
+
+        foreach ($vistas as $nombre => $sql) {
+            $this->crear_vista($conn, $nombre, $sql);
+        }
+    }
+
+    private function crearProcedimientos() {
+        $conn = $this->obtenerConexion();
+
+        $valores = $this->configuracion->get_valores();
+        $motor = strtolower($valores["dbengine"]);
+
+        include ('vistas.php');
+
+        $procedures = $procs[$motor];
+
+        foreach ($procedures as $nombre => $sql) {
+            $this->ejecutar_consulta($conn, $sql);
+        }
+    }
+
+    /**
+     * Devuelve una conexion a bdd con los parametros configurados.
+     * Usa un patron singleton para mantener una sola conexion en caso de se necesario
+     *
+     * @return \Doctrine\DBAL\Connection
+     */
+    private function obtenerConexion($nueva = false) {
+        $config = new Configuration();
+        if ($nueva || empty($this->connection)) {
+            $this->connection = DriverManager::getConnection($this->connectionParams, $config);
+        }
+        return $this->connection;
+    }
+
+    private function ejecutar_insert(\Doctrine\DBAL\Connection $conn, $tabla, $valores) {
         foreach ($valores as $value) {
             $conn->insert($tabla, $value);
         }
-
     }
 
+    private function crear_vista(\Doctrine\DBAL\Connection $conn, $nombre, $sql) {
+        $sm = $conn->getSchemaManager();
+
+        $vista = new View($nombre, $sql);
+
+        // $sm->createView($vista);
+        $sm->dropAndCreateView($vista);
+    }
+
+    private function ejecutar_consulta(\Doctrine\DBAL\Connection $conn, $sql) {
+        $result = false;
+
+        if ($conn) {
+            $stmt = $conn->prepare($sql);
+            $result = $stmt->execute();
+        }
+
+        return $result;
+    }
 }
---
+++ instalador/vistas.php
@@ -0,0 +1,96 @@
+<?php
+$vistas = array(
+    "vdependencia_serie" => "select concat(d.codigo,'.',s.codigo) AS orden_dependencia_serie,d.nombre AS dependencia,s.idserie AS idserie,d.iddependencia AS iddependencia,s.nombre AS nombre,(case s.estado when 1 then 'Activo' else 'Inactivo' end) AS estado,s.codigo AS codigo,(case s.tipo when 1 then 'Serie' when 2 then 'Subserie' else 'Tipo documental' end) AS tipo,s.tipo AS tipo_serie,s.retencion_gestion AS retencion_gestion,s.retencion_central AS retencion_central,s.conservacion AS conservacion,s.seleccion AS seleccion,s.digitalizacion AS digitalizacion,s.procedimiento AS procedimiento,s.tvd AS tvd from serie s join dependencia d join entidad_serie e where e.serie_idserie = s.idserie and e.llave_entidad = d.iddependencia and s.categoria = 2 and d.tipo = 1",
+    "vdocumento" => "select A.iddocumento AS iddocumento,A.estado AS estado,A.descripcion AS descripcion,B.nombre AS serie,A.fecha AS fecha,A.tipo_radicado AS tipo_radicado,A.plantilla AS plantilla from documento A join serie B where A.serie = B.idserie",
+    "vdocumentos_pendientes" => "select A.iddocumento AS iddocumento,A.numero AS numero,A.descripcion AS descripcion,A.fecha_creacion AS fecha_creacion,A.serie AS serie,A.estado AS estado,B.llave_entidad AS llave_entidad,A.plantilla AS plantilla from asignacion B join documento A on A.iddocumento = B.documento_iddocumento where A.estado <> 'ELIMINADO' and B.tarea_idtarea = 2 group by A.iddocumento,A.numero,A.descripcion,A.fecha_creacion,A.serie,A.estado,B.llave_entidad,A.plantilla",
+    "vdocumentos_proceso" => "select A.fecha_limite AS fecha_limite,A.iddocumento AS iddocumento,A.numero AS numero,A.descripcion AS descripcion,A.plantilla AS plantilla,A.fecha AS fecha,A.estado AS estado,A.serie AS serie,A.tipo_ejecutor AS tipo_ejecutor,A.tipo_radicado AS tipo_radicado from documento A left join vdocumentos_pendientes C on A.iddocumento = C.iddocumento where A.estado not in ('GESTION','CENTRAL','HISTORICO','INICIADO','ANULADO','ELIMINADO') and C.iddocumento is not null group by A.iddocumento,A.numero,A.descripcion,A.plantilla,A.fecha,A.estado,A.serie,A.tipo_ejecutor",
+    "vejecutor" => "select A.idejecutor AS idejecutor,A.identificacion AS identificacion,A.nombre AS nombre,A.fecha_ingreso AS fecha_ingreso,A.estado AS estado,B.iddatos_ejecutor AS iddatos_ejecutor,B.ejecutor_idejecutor AS ejecutor_idejecutor,B.direccion AS direccion,B.telefono AS telefono,B.cargo AS cargo,B.ciudad AS ciudad,B.titulo AS titulo,B.empresa AS empresa,B.fecha AS fecha,B.email AS email,B.codigo AS codigo,C.nombre AS ciudad_ejecutor,A.tipo_ejecutor AS tipo_ejecutor from ejecutor A left join datos_ejecutor B on A.idejecutor = B.ejecutor_idejecutor left join municipio C on B.ciudad = C.idmunicipio",
+    "vexpediente_serie" => "select a.propietario AS propietario,c.nombre AS nombre_serie,a.fecha AS fecha,a.nombre AS nombre,a.codigo_numero AS codigo_numero,a.descripcion AS descripcion,a.cod_arbol AS cod_arbol,a.cod_padre AS cod_padre,a.estado_archivo AS estado_archivo,a.fk_idcaja AS fk_idcaja,a.estado_cierre AS estado_cierre,a.idexpediente AS idexpediente,b.entidad_identidad AS identidad_exp,b.llave_entidad AS llave_exp,d.entidad_identidad AS identidad_ser,d.llave_entidad AS llave_ser,d.estado AS estado_entidad_serie,a.prox_estado_archivo AS prox_estado_archivo,a.fecha_extrema_i AS fecha_extrema_i,a.fecha_extrema_f AS fecha_extrema_f,a.no_unidad_conservacion AS no_unidad_conservacion,a.no_folios AS no_folios,a.no_carpeta AS no_carpeta,a.soporte AS soporte,a.notas_transf AS notas_transf,a.tomo_no AS tomo_no,a.agrupador AS agrupador,a.indice_uno AS indice_uno,a.indice_dos AS indice_dos,a.indice_tres AS indice_tres from expediente a left join entidad_expediente b on a.idexpediente = b.expediente_idexpediente left join serie c on a.serie_idserie = c.idserie left join entidad_serie d on c.idserie = d.serie_idserie",
+    "vfuncionario_dc" => "select b.idfuncionario AS idfuncionario,b.funcionario_codigo AS funcionario_codigo,b.login AS login,b.nombres AS nombres,b.apellidos AS apellidos,b.firma AS firma,b.estado AS estado,b.fecha_ingreso AS fecha_ingreso,b.clave AS clave,b.nit AS nit,b.perfil AS perfil,b.debe_firmar AS debe_firmar,b.mensajeria AS mensajeria,b.email AS email,b.sistema AS sistema,b.tipo AS tipo,b.ultimo_pwd AS ultimo_pwd,b.direccion AS direccion,b.telefono AS telefono,c.nombre AS cargo,c.idcargo AS idcargo,c.tipo_cargo AS tipo_cargo,a.nombre AS dependencia,a.estado AS estado_dep,a.codigo AS codigo,a.tipo AS tipo_dep,a.iddependencia AS iddependencia,a.fecha_ingreso AS creacion_dep,a.cod_padre AS cod_padre,a.extension AS extension,a.ubicacion_dependencia AS ubicacion_dependencia,a.logo AS logo,d.iddependencia_cargo AS iddependencia_cargo,d.estado AS estado_dc,d.fecha_inicial AS fecha_inicial,d.fecha_final AS fecha_final,d.fecha_ingreso AS creacion_dc,d.tipo AS tipo_dc from dependencia a join funcionario b join cargo c join dependencia_cargo d where a.iddependencia = d.dependencia_iddependencia and b.idfuncionario = d.funcionario_idfuncionario and c.idcargo = d.cargo_idcargo",
+    "vpantalla_formato" => "select formato.mostrar_pdf AS mostrar_pdf,formato.nombre_tabla AS nombre_tabla,formato.idformato AS idformato,formato.nombre AS nombre,formato.etiqueta AS etiqueta,formato.ruta_adicionar AS ruta_adicionar,formato.ruta_mostrar AS ruta_mostrar,formato.mostrar AS mostrar,formato.cod_padre AS cod_padre,formato.fk_categoria_formato AS fk_categoria_formato,'F' AS formato_pantalla from formato union select '' AS mostrar_pdf,pantalla.nombre AS nombre_tabla,pantalla.idpantalla AS idformato,pantalla.nombre AS nombre,pantalla.etiqueta AS etiqueta,concat('adicionar_',pantalla.nombre,'.php') AS ruta_adicionar,concat('mostrar_',pantalla.nombre,'.php') AS ruta_mostrar,1 AS mostrar,pantalla.cod_padre AS cod_padre,pantalla.fk_idpantalla_categoria AS fk_categoria_formato,'P' AS formato_pantalla from pantalla where pantalla.tipo_pantalla = 2 and (isnull(pantalla.cod_padre) or (pantalla.cod_padre = 0))",
+    // "vreporte_inventario" => "select ft_inventario_retirados.documento_iddocumento AS documento_iddocumento,ft_inventario_retirados.ubicacion AS ubicacion,ft_inventario_retirados.numero_caja AS numero_caja,ft_inventario_retirados.fecha_extrema_inicia AS fecha_extrema_inicia,ft_inventario_retirados.fecha_extrema_final AS fecha_extrema_final,ft_inventario_retirados.folios AS folios,ft_inventario_retirados.observaciones AS observaciones from ft_inventario_retirados union select ft_inventario_jubilados.documento_iddocumento AS documento_iddocumento,ft_inventario_jubilados.ubicacion AS ubicacion,ft_inventario_jubilados.numero_caja AS numero_caja,ft_inventario_jubilados.fecha_extrema_inicia AS fecha_extrema_inicia,ft_inventario_jubilados.fecha_extrema_final AS fecha_extrema_final,ft_inventario_jubilados.folios AS folios,ft_inventario_jubilados.observaciones AS observaciones from ft_inventario_jubilados",
+    "vusuarios_concurrentes" => "select f.funcionario_codigo AS funcionario_codigo,f.nit AS nit,concat(f.nombres,' ',f.apellidos) AS nombre_completo,f.login AS login,count(l.login) AS cant_conexiones from log_acceso l join funcionario f on f.login = l.login where f.login not in ('cerok','mensajero','radicador_web','radicador_salida') and l.exito = 1 and isnull(l.fecha_cierre) group by f.funcionario_codigo,f.nit,f.nombres,f.apellidos,f.login"
+);
+/*
+ * "MySql",
+ * "Oracle",
+ * "SqlServer",
+ * "MSSql",
+ * "Postgres"
+ */
+$procs = [
+    "mysql" => [
+        "sp_asignar_radicado" => "CREATE PROCEDURE sp_asignar_radicado(IN iddoc INT, IN tipo INT, IN funcionario INT)
+BEGIN
+DECLARE valor VARCHAR(50);
+DECLARE sentencia VARCHAR(2000);
+SELECT consecutivo INTO valor FROM contador WHERE idcontador=tipo;
+UPDATE documento SET numero=valor WHERE iddocumento=iddoc;
+UPDATE contador SET consecutivo=consecutivo+1 WHERE idcontador=tipo;
+set sentencia = concat('UPDATE documento SET numero=', valor, ' WHERE iddocumento=',iddoc);
+INSERT INTO evento(funcionario_codigo, fecha, evento, tabla_e, registro_id, estado, codigo_sql, detalle) VALUES(funcionario, CURRENT_TIMESTAMP, 'MODIFICAR', 'documento', valor, 0,sentencia,null);
+set sentencia = concat('UPDATE contador SET consecutivo=', valor+1, ' WHERE idcontador=',tipo);
+INSERT INTO evento(funcionario_codigo, fecha, evento, tabla_e, registro_id, estado, codigo_sql, detalle) VALUES(funcionario, CURRENT_TIMESTAMP, 'MODIFICAR', 'contador', valor+1, 0,sentencia,null);
+END"
+    ],
+    "oracle" => [
+        "SP_ASIGNAR_RADICADO" => "CREATE OR REPLACE PROCEDURE SP_ASIGNAR_RADICADO (iddoc INT, tipo INT, functionario INT)
+is
+declare
+valor INT;
+sentencia_doc CLOB;
+sentencia_cont CLOB;
+BEGIN
+SELECT consecutivo INTO valor FROM contador WHERE idcontador=tipo;
+UPDATE documento SET numero=valor WHERE iddocumento=iddoc;
+UPDATE contador SET consecutivo=consecutivo+1 WHERE idcontador=tipo;
+sentencia_doc  := 'UPDATE documento SET numero=' || valor || ' WHERE iddocumento='  || iddoc;
+sentencia_cont := 'UPDATE contador  SET consecutivo=' || valor+1 || ' WHERE idcontador=' || tipo;
+INSERT INTO evento(funcionario_codigo, fecha, evento, tabla_e, registro_id, estado, codigo_sql) VALUES(funcionario, CURRENT_TIMESTAMP, 'MODIFICAR', 'documento', valor, 0, sentencia_doc);
+INSERT INTO evento(funcionario_codigo, fecha, evento, tabla_e, registro_id, estado, codigo_sql, detalle) VALUES(funcionario, CURRENT_TIMESTAMP, 'MODIFICAR', 'contador', valor+1, 0, sentencia_cont);
+END"
+    ],
+    "sqlserver" => [
+        "sp_asignar_radicado" => "CREATE PROCEDURE [dbo].[sp_asignar_radicado]
+   @iddoc int,
+   @tipo int
+   @funcionario int
+AS
+   BEGIN
+      SET  XACT_ABORT  ON
+      SET  NOCOUNT  ON
+      DECLARE
+         @valor nvarchar(50)
+      DECLARE @sentencia nvarchar(2000)
+      SELECT @valor = CAST(contador.consecutivo AS varchar(50)) FROM dbo.contador WHERE contador.idcontador = @tipo
+
+      UPDATE dbo.documento SET numero = @valor WHERE documento.iddocumento = @iddoc
+
+      UPDATE dbo.contador SET consecutivo = contador.consecutivo + 1 WHERE contador.idcontador = @tipo
+
+      SET @sentencia = concat('UPDATE documento SET numero=', @valor, ' WHERE iddocumento=',@iddoc)
+      INSERT INTO dbo.evento(funcionario_codigo, fecha, evento, tabla_e, registro_id, estado, codigo_sql, detalle) VALUES(@funcionario, CURRENT_TIMESTAMP, 'MODIFICAR', 'documento', @valor, 0, @sentencia, null);
+      SET @sentencia = concat('UPDATE contador SET consecutivo=', @valor+1, ' WHERE idcontador=',@tipo);
+      INSERT INTO dbo.evento(funcionario_codigo, fecha, evento, tabla_e, registro_id, estado, codigo_sql, detalle) VALUES(@funcionario, CURRENT_TIMESTAMP, 'MODIFICAR', 'contador', @valor+1, 0, @sentencia,null);
+   END"
+    ],
+    "postgres" => [
+        "sp_asignar_radicado" => "CREATE OR REPLACE FUNCTION public.sp_asignar_radicado(
+	iddoc integer,
+	tipo integer,
+	funcionario integer)
+    RETURNS void
+    LANGUAGE 'plpgsql'" . 'AS $BODY$' . "
+DECLARE valor integer;
+sentencia VARCHAR(2000);
+BEGIN
+SELECT consecutivo INTO valor FROM contador WHERE idcontador=tipo;
+UPDATE documento SET numero=valor WHERE iddocumento=iddoc;
+UPDATE contador SET consecutivo=consecutivo+1 WHERE idcontador=tipo;
+sentencia = concat('UPDATE documento SET numero=', valor, ' WHERE iddocumento=',iddoc);
+INSERT INTO evento(funcionario_codigo, fecha, evento, tabla_e, registro_id, estado, codigo_sql, detalle) VALUES(funcionario, CURRENT_TIMESTAMP, 'MODIFICAR', 'documento', valor, 0,sentencia,null);
+sentencia = ('UPDATE contador SET consecutivo=', valor+1, ' WHERE idcontador=',tipo);
+INSERT INTO evento(funcionario_codigo, fecha, evento, tabla_e, registro_id, estado, codigo_sql, detalle) VALUES(funcionario, CURRENT_TIMESTAMP, 'MODIFICAR', 'contador', valor+1, 0,sentencia,null);
+END;"
+    ]
+];
