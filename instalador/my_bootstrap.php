0167ae4222bd6992f974f6a7939b6757409f1a45
Usuario generico cerok <info@cerok.com>
2018-02-21 11:01:56 -0500

    filtrar otras tablas sin pk

--- instalador/my_bootstrap.php
+++ instalador/my_bootstrap.php
@@ -14,14 +14,26 @@ $dbParams = array(
     'driver'   => 'pdo_mysql',
     'user'     => 'saia',
     'password' => 'cerok_saia',
-    'dbname'   => 'saia_release2',
+    'dbname'   => 'saia_release1',
 );
 
 //$config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
 $config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/orm"), $isDevMode, null, null, false);
-//$config->setFilterSchemaAssetsExpression("/(?<!^)rcmail_/");
-$config->setFilterSchemaAssetsExpression("/^(?!rcmail_).+/");
+$reg = filtro_excluir_sin_pk();
+//$config->setFilterSchemaAssetsExpression("/^(?!rcmail_).+/");
+$config->setFilterSchemaAssetsExpression($reg);
 
 $entityManager = EntityManager::create($dbParams, $config);
 $entityManager->getConnection()->getDatabasePlatform()->registerDoctrineTypeMapping('enum', 'string');
 
+function filtro_excluir_sin_pk() {
+    $filter_exclude = [
+        // tables with no primary key generate exceptions
+        'rcmail_cache',
+        'rcmail_cache_shared',
+        'rcmail_dictionary',
+        'version_pivote_anexo'
+    ];
+    $exclude_reg = '/^(?!(?:' . implode('|', $filter_exclude) . ')$).*$/i';
+    return $exclude_reg;
+}
