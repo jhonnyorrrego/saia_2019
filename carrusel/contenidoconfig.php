16e846f516ee05e66062765439ca07ab14d9e19b
Usuario generico cerok <info@cerok.com>
2018-04-04 13:46:53 -0500

    se agrega la ruta al logo de la empresa

--- carrusel/contenidoconfig.php
+++ carrusel/contenidoconfig.php
@@ -15,12 +15,11 @@ include_once($ruta_db_superior . "librerias_saia.php");
 echo(estilo_bootstrap());
 ?>
 
-<link href="<?php echo $ruta_db_superior;?>dropzone/dist/dropzone.css" type="text/css" rel="stylesheet" />
-
+<link href="<?php echo $ruta_db_superior;?>dropzone/dist/dropzone_saia.css" type="text/css" rel="stylesheet" />
 <div class="container">
 		<h5>CONFIGURACI&Oacute;N DE CARRUSEL Y CONTENIDOS RELACIONADOS</h5>
 		<br/>
-		
+
 
 <?php
 $campos = array(
@@ -69,13 +68,13 @@ include_once("../footer.php");
 function pintar_formulario($accion) {
     global $ruta_db_superior, $conn;
     if (isset($_REQUEST["id"]) && $_REQUEST["id"]) {
-     $contenido=busca_filtro_tabla("contenidos_carrusel.*,".fecha_db_obtener('fecha_inicio','Y-m-d')." as fecha_inicio,".fecha_db_obtener('fecha_fin','Y-m-d')." as fecha_fin","contenidos_carrusel","idcontenidos_carrusel=".$_REQUEST["id"],"",$conn); 
+     $contenido=busca_filtro_tabla("contenidos_carrusel.*,".fecha_db_obtener('fecha_inicio','Y-m-d')." as fecha_inicio,".fecha_db_obtener('fecha_fin','Y-m-d')." as fecha_fin","contenidos_carrusel","idcontenidos_carrusel=".$_REQUEST["id"],"",$conn);
     }
    include_once("../calendario/calendario.php");
    ?>
 <script type="text/javascript" src="../js/jquery.js"></script>
 <script type="text/javascript" src="../js/jquery.validate.js"></script>
-<script type="text/javascript" src="../js/jquery.spin.js"></script>    
+<script type="text/javascript" src="../js/jquery.spin.js"></script>
 <script type="text/javascript" src="<?php echo $ruta_db_superior; ?>tinymce34/jscripts/tiny_mce/tiny_mce.js"></script>
 <script src="<?php echo $ruta_db_superior;?>dropzone/dist/dropzone.js"></script>
 
@@ -122,28 +121,28 @@ width:"350px"
         '/',
         ['Font','FontSize'],['Bold','Italic','Underline','Strike'],
         ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
-        ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote']        
+        ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote']
     ];
     </script>
-    
+
 		<ul class="nav nav-tabs">
-		
+
 			 <li ><a href='sliderconfig.php'>Inicio</a ></li>
 		<?php if($accion=='adicionar') { ?>
-					
+
 				 <li><a href='sliderconfig.php?accion=adicionar'>Adicionar Carrusel</a ></li>
 				 <li  class="active"><a href='contenidoconfig.php?accion=adicionar'>Adicionar Contenido</a ></li>
-		<?php }else{ ?>	
-				
+		<?php }else{ ?>
+
 				  <li ><a href='sliderconfig.php?accion=adicionar'>Adicionar Carrusel</a ></li>
 			      <li><a href='contenidoconfig.php?accion=adicionar'>Adicionar Contenido</a ></li>
 			      <li class="active"><a href='#'>Editar Contenido</a ></li>
-		<?php } ?>			
-			
-		</ul>		
-		<br/>   
-    
-   <?php    
+		<?php } ?>
+
+		</ul>
+		<br/>
+
+   <?php
     echo "<br /><fieldset><legend>" . ucwords($accion . " contenido") . "</legend></fieldset><br /><br /><form action='contenidoconfig.php' name='form1' method='post' id='form1' enctype='multipart/form-data'><table class='table table-bordered table-striped'>";
    echo "<tr><td  style='text-align: center; background-color:#57B0DE; color: #ffffff;'>NOMBRE*</td><td><input class='required'  type='text' name='nombre' value='".@$contenido[0]["nombre"]."'></td></tr>";
    echo "<tr><td  style='text-align: center; background-color:#57B0DE; color: #ffffff;'>CARRUSEL*</td><td><select class='required'  type='text' name='carrusel_idcarrusel'>";
@@ -151,7 +150,7 @@ width:"350px"
     for ($i = 0; $i < $carrusel["numcampos"]; $i++) {
         echo "<option value='" . $carrusel[$i]["idcarrusel"] . "' ";
         if ($carrusel[$i]["idcarrusel"] == @$contenido[0]["carrusel_idcarrusel"]) {
-         echo " selected "; 
+         echo " selected ";
         }
        echo ">".$carrusel[$i]["nombre"]."</option>";
       }
@@ -181,7 +180,7 @@ width:"350px"
         if ($valor == @$contenido[0]["align"]) {
         echo " checked ";
         }
-      echo ">$nombre&nbsp;&nbsp;"; 
+      echo ">$nombre&nbsp;&nbsp;";
      }
    echo "</td></tr>";
    echo "<tr><td  style='text-align: center; background-color:#57B0DE; color: #ffffff;' width=20%>Orden*</td><td><input class='required'  type='input' name='orden' id='orden' value='".@$contenido[0]["orden"]."'></td></tr>";
@@ -251,7 +250,7 @@ width:"350px"
 function guardar_adicionar($accion, $datos, $carrusel) {
     global $ruta_db_superior, $conn;
  $nwidth=$carrusel[0]["alto"];
- $nheight=$carrusel[0]["alto"]; 
+ $nheight=$carrusel[0]["alto"];
 
     $sql1 = "insert into contenidos_carrusel(" . implode(",", array_keys($datos)) . ") values(" . implode(",", array_values($datos)) . ")";
     phpmkr_query($sql1, $conn);
@@ -262,12 +261,12 @@ function guardar_adicionar($accion, $datos, $carrusel) {
     redirecciona($ruta_db_superior . "carrusel/sliderconfig.php");
       }
 
-  
+
 function guardar_editar($accion, $datos, $carrusel) {
     global $ruta_db_superior, $conn;
  $nwidth=$carrusel[0]["alto"];
- $nheight=$carrusel[0]["alto"]; 
- 
+ $nheight=$carrusel[0]["alto"];
+
     $fields = array();
     foreach($datos as $field => $val) {
         $fields[] = "$field = $val";
@@ -277,7 +276,7 @@ function guardar_editar($accion, $datos, $carrusel) {
     $sql1 = "update contenidos_carrusel set " . implode(",", $fields) . " where idcontenidos_carrusel=" . $id;
  phpmkr_query($sql1,$conn);
     guardar_lob("contenido", "contenidos_carrusel", "idcontenidos_carrusel=" . $id, $_REQUEST["contenido"], "texto", $conn);
- 
+
     if ($accion == "guardar_editar" && @$_REQUEST["borrar_imagen"]) {
         $contenido = busca_filtro_tabla("imagen", "contenidos_carrusel", "idcontenidos_carrusel=$id", "", $conn);
         if (MOTOR == "MySql") {
@@ -287,7 +286,7 @@ function guardar_editar($accion, $datos, $carrusel) {
         }
 		 @unlink($ruta_db_superior.$contenido[0]["imagen"]);
 	}
- 
+
     guardar_archivos($id, $_REQUEST["form_uuid"]);
     redirecciona($ruta_db_superior . "carrusel/sliderconfig.php");
 }
--- filesystem/ActualizarRutas.php
+++ filesystem/ActualizarRutas.php
@@ -1,21 +1,21 @@
 <?php
 $max_salida = 6;
 $ruta_db_superior = $ruta = "";
-while($max_salida > 0) {
-	if (is_file($ruta . "db.php")) {
-		$ruta_db_superior = $ruta;
-	}
-	$ruta .= "../";
-	$max_salida--;
+while ($max_salida > 0) {
+    if (is_file($ruta . "db.php")) {
+        $ruta_db_superior = $ruta;
+    }
+    $ruta .= "../";
+    $max_salida--;
 }
 
 include_once ($ruta_db_superior . "db.php");
 
 if (!@$_SESSION["LOGIN" . LLAVE_SAIA]) {
-	@session_start();
-	$_SESSION["LOGIN" . LLAVE_SAIA] = "radicador_web";
-	$_SESSION["usuario_actual"] = "111222333";
-	$_SESSION["conexion_remota"] = 1;
+    @session_start();
+    $_SESSION["LOGIN" . LLAVE_SAIA] = "radicador_web";
+    $_SESSION["usuario_actual"] = "111222333";
+    $_SESSION["conexion_remota"] = 1;
 }
 
 $prefijo_ruta = "local:///vol1/";
@@ -24,109 +24,119 @@ die();
 $actualizacion->actualizar_rutas();
 
 class ActualizarRutas {
-	private $qry_upd = "update %s set %s='%s' where %s=%s";
-	private $prefijo_ruta;
-	private $tablas = array(
-			"anexos" => array(
-					"ruta"
-			),
-			"anexos_despacho" => array(
-					"ruta"
-			),
-			"anexos_version" => array(
-					"ruta"
-			),
-			"caja" => array(
-					"ruta_qr"
-			),
-			"contenidos_carrusel" => array(
-					"imagen"
-			),
-			"dependencia" => array(
-					"logo"
-			),
-			"documento" => array(
-					"pdf"
-			),
-			"documento_verificacion" => array(
-					"ruta_qr"
-			),
-			"expediente" => array(
-					"ruta_qr"
-			),
-			"funcionario" => array(
-					"foto_original",
-					"foto_recorte"
-			),
-			"noticia_index" => array(
-					"imagen"
-			),
-			"pagina" => array(
-					"ruta",
-					"imagen"
-			),
-			"paso_acitividad_anexo" => array(
-					"ruta"
-			),
-			"tareas_listado_anexos" => array(
-					"ruta"
-			),
-			"version_anexos" => array(
-					"ruta"
-			),
-			"version_documento" => array(
-					"pdf"
-			),
-			"version_pagina" => array(
-					"ruta",
-					"ruta_miniatura"
-			)
-	);
 
-	public function __construct($prefijo_ruta, $tablas = null) {
-		$this->prefijo_ruta = $prefijo_ruta;
-		if ($tablas) {
-			$this->tablas = $tablas;
-		}
-	}
+    private $qry_upd = "update %s set %s='%s' where %s=%s";
 
-	public function actualizar_rutas() {
-		global $conn;
-		$actualizados = 0;
-		foreach ( $this->tablas as $tabla => $campos ) {
-			foreach ( $campos as $campo ) {
-				$key = "id$tabla";
-				if($tabla == "pagina") {
-					$key = "consecutivo";
-				}
-				
-				$datos = busca_filtro_tabla("$key, $campo", $tabla, "$campo not like '{%'", "", $conn);
-				//print_r($datos);die();
-				if ($datos["numcampos"]) {
-					for($i = 0; $i < $datos["numcampos"]; $i++) {
-						$valor_origen = $datos[$i][$campo];
-						$output_array = array();
-						$ruta_alm = "almacenamiento";
-						if(preg_match("#(\.\./)+(almacenamiento[0-9]*)/#", $valor_origen, $output_array)) {
-							$ruta_alm = $output_array[2];
-						}
-						$arr_ruta = preg_split("#(\.\./)+almacenamiento[0-9]*/#", $valor_origen, null, PREG_SPLIT_NO_EMPTY);
-						// {"servidor":"local:///vol1/almacenamiento/","ruta":"APROBADO/2016-11-11/276/pdf/MEMORANDO_54_2016_11_11.pdf"}
-						if (count($arr_ruta)) {
-							$json = array(
-									"servidor" => $this->prefijo_ruta . $ruta_alm . "/",
-									"ruta" => $arr_ruta[0]
-							);
-							$valor = json_encode($json);
-							$qry = sprintf($this->qry_upd, $tabla, $campo, $valor, $key, $datos[$i][$key]);
-							phpmkr_query($qry) or die($qry);
-							//echo "$valor_origen => $qry <br>";
-							$actualizados++;
-						}
-					}
-				}
-			}
-		}
-		echo "$actualizados registros actualizados";
-	}
+    private $prefijo_ruta;
+
+    private $tablas = array(
+        "anexos" => array(
+            "ruta"
+        ),
+        "anexos_despacho" => array(
+            "ruta"
+        ),
+        "anexos_version" => array(
+            "ruta"
+        ),
+        "caja" => array(
+            "ruta_qr"
+        ),
+        "configuracion" => array(
+            "valor"
+        ),
+        "contenidos_carrusel" => array(
+            "imagen"
+        ),
+        "dependencia" => array(
+            "logo"
+        ),
+        "documento" => array(
+            "pdf"
+        ),
+        "documento_verificacion" => array(
+            "ruta_qr"
+        ),
+        "expediente" => array(
+            "ruta_qr"
+        ),
+        "funcionario" => array(
+            "foto_original",
+            "foto_recorte"
+        ),
+        "noticia_index" => array(
+            "imagen"
+        ),
+        "pagina" => array(
+            "ruta",
+            "imagen"
+        ),
+        "paso_acitividad_anexo" => array(
+            "ruta"
+        ),
+        "tareas_listado_anexos" => array(
+            "ruta"
+        ),
+        "version_anexos" => array(
+            "ruta"
+        ),
+        "version_documento" => array(
+            "pdf"
+        ),
+        "version_pagina" => array(
+            "ruta",
+            "ruta_miniatura"
+        )
+    );
+
+    public function __construct($prefijo_ruta, $tablas = null) {
+        $this->prefijo_ruta = $prefijo_ruta;
+        if ($tablas) {
+            $this->tablas = $tablas;
+        }
+    }
+
+    public function actualizar_rutas() {
+        global $conn;
+        $actualizados = 0;
+        foreach ($this->tablas as $tabla => $campos) {
+            foreach ($campos as $campo) {
+                $key = "id$tabla";
+                if ($tabla == "pagina") {
+                    $key = "consecutivo";
+                }
+
+                $cond_extra = "";
+                if ($tabla == "configuracion") {
+                    $cond_extra = " AND nombre='logo'";
+                }
+                $datos = busca_filtro_tabla("$key, $campo", $tabla, "$campo not like '{%'" . $cond_extra, "", $conn);
+                // print_r($datos);die();
+                if ($datos["numcampos"]) {
+                    for ($i = 0; $i < $datos["numcampos"]; $i++) {
+                        $valor_origen = $datos[$i][$campo];
+                        $output_array = array();
+                        $ruta_alm = "almacenamiento";
+                        if (preg_match("#(\.\./)+(almacenamiento[0-9]*)/#", $valor_origen, $output_array)) {
+                            $ruta_alm = $output_array[2];
+                        }
+                        $arr_ruta = preg_split("#(\.\./)+almacenamiento[0-9]*/#", $valor_origen, null, PREG_SPLIT_NO_EMPTY);
+                        // {"servidor":"local:///vol1/almacenamiento/","ruta":"APROBADO/2016-11-11/276/pdf/MEMORANDO_54_2016_11_11.pdf"}
+                        if (count($arr_ruta)) {
+                            $json = array(
+                                "servidor" => $this->prefijo_ruta . $ruta_alm . "/",
+                                "ruta" => $arr_ruta[0]
+                            );
+                            $valor = json_encode($json);
+                            $qry = sprintf($this->qry_upd, $tabla, $campo, $valor, $key, $datos[$i][$key]);
+                            phpmkr_query($qry) or die($qry);
+                            // echo "$valor_origen => $qry <br>";
+                            $actualizados++;
+                        }
+                    }
+                }
+            }
+        }
+        echo "$actualizados registros actualizados";
+    }
 }
