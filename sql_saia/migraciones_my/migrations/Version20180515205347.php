3c065fce5127bf2e28a45ddab0cad3562ef00910
andres.agudelo <andres.agudelo@cerok.com>
2018-05-16 07:58:32 -0500

    Cambios realizados por Giovanny Montes, pide el favor de subir cambios, nuevos reportes

---
+++ sql_saia/migraciones_my/migrations/Version20180515205347.php
@@ -0,0 +1,117 @@
+<?php
+namespace Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180515205347 extends AbstractMigration {
+
+    public function getDescription() {
+        return 'Mover campo busqueda.llave a busqueda_componente.llave';
+    }
+
+    public function preUp(Schema $schema) {
+        date_default_timezone_set("America/Bogota");
+
+        if ($this->connection->getDatabasePlatform()->getName() == "mysql") {
+            $this->platform->registerDoctrineTypeMapping('enum', 'string');
+        }
+    }
+
+    /**
+     *
+     * @param Schema $schema
+     */
+    public function up(Schema $schema) {
+        $tabla_comp = $schema->getTable('busqueda_componente');
+        $this->abortIf($tabla_comp->hasColumn("llave"), 'Ya ha configurado este SAIA para bootstrap table');
+
+        $tabla_comp->addColumn("llave", "string", [
+            "length" => 255,
+            "default" => "iddocumento"
+        ]);
+    }
+
+    public function postUp($schema) {
+        $conn = $this->connection;
+
+        $queryBuilder = $conn->createQueryBuilder();
+        $queryBuilder->select('distinct idbusqueda, llave')->from('busqueda');
+
+        $result = $queryBuilder->execute()->fetchAll();
+
+        if (!empty($result)) {
+            $tipos = array(
+                \PDO::PARAM_STR,
+                \PDO::PARAM_INT
+            );
+            foreach ($result as $row) {
+                if(empty($llave)) {
+                    continue;
+                }
+
+                $data = [
+                    'llave' => $row["llave"]
+                ];
+                $ident = [
+                    'busqueda_idbusqueda' => $row["idbusqueda"]
+                ];
+                $resp = $conn->update('busqueda_componente', $data, $ident, $tipos);
+            }
+        }
+
+        $tabla_busq = $schema->getTable('busqueda');
+        if ($tabla_busq && $tabla_busq->hasColumn("llave")) {
+            $tabla_busq->dropColumn("llave");
+        }
+    }
+
+    /**
+     *
+     * @param Schema $schema
+     */
+    public function down(Schema $schema) {
+        $tabla_comp = $schema->getTable('busqueda_componente');
+        $this->abortIf(!$tabla_comp->hasColumn("llave"), 'campo busqueda_componente.llave no existe');
+
+        $tabla_busq = $schema->getTable('busqueda');
+        $this->abortIf($tabla_busq->hasColumn("llave"), 'campo busqueda.llave ya existe');
+
+        $tabla_busq->addColumn("llave", "string", [
+            "length" => 255,
+            "default" => "iddocumento"
+        ]);
+
+        $conn = $this->connection;
+
+        $queryBuilder = $conn->createQueryBuilder();
+        $queryBuilder->select('distinct busqueda_idbusqueda, llave')->from('busqueda_componente');
+
+        $result = $queryBuilder->execute()->fetchAll();
+        if (!empty($result)) {
+            $tipos = array(
+                PARAM_STR,
+                PARAM_INT
+            );
+            foreach ($result as $row) {
+                if(empty($llave)) {
+                    continue;
+                }
+                $data = [
+                    'llave' => $row["llave"]
+                ];
+                $ident = [
+                    'idbusqueda' => $row["busqueda_idbusqueda"]
+                ];
+                $resp = $conn->update('busqueda', $data, $ident, $tipos);
+            }
+        }
+
+        if ($tabla_comp && $tabla_comp->hasColumn("llave")) {
+            $tabla_comp->dropColumn("llave");
+        }
+    }
+}
