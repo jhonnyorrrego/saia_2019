1351270a209f76fa7ea685b1bb6b801e29b5b660
basic3 <info@cerok.com>
2018-07-09 22:04:46 +0000

    arreglos .gitignore

--- formatos/calificacion_qrsf/buscar_calificacion_qrsf2.php
+++
@@ -1,9 +0,0 @@
-<?php include_once("../librerias/funciones_generales.php"); ?><?php include_once("../librerias/header_formato.php"); ?><legend id="label_formato" class="legend">B&uacute;squeda en formato Calificacion PQRSF</legend><br /><br /><?php include_once("../librerias/funciones_generales.php"); ?><div class="control-group"><label class="string control-label" style="font-size:9pt" for="calificacion_pqrsf"><b>Calificaci&oacute;n PQRSF<input type="hidden" name="bksaiacondicion_g@calificacion_pqrsf" id="bksaiacondicion_g@calificacion_pqrsf" value="="></b></label><div class="controls"><?php genera_campo_listados_editar(342,3982,'',1,'buscar');?><div class="btn-group" data-toggle="buttons-radio" >
-		  <!--button type="button" class="btn btn-mini" data-toggle="button" id="y" onclick="llenar_valor('bqsaiaenlace_g@calificacion_pqrsf',this.id)">
-		    Y
-		  </button>
-		  <button type="button" class="btn btn-mini" data-toggle="button" id="o" onclick="llenar_valor('bqsaiaenlace_g@calificacion_pqrsf',this.id)">
-		    O
-		  </button-->
-		  <input type="hidden" name="bqsaiaenlace_g@calificacion_pqrsf" id="bqsaiaenlace_g@calificacion_pqrsf" value="y" />
-		</div></div></div><div class="control-group"><b>Descripci&oacute;n<input type="hidden" name="bksaiacondicion_g@descripcion" id="bksaiacondicion_g@descripcion" value="like_total"></b><div class="controls"><textarea    id="descripcion" name="bqsaia_g@descripcion"  style="width:500px;height:100px"></textarea></div></div><input type="hidden" name="filtro_adicional" id="filtro_adicional" value=" ft_calificacion_qrsf g @ AND  g.documento_iddocumento=iddocumento "><input type="hidden" name="padre" value="<?php echo $_REQUEST["padre"]; ?>"><input type="hidden" name="anterior" value="<?php echo $_REQUEST["anterior"]; ?>"></body>
--- formatos/politicas_proceso/buscar_politicas_proceso2.php
+++
@@ -1,9 +0,0 @@
-<?php include_once("../librerias/funciones_generales.php"); ?><legend id="label_formato" class="legend">B&uacute;squeda en formato Politicas de operacion de Proceso</legend><br /><br /><?php include_once("../librerias/funciones_generales.php"); ?><div class="control-group"><label class="string control-label" style="font-size:9pt" for="origen_documento"><b>Origen Documento<input type="hidden" name="bksaiacondicion_g@origen_documento" id="bksaiacondicion_g@origen_documento" value="="></b></label><div class="controls"><?php genera_campo_listados_editar(495,6579,'',1,'buscar');?><div class="btn-group" data-toggle="buttons-radio" >
-		  <!--button type="button" class="btn btn-mini" data-toggle="button" id="y" onclick="llenar_valor('bqsaiaenlace_g@origen_documento',this.id)">
-		    Y
-		  </button>
-		  <button type="button" class="btn btn-mini" data-toggle="button" id="o" onclick="llenar_valor('bqsaiaenlace_g@origen_documento',this.id)">
-		    O
-		  </button-->
-		  <input type="hidden" name="bqsaiaenlace_g@origen_documento" id="bqsaiaenlace_g@origen_documento" value="y" />
-		</div></div></div><div class="control-group"><label class="string control-label" style="font-size:9pt" for="estado"><b>estado<input type="hidden" name="bksaiacondicion_g@estado" id="bksaiacondicion_g@estado" value="like_total"></b></label><div class="controls"><?php genera_campo_listados_editar(495,6293,'',1,'buscar');?></div></div><input type="hidden" name="filtro_adicional" id="filtro_adicional" value=" ft_politicas_proceso g @ AND  g.documento_iddocumento=iddocumento "><input type="hidden" name="padre" value="<?php echo $_REQUEST["padre"]; ?>"><input type="hidden" name="anterior" value="<?php echo $_REQUEST["anterior"]; ?>"></body>
