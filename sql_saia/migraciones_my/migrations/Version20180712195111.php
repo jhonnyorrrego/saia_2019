b9659db897a36569e26b61bd66f6aa7fb512d782
andres.agudelo <andres.agudelo@cerok.com>
2018-07-18 23:52:50 +0000

    actualizacion sql vistas postup

--- sql_saia/migraciones_my/migrations/Version20180712195111.php
+++ sql_saia/migraciones_my/migrations/Version20180712195111.php
@@ -40,6 +40,13 @@ class Version20180712195111 extends AbstractMigration {
 		$cadena_sql = "UPDATE campos_formato SET valor='../../test/test_expediente_funcionario.php;2;0;1;1;0;1',predeterminado=0 WHERE nombre LIKE 'serie_idserie' and etiqueta_html<>'hidden' and (valor like '%test_expediente_serie%' or valor like '%test_dependencia_serie%')";
 		$this -> addSql($cadena_sql);
 
+	}
+
+	public function postUp(Schema $schema) {
+		date_default_timezone_set("America/Bogota");
+		$this -> platform -> registerDoctrineTypeMapping('enum', 'string');
+		$conn = $this -> connection;
+
 		$cadena_sql = "CREATE OR REPLACE VIEW vpermiso_serie AS
 		SELECT
 		f.idfuncionario,f.funcionario_codigo,
@@ -57,15 +64,14 @@ class Version20180712195111 extends AbstractMigration {
 		s.idserie,s.nombre as nombre_serie,s.cod_padre,s.codigo,s.tipo,s.categoria,s.tvd,s.cod_arbol,s.estado as estado_serie
 		FROM permiso_serie p, serie s,vfuncionario_dc v 
 		WHERE p.serie_idserie=s.idserie and v.idcargo=p.llave_entidad and p.entidad_identidad=4 and v.estado_dc=1  and p.estado=1";
-		$this -> addSql($cadena_sql);
+		$conn -> executeQuery($cadena_sql);
 
 		$cadena_sql = "CREATE OR REPLACE VIEW vexpediente_serie as 
 		select a.propietario AS propietario,c.nombre AS nombre_serie,a.serie_idserie,a.fecha AS fecha,a.nombre AS nombre,a.descripcion AS descripcion,a.cod_arbol AS cod_arbol,a.cod_padre AS cod_padre,a.estado_archivo AS estado_archivo,a.fk_idcaja AS fk_idcaja,a.estado_cierre AS estado_cierre,a.idexpediente AS idexpediente,b.entidad_identidad AS identidad_exp,b.llave_entidad AS llave_exp,
 		a.prox_estado_archivo AS prox_estado_archivo,a.fecha_extrema_i AS fecha_extrema_i,a.fecha_extrema_f AS fecha_extrema_f,a.no_unidad_conservacion AS no_unidad_conservacion,a.no_folios AS no_folios,a.no_carpeta AS no_carpeta,a.soporte AS soporte,a.notas_transf AS notas_transf,a.tomo_no AS tomo_no,a.agrupador AS agrupador,a.indice_uno AS indice_uno,a.indice_dos AS indice_dos,a.indice_tres AS indice_tres,a.codigo_numero AS codigo_numero 
 		from expediente a left join entidad_expediente b on a.idexpediente = b.expediente_idexpediente 
 		left join serie c on a.serie_idserie = c.idserie";
-		$this -> addSql($cadena_sql);
-
+		$conn -> executeQuery($cadena_sql);
 	}
 
 	public function down(Schema $schema) {
