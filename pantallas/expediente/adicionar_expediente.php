e3572362d01380b5d3f2115f1965b68df0c095d1
Giovanni Montes <giovanni.montes@cerok.com>
2018-07-31 15:40:17 -0500

    Se filtra por la serie padre asociada al expediente actual.

--- pantallas/expediente/adicionar_expediente.php
+++ pantallas/expediente/adicionar_expediente.php
@@ -1,5 +1,13 @@
 <?php
-$max_salida=6; $ruta_db_superior=$ruta=""; while($max_salida>0){ if(is_file($ruta."db.php")){ $ruta_db_superior=$ruta;} $ruta.="../"; $max_salida--; } 
+$max_salida = 6;
+$ruta_db_superior = $ruta = "";
+while ($max_salida > 0) {
+    if (is_file($ruta . "db.php")) {
+        $ruta_db_superior = $ruta;
+    }
+    $ruta .= "../";
+    $max_salida--;
+}
 ?>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <?php include_once($ruta_db_superior."pantallas/lib/librerias_componentes.php"); ?>
@@ -19,14 +27,20 @@ $max_salida=6; $ruta_db_superior=$ruta=""; while($max_salida>0){ if(is_file($rut
 </style>
 <?php include_once($ruta_db_superior."db.php"); ?>
 <script type="text/javascript" src="<?php echo($ruta_db_superior);?>js/jquery-1.7.min.js"></script>
-<?php include_once($ruta_db_superior."librerias_saia.php");
-include_once($ruta_db_superior."pantallas/lib/librerias_cripto.php");
-echo(librerias_notificaciones());
-$dato_padre=busca_filtro_tabla("","expediente a","a.idexpediente=".$_REQUEST["cod_padre"],"",$conn);
+<?php
+
+include_once ($ruta_db_superior . "librerias_saia.php");
+include_once ($ruta_db_superior . "pantallas/lib/librerias_cripto.php");
+echo (librerias_notificaciones());
+
+$dato_padre = busca_filtro_tabla("", "expediente a", "a.idexpediente=" . $_REQUEST["cod_padre"], "", $conn);
+$serie_padre = "";
 ?>
 <form name="formulario_expediente" id="formulario_expediente">
 <input type="hidden" name="estado_cierre" id="estado_cierre" value="1">
-<?php if($dato_padre["numcampos"]){ ?>
+<?php if($dato_padre["numcampos"]){
+    $serie_padre = $dato_padre[0]["serie_idserie"];
+    ?>
 	<input type="hidden" name="estado_archivo" id="estado_archivo" value="<?php echo($dato_padre[0]["estado_archivo"]);?>">
 <?php }else{ ?>
 	<input type="hidden" name="estado_archivo" id="estado_archivo" value="<?php echo($_REQUEST["estado_archivo"]);?>">
@@ -36,11 +50,10 @@ $dato_padre=busca_filtro_tabla("","expediente a","a.idexpediente=".$_REQUEST["co
 <input type="hidden" id="cerrar_higslide" value="<?php echo(@$_REQUEST["cerrar_higslide"]);?>">
 <legend>Crear expediente</legend>
 
-
 <div class="control-group element">
   <label class="control-label" for="agrupador">Seleccione *
   </label>
-  <div class="controls"> 
+  <div class="controls">
 		<input type="radio" name="agrupador" id="agrupado0"  value="0" checked>EXPEDIENTE &nbsp;
 		<input type="radio" name="agrupador" id="agrupado1" value="1">AGRUPADOR
   </div>
@@ -49,7 +62,7 @@ $dato_padre=busca_filtro_tabla("","expediente a","a.idexpediente=".$_REQUEST["co
 <div class="control-group element">
   <label class="control-label" for="nombre">Nombre *
   </label>
-  <div class="controls"> 
+  <div class="controls">
     <input type="text" name="nombre" id="nombre" >
   </div>
 </div>
@@ -57,7 +70,7 @@ $dato_padre=busca_filtro_tabla("","expediente a","a.idexpediente=".$_REQUEST["co
     $(document).ready(function(){
         $('[name="agrupador"]').click(function(){
             var valor=$(this).val();
-            
+
             if(parseInt(valor)){ //agrupador = 1
                 $('#informacion_completa_expediente').hide();
                 $('#serie_idserie').val(-1);
@@ -65,16 +78,16 @@ $dato_padre=busca_filtro_tabla("","expediente a","a.idexpediente=".$_REQUEST["co
                 $('#informacion_completa_expediente').show();
                 $('#serie_idserie').val('');
             }
-           
+
         });
     });
 </script>
 
-<?php 
+<?php
  if($dato_padre['numcampos']){
 ?>
 <div class="control-group element">
-	Este quedar&aacute; vinculado al expediente <b><?php echo($dato_padre[0]["nombre"]); ?></b> 
+	Este quedar&aacute; vinculado al expediente <b><?php echo($dato_padre[0]["nombre"]); ?></b>
 </div>
 
 <?php
@@ -85,7 +98,7 @@ $dato_padre=busca_filtro_tabla("","expediente a","a.idexpediente=".$_REQUEST["co
 <div class="control-group element">
   <label class="control-label" for="fecha">Fecha de creaci&oacute;n *
   </label>
-  <div class="controls"> 
+  <div class="controls">
 		<div id="fecha" class="input-append date">
 			<input data-format="yyyy-MM-dd" type="text" name="fecha" value="<?php echo(date("Y-m-d"));?>" readonly />
 			<span class="add-on">
@@ -96,32 +109,32 @@ $dato_padre=busca_filtro_tabla("","expediente a","a.idexpediente=".$_REQUEST["co
   </div>
 </div>
 <div id="informacion_completa_expediente">
-    
+
 <div class="control-group element">
   <label class="control-label" for="nombre">Descripci&oacute;n
   </label>
-  <div class="controls"> 
+  <div class="controls">
     <textarea name="descripcion" id="descripcion"></textarea>
   </div>
 </div>
 <div class="control-group element">
   <label class="control-label" for="indice_uno">Indice uno
   </label>
-  <div class="controls"> 
+  <div class="controls">
     <input type="text" name="indice_uno" id="indice_uno">
   </div>
 </div>
 <div class="control-group element">
   <label class="control-label" for="indice_dos">Indice Dos
   </label>
-  <div class="controls"> 
+  <div class="controls">
     <input type="text" name="indice_dos" id="indice_dos">
   </div>
 </div>
 <div class="control-group element">
   <label class="control-label" for="indice_tres">Indice Tres
   </label>
-  <div class="controls"> 
+  <div class="controls">
     <input type="text" name="indice_tres" id="indice_tres">
   </div>
 </div>
@@ -135,7 +148,7 @@ $dato_padre=busca_filtro_tabla("","expediente a","a.idexpediente=".$_REQUEST["co
   		$cajas=busca_filtro_tabla("","caja a,entidad_caja e","a.idcaja=e.caja_idcaja and e.estado=1 and ((e.entidad_identidad=1 and e.llave_entidad=".usuario_actual('idfuncionario').") or a.funcionario_idfuncionario=".usuario_actual('idfuncionario').")","",$conn);
 			for($i=0;$i<$cajas["numcampos"];$i++){
 				$selected="";
-				
+
 				if(@$_REQUEST["fk_idcaja"]==$cajas[$i]["idcaja"]){
 					$selected="selected";
 				}
@@ -162,21 +175,21 @@ $dato_padre=busca_filtro_tabla("","expediente a","a.idexpediente=".$_REQUEST["co
   <i class="icon-plus-sign"></i><b>Informaci&oacute;n adicional</b>
 </div>
 <div id="datos_adicionales" class="datos_adicionales collapse opcion_informacion clase_sin_capas">
-	
+
 	<div class="control-group element">
 	  <label class="control-label" for="codigo_numero">Codigo numero
 	  </label>
 	  <div class="controls">
-	      
-	    <?php  
-	        $dep_fun=busca_filtro_tabla("a.codigo","dependencia a,vfuncionario_dc b","a.estado=1 AND b.iddependencia=a.iddependencia AND b.estado_dc=1 AND b.estado_dep=1 AND b.login='".usuario_actual('login')."'","",$conn); 
+
+	    <?php
+	        $dep_fun=busca_filtro_tabla("a.codigo","dependencia a,vfuncionario_dc b","a.estado=1 AND b.iddependencia=a.iddependencia AND b.estado_dc=1 AND b.estado_dep=1 AND b.login='".usuario_actual('login')."'","",$conn);
 	    ?>
-	    <input name="codigo_numero_dependencia" id="codigo_numero_dependencia"  style="width:12%;" readonly> - 
-	    <input name="codigo_numero_serie" id="codigo_numero_serie" style="width:12%;" readonly> - 
+	    <input name="codigo_numero_dependencia" id="codigo_numero_dependencia"  style="width:12%;" readonly> -
+	    <input name="codigo_numero_serie" id="codigo_numero_serie" style="width:12%;" readonly> -
 	    <input name="codigo_numero_consecutivo" id="codigo_numero_consecutivo" style="width:10%;">
 	    <input name="codigo_numero" id="codigo_numero" type="hidden">
 	  </div>
-	  
+
 	  <script>
 	      $(document).ready(function(){
 	          $('#codigo_numero_dependencia,#codigo_numero_serie,#codigo_numero_consecutivo').keyup(function(){
@@ -187,36 +200,36 @@ $dato_padre=busca_filtro_tabla("","expediente a","a.idexpediente=".$_REQUEST["co
 	              var codigo_numero_serie=$('#codigo_numero_serie').val();
 	              if(codigo_numero_serie==''){
 	                  codigo_numero_serie=0;
-	              }	              
+	              }
 	              var codigo_numero_consecutivo=$('#codigo_numero_consecutivo').val();
 	              if(codigo_numero_consecutivo==''){
 	                  codigo_numero_consecutivo=0;
-	              }	  
+	              }
 	              var cadena_parseo=codigo_numero_dependencia+'-'+codigo_numero_serie+'-'+codigo_numero_consecutivo;
 	              $('#codigo_numero').val(cadena_parseo);
-	             
+
 	          });
 	      });
 	  </script>
-	  
+
 	</div>
-	
+
 	<div class="control-group element">
 	  <label class="control-label" for="fondo">Fondo
 	  </label>
-	  <div class="controls"> 
+	  <div class="controls">
 	    <input name="fondo" id="fondo" value="<?php echo($datos[0]["fondo"]); ?>" readonly="readonly">
 	  </div>
 	</div>
-	
+
 	<div class="control-group element">
 	  <label class="control-label" for="proceso">Proceso
 	  </label>
-	  <div class="controls"> 
+	  <div class="controls">
 	    <input name="proceso" id="proceso" value="<?php echo($datos[0]["proceso"]); ?>">
 	  </div>
 	</div>
-	
+
 	<div class="control-group element">
 	  <label class="control-label" for="fecha_extrema_i">Fecha extrema inicial
 	  </label>
@@ -228,10 +241,10 @@ $dato_padre=busca_filtro_tabla("","expediente a","a.idexpediente=".$_REQUEST["co
 			</span>
 		</div>
 	</div>
-	
+
 	<div class="control-group element">
 	  <label class="control-label" for="fecha_extrema_f">Fecha extrema final
-	  </label>	  
+	  </label>
 	  <div id="fecha_extrema_f" class="input-append date">
 			<input data-format="yyyy-MM-dd" type="text" name="fecha_extrema_f" value="<?php echo($datos[0]["fecha_extrema_f"]);?>" readonly />
 			<span class="add-on">
@@ -240,31 +253,31 @@ $dato_padre=busca_filtro_tabla("","expediente a","a.idexpediente=".$_REQUEST["co
 			</span>
 		</div>
 	</div>
-	
+
 	<div class="control-group element">
 	  <label class="control-label" for="no_unidad_conservacion">Unidad de conservaci&oacute;n
 	  </label>
-	  <div class="controls"> 
+	  <div class="controls">
 	    <input name="no_unidad_conservacion" id="no_unidad_conservacion" value="<?php echo($datos[0]["no_unidad_conservacion"]); ?>">
 	  </div>
 	</div>
-	
+
 	<div class="control-group element">
 	  <label class="control-label" for="no_folios">No folios
 	  </label>
-	  <div class="controls"> 
+	  <div class="controls">
 	    <input name="no_folios" id="no_folios" value="<?php echo($datos[0]["no_folios"]); ?>">
 	  </div>
 	</div>
-	
+
 	<div class="control-group element">
 	  <label class="control-label" for="no_carpeta">No carpeta
 	  </label>
-	  <div class="controls"> 
+	  <div class="controls">
 	    <input name="no_carpeta" id="no_carpeta" value="<?php echo($datos[0]["no_carpeta"]); ?>">
 	  </div>
 	</div>
-	
+
 	<div class="control-group element">
 	  <label class="control-label" for="soporte">Soporte
 	  </label>
@@ -282,7 +295,7 @@ $dato_padre=busca_filtro_tabla("","expediente a","a.idexpediente=".$_REQUEST["co
 	  	</select>
 	  </div>
 	</div>
-	
+
 	<div class="control-group element">
 	  <label class="control-label" for="frecuencia_consulta">Frecuencia
 	  </label>
@@ -295,7 +308,7 @@ $dato_padre=busca_filtro_tabla("","expediente a","a.idexpediente=".$_REQUEST["co
 	  	</select>
 	  </div>
 	</div>
-	
+
 	<div class="control-group element">
 	  <label class="control-label" for="ubicacion">Ubicaci&oacute;n
 	  </label>
@@ -308,17 +321,17 @@ $dato_padre=busca_filtro_tabla("","expediente a","a.idexpediente=".$_REQUEST["co
 	  	</select>
 	  </div>
 	</div>
-	
+
 	<div class="control-group element">
 	  <label class="control-label" for="notas_transf">Notas de Transferencia
 	  </label>
-	  <div class="controls"> 
+	  <div class="controls">
 	      <textarea name="notas_transf" id="notas_transf"><?php echo($datos[0]["notas_transf"]); ?></textarea>
 	  </div>
 	</div>
-	
-</div>	
-	
+
+</div>
+
 </div>
 <br />
 
@@ -349,7 +362,7 @@ $dato_padre=busca_filtro_tabla("","expediente a","a.idexpediente=".$_REQUEST["co
   echo(librerias_arboles());
   ?>
   <script type="text/javascript">
-  
+
   function cargar_info_Node(NodeId){
 	  if(treeserie_idserie.isItemChecked(NodeId)){
 	    $("#codigo_numero_serie").val(treeserie_idserie.getUserData(NodeId,"codigo"));
@@ -361,9 +374,11 @@ $dato_padre=busca_filtro_tabla("","expediente a","a.idexpediente=".$_REQUEST["co
 	  	$("#codigo_numero_serie").val("");
 	  }
   }
-  
-  $(document).ready(function (){
-		url2="test/test_serie_funcionario.php?tipo1=1&tipo2=1&tipo3=0&tvd=0";
+
+  $(document).ready(function() {
+	  var serie_padre = "<?php echo $serie_padre?>";
+	  console.log("Padre " + serie_padre);
+		url2="test/test_serie_funcionario.php?tipo1=1&tipo2=1&tipo3=0&tvd=0&id=" + serie_padre;
 		$.ajax({
 			url : "<?php echo($ruta_db_superior);?>test/crear_arbol.php",
 			data:{xml:url2,campo:"serie_idserie",radio:1,ruta_db_superior:"../../",busqueda_item:1,onNodeSelect:"cargar_info_Node"},
@@ -375,8 +390,8 @@ $dato_padre=busca_filtro_tabla("","expediente a","a.idexpediente=".$_REQUEST["co
 				top.noty({text: 'No se pudo cargar el arbol de series',type: 'error',layout: 'topCenter',timeout:5000});
 			}
 		});
-  
-  
+
+
     $(".opcion_informacion").on("hide",function(){
 		  $(this).prev().children("i").removeClass();
 		  $(this).prev().children("i").addClass("icon-plus-sign");
@@ -396,87 +411,87 @@ $(document).ready(function(){
   $('#fecha').datetimepicker({
     language: 'es',
     pick12HourFormat: true,
-    pickTime: false      
+    pickTime: false
   });
   $('#fecha_extrema_i').datetimepicker({
     language: 'es',
     pick12HourFormat: true,
-    pickTime: false      
+    pickTime: false
   });
   $('#fecha_extrema_f').datetimepicker({
     language: 'es',
     pick12HourFormat: true,
-    pickTime: false      
+    pickTime: false
   });
-  
+
   var formulario_expediente=$("#formulario_expediente");
   formulario_expediente.validate({
-    ignore: [],  
+    ignore: [],
   "rules":{
       "nombre":{"required":true},
       "serie_idserie":{"required":true}
-      
+
   },
   submitHandler: function(form) {
   }
   });
-  
-  $("#submit_formulario_expediente").click(function(){  
+
+  $("#submit_formulario_expediente").click(function(){
     if(formulario_expediente.valid()){
     	$('#cargando_enviar').html("<div id='icon-cargando'></div>Procesando");
 			$(this).attr('disabled', 'disabled');
     	<?php if(@$_REQUEST["volver"]&&@$_REQUEST["enlace"]){ ?>
     		window.open('<?php echo($ruta_db_superior.$_REQUEST["enlace"]); ?>?variable_busqueda=idexpediente/**/<?php echo($_REQUEST["cod_padre"]); ?>&idbusqueda_componente=<?php echo($_REQUEST["idbusqueda_componente"]); ?>','_self');
     	<?php }  ?>
-    
+
     <?php encriptar_sqli("formulario_expediente",0,"form_info",$ruta_db_superior); ?>
       $.ajax({
         type:'POST',
         async:false,
         url: "<?php echo($ruta_db_superior);?>pantallas/expediente/ejecutar_acciones.php",
         data: "rand="+Math.round(Math.random()*100000)+"&"+formulario_expediente.serialize(),
-        success: function(html){               
-          if(html){    
-            var objeto=jQuery.parseJSON(html);                  
+        success: function(html){
+          if(html){
+            var objeto=jQuery.parseJSON(html);
             if(objeto.exito){
               $.ajax({
                 type:'POST',
                 async:false,
                 url: "<?php echo($ruta_db_superior);?>pantallas/busquedas/servidor_busqueda.php",
                 data: "idbusqueda_componente=<?php echo($_REQUEST['idbusqueda_componente']); ?>&page=1&rows=1&actual_row=0&expediente_actual="+objeto.idexpediente+"&idexpediente=<?php echo(@$_REQUEST['cod_padre']);?>&variable_busqueda=<?php echo(@$_REQUEST["estado_archivo"]); ?>",
-                success: function(html2){              
-                  if(html2){      
+                success: function(html2){
+                  if(html2){
                     var objeto2=jQuery.parseJSON(html2);
                     if(objeto2.exito!==0){
                       $("#<?php echo($_REQUEST['div_actualiza']);?>", parent.document).prepend(objeto2.rows[0].info);
-                    }  
+                    }
                   }
                 }
-              });   
-              $('#cargando_enviar').html("Terminado ...");  
-              if($("#cerrar_higslide").val()){            
+              });
+              $('#cargando_enviar').html("Terminado ...");
+              if($("#cerrar_higslide").val()){
                 $("#arbol_padre_actualizado", parent.document).val($("#cod_padre").val());
-                parent.window.hs.getExpander().close();                  
-              }                                        
+                parent.window.hs.getExpander().close();
+              }
               if($("#iddocumento").val()==''){
-                notificacion_saia(objeto.mensaje,"success","",2500);                                                                                            
+                notificacion_saia(objeto.mensaje,"success","",2500);
                 window.open("detalles_expediente.php?idexpediente="+objeto.idexpediente+"&idbusqueda_componente=<?php echo($_REQUEST['idbusqueda_componente']);?>&rand="+Math.round(Math.random()*100000),"_self");
               }
               else{
                 window.open("vincular_documento.php?idexpediente="+objeto.idexpediente+"&iddocumento="+$("#iddocumento").val()+"&rand="+Math.round(Math.random()*100000),"_self");
-              }                                           	
+              }
             }
             else{
               $('#cargando_enviar').html("Terminado ...");
               notificacion_saia(objeto.mensaje,"error","",8500);
-            }                  
-          }          
+            }
+          }
         }
     	});
     }
     else{
       notificacion_saia("Formulario con errores","error","",8500);
     }
-  });  
+  });
 });
 </script>
