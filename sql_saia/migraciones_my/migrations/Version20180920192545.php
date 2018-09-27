a2209c90035b3586a755acf1da760d716fc79cc2
Giovanni Montes <giovanni.montes@cerok.com>
2018-09-27 10:06:58 -0500

    modificar la vista vpermiso_serie

--- sql_saia/migraciones_my/migrations/Version20180920192545.php
+++ sql_saia/migraciones_my/migrations/Version20180920192545.php
@@ -30,10 +30,19 @@ class Version20180920192545 extends AbstractMigration {
         if ($table && !$table->hasColumn("permiso")) {
             $table->addColumn("permiso", "string", [
                 "length" => 20,
-                "default" => "l,a,v",
+                "default" => "l",
                 "comment" => "l: Lectura, a: Adición, m: Modificación, e: Eliminación, v: Vinculación"
             ]);
         }
+        $table = $schema->getTable('expediente');
+        if ($table) {
+            $table->changeColumn("serie_idserie", "integer", [
+                "length" => 11,
+                "default" => 0,
+                "notnull" => true
+            ]);
+        }
+        $this->addSql($this->crear_vista());
     }
 
     public function preDown(Schema $schema) {
@@ -53,5 +62,144 @@ class Version20180920192545 extends AbstractMigration {
         if ($table && $table->hasColumn("permiso")) {
             $table->dropColumn("permiso");
         }
+        $this->addSql($this->devolver_vista());
     }
+
+    private function crear_vista() {
+        $vista = <<<FINSQL
+CREATE
+OR REPLACE
+VIEW vpermiso_serie AS select
+    f.idfuncionario AS idfuncionario,
+    f.funcionario_codigo AS funcionario_codigo,
+    s.idserie AS idserie,
+    s.nombre AS nombre_serie,
+    s.cod_padre AS cod_padre,
+    s.codigo AS codigo,
+    s.tipo AS tipo,
+    s.categoria AS categoria,
+    s.tvd AS tvd,
+    s.cod_arbol AS cod_arbol,
+    s.estado AS estado_serie,
+    p.permiso
+from
+     permiso_serie p
+join serie s on p.serie_idserie = s.idserie
+join funcionario f on f.idfuncionario = p.llave_entidad
+where
+     p.entidad_identidad = 1
+    and p.estado = 1
+union select
+    v.idfuncionario AS idfuncionario,
+    v.funcionario_codigo AS funcionario_codigo,
+    s.idserie AS idserie,
+    s.nombre AS nombre_serie,
+    s.cod_padre AS cod_padre,
+    s.codigo AS codigo,
+    s.tipo AS tipo,
+    s.categoria AS categoria,
+    s.tvd AS tvd,
+    s.cod_arbol AS cod_arbol,
+    s.estado AS estado_serie,
+    p.permiso
+from
+    permiso_serie p
+join serie s on p.serie_idserie = s.idserie
+join vfuncionario_dc v on v.iddependencia = p.llave_entidad
+where
+    p.entidad_identidad = 2
+    and v.estado_dc = 1
+    and p.estado = 1
+union select
+    v.idfuncionario AS idfuncionario,
+    v.funcionario_codigo AS funcionario_codigo,
+    s.idserie AS idserie,
+    s.nombre AS nombre_serie,
+    s.cod_padre AS cod_padre,
+    s.codigo AS codigo,
+    s.tipo AS tipo,
+    s.categoria AS categoria,
+    s.tvd AS tvd,
+    s.cod_arbol AS cod_arbol,
+    s.estado AS estado_serie,
+    p.permiso
+from
+    permiso_serie p
+join serie s on p.serie_idserie = s.idserie
+join vfuncionario_dc v on v.idcargo = p.llave_entidad
+where
+    p.entidad_identidad = 4
+    and v.estado_dc = 1
+    and p.estado = 1
+FINSQL;
+        return $vista;
+    }
+
+    private function devolver_vista() {
+        $vista = <<<FINSQL
+CREATE
+OR REPLACE
+VIEW vpermiso_serie AS select
+    f.idfuncionario AS idfuncionario,
+    f.funcionario_codigo AS funcionario_codigo,
+    s.idserie AS idserie,
+    s.nombre AS nombre_serie,
+    s.cod_padre AS cod_padre,
+    s.codigo AS codigo,
+    s.tipo AS tipo,
+    s.categoria AS categoria,
+    s.tvd AS tvd,
+    s.cod_arbol AS cod_arbol,
+    s.estado AS estado_serie
+from
+     permiso_serie p
+join serie s on p.serie_idserie = s.idserie
+join funcionario f on f.idfuncionario = p.llave_entidad
+where
+     p.entidad_identidad = 1
+    and p.estado = 1
+union select
+    v.idfuncionario AS idfuncionario,
+    v.funcionario_codigo AS funcionario_codigo,
+    s.idserie AS idserie,
+    s.nombre AS nombre_serie,
+    s.cod_padre AS cod_padre,
+    s.codigo AS codigo,
+    s.tipo AS tipo,
+    s.categoria AS categoria,
+    s.tvd AS tvd,
+    s.cod_arbol AS cod_arbol,
+    s.estado AS estado_serie
+from
+    permiso_serie p
+join serie s on p.serie_idserie = s.idserie
+join vfuncionario_dc v on v.iddependencia = p.llave_entidad
+where
+    p.entidad_identidad = 2
+    and v.estado_dc = 1
+    and p.estado = 1
+union select
+    v.idfuncionario AS idfuncionario,
+    v.funcionario_codigo AS funcionario_codigo,
+    s.idserie AS idserie,
+    s.nombre AS nombre_serie,
+    s.cod_padre AS cod_padre,
+    s.codigo AS codigo,
+    s.tipo AS tipo,
+    s.categoria AS categoria,
+    s.tvd AS tvd,
+    s.cod_arbol AS cod_arbol,
+    s.estado AS estado_serie
+from
+    permiso_serie p
+join serie s on p.serie_idserie = s.idserie
+join vfuncionario_dc v on v.idcargo = p.llave_entidad
+where
+    p.entidad_identidad = 4
+    and v.estado_dc = 1
+    and p.estado = 1
+FINSQL;
+        return $vista;
+    }
+
 }
