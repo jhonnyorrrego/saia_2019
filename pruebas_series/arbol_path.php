55df81c51769968b4bc8106d860cb7d1c7b92956
Giovanni Montes <giovanni.montes@cerok.com>
2018-10-04 17:46:43 -0500

    crear arbol

--- pruebas_series/arbol_path.php
+++ pruebas_series/arbol_path.php
@@ -15,16 +15,76 @@ for ($i = 0; $i < $dependencias["numcampos"]; $i++) {
         "key" => $dependencias[$i]["iddependencia"],
         "title" => $dependencias[$i]["nombre"] . " (" . $dependencias[$i]["codigo"] . ")",
         "padre" => $dependencias[$i]["cod_padre"],
-        "path" => $dependencias[$i]["codigo_arbol"]
+
     ];
-    $elementos[] = $item;
+    $elementos[$dependencias[$i]["codigo_arbol"]] = $item;
 }
 
-$arbol = pathToTree($elementos);
+$arbol = explodeTree($elementos, ".");
+
+// $arbol = pathToTree($elementos);
 
 print_r($arbol);
 
-function pathToTree($array) {
+function explodeTree($array, $delimiter = '_') {
+    if (!is_array($array)) {
+        return false;
+    }
+    $splitRE = '/' . preg_quote($delimiter, '/') . '/';
+    $returnArr = array();
+    foreach ($array as $key => $val) {
+        // Get parent parts and the current leaf
+        $parts = preg_split($splitRE, $key, -1, PREG_SPLIT_NO_EMPTY);
+        $leafPart = array_pop($parts);
+
+        // Build parent structure
+        // Might be slow for really deep and large structures
+        $parentArr = &$returnArr;
+        foreach ($parts as $part) {
+            $nodo_existe = &existe($parentArr, $part);
+            if (!$nodo_existe) {
+                $parentArr[] = busca_dependencia($part);
+            } elseif (!is_array($nodo_existe)) {
+                $parentArr[] = busca_dependencia($part);
+            }
+            //$parentArr = &$parentArr[$part];
+            $parentArr = &existe($parentArr, $part)["children"];
+        }
+
+        // Add the final part to the structure
+        if (!existe($parentArr, $leafPart)) {
+            $parentArr[] = $val;
+        }
+    }
+    return $returnArr;
+}
+
+function &existe(&$array, $id) {
+    $longitud = count($array);
+    for ($i = 0; $i < $longitud; $i++) {
+        if ($array[$i]["key"] == $id) {
+            return $array[$i];
+        }
+    }
+    return false;
+}
+
+function busca_dependencia($iddep) {
+    $dependencias = busca_filtro_tabla("iddependencia, cod_padre, codigo, nombre, codigo_arbol", "dependencia", "iddependencia = $iddep", "", $conn);
+
+    if($dependencias["numcampos"]) {
+        $item = [
+            "key" => $iddep,
+            "title" => $dependencias[0]["nombre"] . " (" . $dependencias[$i]["codigo"] . ")",
+            "padre" => $dependencias[0]["cod_padre"],
+            "children" => null
+        ];
+        return $item;
+    }
+    return null;
+}
+
+function pathToTree_orig($array) {
     $tree = array();
     foreach ($array as $item) {
         $pathIds = explode(".", $item["path"]);
@@ -32,20 +92,17 @@ function pathToTree($array) {
         foreach ($pathIds as $id) {
             if (!isset($current["children"][$id])) {
                 $current["children"][$id] = array();
-                //$current["children"][$id]["hola"] = "hola";
-                $deps = busca_filtro_tabla("iddependencia, cod_padre, codigo, nombre, codigo_arbol",
-                    "dependencia", "iddependencia = $id", "", $conn);
+                // $current["children"][$id]["hola"] = "hola";
+                $deps = busca_filtro_tabla("iddependencia, cod_padre, codigo, nombre, codigo_arbol", "dependencia", "iddependencia = $id", "", $conn);
                 $current["children"][$id]["key"] = $id;
                 $current["children"][$id]["title"] = $deps[0]["nombre"] . " (" . $deps[0]["codigo"] . ")";
             }
             $current = &$current["children"][$id];
             if ($id == $item["key"]) {
-                //TODO: Aqui llenar serie
+                // TODO: Aqui llenar serie
                 $current = $item;
             }
         }
     }
     return $tree["children"];
 }
-
-
