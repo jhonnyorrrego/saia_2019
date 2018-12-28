71b01e35f6d4abf616b2b74446f274c446a93d4e
Giovanni Montes <giovanni.montes@cerok.com>
2018-12-28 10:55:40 -0500

    Creacion del campo colilla en correspondencia de entrada

--- sql_saia/migraciones_my/migrations/Version20181228154312.php
+++ sql_saia/migraciones_my/migrations/Version20181228154312.php
@@ -10,8 +10,7 @@ use Doctrine\DBAL\Schema\Schema;
  */
 class Version20181228154312 extends AbstractMigration
 {
-    private $campos_formato = array(
-        array('idcampos_formato' => '7009','formato_idformato' => '3','nombre' => 'colilla','etiqueta' => 'Colilla','tipo_dato' => 'INT','longitud' => '11','obligatoriedad' => '1','valor' => '1,Vertical;2,Horizontal','acciones' => 'a,b','ayuda' => NULL,'predeterminado' => '1','banderas' => NULL,'etiqueta_html' => 'radio','orden' => '31','mascara' => NULL,'adicionales' => NULL,'autoguardado' => '0','fila_visible' => '1','placeholder' => NULL,'longitud_vis' => NULL,'opciones' => NULL,'estilo' => NULL));
+    private $campos_formato = array('idcampos_formato' => '7009','formato_idformato' => '3','nombre' => 'colilla','etiqueta' => 'Colilla','tipo_dato' => 'INT','longitud' => '11','obligatoriedad' => '1','valor' => '1,Vertical;2,Horizontal','acciones' => 'a,b','ayuda' => NULL,'predeterminado' => '1','banderas' => NULL,'etiqueta_html' => 'radio','orden' => '31','mascara' => NULL,'adicionales' => NULL,'autoguardado' => '0','fila_visible' => '1','placeholder' => NULL,'longitud_vis' => NULL,'opciones' => NULL,'estilo' => NULL);
 
     public function getDescription() {
         return 'Actualizar campo colilla radicacion de correspondencia';
