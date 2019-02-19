00329c854970fa1c3ff64ec5f04e648c9c610eb5
Alejandro Carvajal <>
2019-02-19 21:39:50 +0000

    correccion carta

---
+++ formatos/carta/previo_mostrar_carta.php
@@ -0,0 +1,2 @@
+<?php include_once('../librerias/estilo_formulario.php'); include_once('../librerias/funciones_formatos_generales.php');?><p>pppppp</p>
+<?php listado_hijos_formato(1,$_REQUEST["iddoc"]); ?>
