857f7bed83fe2b6ecd0760fd8fef1781c45b6a9f
jhon sebastian valencia <jhon.valencia@cerok.com>
2019-02-26 14:43:03 -0500

    validaciones en el mostrar pdf

--- formatos/librerias/footer_nuevo.php
+++ formatos/librerias/footer_nuevo.php
@@ -1,19 +1,22 @@
 <?php
-if (isset($_REQUEST["vista"]) && $_REQUEST["vista"]) {
-    $vista = busca_filtro_tabla("pie_pagina", "vista_formato", "idvista_formato='" . $_REQUEST["vista"] . "'", "", $conn);
-    $contenido_pie = busca_filtro_tabla("contenido", "encabezado_formato", "idencabezado_formato='" . $vista[0]["pie_pagina"] . "'", "", $conn);
-    $pie = busca_filtro_tabla("encabezado", $formato[0]["nombre_tabla"], "documento_iddocumento='" . $_REQUEST["iddoc"] . "'", "", $conn);
-} else {
-    $contenido_pie = busca_filtro_tabla("contenido", "encabezado_formato", "idencabezado_formato='" . $formato[0]["pie_pagina"] . "'", "", $conn);
-    $pie = busca_filtro_tabla("encabezado", $formato[0]["nombre_tabla"], "documento_iddocumento='" . $_REQUEST["iddoc"] . "'", "", $conn);
-} ?>
+if(!$_REQUEST['tipo'] || ($_REQUEST['tipo'] && $_REQUEST['tipo'] != 5)){
+    if (isset($_REQUEST["vista"]) && $_REQUEST["vista"]) {
+        $vista = busca_filtro_tabla("pie_pagina", "vista_formato", "idvista_formato='" . $_REQUEST["vista"] . "'", "", $conn);
+        $contenido_pie = busca_filtro_tabla("contenido", "encabezado_formato", "idencabezado_formato='" . $vista[0]["pie_pagina"] . "'", "", $conn);
+        $pie = busca_filtro_tabla("encabezado", $formato[0]["nombre_tabla"], "documento_iddocumento='" . $_REQUEST["iddoc"] . "'", "", $conn);
+    } else {
+        $contenido_pie = busca_filtro_tabla("contenido", "encabezado_formato", "idencabezado_formato='" . $formato[0]["pie_pagina"] . "'", "", $conn);
+        $pie = busca_filtro_tabla("encabezado", $formato[0]["nombre_tabla"], "documento_iddocumento='" . $_REQUEST["iddoc"] . "'", "", $conn);
+    } 
+}
+?>
 </div>
 </div>
-<div class="page_margin_bottom" id="doc_footer">
-    <?php if ($pie[0][0] && $contenido_pie["numcampos"]) {
-        echo crear_encabezado_pie_pagina(stripslashes($contenido_pie[0][0]), $_REQUEST["iddoc"], $formato[0]["idformato"]);
-    } ?>
-</div>
+<?php if ($pie[0][0] && $contenido_pie["numcampos"]): ?>
+    <div class="page_margin_bottom" id="doc_footer">
+        <?= crear_encabezado_pie_pagina(stripslashes($contenido_pie[0][0]), $_REQUEST["iddoc"], $formato[0]["idformato"]) ?>
+    </div>
+<?php endif; ?>
 </div> <!-- end page-n -->
 </div> <!-- end #documento-->
 </div> <!-- end .container -->
