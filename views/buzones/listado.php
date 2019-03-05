81216b6f6eb689c1519b54dc9fbc66dd4105c2e4
jhon sebastian valencia <jhon.valencia@cerok.com>
2019-03-05 08:38:42 -0500

    cambio pie pagina de bootstrap table y actullizacion de la libreria

--- views/buzones/listado.php
+++ views/buzones/listado.php
@@ -14,9 +14,8 @@ include_once $ruta_db_superior . 'db.php';
 include_once $ruta_db_superior . 'assets/librerias.php';
 
 $params = (!empty($_REQUEST)) ? $_REQUEST : [];
-
-global $conn;
-$component = busca_filtro_tabla('a.ruta_libreria_pantalla,b.encabezado_componente', 'busqueda a,busqueda_componente b', 'a.idbusqueda = b.busqueda_idbusqueda and b.idbusqueda_componente=' . $_REQUEST['idbusqueda_componente'], '', $conn);
+$sql = "select a.cantidad_registros,a.ruta_libreria_pantalla,b.encabezado_componente from busqueda a,busqueda_componente b where a.idbusqueda = b.busqueda_idbusqueda and b.idbusqueda_componente={$_REQUEST['idbusqueda_componente']}";
+$component = StaticSql::search($sql);
 ?>
 <?= bootstrapTable() ?>
 <div class="container px-0">
@@ -26,45 +25,43 @@ $component = busca_filtro_tabla('a.ruta_libreria_pantalla,b.encabezado_component
     </div>
 </div>
 <script>
-    $(function(){
+    $(function() {
         var baseUrl = $("script[data-baseurl]").data('baseurl');
         var params = <?= json_encode($params); ?>;
-        var component = params.idbusqueda_componente;
         var encabezado = '<?= $component[0]["encabezado_componente"] ?>'
         var table = $('#table');
-        
+
         table.bootstrapTable({
             url: `${baseUrl}app/busquedas/datosBootstrapTable.php`,
-            queryParams: function (queryParams) {
-                queryParams=$.extend(queryParams, params);                
+            queryParams: function(queryParams) {
+                queryParams = $.extend(queryParams, params);
                 return queryParams;
             },
             sidePagination: 'server',
             queryParamsType: 'other',
-            cardView : true,
+            cardView: true,
             pagination: true,
             maintainSelected: true,
-            pageSize: 15,
+            pageSize: '<?= $component[0]['cantidad_registros'] ?>',
             height: $(window).height() - $('#header_list').height(),
             columns: [{
                 field: 'info',
                 title: ''
             }],
-            responseHandler: function(response){
+            responseHandler: function(response) {
                 for (let index = 0; index < response.rows.length; index++) {
                     let node = $(response.rows[index].info);
                     let identificador = node.find('.identificator').val();
-                    if(identificador){
+                    if (identificador) {
                         node.find('#checkbox_location').html(`<input data-index="${index}" data-id="${identificador}" name="btSelectItem" type="checkbox">`);
                         response.rows[index].info = node.prop('outerHTML');
                     }
                 }
                 return response;
             },
-            onPostBody: function(){
-                $('.card-view .title').hide();
-                table.parents('.bootstrap-table').addClass('w-100');
-                
+            onPostBody: function() {
+                beautyTable();
+
                 var selections = table.data('selections').split(',').map(Number).filter(n => n > 0);
                 selections.forEach(s => {
                     $(`:checkbox[data-id=${s}]`).prop('checked', true);
@@ -72,27 +69,27 @@ $component = busca_filtro_tabla('a.ruta_libreria_pantalla,b.encabezado_component
 
                 paintSelected();
             },
-            onClickRow: function(row, element, field){
+            onClickRow: function(row, element, field) {
                 paintSelected();
                 element.addClass('selected');
-            }            
+            }
         });
 
-        table.on('check.bs.table uncheck.bs.table', function (row, data) {
+        table.on('check.bs.table uncheck.bs.table', function(row, data) {
             var selections = $(this).data('selections').split(',').map(Number).filter(n => n > 0);
             let index = $(data.info).find(':checkbox').data('index');
             let checkbox = $(`:checkbox[data-index=${index}]`);
             let checked = checkbox.is(':checked');
-            
+
             $(`:checkbox[data-id=${checkbox.data('id')}]`).prop('checked', checked);
 
-            $('[name="btSelectItem"]').each(function (i, element) {
+            $('[name="btSelectItem"]').each(function(i, element) {
                 let id = $(element).data('id');
 
-                if(element.checked && $.inArray(id, selections) == -1){
+                if (element.checked && $.inArray(id, selections) == -1) {
                     selections.push(id);
-                }else if(!element.checked){
-                    selections = selections.filter(n => n != id );
+                } else if (!element.checked) {
+                    selections = selections.filter(n => n != id);
                 }
             });
 
@@ -100,33 +97,40 @@ $component = busca_filtro_tabla('a.ruta_libreria_pantalla,b.encabezado_component
             paintSelected();
         });
 
-        function paintSelected(){
+        function paintSelected() {
             var selections = table.data('selections').split(',').map(Number).filter(n => n > 0);
             $("#selected_rows").text(selections.length);
             $('tr[data-index]').removeClass('selected');
 
-            if(selections.length){
-                $('[name="btSelectItem"]:checked').each(function (i, e) {
+            if (selections.length) {
+                $('[name="btSelectItem"]:checked').each(function(i, e) {
                     $(e).parents('tr[data-index]').addClass('selected');
                 });
             }
         }
 
-        if(encabezado){
-            $("#header_list").load(baseUrl+encabezado,{
-                idbusqueda_componente: component
-            }, function(){
+        function beautyTable() {
+            table.parents('.bootstrap-table').addClass('w-100');
+            table.find('tr[data-index] td').addClass('p-2');
+            $('.card-view .title').remove();
+            $('.pagination-detail .page-list').remove();
+        }
+
+        if (encabezado) {
+            $("#header_list").load(baseUrl + encabezado, {
+                idbusqueda_componente: params.idbusqueda_componente
+            }, function() {
                 $('[data-toggle="tooltip"]').tooltip();
             });
         }
     });
 </script>
 <?php
-if ($component['numcampos'] && $component[0]['ruta_libreria_pantalla']) {
+if ($component[0]['ruta_libreria_pantalla']) {
     $libraries = explode(',', $component[0]['ruta_libreria_pantalla']);
 
     foreach ($libraries as $librarie) {
         include_once $ruta_db_superior . $librarie;
     }
 }
-?>
+?> 
