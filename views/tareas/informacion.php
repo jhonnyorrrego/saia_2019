7c1c3c378437acd402ba2c627b436b083306d49b
jhon sebastian valencia <jhon.valencia@cerok.com>
2019-03-19 20:22:26 -0500

    envio de correo desde tareas

--- views/tareas/informacion.php
+++ views/tareas/informacion.php
@@ -18,18 +18,12 @@ while ($max_salida > 0) {
     <label class="my-0" for="name">Nombre de la tarea<span class="text-danger">*</span></label>
     <input type="text" class="form-control" id="name" placeholder="Qué desea que se realice?">
 </div>
-<div class="row">
-    <div class="col">
-        <div class="form-group">
-            <label class="my-0" for="manager">Responsable</label>
-            <select class="form-control" id="manager" multiple="multiple" placeholder="Quien desea que la realice?"></select>                                
-        </div>
-    </div>
-    <div class="col-auto py-3">
-        <div class="form-check">
-            <input type="checkbox" class="form-check-input" id="send_notification">
-            <label class="form-check-label" for="send_notification">Desea notificar por email?</label>
-        </div>                            
+<div class="form-group">
+    <label class="my-0" for="manager">Responsable</label>
+    <select class="form-control" id="manager" multiple="multiple" placeholder="Quien desea que la realice?"></select>
+    <div class="form-check">
+        <input type="checkbox" class="form-check-input" id="send_notification">
+        <label class="form-check-label" for="send_notification">Desea notificar por email?</label>
     </div>
 </div>
 <div class="form-group">
@@ -47,9 +41,10 @@ while ($max_salida > 0) {
 </div>
 <div class="form-group text-right">
     <button class="btn btn-complete" id="save_task"></button>
+    <div class="float-right progress-circle-indeterminate" id="spiner" style="display:none"></div>
 </div>
 <script src="<?= $ruta_db_superior ?>assets/theme/assets/plugins/select2/js/select2.min.js"></script>
 <script src="<?= $ruta_db_superior ?>assets/theme/assets/plugins/select2/js/i18n/es.js"></script>
 <script src="<?= $ruta_db_superior ?>assets/theme/assets/plugins/bootstrap-datetimepicker/js/bootstrap-datetimepicker.js"></script>
 <script src="<?= $ruta_db_superior ?>assets/theme/assets/plugins/bootstrap-datetimepicker/js/locales/es.js"></script>
-<script src="<?= $ruta_db_superior ?>views/tareas/js/informacion.js"></script>
+<script src="<?= $ruta_db_superior ?>views/tareas/js/informacion.js"></script> 
--- views/tareas/js/informacion.js
+++ views/tareas/js/informacion.js
@@ -1,32 +1,32 @@
-$(function(){
+$(function() {
     let baseUrl = Session.getBaseUrl();
-    let params = $('script[data-params]').data('params');
+    let params = $("script[data-params]").data("params");
     let language = {
-        errorLoading: function () {
-            return "La carga falló" 
+        errorLoading: function() {
+            return "La carga falló";
         },
-        inputTooLong: function (e) {
+        inputTooLong: function(e) {
             var t = e.input.length - e.maximum,
                 n = "Por favor,elimine " + t + " car";
-            return t == 1 ? n += "ácter" : n += "acteres"; 
+            return t == 1 ? (n += "ácter") : (n += "acteres");
         },
-        inputTooShort: function (e) {
+        inputTooShort: function(e) {
             var t = e.minimum - e.input.length,
-            n = "Por favor,introduzca " + t + " car";
-            return t == 1 ? n += "ácter" : n += "acteres";
+                n = "Por favor,introduzca " + t + " car";
+            return t == 1 ? (n += "ácter") : (n += "acteres");
         },
-        loadingMore: function () {
-            return "Cargando más resultados…" 
+        loadingMore: function() {
+            return "Cargando más resultados…";
         },
-        maximumSelected: function (e) {
+        maximumSelected: function(e) {
             var t = "Sólo puede seleccionar " + e.maximum + " elemento";
-            return e.maximum != 1 && (t += "s"); 
+            return e.maximum != 1 && (t += "s");
         },
-        noResults: function () {
-            return "No se encontraron resultados" 
+        noResults: function() {
+            return "No se encontraron resultados";
         },
-        searching: function () {
-            return "Buscando…" 
+        searching: function() {
+            return "Buscando…";
         }
     };
 
@@ -36,141 +36,173 @@ $(function(){
         if (!params.id) {
             createDatePicker();
             checkName();
-        }else{
+        } else {
             findFormData(params.id);
         }
     })();
-    
+
     $("#manager").select2({
         minimumInputLength: 3,
         language: language,
         ajax: {
             url: `${baseUrl}app/funcionario/autocompletar.php`,
-            dataType: 'json',
-            data: function (params) {
+            dataType: "json",
+            data: function(params) {
                 return {
                     term: params.term,
-                    key: localStorage.getItem('key')
-                }
+                    key: localStorage.getItem("key")
+                };
             },
-            processResults: function (response) {                                
-                return response.success ? {results: response.data} : {};
+            processResults: function(response) {
+                return response.success ? { results: response.data } : {};
             }
         }
     });
 
-    $("#manager").on('select2:unselect', function (e) {
+    $("#manager").on("select2:unselect", function(e) {
         let id = e.params.data.id;
-        $(this).find(`[value="${id}"]`).remove();
+        $(this)
+            .find(`[value="${id}"]`)
+            .remove();
     });
 
-    $(document).on('keyup', '#name', function () {
+    $(document).on("keyup", "#name", function() {
         checkName();
     });
-    
-    $('#save_task').on('click', function(){
-        let key = localStorage.getItem('key');
-        let managers = $('#manager').val();
-        let initial = moment($('#final_date').val(), 'DD/MM/YYYY hh:mm a')
-            .subtract(30, "minutes").format('YYYY-MM-DD HH:mm:ss');
-        let final = moment($('#final_date').val(), 'DD/MM/YYYY hh:mm a').format('YYYY-MM-DD HH:mm:ss');
+
+    $("#save_task").on("click", function() {
+        let key = localStorage.getItem("key");
+        let managers = $("#manager").val();
+        let initial = moment($("#final_date").val(), "DD/MM/YYYY hh:mm a")
+            .subtract(30, "minutes")
+            .format("YYYY-MM-DD HH:mm:ss");
+        let final = moment($("#final_date").val(), "DD/MM/YYYY hh:mm a").format(
+            "YYYY-MM-DD HH:mm:ss"
+        );
 
         data = {
             task: params.id || 0,
             key: key,
-            name: $('#name').val(),
+            name: $("#name").val(),
             managers: managers.length ? managers : [key],
-            notification: $('#send_notification').is(':checked') ? 1 : 0,
+            notification: $("#send_notification").is(":checked") ? 1 : 0,
             initialDate: initial,
             finalDate: final,
-            description: $('#description').val(),
+            description: $("#description").val(),
             documentId: params.documentId
-        }
-        
-        $.post(`${baseUrl}/app/tareas/guardar.php`, data, function(response){
-            if(response.success){
-                top.notification({
-                    type: 'success',
-                    message: response.message
-                });
+        };
 
-                let newParams = JSON.stringify({ id: response.data });
-                $('script[data-params]').attr('data-params', newParams);
-                $('.tasktab.disabled').removeClass('disabled');
-                $('#iframe_workspace').contents().find('.fc-refresh-button').trigger('click');
-            }
-        }, 'json')
+        $("#save_task,#spiner").toggle();
+        $.post(
+            `${baseUrl}/app/tareas/guardar.php`,
+            data,
+            function(response) {
+                $("#save_task,#spiner").toggle();
+
+                if (response.success) {
+                    top.notification({
+                        type: "success",
+                        message: response.message
+                    });
+
+                    let newParams = JSON.stringify({ id: response.data });
+                    $("script[data-params]").attr("data-params", newParams);
+                    $(".tasktab.disabled").removeClass("disabled");
+                    $("#iframe_workspace")
+                        .contents()
+                        .find(".fc-refresh-button")
+                        .trigger("click");
+                } else {
+                    top.notification({
+                        type: "error",
+                        message: response.message
+                    });
+                }
+            },
+            "json"
+        );
     });
 
-    function findFormData(id){
-        $.post(`${baseUrl}app/tareas/consulta.php`, {
-            key: localStorage.getItem('key'),
-            task: id
-        }, function(response){
-            if(response.success){
-                fillForm(response.data);
-            }else{
-                top.notification({
-                    type: 'error',
-                    message: response.message
-                })
-            }
-        }, 'json');
+    function findFormData(id) {
+        $.post(
+            `${baseUrl}app/tareas/consulta.php`,
+            {
+                key: localStorage.getItem("key"),
+                task: id
+            },
+            function(response) {
+                if (response.success) {
+                    fillForm(response.data);
+                } else {
+                    top.notification({
+                        type: "error",
+                        message: response.message
+                    });
+                }
+            },
+            "json"
+        );
     }
 
-    function fillForm(data){
-        $('#name').val(data.task.nombre);        
-        let finaldate = moment(data.task.fecha_final, 'YYYY-MM-DD HH:mm:ss').format('DD/MM/YYYY hh:mm a');
-        $('#final_date').val(finaldate);
-        $('#description').val(data.task.descripcion);
-        fillSelect('#manager', data.users.managers);
+    function fillForm(data) {
+        $("#name").val(data.task.nombre);
+        let finaldate = moment(
+            data.task.fecha_final,
+            "YYYY-MM-DD HH:mm:ss"
+        ).format("DD/MM/YYYY hh:mm a");
+        $("#final_date").val(finaldate);
+        $("#description").val(data.task.descripcion);
+        fillSelect("#manager", data.users.managers);
     }
 
-    function fillSelect(selector, data){
-        if(data.length){
+    function fillSelect(selector, data) {
+        if (data.length) {
             data.forEach(u => {
                 $(selector).append(
-                    $('<option>',{
+                    $("<option>", {
                         value: u.id,
                         text: u.name,
                         selected: true
                     })
                 );
-            })
+            });
         }
     }
 
-    function checkName() {        
-        $('#save_task').attr('disabled', $('#name').val().length ? false : true);
+    function checkName() {
+        $("#save_task").attr(
+            "disabled",
+            $("#name").val().length ? false : true
+        );
     }
 
     function createDatePicker() {
-        let time = params.finalTime ?
-            moment(params.finalTime, 'YYYY-MM-DD HH:mm:ss') :
-            moment();
-        
-        $('#final_date').datetimepicker({
+        let time = params.finalTime
+            ? moment(params.finalTime, "YYYY-MM-DD HH:mm:ss")
+            : moment();
+
+        $("#final_date").datetimepicker({
             widgetPositioning: {
-                horizontal: 'auto',
-                vertical: 'bottom'
+                horizontal: "auto",
+                vertical: "bottom"
             },
             defaultDate: time,
-            widgetParent: $('#modal_body'),
+            widgetParent: $("#modal_body"),
             keepOpen: true,
-            locale: 'es',
+            locale: "es",
             format: "DD/MM/YYYY hh:mm a"
         });
-        
-        $('#final_date').on('dp.change', function (e) {
-            $(this).trigger('click');
+
+        $("#final_date").on("dp.change", function(e) {
+            $(this).trigger("click");
         });
     }
 
     function defineButtonLabel() {
         if (params.id) {
-            $('#save_task').text('Guardar');
+            $("#save_task").text("Guardar");
         } else {
-            $('#save_task').text('Crear Tarea');
+            $("#save_task").text("Crear Tarea");
         }
     }
 });
