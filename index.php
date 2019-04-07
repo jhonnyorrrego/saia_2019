0a3e120748eaed93ebdf05424493412d754a960d
jhon sebastian valencia <jhon.valencia@cerok.com>
2019-04-07 14:19:39 -0500

    ajustes nombres de imagenes para manifiesto

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-if(isset($_SESSION['idfuncionario'], $_SESSION['idsesion_php'])){
+if(isset($_SESSION['idfuncionario'])){
     header("Location: views/dashboard/dashboard.php");
 }else{
     header("Location: views/login/login.php");
