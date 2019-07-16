fdbfd6001e3f4e93ba3140f1d9f01477443c373d
jhon sebastian valencia <jhon.valencia@cerok.com>
2019-07-16 11:37:34 -0500

    busqueda avanzada sin formatos

---
+++ migrations/list/Version20190716133622.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SAIA\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190716133622 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->connection->delete('busqueda', [
+            'nombre' => 'busqueda_admin'
+        ]);
+        $this->connection->delete('busqueda', [
+            'nombre' => 'busqueda_general'
+        ]);
+        $this->connection->delete('busqueda_componente', [
+            'nombre' => 'busqueda_admin'
+        ]);
+        $this->connection->delete('busqueda_componente', [
+            'nombre' => 'busqueda_general'
+        ]);
+        $this->connection->delete('busqueda_condicion', [
+            'etiqueta_condicion' => 'busqueda_admin'
+        ]);
+        $this->connection->delete('busqueda_condicion', [
+            'etiqueta_condicion' => 'busqueda_general'
+        ]);
+
+        $this->connection->update('busqueda_condicion', [
+            'codigo_where' => "a.archivo_idarchivo = b.iddocumento and a.nombre <> 'LEIDO' and a.nombre not like 'ELIMINA_%' and b.estado <> 'ELIMINADO' and a.recibido = 1 and {*transfers*}"
+        ], [
+            'etiqueta_condicion' => 'busqueda_general_documentos'
+        ]);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
