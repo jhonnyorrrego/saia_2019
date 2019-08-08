c3a794645e3d1689240ce28ceb9e916a9ad6fbd1
jhon sebastian valencia <jhon.valencia@cerok.com>
2019-08-08 11:26:19 -0500

    limpieza carpeta pantallas

--- pantallas/serie/serielist.php
+++
@@ -1,4 +0,0 @@
-<frameset cols="40%,*">
-	<frame id="arbol" name="arbol" src="arbolserie.php" border="1" marginwidth="10" marginheight="50">
-	<frame id="serielist" name="serielist" src="" marginwidth="10" marginheight="10" >
-</frameset>
