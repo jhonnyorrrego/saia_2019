12f0b03e076470e5e23057fff04b5557d6c1ff58
cristian.agudelo <cristian.agudelo@cerok.com>
2019-08-16 11:28:18 -0500

    busquedas de distribucion

--- migrations/list/Version20190813211623.php
+++ migrations/list/Version20190813211623.php
@@ -26,7 +26,8 @@ final class Version20190813211623 extends AbstractMigration
         ]);
 
         $this->connection->update('busqueda_componente', [
-            'enlace_adicionar' => 'views/distribucion/ventanillas.php?table=cf_ventanilla'
+            'enlace_adicionar' => 'views/distribucion/ventanillas.php?table=cf_ventanilla',
+            'busqueda_avanzada' => 'views/distribucion/busqueda_transportadoras.php'
         ], [
             'idbusqueda_componente' => 322
         ]);
@@ -52,16 +53,26 @@ final class Version20190813211623 extends AbstractMigration
 
         $this->connection->update('busqueda_componente', [
             'url' => 'views/buzones/grilla.php?idbusqueda_componente=16',
-            'info' => 'N&uacute;mero|{*mostrar_numero_enlace@numero,iddocumento*}|center|-|Fecha|{*fecha*}|center|-|Asunto|{*descripcion*}'
+            'info' => 'N&uacute;mero|{*mostrar_numero_enlace@numero,iddocumento*}|center|-|Fecha|{*fecha*}|center|-|Asunto|{*descripcion*}',
+            'busqueda_avanzada' => 'views/distribucion/busqueda_pendientes.php'
         ], [
             'idbusqueda_componente' => 16
         ]);
 
         $this->connection->update('busqueda_componente', [
             'url' => 'views/buzones/grilla.php?idbusqueda_componente=280',
-            'info' => 'N&uacute;mero|{*mostrar_numero_enlace@numero,iddocumento*}|center|-|Fecha|{*fecha*}|center|-|Asunto|{*descripcion*}'
+            'info' => 'N&uacute;mero|{*mostrar_numero_enlace@numero,iddocumento*}|center|-|Fecha|{*fecha*}|center|-|Asunto|{*descripcion*}',
+            'busqueda_avanzada' => 'views/distribucion/busqueda_pendientes.php'
         ], [
             'idbusqueda_componente' => 280
+        ]);
+
+        $this->connection->update('busqueda_componente', [
+            'url' => 'views/buzones/grilla.php?idbusqueda_componente=299&table=cf_ventanilla',
+            'enlace_adicionar' => 'views/distribucion/transportadoras.php?table=cf_empresa_trans',
+            'busqueda_avanzada' => 'views/distribucion/busqueda_transportadoras.php'
+        ], [
+            'idbusqueda_componente' => 299
         ]);
 
         $this->connection->update('campos_formato', [
@@ -70,6 +81,52 @@ final class Version20190813211623 extends AbstractMigration
             'idcampos_formato' => 52
         ]);
 
+        $this->connection->update('busqueda', [
+            'ruta_libreria_pantalla' => 'views/buzones/utilidades/acciones_cf.php'
+        ], [
+            'idbusqueda' => 133
+        ]);
+
+        $this->connection->update('busqueda_componente', [
+            'busqueda_avanzada' => 'views/distribucion/busqueda_distribucion.php',
+            'url' => 'views/buzones/grilla.php?idbusqueda_componente=300',
+        ], [
+            'idbusqueda_componente' => 300
+        ]);
+
+        $this->connection->update('busqueda_componente', [
+            'busqueda_avanzada' => 'views/distribucion/busqueda_distribucion.php',
+            'url' => 'views/buzones/grilla.php?idbusqueda_componente=301',
+        ], [
+            'idbusqueda_componente' => 301
+        ]);
+
+        $this->connection->update('busqueda_componente', [
+            'busqueda_avanzada' => 'views/distribucion/busqueda_distribucion.php',
+            'url' => 'views/buzones/grilla.php?idbusqueda_componente=302',
+        ], [
+            'idbusqueda_componente' => 302
+        ]);
+
+        $this->connection->update('busqueda_componente', [
+            'busqueda_avanzada' => 'views/distribucion/busqueda_distribucion.php',
+            'url' => 'views/buzones/grilla.php?idbusqueda_componente=303',
+        ], [
+            'idbusqueda_componente' => 303
+        ]);
+        
+        $query = $this->connection->fetchAll("select idfunciones_formato from funciones_formato where idfunciones_formato=1472");
+		if(!$query[0]['idmodulo']){
+            $this->connection->insert('funciones_formato', [
+                'idfunciones_formato' => '1472',
+                'nombre' => '{*mostrar_num_pagina*}',
+                'nombre_funcion' => 'mostrar_num_pagina',
+                'parametros' => NULL,'etiqueta' => 'mostrar_num_pagina',
+                'descripcion' => NULL,'ruta' => '../librerias/encabezado_pie_pagina.php',
+                'formato' => '',
+                'acciones' => 'm'
+            ]);
+        }  
     }
 
     public function down(Schema $schema) : void
