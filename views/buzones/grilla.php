e934d174d56e1a825ea6539ca27bee21b47e11e5
jhon sebastian valencia <jhon.valencia@cerok.com>
2019-08-16 08:55:14 -0500

    grilla con buscar y adicionar en ventana modal

--- views/buzones/grilla.php
+++ views/buzones/grilla.php
@@ -19,30 +19,62 @@ $params = json_encode(array_merge($_REQUEST, [
     'pageSize' => $BusquedaComponente->getSearch()->cantidad_registros,
     'columns' => $BusquedaComponente->info
 ]));
-?>
-<?= jquery() ?>
-<?= bootstrap() ?>
-<?= bootstrapTable() ?>
-<?= bootstrapTableFilter() ?>
-<?= bootstrapTableExport() ?>
-<?= theme() ?>
-<?= icons() ?>
+$btn_search = $btn_add = $actions = '';
+$query = "idbusqueda_componente={$BusquedaComponente->getPK()}";
 
+echo jquery();
+echo bootstrap();
+echo bootstrapTable();
+echo bootstrapTableExport();
+echo theme();
+echo icons();
+
+$phpLibraries = explode(",", $BusquedaComponente->getSearch()->ruta_libreria);
+$jsLibraries = explode(",", $BusquedaComponente->getSearch()->ruta_libreria_pantalla);
+$libraries = array_merge($phpLibraries, $jsLibraries);
+
+foreach ($libraries as $library) {
+    include_once $ruta_db_superior . $library;
+}
+
+if ($BusquedaComponente->busqueda_avanzada) {
+    $btn_search = "<button class='btn btn-secondary' title='Buscar' id='btn_search' data-url='{$BusquedaComponente->busqueda_avanzada}?{$query}'>
+        <i class='fa fa-search'></i>
+        <span class='d-none d-sm-inline'>Buscar</span>
+    </button>";
+}
+
+if ($BusquedaComponente->enlace_adicionar) {
+    $BusquedaComponente->enlace_adicionar .= (strpos($BusquedaComponente->enlace_adicionar, '?') === false ? '?' : '&') . $query;
+
+    $btn_add = "<button class='btn btn-secondary' title='Adicionar' id='btn_add' data-url='{$BusquedaComponente->enlace_adicionar}'>
+        <i class='fa fa-plus'></i>
+        <span class='d-none d-sm-inline'>Adicionar</span>
+    </button>";
+}
+
+if ($BusquedaComponente->acciones_seleccionados) {
+    $datos_reporte = [
+        'idbusqueda_componente' => $BusquedaComponente->getPK(),
+        'variable_busqueda' => $_REQUEST["variable_busqueda"] ?? null
+    ];
+
+    $acciones = explode(",", $BusquedaComponente->acciones_seleccionados);
+    foreach ($acciones as $key => $value) {
+        $actions = $value($datos_reporte);
+    }
+}
+?>
 <div class="container-fluid px-0">
     <div class="row mx-0" id="content">
         <div class="col-12">
+            <div id="toolbar">
+                <?= $btn_search ?>
+                <?= $btn_add ?>
+                <?= $actions ?>
+            </div>
             <table id="table" data-selections=""></table>
         </div>
     </div>
 </div>
 <script src="<?= $ruta_db_superior ?>views/buzones/js/grilla.js" id="script_grid" data-params='<?= $params ?>'></script>
-
-<?php
-if ($BusquedaComponente->getSearch()->ruta_libreria_pantalla) {
-    $libraries = explode(',', $BusquedaComponente->getSearch()->ruta_libreria_pantalla);
-
-    foreach ($libraries as $library) {
-        include_once $ruta_db_superior . $library;
-    }
-}
-?>
--- views/buzones/js/grilla.js
+++ views/buzones/js/grilla.js
@@ -1,18 +1,21 @@
 $(function() {
     var params = $('#script_grid').data('params');
     var baseUrl = params.baseUrl;
-    var sessionVars = {
+    var $table = $('#table');
+    var request = {
         key: localStorage.getItem('key'),
         token: localStorage.getItem('token'),
-        idbusqueda_componente: params.idbusqueda_componente
+        idbusqueda_componente: params.idbusqueda_componente,
+        idbusqueda_filtro_temp: null
     };
 
-    $('#table').bootstrapTable({
+    $table.bootstrapTable({
         url: baseUrl + 'app/busquedas/datosBootstrapTable.php',
         queryParams: function(queryParams) {
-            queryParams = $.extend(queryParams, sessionVars);
+            queryParams = $.extend(queryParams, request);
             return queryParams;
         },
+        toolbar: '#toolbar',
         classes: 'table table-hover mt-0',
         theadClasses: 'thead-light',
         sidePagination: 'server',
@@ -23,10 +26,55 @@ $(function() {
         paginationSuccessivelySize: 1,
         paginationPagesBySide: 1,
         columns: getColumns(),
-        filterControl: true,
         showExport: true,
         exportTypes: ['csv', 'txt', 'excel', 'pdf'],
         exportDataType: 'all'
+    });
+
+    $('#btn_search').on('click', function() {
+        top.topModal({
+            url: baseUrl + $(this).data('url'),
+            size: 'modal-xl',
+            title: 'Búsqueda',
+            buttons: {
+                success: {
+                    label: 'Buscar',
+                    class: 'btn btn-complete'
+                },
+                cancel: {
+                    label: 'Cerrar',
+                    class: 'btn btn-danger'
+                }
+            },
+            onSuccess: function(data) {
+                let params = getParams(data.url);
+                request.idbusqueda_filtro_temp = params.idbusqueda_filtro_temp;
+                $table.bootstrapTable('refresh');
+                top.closeTopModal();
+            }
+        });
+    });
+
+    $('#btn_add').on('click', function() {
+        top.topModal({
+            url: baseUrl + $(this).data('url'),
+            size: 'modal-xl',
+            title: 'Crear',
+            onSuccess: function() {
+                top.closeTopModal();
+                $table.bootstrapTable('refresh');
+            },
+            buttons: {
+                success: {
+                    label: 'Guardar',
+                    class: 'btn btn-complete'
+                },
+                cancel: {
+                    label: 'Cancelar',
+                    class: 'btn btn-danger'
+                }
+            }
+        });
     });
 
     function getColumns() {
@@ -36,5 +84,18 @@ $(function() {
             return c;
         });
         return data;
+    }
+
+    function getParams(url) {
+        let queryString = url.split('?')[1];
+        let portions = queryString.split('&');
+        let params = new Object();
+
+        portions.forEach(p => {
+            let param = p.split('=');
+            params[param[0]] = param[1];
+        });
+
+        return params;
     }
 });
