111b51ceec0bd0fa9fa1d3bf83c89e11efe3e32a
Julian Otalvaro <jotal8@gmail.com>
2019-09-09 11:04:05 -0500

    Boton adicionar formato, Correciones en generacion y mostrar de ruta de distrubucion

--- views/generador/js/funciones_generador_js.php
+++ views/generador/js/funciones_generador_js.php
@@ -2,6 +2,6 @@
 
 function opciones_acciones_formato($datos)
 {
-    $botonAdicionar = '<div class="kenlace_saia" enlace="views/generador/index.php" conector="iframe" titulo="Crear ruta"><center><button class="btn btn-secondary"><i class="fa fa-plus"></i><span class="d-sm-inline"> Adicionar</span></button></center></div>';
+    $botonAdicionar = '<div class="kenlace_saia" enlace="views/generador/index.php" conector="iframe" titulo="Crear formato nuevo"><center><button class="btn btn-secondary"><i class="fa fa-plus"></i><span class="d-sm-inline"> Adicionar</span></button></center></div>';
     return $botonAdicionar;
 }
