58a0a72c5ba7fc855a05fc723894689fc425d746
Julian Otalvaro <jotal8@gmail.com>
2019-09-17 07:14:16 -0500

    Solucionado el error al guardar un encabezado o pie editado

--- views/generador/js/editor_encabezado.js
+++ views/generador/js/editor_encabezado.js
@@ -7,53 +7,55 @@ $(function() {
         findData();
     })();
 
-    $('#btn_success').on('click', function(e) {
-        $('#form_header').validate({
-            ignore: [],
-            debug: false,
-            rules: {
-                name: {
-                    required: true,
-                    minlength: 1
-                },
-                content: {
-                    required: function() {
-                        CKEDITOR.instances.content.updateElement();
+    $(document)
+        .off('click', '#btn_success')
+        .on('click', '#btn_success', function(e) {
+            $('#form_header').validate({
+                ignore: [],
+                debug: false,
+                rules: {
+                    name: {
+                        required: true,
+                        minlength: 1
                     },
-                    minlength: 1
+                    content: {
+                        required: function() {
+                            CKEDITOR.instances.content.updateElement();
+                        },
+                        minlength: 1
+                    }
                 }
+            });
+            if ($('#form_header').valid()) {
+                var content = CKEDITOR.instances['content'].getData();
+
+                $.post(
+                    `${params.baseUrl}app/generador/actualizar_contenido_encabezado.php`,
+                    {
+                        key: localStorage.getItem('key'),
+                        token: localStorage.getItem('token'),
+                        identificator: $('#identificator').val(),
+                        name: $('#name').val(),
+                        content: content
+                    },
+                    function(response) {
+                        if (response.success) {
+                            top.notification({
+                                type: 'success',
+                                message: 'Encabezado modificado'
+                            });
+                            top.successModalEvent();
+                        } else {
+                            top.notification({
+                                type: 'error',
+                                message: response.message
+                            });
+                        }
+                    },
+                    'json'
+                );
             }
         });
-        if ($('#form_header').valid()) {
-            var content = CKEDITOR.instances['content'].getData();
-
-            $.post(
-                `${params.baseUrl}app/generador/actualizar_contenido_encabezado.php`,
-                {
-                    key: localStorage.getItem('key'),
-                    token: localStorage.getItem('token'),
-                    identificator: $('#identificator').val(),
-                    name: $('#name').val(),
-                    content: content
-                },
-                function(response) {
-                    if (response.success) {
-                        top.notification({
-                            type: 'success',
-                            message: 'Encabezado modificado'
-                        });
-                        top.successModalEvent();
-                    } else {
-                        top.notification({
-                            type: 'error',
-                            message: response.message
-                        });
-                    }
-                },
-                'json'
-            );
-        }
-    });
 
     function findData() {
         if (params.identificator) {
