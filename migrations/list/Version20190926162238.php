e3a07d716fe268262004dfbfe8d6ea4403df6ee4
Julian Otalvaro <jotal8@gmail.com>
2019-09-26 13:46:34 -0500

    Corrigiendo migracion

--- migrations/list/Version20190926162238.php
+++ migrations/list/Version20190926162238.php
@@ -21,12 +21,12 @@ final class Version20190926162238 extends AbstractMigration
     {
         $value = <<<INFO
         [{"title":"N. Registro","field":"{*ver_numero_registro@iddocumento,tipo_origen,fecha*}","align":"center","width":"180"},{"title":"N. Item","field":"{*ver_documento_distribucion@iddocumento,tipo_origen*}","align":"center","width":"110"},{"title":"Fecha","field":"{*fecha_distribucion@fecha*}","align":"center","width":"150"},{"title":"Asunto","field":"{*descripcion*}","align":"left"},{"title":"Sede Origen","field":"{*ventanilla*}","align":"left"},{"title":"Origen","field":"{*mostrar_origen_distribucion@tipo_origen,origen*}","align":"left"},{"title":"Destino","field":"{*mostrar_destino_distribucion@tipo_destino,destino*}","align":"left"},{"title":"Sede Destino","field":"{*ventanilla*}","align":"left"},{"title":"Trámite","field":"{*mostrar_diligencia_distribucion@tipo_origen,estado_recogida*}","align":"center"},{"title":"Ruta","field":"{*mostrar_nombre_ruta_distribucion@iddistribucion*}","align":"left"},{"title":"Mensajero","field":"{*select_mensajeros_ruta_distribucion@iddistribucion*}","align":"left"},{"title":"Estado","field":"{*ver_estado_distribucion@estado_distribucion*}","align":"center"},{"title":"Planilla Asociada","field":"{*mostrar_planilla_diligencia_distribucion@iddistribucion*}","align":"center"}]
-        INFO;
+INFO;
 
         $this->connection->update(
             'busqueda_componente',
             [
-                'etiqueta' => $value,
+                'info' => $value,
             ],
             [
                 'nombre' => 'reporte_distribucion_general_pendientes'
