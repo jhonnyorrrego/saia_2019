36bfb5b35837358f2e0839a6e0f69bd28e191e79
jhon sebastian valencia <jhon.valencia@cerok.com>
2019-10-07 14:50:57 -0500

    componente de terceros terminado en el adicionar

---
+++ migrations/list/Version20191007164333.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SAIA\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191007164333 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $table = $schema->getTable('tercero');
+        $table->addColumn('estado', 'integer', [
+            'default' => '1',
+            'length' => 1,
+        ]);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+
+    public function preUp(Schema $schema): void
+    {
+        date_default_timezone_set("America/Bogota");
+
+        if ($this->connection->getDatabasePlatform()->getName() == "mysql") {
+            $this->platform->registerDoctrineTypeMapping('enum', 'string');
+        }
+    }
+
+    public function preDown(Schema $schema): void
+    {
+        date_default_timezone_set("America/Bogota");
+
+        if ($this->connection->getDatabasePlatform()->getName() == "mysql") {
+            $this->platform->registerDoctrineTypeMapping('enum', 'string');
+        }
+    }
+}
